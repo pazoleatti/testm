0cf80812963639ea5d608cf9ce9f480eb498c7a4
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-08-14 12:34:23 +0000

    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@9 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/FormDaoImpl.java
@@ -0,0 +1,17 @@
+package com.aplana.sbrf.taxaccounting.dao.impl;
+
+import com.aplana.sbrf.taxaccounting.dao.FormDao;
+import com.aplana.sbrf.taxaccounting.model.Form;
+
+public class FormDaoImpl implements FormDao {
+
+	@Override
+	public Form getForm(int formId) {
+		return null;
+	}
+
+	@Override
+	public int saveForm(Form form) {
+		return 0;
+	}
+}
