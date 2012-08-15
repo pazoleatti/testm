ecc69c0d99388f26cfecb76a5b5e780bc60ef946
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-08-15 12:35:52 +0000

    Исправлено несолько ошибок в конфигах
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@24 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- portlets/src/main/java/com/aplana/sbrf/taxaccounting/controller/forms/FormsController.java
+++ portlets/src/main/java/com/aplana/sbrf/taxaccounting/controller/forms/FormsController.java
@@ -1,9 +1,11 @@
 package com.aplana.sbrf.taxaccounting.controller.forms;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 @Controller
+@RequestMapping("VIEW")
 public class FormsController {
 	@RenderMapping
 	String showList() {
