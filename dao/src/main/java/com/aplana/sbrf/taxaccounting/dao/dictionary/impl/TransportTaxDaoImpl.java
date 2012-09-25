88a7e172abdcf25f0c9c944a58092d2a9251d9a9
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-09-25 09:11:59 +0000

    Полностью перевёл TransportTaxDao на использование MyBatis-маппера
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@106 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/dictionary/impl/TransportTaxDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/dictionary/impl/TransportTaxDaoImpl.java
@@ -1,21 +1,18 @@
 package com.aplana.sbrf.taxaccounting.dao.dictionary.impl;
 
 import java.math.BigDecimal;
-import java.sql.Types;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.aplana.sbrf.taxaccounting.dao.dictionary.TransportTaxDao;
 import com.aplana.sbrf.taxaccounting.dao.dictionary.mapper.TransportTaxMapper;
-import com.aplana.sbrf.taxaccounting.dao.impl.AbstractDao;
+import com.aplana.sbrf.taxaccounting.dao.exсeption.DaoException;
 
 @Repository("transportTaxDao")
 @Transactional(readOnly = true)
-public class TransportTaxDaoImpl extends AbstractDao implements TransportTaxDao {
+public class TransportTaxDaoImpl implements TransportTaxDao {
 	@Autowired
 	private TransportTaxMapper transportTaxMapper;
 	
@@ -26,20 +23,15 @@ public class TransportTaxDaoImpl extends AbstractDao implements TransportTaxDao 
 
 	@Override
 	public int getTaxRate(String code, BigDecimal age, BigDecimal power) {
-		String sql = "select value from transport_tax_rate " +
-			"where code = ? " +
-			"and (min_age is null or min_age <= ?) and (max_age is null or max_age >= ?)" +
-			"and (min_power is null or min_power <= ?) and (max_power is null or max_power >= ?)";
-		Object[] params = new Object[] {code, age, age, power, power};
-		int[] types = new int[] { Types.VARCHAR, Types.NUMERIC, Types.NUMERIC, Types.NUMERIC, Types.NUMERIC };
-		try {
-			try {
-				return getJdbcTemplate().queryForInt(sql, params, types);
-			} catch (EmptyResultDataAccessException e) {
-				return getJdbcTemplate().queryForInt(sql, params, types);
-			}
-		} catch (DataAccessException e) {
-			throw new RuntimeException("Не удалось определить значение ставки налога для транспортного средства"); 
+		Integer result = transportTaxMapper.getTransportTaxRate(code, age.intValue(), power.intValue());
+		if (result == null && code.length() == 5) {
+			// Если не найдём совпадения по точному коду, то ищем совпадение по коду с плейсхолдерами
+			code = code.substring(0, 3) + "xx";
+			result = transportTaxMapper.getTransportTaxRate(code, age.intValue(), power.intValue());
 		}
+		if (result == null) {
+			throw new DaoException("Не удалось определить значение ставки налога для транспортного средства");
+		}
+		return result.intValue();
 	}
 }
--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/dictionary/mapper/TransportTaxMapper.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/dictionary/mapper/TransportTaxMapper.java
@@ -5,5 +5,13 @@ import org.apache.ibatis.annotations.Select;
 
 public interface TransportTaxMapper {
 	@Select("select name from okato where okato = #{okato}")
-	String getRegionName(@Param("okato") String okato);
+	String getRegionName(@Param("okato")String okato);
+	
+	@Select({
+		"select value from transport_tax_rate", 
+		"where code = #{transportCode}",
+		"and (min_age is null or min_age <= #{age}) and (max_age is null or max_age >= #{age})",
+		"and (min_power is null or min_power <= #{power}) and (max_power is null or max_power >= #{power})"
+	})
+	Integer getTransportTaxRate(@Param("transportCode")String transportCode, @Param("age")int age, @Param("power")int power);
 }
