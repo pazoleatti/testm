2bd92776c4e547e1e0ce57195cb335dc231a2cc8
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-09-25 04:59:23 +0000

    AbstractDao в своей основе теперь имеет namedParameterJdbcTemplate, а не наследуется от JdbcDaoSupport
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@103 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/AbstractDao.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/AbstractDao.java
@@ -1,18 +1,26 @@
 package com.aplana.sbrf.taxaccounting.dao.impl;
 
-import javax.sql.DataSource;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 /**
  * Абстрактный класс, позволяющий делать автовайринг с DataSource'ом
  * и содержащий вспомогательные методы, востребованные насле
  */
-public abstract class AbstractDao extends JdbcDaoSupport {
+public abstract class AbstractDao {
+	protected Log logger = LogFactory.getLog(getClass());
 	@Autowired
-	protected void setAutowiredDataSource(DataSource ds) {
-		setDataSource(ds);
+	private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+	
+	protected JdbcTemplate getJdbcTemplate() {
+		return (JdbcTemplate)namedParameterJdbcTemplate.getJdbcOperations();
+	}
+	
+	protected NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+		return namedParameterJdbcTemplate;
 	}
 	
 	protected <T extends Number> T generateId(String sequenceName, Class<T> resultType) {
