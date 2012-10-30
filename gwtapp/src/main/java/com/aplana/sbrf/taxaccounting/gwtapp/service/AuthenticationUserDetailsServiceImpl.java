a4a3e34eeafdeb1c011198d60db14414cb1a55b6
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-10-30 07:02:43 +0000

    Подключил Spring Security к проекту (первичная найтройка - пускаем всех)
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@183 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/gwtapp/service/AuthenticationUserDetailsServiceImpl.java
@@ -0,0 +1,48 @@
+package com.aplana.sbrf.taxaccounting.gwtapp.service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.aplana.sbrf.taxaccounting.dao.security.TAUserDao;
+import com.aplana.sbrf.taxaccounting.model.security.TARole;
+import com.aplana.sbrf.taxaccounting.model.security.TAUser;
+
+public class AuthenticationUserDetailsServiceImpl implements AuthenticationUserDetailsService<Authentication> {
+	private final Log logger = LogFactory.getLog(getClass());
+	
+	@Autowired
+	private TAUserDao userDao;
+
+	@Override
+	public UserDetails loadUserDetails(Authentication token)	throws UsernameNotFoundException {
+		String userName = token.getName();
+		TAUser user = userDao.getUser(userName);
+		if (user == null) {
+			String message = "User with login '" + userName + "' was not found in TaxAccounting database";
+			logger.error(message);
+			throw new UsernameNotFoundException(message);
+		}
+		
+		Collection<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>(user.getRoles().size());
+		
+		for (TARole role: user.getRoles()) {
+			grantedAuthorities.add(new SimpleGrantedAuthority(role.getAlias()));
+		}
+		
+		// TODO: у User есть дополнительные флаги: expired, enabled и т.д.
+		// возможно в будущем задействуем и их
+		return new User(userName, "notused", grantedAuthorities);
+	}
+
+}
