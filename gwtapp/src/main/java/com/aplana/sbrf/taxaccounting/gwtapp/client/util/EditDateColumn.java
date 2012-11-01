c164a833e8683ed2247420e01a4e239efda7ad5d
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-01 05:44:09 +0000

    Добавил возможность изменения дат, если их значения равны null
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@202 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/gwtapp/client/util/EditDateColumn.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/gwtapp/client/util/EditDateColumn.java
@@ -2,9 +2,9 @@ package com.aplana.sbrf.taxaccounting.gwtapp.client.util;
 
 import java.util.Date;
 
+import com.aplana.sbrf.taxaccounting.gwtapp.cell.NullableDatePickerCell;
 import com.aplana.sbrf.taxaccounting.model.DataRow;
 import com.aplana.sbrf.taxaccounting.model.DateColumn;
-import com.google.gwt.cell.client.DatePickerCell;
 import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.i18n.client.DateTimeFormat;
 
@@ -14,7 +14,7 @@ public class EditDateColumn extends DataRowColumn<Date> {
     private static final DateTimeFormat FORMAT = DateTimeFormat.getFormat("dd.MM.yyyy");
 
     public EditDateColumn(DateColumn dateColumn) {
-        super(new DatePickerCell(FORMAT), dateColumn);
+        super(new NullableDatePickerCell(FORMAT), dateColumn);
         this.setFieldUpdater(new FieldUpdater<DataRow, Date>() {
 			@Override
 			public void update(int index, DataRow dataRow, Date value) {
