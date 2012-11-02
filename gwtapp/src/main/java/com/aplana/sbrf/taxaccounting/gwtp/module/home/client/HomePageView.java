b831d7c9f30fde73d333de3668f71ab33ced806e
Sgoryachkin <Sgoryachkin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-02 07:03:34 +0000

    SBRFACCTAX-263 Почистил
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@209 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/gwtp/module/home/client/HomePageView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/gwtp/module/home/client/HomePageView.java
@@ -1,41 +1,21 @@
-/**
- * Copyright 2011 ArcBees Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 package com.aplana.sbrf.taxaccounting.gwtp.module.home.client;
 
 import com.aplana.sbrf.taxaccounting.gwtp.module.home.client.HomePagePresenter.MyView;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Widget;
-
+import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
 
-/**
- * @author Christian Goudreau
- */
-public class HomePageView extends ViewImpl implements MyView {
-  interface HomeViewUiBinder extends UiBinder<Widget, HomePageView> {
-  }
 
-  private static HomeViewUiBinder uiBinder = GWT.create(HomeViewUiBinder.class);
+public class HomePageView extends ViewImpl implements MyView {
+  interface Binder extends UiBinder<Widget, HomePageView> {
+  }
 
   private final Widget widget;
 
-  public HomePageView() {
-    widget = uiBinder.createAndBindUi(this);
+  @Inject
+  public HomePageView(final Binder binder) {
+    widget = binder.createAndBindUi(this);
   }
 
   @Override
