5c284332c1ecf5d41802a521636c849b6c610e6d
Sgoryachkin <Sgoryachkin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-07 10:10:04 +0000

    Привел в порядок меню
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@230 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdatalist/client/FormDataListNameTokens.java
@@ -0,0 +1,11 @@
+package com.aplana.sbrf.taxaccounting.web.module.formdatalist.client;
+
+public class FormDataListNameTokens {
+
+	public static final String formDataListPage = "!formDataList";
+
+	public static String getFormDataListPage() {
+		return formDataListPage;
+	}
+
+}
