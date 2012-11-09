ac93062f29d2b362e2e9182d3b23b8d59a91358b
Vsamolovskiy <Vsamolovskiy@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-09 09:00:08 +0000

    Deleted gwtapp package
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@241 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/gwtapp/shared/GetFormDataList.java
+++
@@ -1,7 +0,0 @@
-package com.aplana.sbrf.taxaccounting.gwtapp.shared;
-
-import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
-
-public class GetFormDataList extends UnsecuredActionImpl<GetFormDataListResult>{
-	// TODO: Добавить параметры отбора значений
-}
