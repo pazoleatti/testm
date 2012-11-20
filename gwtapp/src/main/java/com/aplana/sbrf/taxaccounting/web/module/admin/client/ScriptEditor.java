5e7d5f2f57ff91b52222e5450ddb25f2177259b3
Vsamolovskiy <Vsamolovskiy@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-20 05:51:34 +0000

    SBRFACCTAX-325: script editor comments
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@281 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/admin/client/ScriptEditor.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/admin/client/ScriptEditor.java
@@ -6,17 +6,33 @@ import com.google.gwt.editor.client.Editor;
 import com.google.gwt.editor.client.SimpleBeanEditorDriver;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.TakesValue;
 import com.google.gwt.user.client.ui.*;
 
 /**
+ * The editor for all scripts. You can use it in UiBinder. Just add schema
+ * <p/>
+ * <pre>xmlns:u="urn:import:com.aplana.sbrf.taxaccounting.web.module.admin.client"</pre>
+ * <p/>
+ * <pre>&lt;u:ScriptEditor ui:field="scriptEditor"/&gt;</pre>
+ * <p/>
+ * You also can use it as widget.
+ * <p/>
+ * You can set and get value, but when you call <code>getValue</code> it merges changes to bean.
+ *
  * @author Vitalii Samolovskikh
  */
-public class ScriptEditor extends Composite implements Editor<Script> {
-	interface MyUiBinder extends UiBinder<Widget, ScriptEditor> {}
+public class ScriptEditor extends Composite implements Editor<Script>, TakesValue<Script> {
+	interface MyUiBinder extends UiBinder<Widget, ScriptEditor> {
+	}
+
 	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
 
 	// Empty interface declaration, similar to UiBinder
-	interface Driver extends SimpleBeanEditorDriver<Script, ScriptEditor> {}
+	interface Driver extends SimpleBeanEditorDriver<Script, ScriptEditor> {
+	}
+
+	private final Driver driver = GWT.create(Driver.class);
 
 	@UiField
 	TextBox name;
@@ -31,22 +47,40 @@ public class ScriptEditor extends Composite implements Editor<Script> {
 	@UiField
 	TextArea body;
 
-	private final Driver driver = GWT.create(Driver.class);
-
+	/**
+	 * Constructs new editor.
+	 */
 	public ScriptEditor() {
 		super();
 		initWidget(uiBinder.createAndBindUi(this));
 		driver.initialize(this);
 	}
 
-	public void setValue(Script script){
+	/**
+	 * Sets script bean value.
+	 *
+	 * @param script script for edit
+	 */
+	@Override
+	public void setValue(Script script) {
 		driver.edit(script);
 	}
 
-	public Script getValue(){
+	/**
+	 * It'a alias for <code>flush</code> method.
+	 *
+	 * @return merged script
+	 */
+	@Override
+	public Script getValue() {
 		return flush();
 	}
 
+	/**
+	 * Merges changes to bean and returns bean.
+	 *
+	 * @return merged script
+	 */
 	public Script flush() {
 		return driver.flush();
 	}
