a5b5f4f45c4f65398dbcc9a12f00563399972bcc
Vsamolovskiy <Vsamolovskiy@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-27 12:01:00 +0000

    SBRFACCTAX-517: don't save empty datarows
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@403 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/FormTemplateDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/FormTemplateDaoImpl.java
@@ -5,6 +5,7 @@ import com.aplana.sbrf.taxaccounting.dao.FormTemplateDao;
 import com.aplana.sbrf.taxaccounting.dao.FormTypeDao;
 import com.aplana.sbrf.taxaccounting.dao.ScriptDao;
 import com.aplana.sbrf.taxaccounting.dao.impl.util.XmlSerializationUtils;
+import com.aplana.sbrf.taxaccounting.model.DataRow;
 import com.aplana.sbrf.taxaccounting.model.FormTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
@@ -19,7 +20,7 @@ import java.sql.Types;
 import java.util.List;
 
 @Repository
-@Transactional(readOnly=true)
+@Transactional(readOnly = true)
 public class FormTemplateDaoImpl extends AbstractDao implements FormTemplateDao {
 	@Autowired
 	private FormTypeDao formTypeDao;
@@ -31,17 +32,19 @@ public class FormTemplateDaoImpl extends AbstractDao implements FormTemplateDao 
 
 	private class FormMapper implements RowMapper<FormTemplate> {
 		private boolean deepFetch;
+
 		public FormMapper(boolean deepFetch) {
-			this.deepFetch = deepFetch; 
+			this.deepFetch = deepFetch;
 		}
+
 		public FormTemplate mapRow(ResultSet rs, int index) throws SQLException {
 			FormTemplate form = new FormTemplate();
 			form.setId(rs.getInt("id"));
 			form.setType(formTypeDao.getType(rs.getInt("type_id")));
-			
+
 			if (deepFetch) {
 				form.getColumns().addAll(columnDao.getFormColumns(form.getId()));
-				scriptDao.fillFormScripts(form);				
+				scriptDao.fillFormScripts(form);
 				String stRowsData = rs.getString("data_rows");
 				if (stRowsData != null) {
 					form.getRows().addAll(xmlSerializationUtils.deserialize(stRowsData, form.getColumns()));
@@ -58,26 +61,35 @@ public class FormTemplateDaoImpl extends AbstractDao implements FormTemplateDao 
 		logger.info("Fetching FormTemplate with id = " + formId);
 		JdbcTemplate jt = getJdbcTemplate();
 		final FormTemplate form = jt.queryForObject(
-			"select * from form where id = ?",
-			new Object[] { formId },
-			new int[] { Types.NUMERIC },
-			new FormMapper(true)
+				"select * from form where id = ?",
+				new Object[]{formId},
+				new int[]{Types.NUMERIC},
+				new FormMapper(true)
 		);
 		return form;
 	}
 
-	@Transactional(readOnly=false)
-	@CacheEvict(value="Form", key="#form.id")
+	@Transactional(readOnly = false)
+	@CacheEvict(value = "Form", key = "#form.id")
 	public int save(final FormTemplate form) {
 		final int formTemplateId = form.getId().intValue();
 
-		String xml = xmlSerializationUtils.serialize(form.getRows());
-		// TODO: создание новых версий формы потребует инсертов в form
-		getJdbcTemplate().update(
-			"update form set data_rows = ? where id = ?",
-			new Object[] { xml, formTemplateId },
-			new int[] { Types.VARCHAR, Types.NUMERIC }
-		);
+		List<DataRow> rows = form.getRows();
+		if (rows != null && !rows.isEmpty()) {
+			String xml = xmlSerializationUtils.serialize(rows);
+			// TODO: создание новых версий формы потребует инсертов в form
+			getJdbcTemplate().update(
+					"update form set data_rows = ? where id = ?",
+					new Object[]{xml, formTemplateId},
+					new int[]{Types.VARCHAR, Types.NUMERIC}
+			);
+		} else {
+			getJdbcTemplate().update(
+					"update form set data_rows = null where id = ?",
+					new Object[]{formTemplateId},
+					new int[]{Types.NUMERIC}
+			);
+		}
 
 		columnDao.saveFormColumns(form);
 		scriptDao.saveFormScripts(form);
