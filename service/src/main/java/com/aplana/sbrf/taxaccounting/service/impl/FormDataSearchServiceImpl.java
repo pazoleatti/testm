01fa548849912e0d46cdd600ab20ab9700d6c438
Srybakov <Srybakov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-28 15:47:05 +0000

    Filter by ALL param added
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@435 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- service/src/main/java/com/aplana/sbrf/taxaccounting/service/impl/FormDataSearchServiceImpl.java
+++ service/src/main/java/com/aplana/sbrf/taxaccounting/service/impl/FormDataSearchServiceImpl.java
@@ -32,21 +32,74 @@ public class FormDataSearchServiceImpl implements FormDataSearchService {
 
 	@Override
 	public List<FormData> findDataByUserIdAndFilter(int userId, FormDataFilter formDataFilter) {
-		/*TODO: сервис должен быть переписан после добавления параметра --ВСЕ--, так же на данный момент не используется
-		параметр userId.  ==> это только прототип сервиса!
-		*/
+		/*TODO: на данный момент параметр ВСЕ обрабатывается, но пока-что не учитывается userId
+		* Еще один момент: пока что параметр ВСЕ передается не как null, а как Long.MAX_VALUE,
+		* будет исправлено, когда решится проблема с LongListBoxEditor*/
+
 		List<FormData> formDataList = new ArrayList<FormData>();
 		FormDataDaoFilter formDataDaoFilter = new FormDataDaoFilter();
 
-		if(false){
-			/*В этой ветке будем обрабатывать параметр -ВСЕ-*/
+		if(formDataFilter.getDepartment() == Long.MAX_VALUE){
+			List<Department> departmentList = departmentDao.listDepartments();
+			List<Long> departmentLongList = new ArrayList<Long>();
+			for(Department department : departmentList){
+				departmentLongList.add((long)department.getId());
+			}
+			formDataDaoFilter.setDepartment(departmentLongList);
 		} else {
 			formDataDaoFilter.setDepartment(Arrays.asList(formDataFilter.getDepartment()));
-			formDataDaoFilter.setFormtype(Arrays.asList(formDataFilter.getFormtype()));
-			formDataDaoFilter.setPeriod(Arrays.asList(formDataFilter.getPeriod()));
-			formDataDaoFilter.setKind(Arrays.asList(formDataFilter.getKind()));
-			formDataList = formDataSearchDao.findByFilter(formDataDaoFilter);
 		}
+
+		if(formDataFilter.getPeriod() == Long.MAX_VALUE){
+			List<ReportPeriod> reportPeriodList = reportPeriodDao.listAllPeriodsByTaxType(formDataFilter
+					.getTaxType());
+			List<Long> reportPeriodLongList = new ArrayList<Long>();
+			/*TODO: По поводу обработки ситуации, когда у нас нету Отчетного периода или Типа формы с определенным TaxType,
+			* нужно поговорить. Может все-таки имеет смысл обрабатывать это подобным образом на сервисном слое, чем многократно
+			* усложнять логику DAO слоя дополнительными SQL скриптами?!?*/
+			System.out.println("Check on empty reportPeriodList. isEmpty() = " + reportPeriodList.isEmpty());
+ 			if(!reportPeriodList.isEmpty()){
+				for (ReportPeriod reportPeriod : reportPeriodList){
+					reportPeriodLongList.add((long)reportPeriod.getId());
+				}
+			} else {
+				reportPeriodLongList.add(-1L);
+			}
+			formDataDaoFilter.setPeriod(reportPeriodLongList);
+		} else {
+			formDataDaoFilter.setPeriod(Arrays.asList(formDataFilter.getPeriod()));
+		}
+
+		if(formDataFilter.getKind() == Long.MAX_VALUE){
+			FormDataKind[] formDataKinds = FormDataKind.values();
+			List<Long> formDataKindLongList = new ArrayList<Long>();
+			for(int i = 0; i < formDataKinds.length; i++){
+				formDataKindLongList.add((long)formDataKinds[i].getId());
+			}
+			formDataDaoFilter.setKind(formDataKindLongList);
+		} else {
+			formDataDaoFilter.setKind(Arrays.asList(formDataFilter.getKind()));
+		}
+
+		if(formDataFilter.getFormtype() == Long.MAX_VALUE){
+			List<FormType> formTypeList = formTypeDao.listAllByTaxType(formDataFilter.getTaxType());
+			List<Long> formTypeLongList = new ArrayList<Long>();
+			//TODO: см коммент выше.
+			System.out.println("Check on empty formTypeList. isEmpty() = " + formTypeList.isEmpty());
+			if(!formTypeList.isEmpty()){
+				for(FormType formType : formTypeList){
+					formTypeLongList.add((long)formType.getId());
+				}
+			} else {
+				formTypeLongList.add(-1L);
+			}
+			formDataDaoFilter.setFormtype(formTypeLongList);
+		} else {
+			formDataDaoFilter.setFormtype(Arrays.asList(formDataFilter.getFormtype()));
+		}
+
+		formDataList = formDataSearchDao.findByFilter(formDataDaoFilter);
+
 		return formDataList;
 	}
 
@@ -69,4 +122,5 @@ public class FormDataSearchServiceImpl implements FormDataSearchService {
 	public List<ReportPeriod> listReportPeriodsByTaxType(TaxType taxType) {
 		return reportPeriodDao.listAllPeriodsByTaxType(taxType);
 	}
+
 }
