98276e01f6fbfb8f294678748a7d9ff7a9f88fe5
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-28 04:27:28 +0000

    SBRFACCTAX-544
    Заготовка интерфейса для FormDataService
    и новый класс-исключение, чтобы бросать в случае нехватки прав доступа
    
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@408 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/service/FormDataService.java
@@ -0,0 +1,57 @@
+package com.aplana.sbrf.taxaccounting.service;
+
+import com.aplana.sbrf.taxaccounting.log.Logger;
+import com.aplana.sbrf.taxaccounting.model.FormData;
+import com.aplana.sbrf.taxaccounting.model.FormDataKind;
+
+/**
+ * Сервис для работы с {@link FormData данными по налоговым формам}
+ * @author dsultanbekov
+ */
+public interface FormDataService {
+	/**
+	 * Создать налоговую форму заданного типа
+	 * При создании формы выполняются следующие действия:
+	 * 1) создаётся пустой объект
+	 * 2) если в объявлении формы заданы строки по-умолчанию (начальные данные), то эти строки копируются в созданную форму
+	 * 3) если в объявлении формы задан скрипт создания, то этот скрипт выполняется над создаваемой формой
+	 * @param logger логгер-объект для фиксации диагностических сообщений
+	 * @param userId идентификатор пользователя, запросившего операцию
+	 * @param formTemplateId идентификатор шаблона формы, по которой создавать объект
+	 * @param departmentId идентификатор {@link Department подразделения}, к которому относится форма
+	 * @param kind {@link FormDataKind тип налоговой формы} (первичная, сводная, и т.д.), это поле необходимо, так как некоторые виды
+	 *		налоговых форм в одном и том же подразделении могут существовать в нескольких вариантах (например один и тот же РНУ  на уровне ТБ
+	 *		- в виде первичной и консолидированной) 
+	 * @return созданный и проинициализированный объект данных.
+	 * @throws AccessDeniedException если у пользователя нет прав создавать налоговую форму с такими параметрами
+	 * @throws ServiceException если при создании формы произошли ошибки, вызванные несоблюдением каких-то бизнес-требований, например отсутствием
+	 *		обязательных параметров
+	 */
+	FormData createFormData(Logger logger, int userId, int formTemplateId, int departmentId, FormDataKind kind);
+	
+	/**
+	 * Выполнить расчёты по налоговой форме
+	 * @param logger логгер-объект для фиксации диагностических сообщений
+	 * @param userId идентификатор пользователя, запросившего операцию
+	 * @param formData объект с данными по налоговой форме
+	 */
+	void doCalc(Logger logger, int userId, FormData formData);
+	
+	/**
+	 * Сохранить данные по налоговой форме
+	 * @param userId идентификатор пользователя, выполняющего операцию
+	 * @param formData объект с данными налоговой формы
+	 * @return идентификатор сохранённой записи
+	 * @throws AccessDeniedException если у пользователя нет прав редактировать налоговую форму с такими параметрами
+	 */
+	long saveFormData(int userId, FormData formData);
+	
+	/**
+	 * Получить данные по налоговой форме
+	 * @param userId идентификатор пользователя, выполняющего операцию
+	 * @param formDataId идентификатор записи, которую необходимо считать
+	 * @return объект с данными по налоговой форме
+	 * @throws AccessDeniedException если у пользователя нет прав просматривать налоговую форму с такими параметрами
+	 */
+	FormData getFormData(int userId, long formDataId);
+}
---
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/service/exception/AccessDeniedException.java
@@ -0,0 +1,14 @@
+package com.aplana.sbrf.taxaccounting.service.exception;
+
+/**
+ * Класс-исключение, использующийся когда на сервисном слое происходит ошибка,
+ * вызванная нехваткой прав у пользователя
+ * @author dsultanbekov
+ */
+public class AccessDeniedException extends ServiceException {
+	private static final long serialVersionUID = 1L;
+
+	public AccessDeniedException(String message, Object... params) {
+		super(message, params);
+	}	
+}
