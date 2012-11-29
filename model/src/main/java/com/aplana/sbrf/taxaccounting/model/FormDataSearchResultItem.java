482900440cb27320ed0e580efbe810ba2afcb1d7
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-29 07:18:41 +0000

    Создал DTO-класс, для того, чтобы представлять информацию по налоговой форме в результатах поиска
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@446 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/FormDataSearchResultItem.java
@@ -0,0 +1,98 @@
+package com.aplana.sbrf.taxaccounting.model;
+
+/**
+ * DTO-Класс, содержащий информацию о параметрах налоговых форм и связанных с ним объектов в "плоском" виде
+ * Используется для того, чтобы отображать результаты поисковых запросов по налоговым формам в таблицах, без необходимости
+ * запрашивать из БД сведения по связанным объектам (название подразделения, вид налога и т.п.)
+ */
+public class FormDataSearchResultItem {
+	// Идентификатор записи с данными налоговой формы
+	private Long formDataId;
+	// Тип налоговой формы
+	private FormDataKind formDataKind;
+	// Стадия жизненного цикла
+	private WorkflowState state;
+	// Идентификатор вида налоговой формы
+	private Integer formTypeId;
+	// Название вида налоговой формы
+	private String formTypeName;
+	// Вид налога
+	private TaxType taxType;
+	// Идентификатор подразделения
+	private Integer departmentId;
+	// Название подразделения
+	private String departmentName;
+	// Тип подразделения
+	private DepartmentType departmentType;
+	// Идентификатор отчётного периода
+	private Integer reportPeriodId;
+	// Название отчётного периода
+	private String reportPeriodName;
+	
+	public Long getFormDataId() {
+		return formDataId;
+	}
+	public void setFormDataId(Long formDataId) {
+		this.formDataId = formDataId;
+	}
+	public FormDataKind getFormDataKind() {
+		return formDataKind;
+	}
+	public void setFormDataKind(FormDataKind formDataKind) {
+		this.formDataKind = formDataKind;
+	}
+	public WorkflowState getState() {
+		return state;
+	}
+	public void setState(WorkflowState state) {
+		this.state = state;
+	}
+	public Integer getFormTypeId() {
+		return formTypeId;
+	}
+	public void setFormTypeId(Integer formTypeId) {
+		this.formTypeId = formTypeId;
+	}
+	public String getFormTypeName() {
+		return formTypeName;
+	}
+	public void setFormTypeName(String formTypeName) {
+		this.formTypeName = formTypeName;
+	}
+	public TaxType getTaxType() {
+		return taxType;
+	}
+	public void setTaxType(TaxType taxType) {
+		this.taxType = taxType;
+	}
+	public Integer getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(Integer departmentId) {
+		this.departmentId = departmentId;
+	}
+	public String getDepartmentName() {
+		return departmentName;
+	}
+	public void setDepartmentName(String departmentName) {
+		this.departmentName = departmentName;
+	}
+	public DepartmentType getDepartmentType() {
+		return departmentType;
+	}
+	public void setDepartmentType(DepartmentType departmentType) {
+		this.departmentType = departmentType;
+	}
+	public Integer getReportPeriodId() {
+		return reportPeriodId;
+	}
+	public void setReportPeriodId(Integer reportPeriodId) {
+		this.reportPeriodId = reportPeriodId;
+	}
+	public String getReportPeriodName() {
+		return reportPeriodName;
+	}
+	public void setReportPeriodName(String reportPeriodName) {
+		this.reportPeriodName = reportPeriodName;
+	}
+}
