73c2218302bbf28ad35660c114ccbaa305d3d75e
Srybakov <Srybakov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-11-29 08:14:57 +0000

    Comments for interfaces
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@451 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- core/src/main/java/com/aplana/sbrf/taxaccounting/service/FormDataSearchService.java
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/service/FormDataSearchService.java
@@ -11,13 +11,39 @@ import java.util.List;
  */
 public interface FormDataSearchService {
 
+	/**
+	 * Данный метод, основываясь на userId и formDataFilter, формирует параметры фильтра {@link FormDataDaoFilter}, а
+	 * затем сформированный FormDataDaoFilter передает в функцию {@link FormDataSearchDao.findByFilter()} для выполнения
+	 * запроса к базе по заданным параметрам фильтра.
+	 * @param userId идентификатор пользователя
+	 * @param formDataFilter фильтр, по параметрам которого происходит поиск данных по отчетной форме
+	 * @return список идентификаторов данных по отчётным формам, соответствующие критериям поиска.
+	 */
 	List<FormData> findDataByUserIdAndFilter(int userId, FormDataFilter formDataFilter);
 
+	/**
+	 * Получить список всех отчетных периодов
+	 * @return список всех отчетных периодов
+	 */
 	List<Department> listDepartments();
 
+	/**
+	 * Получить список всех видов налоговых форм
+	 * @return список всех налоговых форм
+	 */
 	List<FormType> listFormTypes();
 
+	/**
+	 * Получить список видов налоговых форм с определенным видом налога
+	 * @param taxType вид налога
+	 * @return список налоговых форм с определенным видом налога
+	 */
 	List<FormType> listFormTypesByTaxType(TaxType taxType);
 
+	/**
+	 * Получить список отчетных периодов с определенным видом налога
+	 * @param taxType вид налога
+	 * @return список отчетных периодов определенным видом налога
+	 */
 	List<ReportPeriod> listReportPeriodsByTaxType(TaxType taxType);
 }
