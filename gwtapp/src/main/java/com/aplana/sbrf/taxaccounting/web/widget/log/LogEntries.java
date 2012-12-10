6de1972c1dcf431d686c5451f102f8e2e54f4271
Sgoryachkin <Sgoryachkin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2012-12-10 09:01:57 +0000

    Добавлен виджет для отображения лога
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@565 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/log/LogEntries.java
@@ -0,0 +1,11 @@
+package com.aplana.sbrf.taxaccounting.web.widget.log;
+
+import java.util.List;
+
+import com.aplana.sbrf.taxaccounting.model.log.LogEntry;
+
+public interface LogEntries {
+	
+	void setLogEntries(List<LogEntry> entries);
+
+}
---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/log/LogEntriesWidget.java
@@ -0,0 +1,41 @@
+package com.aplana.sbrf.taxaccounting.web.widget.log;
+
+import java.util.List;
+
+import com.aplana.sbrf.taxaccounting.model.log.LogEntry;
+import com.aplana.sbrf.taxaccounting.web.widget.cell.LogEntryCell;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+
+public class LogEntriesWidget extends Composite implements LogEntries {
+
+	private static LogEntryWidgetUiBinder uiBinder = GWT
+			.create(LogEntryWidgetUiBinder.class);
+
+	interface LogEntryWidgetUiBinder extends UiBinder<Widget, LogEntriesWidget> {
+	}
+
+	public LogEntriesWidget() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	@UiField(provided = true)
+	CellList<LogEntry> cellList = new CellList<LogEntry>(new LogEntryCell());
+
+	@Override
+	public void setLogEntries(List<LogEntry> entries) {
+		if (entries != null) {
+			cellList.setRowCount(entries.size());
+			cellList.setRowData(entries);
+
+		} else {
+			cellList.setRowCount(0);
+		}
+		cellList.redraw();
+	}
+
+}
