1e7469dd266e4770974c8aaa914ec8dfd43e6100
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-01-17 11:53:04 +0000

    SBRFACCTAX-953
    Добавил заполнение столбца MAX_LENGTH в тестовых данных (чтобы не падали юнит-тесты)
    
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@790 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/test/resources/data/FormTemplate.sql
+++ dao/src/test/resources/data/FormTemplate.sql
@@ -1,7 +1,7 @@
 insert into form (id, type_id, data_rows, version, is_active, edition, numbered_columns) values (1, 1, null, '0.1', 1, 1, 0);
 
-insert into form_column (id, name, form_id, ord, alias, type, editable, mandatory, width, precision, dictionary_code, group_name) 
-	values (1, 'Строковый столбец', 1, 1, 'stringColumn', 'S', 1, 0, 10, null, null, null);
+insert into form_column (id, name, form_id, ord, alias, type, editable, mandatory, width, precision, dictionary_code, group_name, max_length) 
+	values (1, 'Строковый столбец', 1, 1, 'stringColumn', 'S', 1, 0, 10, null, null, null, 500);
 
 insert into form_column (id, name, form_id, ord, alias, type, editable, mandatory, width, precision, dictionary_code, group_name)
 	values (2, 'Числовой столбец', 1, 2, 'numericColumn', 'N', 1, 0, 10, 2, null, null);
