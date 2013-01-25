6f7c94d9f90569ba3d6830780c504765b88d0edc
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-01-25 11:43:29 +0000

    Убрал неиспользуемый импорт
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@861 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/ObjectLockDaoTest.java
+++ dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/ObjectLockDaoTest.java
@@ -1,12 +1,10 @@
 package com.aplana.sbrf.taxaccounting.dao.impl;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-
-import java.util.Calendar;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
