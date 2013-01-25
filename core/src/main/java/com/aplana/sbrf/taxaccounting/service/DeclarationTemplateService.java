800d114e918ec4b7f1ace2f335721e461aaeab25
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-01-25 12:18:04 +0000

    SBRFACCTAX-1076
    Интерфейс сервиса для работы с шаблонами деклараций
    
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@864 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/service/DeclarationTemplateService.java
@@ -0,0 +1,48 @@
+package com.aplana.sbrf.taxaccounting.service;
+
+import com.aplana.sbrf.taxaccounting.model.DeclarationTemplate;
+import com.aplana.sbrf.taxaccounting.service.exception.AccessDeniedException;
+import com.aplana.sbrf.taxaccounting.service.exception.ServiceException;
+
+/**
+ * Сервис для работы с шаблонами деклараций 
+ * @author dsultanbekov
+  */
+public interface DeclarationTemplateService {
+	/**
+	 * Получить шаблон декларации
+	 * @param declarationTemplateId идентификатор шаблона декларации
+	 * @return объект шаблона декларации
+	 */
+	DeclarationTemplate get(int declarationTemplateId);
+	/**
+	 * Сохранить шаблон декларации.
+	 * Если сохраняется новый объект, то у него должен быть пустой id (id == null), в этом случае он будет сгенерирован
+	 * @param declarationTemplate объект шаблона декларации
+	 * @return идентификатор сохранённой записи в БД
+	 * @throws AccessDeniedException если у пользователя нет прав на изменение шаблона декларации 
+	 */
+	int save(DeclarationTemplate declarationTemplate);
+	/**
+	 * Задать шаблон Jrxml-файла
+	 * Метод компилирует jrxml-файл и записиывает в БД, как сам jrxml-файл, так и его откомпилированную версию (jasper-файл).
+	 * @param declarationTemplateId идентификатор шаблона декларации 
+	 * @param jrxml jrxml в виде строки
+	 * @throws AccessDeniedException если у пользователя нет прав на изменение шаблона декларации
+	 * @throws ServiceException если при компиляции jrxml произошла ошибка
+	 */
+	void setJrxml(int declarationTemplateId, String jrxml);
+	/**
+	 * Получить шаблон Jrxml-файла
+	 * @param declarationTemplateId
+	 * @return jrxml-файл в виде строки
+	 */
+	String getJrxml(int declarationTemplateId);
+	
+	/**
+	 * Получить jasper-файл
+	 * @param declarationTemplateId
+	 * @return jasper-файл в виде байтового массива
+	 */
+	byte[] getJasper(int declarationTemplateId);	
+}
