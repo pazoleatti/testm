e781a65f75f8ffff5ced7c558a1e4d88f1eb1faf
Srybakov <Srybakov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-01-29 06:58:07 +0000

    http://jira.aplana.com/browse/SBRFACCTAX-1106 Доработать диалоговое окно с ошибкой чтобы оно не отображало стек трейс сразу (GUI)
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@884 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/log/ThrowableWidget.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/log/ThrowableWidget.java
@@ -2,8 +2,10 @@ package com.aplana.sbrf.taxaccounting.web.widget.log;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.*;
 
 public class ThrowableWidget extends Composite implements ThrowableView {
@@ -18,7 +20,10 @@ public class ThrowableWidget extends Composite implements ThrowableView {
 
 	@UiField
 	Label message;
-	
+
+	@UiField
+	Label showTextLabel;
+
 	@UiField
 	TextArea text;
 
@@ -30,19 +35,29 @@ public class ThrowableWidget extends Composite implements ThrowableView {
 	@Override
 	public void setThrowable(Throwable throwable) {
 		if (throwable != null) {
-			text.setVisible(true);
+
 			StackTraceElement[] trace = throwable.getStackTrace();
 			StringBuilder sb = new StringBuilder();
 			for (StackTraceElement stackTraceElement : trace) {
 				sb.append(stackTraceElement.toString()).append("\n");
 			}
 			text.setText(sb.toString());
+			text.setVisible(false);
+			showTextLabel.setVisible(true);
 			message.setText(throwable.getLocalizedMessage());
+
 		} else {
+			showTextLabel.setVisible(false);
 			text.setVisible(false);
 			text.setText("");
 			message.setText("");
 		}
+	}
+
+	@UiHandler("showTextLabel")
+	void onShowTextLabelClicked(ClickEvent event){
+		showTextLabel.setVisible(false);
+		text.setVisible(true);
 	}
 
 }
