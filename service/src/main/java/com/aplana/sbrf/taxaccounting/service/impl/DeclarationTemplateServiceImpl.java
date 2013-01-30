e63229105895e3d439f00537e684de2417aab94b
Estetsenko <Estetsenko@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-01-30 12:36:08 +0000

    <SBRFACCTAX-1076> Первая имплементация сервиса для шаблонов деклараций
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@904 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ service/src/main/java/com/aplana/sbrf/taxaccounting/service/impl/DeclarationTemplateServiceImpl.java
@@ -0,0 +1,64 @@
+package com.aplana.sbrf.taxaccounting.service.impl;
+
+import com.aplana.sbrf.taxaccounting.dao.DeclarationTemplateDao;
+import com.aplana.sbrf.taxaccounting.model.DeclarationTemplate;
+import com.aplana.sbrf.taxaccounting.service.DeclarationTemplateService;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.design.JasperDesign;
+import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+import java.util.List;
+
+/**
+ * Сервис для работы с шаблонами деклараций
+ * @author Eugene Stetsenko
+ */
+@Service
+public class DeclarationTemplateServiceImpl implements DeclarationTemplateService {
+
+	@Autowired
+	DeclarationTemplateDao declarationTemplateDao;
+
+	@Override
+	public List<DeclarationTemplate> listAll() {
+		return declarationTemplateDao.listAll();
+	}
+
+	@Override
+	public DeclarationTemplate get(int declarationTemplateId) {
+		return declarationTemplateDao.get(declarationTemplateId);
+	}
+
+	@Override
+	public int save(DeclarationTemplate declarationTemplate) {
+		return declarationTemplateDao.save(declarationTemplate);
+	}
+
+	@Override
+	public void setJrxml(int declarationTemplateId, String jrxml) {
+		ByteArrayOutputStream  xlsReport = new ByteArrayOutputStream();
+		JasperDesign jasperDesign = null;
+		try {
+			jasperDesign = JRXmlLoader.load(new ByteArrayInputStream(jrxml.getBytes()));
+			JasperCompileManager.compileReportToStream(jasperDesign, xlsReport);
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+
+		declarationTemplateDao.setJrxmlAndJasper(declarationTemplateId, jrxml, xlsReport.toByteArray());
+	}
+
+	@Override
+	public String getJrxml(int declarationTemplateId) {
+		return declarationTemplateDao.getJrxml(declarationTemplateId);
+	}
+
+	@Override
+	public byte[] getJasper(int declarationTemplateId) {
+		return declarationTemplateDao.getJasper(declarationTemplateId);
+	}
+}
