17c90ed0e45794d638442c6a1a2e5d7c0c668b8f
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-01 12:47:02 +0000

    SBRFACCTAX-1130
    Интерфейс dao для работы с информацией об источниках
    
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@933 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/DepartmentFormTypeDao.java
@@ -0,0 +1,40 @@
+package com.aplana.sbrf.taxaccounting.dao;
+
+import java.util.List;
+
+import com.aplana.sbrf.taxaccounting.model.DepartmentFormType;
+import com.aplana.sbrf.taxaccounting.model.FormDataKind;
+import com.aplana.sbrf.taxaccounting.model.TaxType;
+
+/**
+ * Интерфейс Dao для работы с привязкой департаментов к подразделениям
+ * @author dsultanbekov
+ */
+public interface DepartmentFormTypeDao {
+	/**
+	 * Возвращает список идентфикаторов подразделений, формы которых являются источниками для налоговых форм заданного подразеления 
+	 * по заданному виду налога.
+	 * @param departmentId идентификатор подразделения
+	 * @param taxType вид налога
+	 * @return список идентфикатор подразделений
+	 */
+	List<Integer> getSourceDepartmentIds(int departmentId, TaxType taxType);
+
+	/**
+	 * Возвращает информацию об источниках, которые должны использоваться при формировании налоговой формы
+	 * @param departmentId идентификатор подразделения формируемой налоговой формы
+	 * @param formTypeId вид налоговой формы
+	 * @param kind тип налоговой формы
+	 * @return информация о формах-источниках в виде списка {@link DepartmentFormType}
+	 */
+	List<DepartmentFormType> getSources(int departmentId, int formTypeId, FormDataKind kind);
+	
+	/**
+	 * Возвращает информацию о формах-потребителях, которые должны использовать информацию из данной налоговой формы в качестве источника
+	 * @param sourceDepartmentId идентификатор подразделения формы-источника
+	 * @param sourceFormTypeId вид налоговой формы-источника
+	 * @param sourceKind тип налоговой формы-источника
+	 * @return информация о формах-потребителях в виде списка {@link DepartmentFormType}
+	 */
+	List<DepartmentFormType> getDestanations(int sourceDepartmentId, int sourceFormTypeId, FormDataKind sourceKind);
+}
