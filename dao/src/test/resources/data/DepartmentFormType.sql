4a5cf12636368f75618d4707e47c8a4a59487422
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-01 11:32:16 +0000

    SBRFACCTAX-1126
    Добавил заполнение столбца Department_form_type.kind в тестовых данных
    
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@932 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/test/resources/data/DepartmentFormType.sql
+++ dao/src/test/resources/data/DepartmentFormType.sql
@@ -1,7 +1,7 @@
-insert into department_form_type (id, department_id, form_type_id) values (1, 1, 1);
-insert into department_form_type (id, department_id, form_type_id) values (2, 1, 2);
-insert into department_form_type (id, department_id, form_type_id) values (3, 1, 3);
-insert into department_form_type (id, department_id, form_type_id) values (4, 1, 4);
+insert into department_form_type (id, department_id, form_type_id, kind) values (1, 1, 1, 3);
+insert into department_form_type (id, department_id, form_type_id, kind) values (2, 1, 2, 3);
+insert into department_form_type (id, department_id, form_type_id, kind) values (3, 1, 3, 3);
+insert into department_form_type (id, department_id, form_type_id, kind) values (4, 1, 4, 3);
 
-insert into department_form_type (id, department_id, form_type_id) values (11, 2, 1);
-insert into department_form_type (id, department_id, form_type_id) values (12, 2, 2);
+insert into department_form_type (id, department_id, form_type_id, kind) values (11, 2, 1, 3);
+insert into department_form_type (id, department_id, form_type_id, kind) values (12, 2, 2, 3);
