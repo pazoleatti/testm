3fb7705593d6e456a8597daf24578655c3ed71f9
Ozherebtsova <Ozherebtsova@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-06 10:32:23 +0000

    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@973 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- src/main/resources/ddl/create_main.sql
+++ src/main/resources/ddl/create_main.sql
@@ -37,7 +37,7 @@ id number(9) not null,
 name varchar2(50) not null,
 tax_type char(1) not null,
 is_active number(1) default 1 not null,
-months    number(2) not nullб
+months    number(2) not null,
 tax_period_id number(9) not null,
 ord           number(2) not null
 );
