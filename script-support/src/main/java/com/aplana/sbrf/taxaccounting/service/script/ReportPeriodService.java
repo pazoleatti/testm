8869e394efa154a3e7763af470b30bfd1e0e6701
Auldanov <Auldanov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-13 10:17:20 +0000

    ДОбавлен сервис для работы с отчетными периодами для скриптов
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1049 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/ReportPeriodService.java
@@ -0,0 +1,35 @@
+package com.aplana.sbrf.taxaccounting.service.script;
+
+import java.util.List;
+
+
+import com.aplana.sbrf.taxaccounting.model.ReportPeriod;
+import com.aplana.sbrf.taxaccounting.util.ScriptExposed;
+
+@ScriptExposed
+public interface ReportPeriodService {
+	
+	/**
+	 * Получить объект отчётного периода по идентификатору периода
+	 * @param reportPeriodId идентификатор отчётного периода
+	 * @return объект, задаваемый идентификатором
+	 * @throws DAOException если периода с заданным идентификатором не существует 
+	 */
+	ReportPeriod get(int reportPeriodId);
+	
+	/**
+	 * Возвращает список отчётных периодов, входящий в данный налоговый период. 
+	 * Список отсортирован по {@link ReportPeriod#getOrder() порядковым номерам} отчётных периодов
+	 * @param taxPeriodId
+	 * @return список отчётных периодов, входящий в данный налоговый период, отсортированный по порядковому номеру
+	 */
+	List<ReportPeriod> listByTaxPeriod(int taxPeriodId);
+	
+	/**
+	 * Возвращает предыдущий отчетный период, если такой период не найден то null 
+	 * @param reportPeriodId
+	 * @return следующий отчетный период
+	 */
+	ReportPeriod getPrevReportPeriod(int reportPeriodId);
+	
+}
---
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/impl/ReportPeriodServiceImpl.java
@@ -0,0 +1,49 @@
+package com.aplana.sbrf.taxaccounting.service.script.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aplana.sbrf.taxaccounting.dao.ReportPeriodDao;
+import com.aplana.sbrf.taxaccounting.dao.impl.AbstractDao;
+import com.aplana.sbrf.taxaccounting.model.ReportPeriod;
+import com.aplana.sbrf.taxaccounting.service.script.ReportPeriodService;
+
+
+/*
+ * Реализация ReportPeriodService
+ * @author auldanov
+ */
+@Repository("reportPeriodService")
+@Transactional(readOnly = true)
+public class ReportPeriodServiceImpl extends AbstractDao implements ReportPeriodService {
+
+	@Autowired
+	ReportPeriodDao dao;
+	
+	@Override
+	public ReportPeriod get(int reportPeriodId) {
+		return dao.get(reportPeriodId);
+	}
+
+	@Override
+	public List<ReportPeriod> listByTaxPeriod(int taxPeriodId) {
+		return dao.listByTaxPeriod(taxPeriodId);
+	}
+
+	@Override
+	public ReportPeriod getPrevReportPeriod(int reportPeriodId) {
+		ReportPeriod thisReportPeriod= dao.get(reportPeriodId);
+		List<ReportPeriod> listByTaxPeriod = dao.listByTaxPeriod(thisReportPeriod.getTaxPeriodId());
+		
+		for (int i = 0; i < listByTaxPeriod.size(); i++){
+			if (listByTaxPeriod.get(i).getTaxPeriodId() == reportPeriodId && i!=0){
+				return listByTaxPeriod.get(i-1);
+			}
+		}
+		
+		return null;
+	}
+}
