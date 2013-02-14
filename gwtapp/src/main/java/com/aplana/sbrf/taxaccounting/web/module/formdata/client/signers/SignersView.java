2a7e213e111851963db43f575058ea8b2db8121f
Ibukanov <Ibukanov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-14 09:08:49 +0000

    SBRFACCTAX-1252 Добавить возможность указывать информацию о подписантах и исполнителе налоговой формы осталась серверная часть
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1063 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdata/client/signers/SignersView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdata/client/signers/SignersView.java
@@ -3,6 +3,7 @@ package com.aplana.sbrf.taxaccounting.web.module.formdata.client.signers;
 import com.aplana.sbrf.taxaccounting.model.FormDataPerformer;
 import com.aplana.sbrf.taxaccounting.model.FormDataSigner;
 import com.aplana.sbrf.taxaccounting.web.widget.cell.KeyPressableTextInputCell;
+import com.google.gwt.cell.client.FieldUpdater;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -11,12 +12,14 @@ import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.cellview.client.DataGrid;
 import com.google.gwt.user.cellview.client.TextColumn;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 import com.google.gwt.view.client.SingleSelectionModel;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.PopupViewWithUiHandlers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -56,44 +59,16 @@ public class SignersView extends PopupViewWithUiHandlers<SignersUiHandlers> impl
 
 	private final PopupPanel widget;
 	private List<FormDataSigner> signers;
+	private List<FormDataSigner> clonedSigners;
+	private FormDataPerformer clonedPerformer;
 	private final SingleSelectionModel<FormDataSigner> selectionModel = new SingleSelectionModel<FormDataSigner>();
 
 	@Inject
 	public SignersView(Binder uiBinder, EventBus eventBus) {
 		super(eventBus);
 		widget = uiBinder.createAndBindUi(this);
-		asPopupPanel().setModal(true);
 		widget.setAnimationEnabled(true);
-
-		signersTable.setSelectionModel(selectionModel);
-		TextColumn<FormDataSigner> idColumn = new TextColumn<FormDataSigner>() {
-			@Override
-			public String getValue(FormDataSigner object) {
-				return "" + (signers.indexOf(object) + 1);
-			}
-		};
-		idColumn.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
-		signersTable.addColumn(idColumn, "№ пп");
-		signersTable.setColumnWidth(idColumn, 40, Style.Unit.PX);
-
-		KeyPressableTextInputCell nameCell = new KeyPressableTextInputCell();
-		Column<FormDataSigner, String> nameColumn = new Column<FormDataSigner, String>(nameCell) {
-			@Override
-			public String getValue(FormDataSigner object) {
-				return object.getName();
-			}
-		};
-		signersTable.addColumn(nameColumn, "ФИО подписанта");
-
-		KeyPressableTextInputCell positionCell = new KeyPressableTextInputCell();
-		Column<FormDataSigner, String> positionColumn = new Column<FormDataSigner, String>(positionCell) {
-			@Override
-			public String getValue(FormDataSigner object) {
-				return object.getPosition();
-			}
-		};
-		signersTable.addColumn(positionColumn, "Должность");
-
+		initTable();
 	}
 
 	@Override
@@ -103,6 +78,10 @@ public class SignersView extends PopupViewWithUiHandlers<SignersUiHandlers> impl
 
 	@Override
 	public void setPerformer(FormDataPerformer performer) {
+		clonedPerformer = new FormDataPerformer();
+		clonedPerformer.setName(performer.getName());
+		clonedPerformer.setPhone(performer.getPhone());
+
 		this.performer.setText(performer.getName());
 		this.phone.setText(performer.getPhone());
 	}
@@ -110,6 +89,13 @@ public class SignersView extends PopupViewWithUiHandlers<SignersUiHandlers> impl
 	@Override
 	public void setSigners(List<FormDataSigner> signers) {
 		this.signers = signers;
+		clonedSigners = new ArrayList<FormDataSigner>();
+		for (FormDataSigner signer : signers) {
+			FormDataSigner clonedSigner = new FormDataSigner();
+			clonedSigner.setName(signer.getName());
+			clonedSigner.setPosition(signer.getPosition());
+			clonedSigners.add(clonedSigner);
+		}
 		setSigners();
 	}
 
@@ -173,6 +159,76 @@ public class SignersView extends PopupViewWithUiHandlers<SignersUiHandlers> impl
 
 	@UiHandler("cancelButton")
 	public void onCancel(ClickEvent event){
-		getUiHandlers().onCancel();
+		if (isEqualClonedAndCurrentSignersAndReporter()) {
+			getUiHandlers().onCancel();
+		}
+		else {
+			if (Window.confirm("Данные изменились, хотите сохранить изменения?")) {
+				getUiHandlers().onSave();
+			} else {
+				getUiHandlers().onCancel();
+			}
+		}
+	}
+
+	private void initTable() {
+		signersTable.setSelectionModel(selectionModel);
+		TextColumn<FormDataSigner> idColumn = new TextColumn<FormDataSigner>() {
+			@Override
+			public String getValue(FormDataSigner object) {
+				return "" + (signers.indexOf(object) + 1);
+			}
+		};
+		idColumn.setHorizontalAlignment(HasHorizontalAlignment.ALIGN_CENTER);
+		signersTable.addColumn(idColumn, "№ пп");
+		signersTable.setColumnWidth(idColumn, 40, Style.Unit.PX);
+
+		KeyPressableTextInputCell nameCell = new KeyPressableTextInputCell();
+		Column<FormDataSigner, String> nameColumn = new Column<FormDataSigner, String>(nameCell) {
+			@Override
+			public String getValue(FormDataSigner object) {
+				return object.getName();
+			}
+		};
+		nameColumn.setFieldUpdater(new FieldUpdater<FormDataSigner, String>() {
+			@Override
+			public void update(int index, FormDataSigner signer, String value) {
+				signer.setName(value);
+			}
+		});
+		signersTable.addColumn(nameColumn, "ФИО подписанта");
+
+		KeyPressableTextInputCell positionCell = new KeyPressableTextInputCell();
+		Column<FormDataSigner, String> positionColumn = new Column<FormDataSigner, String>(positionCell) {
+			@Override
+			public String getValue(FormDataSigner object) {
+				return object.getPosition();
+			}
+		};
+		positionColumn.setFieldUpdater(new FieldUpdater<FormDataSigner, String>() {
+			@Override
+			public void update(int index, FormDataSigner signer, String value) {
+				signer.setPosition(value);
+			}
+		});
+		signersTable.addColumn(positionColumn, "Должность");
+	}
+
+	private boolean isEqualClonedAndCurrentSignersAndReporter() {
+		if (performer.getText().compareTo(clonedPerformer.getName()) != 0 ||
+				phone.getText().compareTo(clonedPerformer.getPhone()) != 0) {
+			return false;
+		}
+		if (clonedSigners.size() == signers.size()) {
+			for (int i = 0; i < signers.size(); i++) {
+				if (signers.get(i).getName().compareTo(clonedSigners.get(i).getName()) != 0 ||
+						signers.get(i).getPosition().compareTo(clonedSigners.get(i).getPosition()) != 0) {
+					return false;
+				}
+			}
+		} else {
+			return false;
+		}
+		return true;
 	}
 }
