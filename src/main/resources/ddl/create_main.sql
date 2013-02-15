e69dfd28c76bed2827774167c1d597b3b3a6da66
Ozherebtsova <Ozherebtsova@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-15 05:31:46 +0000

    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1070 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- src/main/resources/ddl/create_main.sql
+++ src/main/resources/ddl/create_main.sql
@@ -130,7 +130,7 @@ create table form_column (
 	alias varchar(100) not null,
 	type char(1) not null,
 	editable number(9) default 1 not null,
-	mandatory number(9)  default 1 not null,
+	mandatory number(9)  default 1,
 	width number(9) not null,
 	precision number(9),
 	dictionary_code varchar2(30),
