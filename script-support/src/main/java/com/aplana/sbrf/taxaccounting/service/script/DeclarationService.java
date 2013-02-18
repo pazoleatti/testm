e64d2019b7eef04752b3b90920306efe1e0ce2a3
Auldanov <Auldanov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-18 04:52:40 +0000

    ДОбавлен сервис declarationService для поиска деклараций в скриптах
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1098 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/DeclarationService.java
@@ -0,0 +1,21 @@
+package com.aplana.sbrf.taxaccounting.service.script;
+
+
+
+import com.aplana.sbrf.taxaccounting.model.Declaration;
+import com.aplana.sbrf.taxaccounting.util.ScriptExposed;
+
+
+@ScriptExposed
+public interface DeclarationService {
+	
+	/**
+	 * Ищет декларацию по заданным параметрам.
+	 * @param declarationTypeId идентификатор типа декларации
+	 * @param departmentId идентификатор {@link com.aplana.sbrf.taxaccounting.model.Department подразделения}
+	 * @param reportPeriodId идентификатор {@link com.aplana.sbrf.taxaccounting.model.ReportPeriod отчетного периода}
+	 * @return декларацию или null, если такой декларации не найдено
+	 * @throws DaoException если будет найдено несколько записей, удовлетворяющих условию поиска
+	 */
+	Declaration find(int declarationTypeId, int departmentId, int reportPeriodId);
+}
---
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/impl/DeclarationServiceImpl.java
@@ -0,0 +1,26 @@
+package com.aplana.sbrf.taxaccounting.service.script.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.aplana.sbrf.taxaccounting.dao.DeclarationDao;
+import com.aplana.sbrf.taxaccounting.model.Declaration;
+import com.aplana.sbrf.taxaccounting.service.script.DeclarationService;
+
+
+/*
+ * author auldanov
+ */
+
+@Repository("declarationService")
+public class DeclarationServiceImpl implements DeclarationService{
+
+	@Autowired
+	DeclarationDao dao;
+	
+	@Override
+	public Declaration find(int declarationTypeId, int departmentId, int reportPeriodId) {
+		return dao.find(declarationTypeId, departmentId, reportPeriodId);
+	}
+
+}
