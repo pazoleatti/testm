a7dfe27d1dc47afc316162b2e325e74ad6e01eb9
Sgoryachkin <Sgoryachkin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-20 13:29:28 +0000

    SBRFACCTAX-1347 Добавил модельный класс DeclarationDataFilterAvailableValues
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1149 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/DeclarationDataFilterAvailableValues.java
@@ -0,0 +1,62 @@
+package com.aplana.sbrf.taxaccounting.model;
+
+import java.util.List;
+
+
+/**
+ * Модельный класс, содержащий информацию о значениях, которые допустимы
+ * в фильтре по декларациям.
+ * 
+ * @author sgoryachkin
+ */
+public class DeclarationDataFilterAvailableValues {
+	
+	private List<Integer> departmentIds;
+	
+	private List<DepartmentType> declarationTypes;
+
+	/**
+	 * @return the departmentIds
+	 */
+	public List<Integer> getDepartmentIds() {
+		return departmentIds;
+	}
+
+	/**
+	 * @param departmentIds the departmentIds to set
+	 */
+	public void setDepartmentIds(List<Integer> departmentIds) {
+		this.departmentIds = departmentIds;
+	}
+
+	/**
+	 * @return the declarationTypes
+	 */
+	public List<DepartmentType> getDeclarationTypes() {
+		return declarationTypes;
+	}
+
+	/**
+	 * @param declarationTypes the declarationTypes to set
+	 */
+	public void setDeclarationTypes(List<DepartmentType> declarationTypes) {
+		this.declarationTypes = declarationTypes;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("DeclarationDataFilterAvailableValues [departmentIds=");
+		builder.append(departmentIds);
+		builder.append(", declarationTypes=");
+		builder.append(declarationTypes);
+		builder.append("]");
+		return builder.toString();
+	}
+
+
+
+}
