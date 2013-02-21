6b91ee4d339d8db73d04cb4393793292e596bcf9
Avanteev <Avanteev@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-21 12:52:31 +0000

    Разработка SBRFACCTAX-1297 : Недокоммитил DepartmentParamTransport.
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1174 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/DepartmentParamTransport.java
@@ -0,0 +1,62 @@
+package com.aplana.sbrf.taxaccounting.model;
+
+public class DepartmentParamTransport {
+	
+	private int departmentId;
+	private int signatoryId;
+	private String signatorySurname;
+	private String signatoryFirstname;
+	private String signatoryLasttname;
+	private String approveDocName;
+	private String approveOrgName;
+	private String taxPlaceTypeCode;
+	public int getDepartmentId() {
+		return departmentId;
+	}
+	public void setDepartmentId(int departmentId) {
+		this.departmentId = departmentId;
+	}
+	public int getSignatoryId() {
+		return signatoryId;
+	}
+	public void setSignatoryId(int signatoryId) {
+		this.signatoryId = signatoryId;
+	}
+	public String getSignatorySurname() {
+		return signatorySurname;
+	}
+	public void setSignatorySurname(String signatorySurname) {
+		this.signatorySurname = signatorySurname;
+	}
+	public String getSignatoryFirstname() {
+		return signatoryFirstname;
+	}
+	public void setSignatoryFirstname(String signatoryFirstname) {
+		this.signatoryFirstname = signatoryFirstname;
+	}
+	public String getSignatoryLasttname() {
+		return signatoryLasttname;
+	}
+	public void setSignatoryLasttname(String signatoryLasttname) {
+		this.signatoryLasttname = signatoryLasttname;
+	}
+	public String getApproveDocName() {
+		return approveDocName;
+	}
+	public void setApproveDocName(String approveDocName) {
+		this.approveDocName = approveDocName;
+	}
+	public String getApproveOrgName() {
+		return approveOrgName;
+	}
+	public void setApproveOrgName(String approveOrgName) {
+		this.approveOrgName = approveOrgName;
+	}
+	public String getTaxPlaceTypeCode() {
+		return taxPlaceTypeCode;
+	}
+	public void setTaxPlaceTypeCode(String taxPlaceTypeCode) {
+		this.taxPlaceTypeCode = taxPlaceTypeCode;
+	}
+
+}
