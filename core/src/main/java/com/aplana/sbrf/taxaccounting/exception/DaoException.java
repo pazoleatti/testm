aeb49715c8d55f97c2cc44fcfef6b84f820501cb
Dsultanbekov <Dsultanbekov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-21 11:49:05 +0000

    SBRFACCTAX-809
    Выделил у всех классов-исключений общий супер-класс
    
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1166 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- core/src/main/java/com/aplana/sbrf/taxaccounting/exception/DaoException.java
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/exception/DaoException.java
@@ -7,17 +7,10 @@ package com.aplana.sbrf.taxaccounting.exception;
  * а не техническими проблемами.
  * Текст сообщения должен быть понятен пользователю
  */
-public class DaoException extends RuntimeException {
+public class DaoException extends TAException {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Создаёт объект исключения с заданным текстом ошибки. Текст может содержать плейсхолдеры,
-	 * которые будут заполнены на основе значений, переданных в последующих опциональных аргументах.
-	 * Сигнатура метода повторяет метод {@link String#format(String, Object...)} 
-	 * @param message текст ошибки, опционально содержащий плейсхолдеры
-	 * @param params список объектов для подставноки в текст сообщения
-	 */
 	public DaoException(String message, Object... params) {
-		super(String.format(message, params));
+		super(message, params);
 	}
 }
---
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/exception/TAException.java
@@ -0,0 +1,25 @@
+package com.aplana.sbrf.taxaccounting.exception;
+
+/**
+ * Базовый класс для исключений, создаваемых в рамках проекта.
+ * Предполагается, что исключения, отнаследованные от этого объекта, должны содержать текст с описанием ошибки,
+ * понятный пользователю.
+ * 
+ * NOTE: Это не означает, что все исключения, выбрасываемые в проекте должны быть отнаследованы от TAException.
+ * Во многих случаях вполне можно использовать "стандартные" классы исключений - NullPointerException, IllegalArgumentException и т.д. 
+ * @author dsultanbekov
+ */
+public abstract class TAException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * Создаёт объект исключения с заданным текстом ошибки. Текст может содержать плейсхолдеры,
+	 * которые будут заполнены на основе значений, переданных в последующих опциональных аргументах.
+	 * Сигнатура метода повторяет метод {@link String#format(String, Object...)} 
+	 * @param message текст ошибки, опционально содержащий плейсхолдеры
+	 * @param params список объектов для подставноки в текст сообщения
+	 */
+	protected TAException(String message, Object... params) {
+		super(String.format(message, params));
+	}
+}
