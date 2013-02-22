5bd6d430dd44ba171b759d791b959bc781cd3985
Auldanov <Auldanov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-22 11:56:14 +0000

    fix repository
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1205 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/dictionary/impl/DictionaryRegionServiceImpl.java
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/dictionary/impl/DictionaryRegionServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository("declarationService")
+@Repository("dictionaryRegionService")
 public class DictionaryRegionServiceImpl implements DictionaryRegionService {
     @Autowired
     DictionaryRegionDao dictionaryRegionDao;
