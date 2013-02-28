4a6c17fe3948a23cee7026ed915ccaf2a076306f
Estetsenko <Estetsenko@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-02-28 13:31:20 +0000

    <SBRFACCTAX-1312> Реализован алгоритм формирования имени файла для деклараций
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1301 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/mvc/DeclarationDataController.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/mvc/DeclarationDataController.java
@@ -1,7 +1,11 @@
 package com.aplana.sbrf.taxaccounting.web.mvc;
 
+import com.aplana.sbrf.taxaccounting.model.DeclarationData;
+import com.aplana.sbrf.taxaccounting.model.DepartmentParam;
 import com.aplana.sbrf.taxaccounting.model.TAUser;
 import com.aplana.sbrf.taxaccounting.service.DeclarationDataService;
+import com.aplana.sbrf.taxaccounting.service.DeclarationTemplateService;
+import com.aplana.sbrf.taxaccounting.service.DepartmentService;
 import com.aplana.sbrf.taxaccounting.web.main.api.server.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,16 +16,28 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.UUID;
 
 
 @Controller
 public class DeclarationDataController {
+
+	public static final String DATE_FORMAT = "yyyyMMdd";
 
 	@Autowired
 	DeclarationDataService declarationService;
 
 	@Autowired
 	private SecurityService securityService;
+
+	@Autowired
+	private DeclarationTemplateService declarationTemplateService;
+
+	@Autowired
+	private DepartmentService departmentService;
 
 	@RequestMapping(value = "/downloadExcel/{declarationId}",method = RequestMethod.GET)
 	public void processDownloadExcel(@PathVariable int declarationId, HttpServletResponse resp)
@@ -31,7 +47,7 @@ public class DeclarationDataController {
 
 		if (declarationService.getXlsxData(declarationId, userId) != null) {
 			OutputStream respOut = resp.getOutputStream();
-			String fileName = "Declaration_" + declarationId + ".xlsx";
+			String fileName = generateFileName(declarationId, userId, "xlsx");
 			resp.setContentType("application/octet-stream");
 			resp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
 			respOut.write(declarationService.getXlsxData(declarationId, userId));
@@ -50,7 +66,7 @@ public class DeclarationDataController {
 
 		if (declarationService.getXmlData(declarationId, userId) != null) {
 			OutputStream respOut = resp.getOutputStream();
-			String fileName = "Declaration_" + declarationId + ".xml";
+			String fileName = generateFileName(declarationId, userId, "xml");
 			resp.setContentType("application/octet-stream");
 			resp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
 			respOut.write(declarationService.getXmlData(declarationId, userId).getBytes());
@@ -59,5 +75,25 @@ public class DeclarationDataController {
 		else {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
+	}
+
+	protected String generateFileName(int declarationId, int userId, String fileExtension) {
+		DeclarationData declaration = declarationService.get(declarationId, userId);
+		DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		String declarationPrefix =
+				declarationTemplateService.get(
+						declaration.getDeclarationTemplateId()
+				).getDeclarationType().getTaxType().getDeclarationPrefix();
+		DepartmentParam departmentParam = departmentService.getDepartmentParam(declaration.getDepartmentId());
+		Calendar calendar = Calendar.getInstance();
+		StringBuilder stringBuilder = new StringBuilder(declarationPrefix);
+		stringBuilder.append('_' +
+				departmentParam.getTaxOrganCode() + '_' +
+				departmentParam.getTaxOrganCode() + '_' +
+				departmentParam.getInn() + departmentParam.getKpp() + '_' +
+				dateFormat.format(calendar.getTime()) + '_' +
+				UUID.randomUUID().toString().toUpperCase() + '.' +
+				fileExtension);
+		return stringBuilder.toString();
 	}
 }
--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/TaxType.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/TaxType.java
@@ -4,30 +4,33 @@ package com.aplana.sbrf.taxaccounting.model;
  * Типы налогов
  * TODO: возможно в будущем стоит переделать в обычный класс и создать таблицу в БД
  */
+//TODO: Изменить префиксы для "Налог на имущество" и "НДС"
 public enum TaxType {
 	/**
 	 * Налог на прибыль
 	 */
-	INCOME('I', "Налог на прибыль"),
+	INCOME('I', "Налог на прибыль", "NO_PRIB"),
 	/**
 	 * Налог на имущество
 	 */
-	PROPERTY('P', "Налог на имущество"),
+	PROPERTY('P', "Налог на имущество", "NO_IM"),
 	/**
 	 * Транспортный налог
 	 */
-	TRANSPORT('T', "Транспортный налог"),
+	TRANSPORT('T', "Транспортный налог", "NO_TRAND"),
 	/**
 	 * НДС
 	 */
-	VAT('V', "НДС");
+	VAT('V', "НДС", "NO_VAT");
 	
 	private final char code;
 	private final String name;
+	private final String declarationPrefix;
 	
-	private TaxType(char code, String name) {
+	private TaxType(char code, String name, String declarationPrefix) {
 		this.code = code;
 		this.name = name;
+		this.declarationPrefix = declarationPrefix;
 	}
 	
 	/**
@@ -42,6 +45,13 @@ public enum TaxType {
 	 */
 	public String getName() {
 		return name;
+	}
+
+	/**
+	 * Получить значение префикса
+	 */
+	public String getDeclarationPrefix() {
+		return declarationPrefix;
 	}
 	
 	/**
