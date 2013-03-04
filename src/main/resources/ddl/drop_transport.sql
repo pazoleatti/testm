a68f6b9c77fef41b7c5a58d68878c3bbe976d1aa
Ozherebtsova <Ozherebtsova@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-03-04 09:41:29 +0000

    SBRFACCTAX-1486
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1326 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- src/main/resources/ddl/drop_transport.sql
+++
@@ -1,6 +0,0 @@
-drop table transport_okato;
-drop table transport_tax_rate;
-drop table transport_type_code;
-drop table transport_tax_benefit_code;
-drop table transport_unit_code;
-drop table transport_eco_class;
