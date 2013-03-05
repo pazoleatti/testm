d94a5369a6f5591bac4fa3ec56aac7eb6b87085c
Auldanov <Auldanov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-03-05 11:28:53 +0000

    Добавлены методы для получения параметров подразделения по транспорту и по прибыли
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1344 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/DepartmentService.java
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/DepartmentService.java
@@ -1,6 +1,8 @@
 package com.aplana.sbrf.taxaccounting.service.script;
 
 import com.aplana.sbrf.taxaccounting.model.DepartmentParam;
+import com.aplana.sbrf.taxaccounting.model.DepartmentParamIncome;
+import com.aplana.sbrf.taxaccounting.model.DepartmentParamTransport;
 import com.aplana.sbrf.taxaccounting.util.ScriptExposed;
 
 @ScriptExposed
@@ -13,4 +15,20 @@ public interface DepartmentService {
 	 * @throws com.aplana.sbrf.taxaccounting.exception.DaoException если подразделение с таким идентификатором не существует
 	 */
 	DepartmentParam getDepartmentParam(int departmentId);
+	
+	/**
+	 * Получить по id подразделения его параметры по налогу на прибыль
+	 * @param departmentId идентфикатор подразделения
+	 * @return параметры подразделения по налогу на прибыль
+	 * @throws com.aplana.sbrf.taxaccounting.exception.DaoException если подразделение с таким идентификатором не существует
+	 */
+	DepartmentParamIncome getDepartmentParamIncome(int departmentId);
+	
+	/**
+	 * Получить по id подразделения его параметры по транспортному налогу
+	 * @param departmentId идентфикатор подразделения
+	 * @return параметры подразделения по налогу на прибыль
+	 * @throws com.aplana.sbrf.taxaccounting.exception.DaoException если подразделение с таким идентификатором не существует
+	 */
+	DepartmentParamTransport getDepartmentParamTransport(int departmentId);
 }
--- script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/impl/DepartmentServiceImpl.java
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/impl/DepartmentServiceImpl.java
@@ -1,10 +1,13 @@
 package com.aplana.sbrf.taxaccounting.service.script.impl;
 
-import com.aplana.sbrf.taxaccounting.dao.DepartmentParamDao;
-import com.aplana.sbrf.taxaccounting.model.DepartmentParam;
-import com.aplana.sbrf.taxaccounting.service.script.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.aplana.sbrf.taxaccounting.dao.DepartmentParamDao;
+import com.aplana.sbrf.taxaccounting.model.DepartmentParam;
+import com.aplana.sbrf.taxaccounting.model.DepartmentParamIncome;
+import com.aplana.sbrf.taxaccounting.model.DepartmentParamTransport;
+import com.aplana.sbrf.taxaccounting.service.script.DepartmentService;
 
 @Service("departmentService")
 public class DepartmentServiceImpl implements DepartmentService {
@@ -15,5 +18,15 @@ public class DepartmentServiceImpl implements DepartmentService {
 	@Override
 	public DepartmentParam getDepartmentParam(int departmentId) {
 		return dao.getDepartmentParam(departmentId);
+	}
+
+	@Override
+	public DepartmentParamIncome getDepartmentParamIncome(int departmentId) {
+		return dao.getDepartmentParamIncome(departmentId);
+	}
+
+	@Override
+	public DepartmentParamTransport getDepartmentParamTransport(int departmentId) {
+		return dao.getDepartmentParamTransport(departmentId);
 	}
 }
