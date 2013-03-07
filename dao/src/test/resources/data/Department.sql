79f012c689f2e8596d2f8df9a283cd0fa8f45473
Mfayzullin <Mfayzullin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-03-07 04:54:43 +0000

    http://jira.aplana.com/browse/SBRFACCTAX-1529 изменил запрос для ДАО-тестов
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1355 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/test/resources/data/Department.sql
+++ dao/src/test/resources/data/Department.sql
@@ -1,3 +1,6 @@
-insert into department (id, name, parent_id, type) values (1, 'Банк', null, 1);
-insert into department (id, name, parent_id, type) values (2, 'ТБ1', 1, 2);
-insert into department (id, name, parent_id, type) values (3, 'ТБ2', 1, 2);
+insert into DEPARTMENT (id, name, parent_id, type, shortname, dict_region_id, tb_index, sbrf_code)
+  values (1, 'Банк', null, 1, null, null, null, null);
+insert into DEPARTMENT (id, name, parent_id, type, shortname, dict_region_id, tb_index, sbrf_code)
+  values (2, 'ТБ1', 1, 2, null, null, null, null);
+insert into DEPARTMENT (id, name, parent_id, type, shortname, dict_region_id, tb_index, sbrf_code)
+  values (3, 'ТБ2', 1, 2, null, null, null, null);
