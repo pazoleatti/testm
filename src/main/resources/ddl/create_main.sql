dd98ef9998226d2412ec6e3925e69f6e2e5bf3fd
Ozherebtsova <Ozherebtsova@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-03-11 12:03:11 +0000

    изменение pk на таблицы income_101 и income_102
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1391 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- src/main/resources/ddl/create_main.sql
+++ src/main/resources/ddl/create_main.sql
@@ -204,9 +204,8 @@ comment on column report_period.ord is 'Номер отчетного перио
 create sequence seq_report_period start with 100;
 ----------------------------------------------------------------------------------------------------
 create table income_101
-(id number(9) not null,
- report_period_id number(9) not null,
- account varchar2(255),
+(report_period_id number(9) not null,
+ account varchar2(255) not null,
  income_debet_remains number(22,4),
  income_credit_remains number(22,4),
  debet_rate number(22,4),
@@ -215,11 +214,10 @@ create table income_101
  outcome_credit_remains number(22,4)
 );
 
-alter table income_101 add constraint income_101_pk primary key(id);
+alter table income_101 add constraint income_101_pk primary key(report_period_id,account);
 alter table income_101 add constraint income_101_fk_report_period_id foreign key (report_period_id) references report_period(id);
 
 comment on table income_101 is 'Оборотная ведомость (Форма 0409101-СБ)';
-comment on column income_101.id is 'Первичный ключ';
 comment on column income_101.report_period_id is 'Идентификатор отчетного периода';
 comment on column income_101.account is 'Номер счета';
 comment on column income_101.income_debet_remains is 'Входящие остатки по дебету';
@@ -231,10 +229,10 @@ comment on column income_101.outcome_credit_remains is 'Исходящие ос�
 -------------------------------------------------------------------------------------------------------------------------------------------
 create table income_102
 (report_period_id number(9) not null,
- opu_code varchar2(25),
+ opu_code varchar2(25) not null,
  total_sum number(22,4));
  
-alter table income_102 add constraint income_102_pk primary key(report_period_id);
+alter table income_102 add constraint income_102_pk primary key(report_period_id,opu_code);
 alter table income_102 add constraint income_102_fk_report_period_id foreign key (report_period_id) references report_period(id);
 
 comment on table income_102 is 'Отчет о прибылях и убытках (Форма 0409102-СБ)';
