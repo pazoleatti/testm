fb7c73ceb47b99d1bfffed9303f604e116595ad1
Sgoryachkin <Sgoryachkin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-03-12 05:43:02 +0000

    SBRFACCTAX-1524 Перегенерил serializationUUID в  FormType
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1401 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/FormType.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/FormType.java
@@ -4,12 +4,12 @@ import java.io.Serializable;
 
 /**
  * Вид налоговой формы (название вида формы, без привязки к версии)
- * Каждому виду формы (FormType) может соответствовать несколько версий формы (Form).
+ * Каждому виду формы (FormType) может соответствовать несколько версий формы (FormTemplate).
  * @author dsultanbekov
  */
 public class FormType implements Serializable {
-	private static final long serialVersionUID = 1L;
-
+	private static final long serialVersionUID = 3632916658352875666L;
+	
 	private int id;
 	private String name;
 	private TaxType taxType;
