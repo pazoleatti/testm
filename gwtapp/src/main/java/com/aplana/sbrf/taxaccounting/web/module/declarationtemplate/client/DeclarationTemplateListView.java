5f43113c7afe2adbb65c2e921cac52182db71fb9
Ibukanov <Ibukanov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-03-12 10:29:47 +0000

    SBRFACCTAX-1586 Изменить структуру таблиц в админке для списка шаблонов деклараций и налоговых форм список шаблонов деклараций
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1412 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationtemplate/client/DeclarationTemplateListView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationtemplate/client/DeclarationTemplateListView.java
@@ -1,7 +1,9 @@
 package com.aplana.sbrf.taxaccounting.web.module.declarationtemplate.client;
 
 import com.aplana.sbrf.taxaccounting.model.DeclarationTemplate;
-import com.google.gwt.cell.client.ActionCell;
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.CheckboxCell;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellTable;
@@ -34,28 +36,44 @@ public class DeclarationTemplateListView extends ViewWithUiHandlers<DeclarationT
 	public DeclarationTemplateListView(Binder binder) {
 		widget = binder.createAndBindUi(this);
 
-		// колонка с название декларации
+		// колонка Наименование декларации
+		Column<DeclarationTemplate, DeclarationTemplate> linkColumn = new Column<DeclarationTemplate, DeclarationTemplate>(
+				new AbstractCell<DeclarationTemplate>() {
+					@Override
+					public void render(Context context,
+									   DeclarationTemplate declaration,
+									   SafeHtmlBuilder sb) {
+						if (declaration == null) {
+							return;
+						}
+						sb.appendHtmlConstant("<a href=\"#"
+								+ DeclarationTemplateTokens.declarationTemplate + ";"
+								+ DeclarationTemplateTokens.declarationTemplateId + "="
+								+ declaration.getId() + "\">"
+								+ declaration.getDeclarationType().getName() + "</a>");
+					}
+				}) {
+			@Override
+			public DeclarationTemplate getValue(DeclarationTemplate object) {
+				return object;
+			}
+		};
+		declarationTemplateTable.addColumn(linkColumn, "Наименование");
+
+		declarationTemplateTable.addColumn(new Column<DeclarationTemplate, Boolean>(
+				new CheckboxCell()) {
+			@Override
+			public Boolean getValue(DeclarationTemplate declarationTemplate) {
+				return declarationTemplate.isActive();
+			}
+		}, "Активен");
+
 		declarationTemplateTable.addColumn(new TextColumn<DeclarationTemplate>() {
 			@Override
 			public String getValue(DeclarationTemplate declarationTemplate) {
-				return declarationTemplate.getDeclarationType().getName();
+				return declarationTemplate.getVersion();
 			}
-		});
-
-		// колонка с кнопкой "Изменить"
-		declarationTemplateTable.addColumn(new Column<DeclarationTemplate, Integer>(
-				new ActionCell<Integer>("Изменить", new ActionCell.Delegate<Integer>() {
-					@Override
-					public void execute(Integer id) {
-						getUiHandlers().selectDeclaration(id);
-					}
-				})
-		) {
-			@Override
-			public Integer getValue(DeclarationTemplate declarationTemplate) {
-				return declarationTemplate.getId();
-			}
-		});
+		}, "Версия");
 	}
 
 	@Override
