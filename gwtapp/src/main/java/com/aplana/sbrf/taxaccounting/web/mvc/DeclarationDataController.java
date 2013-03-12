bdeff5eae83707b751a9d413128a98d162480bdb
Sgoryachkin <Sgoryachkin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-03-12 13:56:33 +0000

    Перенес получении имени в сервис. ( Почистил импорт)
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@1423 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/mvc/DeclarationDataController.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/mvc/DeclarationDataController.java
@@ -2,10 +2,8 @@ package com.aplana.sbrf.taxaccounting.web.mvc;
 
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.StringReader;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.w3c.dom.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import com.aplana.sbrf.taxaccounting.model.TAUser;
@@ -26,37 +20,35 @@ import com.aplana.sbrf.taxaccounting.web.main.api.server.SecurityService;
 @Controller
 public class DeclarationDataController {
 
-	private static final String ATTR_FILE_ID = "ИдФайл";
-	private static final String TAG_FILE = "Файл";
-
 	@Autowired
 	DeclarationDataService declarationService;
 
 	@Autowired
 	private SecurityService securityService;
 
-	@RequestMapping(value = "/downloadExcel/{declarationId}",method = RequestMethod.GET)
-	public void processDownloadExcel(@PathVariable int declarationId, HttpServletResponse resp)
-			throws IOException {
+	@RequestMapping(value = "/downloadExcel/{declarationId}", method = RequestMethod.GET)
+	public void processDownloadExcel(@PathVariable int declarationId,
+			HttpServletResponse resp) throws IOException {
 		TAUser user = securityService.currentUser();
 		Integer userId = user.getId();
 
 		if (declarationService.getXlsxData(declarationId, userId) != null) {
 			OutputStream respOut = resp.getOutputStream();
-			String fileName = "declaration_" + declarationId + ".xlsx";
+			String fileName = getFileName(declarationId, userId, "xlsx");;
 			resp.setContentType("application/octet-stream");
-			resp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+			resp.setHeader("Content-Disposition", "attachment; filename=\""
+					+ fileName + "\"");
 			respOut.write(declarationService.getXlsxData(declarationId, userId));
 			respOut.close();
-		}
-		else {
+		} else {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
 	}
 
-	@RequestMapping(value = "/downloadXml/{declarationId}",method = RequestMethod.GET)
-	public void processDownloadXml(@PathVariable int declarationId, HttpServletResponse resp)
-			throws IOException, ParserConfigurationException, SAXException {
+	@RequestMapping(value = "/downloadXml/{declarationId}", method = RequestMethod.GET)
+	public void processDownloadXml(@PathVariable int declarationId,
+			HttpServletResponse resp) throws IOException,
+			ParserConfigurationException, SAXException {
 		TAUser user = securityService.currentUser();
 		Integer userId = user.getId();
 
@@ -64,18 +56,19 @@ public class DeclarationDataController {
 			String fileName = getFileName(declarationId, userId, "xml");
 			OutputStream respOut = resp.getOutputStream();
 			resp.setContentType("application/octet-stream");
-			resp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
-			respOut.write(declarationService.getXmlData(declarationId, userId).getBytes());
+			resp.setHeader("Content-Disposition", "attachment; filename=\""
+					+ fileName + "\"");
+			respOut.write(declarationService.getXmlData(declarationId, userId)
+					.getBytes());
 			respOut.close();
-		}
-		else {
+		} else {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
 	}
 
-	@RequestMapping(value = "/downloadPDF/{declarationId}",method = RequestMethod.GET)
-	public void processDownloadPdf(@PathVariable int declarationId, HttpServletResponse resp)
-			throws IOException {
+	@RequestMapping(value = "/downloadPDF/{declarationId}", method = RequestMethod.GET)
+	public void processDownloadPdf(@PathVariable int declarationId,
+			HttpServletResponse resp) throws IOException {
 		TAUser user = securityService.currentUser();
 		Integer userId = user.getId();
 
@@ -84,25 +77,15 @@ public class DeclarationDataController {
 			resp.setContentType("application/pdf");
 			respOut.write(declarationService.getPdfData(declarationId, userId));
 			respOut.close();
-		}
-		else {
+		} else {
 			resp.sendError(HttpServletResponse.SC_NOT_FOUND);
 		}
 	}
-	
-	
-	//TODO: Получение имени должно быть в сервисе.
-	protected String getFileName(int declarationId, int userId, String fileExtension) {
-		String xml = declarationService.getXmlData(declarationId, userId);
-		InputSource inputSource = new InputSource(new StringReader(xml));
-		try {
-			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(inputSource);
-			Node fileNode = document.getElementsByTagName(TAG_FILE).item(0);
-			NamedNodeMap attributes = fileNode.getAttributes();
-			Node fileNameNode = attributes.getNamedItem(ATTR_FILE_ID);
-			return fileNameNode.getTextContent() + '.' + fileExtension;
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
+
+	// TODO: Получение имени должно быть в сервисе.
+	protected String getFileName(int declarationId, int userId,
+			String fileExtension) {
+		return declarationService.getXmlDataFileName(declarationId, userId)
+				+ '.' + fileExtension;
 	}
 }
