1e2769e4baa14505840ee724adcf394f85343de5
Avanteev <Avanteev@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-05-21 07:49:45 +0000

    Разработка http://jira.aplana.com/browse/SBRFACCTAX-2538 : Докоммит.
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@2101 34aa7802-8fb0-764d-beb8-9b5b9d595954

---
+++ service/src/main/java/com/aplana/sbrf/taxaccounting/service/impl/print/AbstractXlsxReportBuilder.java
@@ -0,0 +1,48 @@
+package com.aplana.sbrf.taxaccounting.service.impl.print;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * User: avanteev
+ * Date: 20.05.13
+ */
+public abstract class AbstractXlsxReportBuilder {
+
+    protected Workbook workBook;
+
+    protected Sheet sheet;
+
+    protected static String fileName;
+
+    protected static final int cellWidth = 30;
+
+    public final String createReport() throws IOException {
+        fillHeader();
+        createTableHeaders();
+        createDataForTable();
+        fillFooter();
+        return flush();
+    }
+
+    protected abstract void createTableHeaders();
+
+    protected abstract void fillHeader();
+
+    protected abstract void createDataForTable();
+
+    protected abstract void fillFooter();
+
+    private String flush() throws IOException {
+        File file = File.createTempFile(fileName, ".xlsx");
+        OutputStream out = new FileOutputStream(file);
+        workBook.write(out);
+
+        return file.getAbsolutePath();
+    }
+}
