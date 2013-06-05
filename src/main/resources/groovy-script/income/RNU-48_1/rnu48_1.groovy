1db783f0d14a8bf2a6d246d0145cd90a8bcf24b7
Vsergeev <Vsergeev@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-06-05 09:00:28 +0000

    SBRFACCTAX-2347 Реализовать логку расчетов и проверок НФ РНУ-48.1 : завершено
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@2206 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- src/main/resources/groovy-script/income/RNU-48_1/rnu48_1.groovy
+++ src/main/resources/groovy-script/income/RNU-48_1/rnu48_1.groovy
@@ -3,12 +3,9 @@
  * Форма "3.1	(РНУ-48.1) Регистр налогового учёта «ведомость ввода в эксплуатацию инвентаря и принадлежностей
  * до 40 000 руб.  ".
  *
- * Версия ЧТЗ: 57
+ * Версия ЧТЗ: 64
  *
  * TODO:
- *          -   избавиться от обращений напрямую к formData.dataRows, когда будет готова обертка
- *          -   выяснить, как найти границы отчетного периода
- *          -   уточнить проверки строки ИТОГО, когда ее нет (пользователь еще не нажимал "Рассчитать")
  *          -   что за сквозная нумерация в рамках текущего отчетного года, как ее реализовывать?
  *
  * @author vsergeev
@@ -21,139 +18,74 @@
  */
 switch (formDataEvent) {
     case FormDataEvent.CHECK :
-        logicalCheck()
+        logicalCheckWithTotalDataRowCheck()
         break
     case FormDataEvent.CALCULATE :
-        if (logicalCheck(false)) {
+        if (logicalCheckWithoutTotalDataRowCheck()) {
             calc()
         }
         break
     case FormDataEvent.ADD_ROW :
-        addNewRowAction()
+        addNewRow()
         break
     case FormDataEvent.DELETE_ROW :
-        deleteRow()
+        deleteCurrentRow()
         break
 }
 
 /**
- * Добавить новую строку.
- */
-def addNewRowAction() {
-    def totalAmountRow = tryFindTotalAmountRow()
-    if (totalAmountRow != null) {
-        //добавляем новую строку перед ИТОГО
-        def tmpTotalAmountValue = totalAmountRow.amount
-        formData.deleteDataRow(totalAmountRow)
-        addNewRowAction()
-        def newTotalAmountRow = addNewRow(true)
-        newTotalAmountRow.amount = tmpTotalAmountValue
-    } else {
-        //добавляем новую строку в конец таблицы
-        def newRow = addNewRow()
-        newRow.number = formData.dataRows.size()
-    }
-}
-
-/**
- * Расчеты. Алгоритмы заполнения полей формы.
+ * Расчеты
  */
 void calc() {
-    //удаляем строку с итогами предыдущих расчетов (если она есть)
-    def totalAmountRow = tryFindTotalAmountRow()
-    if (totalAmountRow != null) {
-        formData.deleteDataRow(totalAmountRow)
-    }
     //расчитываем новые итоговые значения
-    def totalAmount = getTotalAmount()
-    def newRow = addNewRow(true)
-    newRow.amount = totalAmount
+    def totalResults = getTotalResultsForCols()
+    def totalRow = formData.getDataRow(getTotalDataRowAlias())
+    getTotalColsAliases().each { colName ->
+        totalRow[colName] = totalResults[colName]
+    }
 }
 
 /**
- * Получаем новую строку
- * @param isTotalAmountRow = {@value true когда нужно получить строку итогов}
- * @return
+ * проверяем все строки, включая итоговые
  */
-private def addNewRow(boolean isTotalAmountRow = false) {
-    def newRow
-    if (! isTotalAmountRow) {                   //полуаем просто новую строку, ячейки разрешены для редактирования
-        newRow = formData.appendDataRow()
-        def editableColsNames = getEditableColsNames()
-        editableColsNames.each{ value ->
-            newRow.getCell(value).editable = true
-        }
-    } else {                                    //получаем строку для ИТОГО
-        newRow = formData.appendDataRow('total')
-        newRow.inventoryNumber = 'Итого'
+boolean logicalCheckWithTotalDataRowCheck(){
+    if (logicalCheckWithoutTotalDataRowCheck()) {
+        checkTotalDataRowValid()
     }
-
-    return newRow
 }
 
-boolean logicalCheck(boolean checkTotalAmount = true){
+/**
+ * проверяем все строки, за исключением итоговых
+ */
+boolean logicalCheckWithoutTotalDataRowCheck(){
     def formIsValid = true
     //  проверка, что в таблице есть хотя бы одна строка
-    if (formData.dataRows.size() == 0) {
+    if (formData.dataRows.isEmpty()) {
         logger.error ('В таблице отсутствуют заполненные строки!')
         return false
     }
 
-    def currentReportPeriod = reportPeriodService.get(formData.reportPeriodId)
+    // 1.  Обязательность заполнения поля графы (с 1 по 4)
+    if (! requiredColsFilled()) {
+        logger.error('Не заполнены обязательные значения!')
+        return false
+    }
+
+    def reportPeriodRange = getReportPeriodRange()
 
     //построчно проверяем данные формы
     for (def row : formData.dataRows) {
-        if (! isTotalRow(row)) {
-            // 1.  Обязательность заполнения поля графы (с 1 по 4)
-            if (! requiredColsFilled()) {
-                formIsValid = false
-                break
-            }
-
+        if ( ! isInTotalRowsAliases(row.getAlias())) {
             // 2.  Проверка даты ввода в эксплуатацию и границ отчетного периода
             // Дата начала отчётного периода ≤ графа 3 ≤ дата окончания отчётного периода
-            if (! dateInReportPeriod(row.usefulDate, currentReportPeriod)){
+            if (! reportPeriodRange.containsWithinBounds(row.usefulDate)){
                 formIsValid = false
                 logger.error('Дата ввода в эксплуатацию вне границ отчетного периода!')
-            }
-        } else if (checkTotalAmount) {
-            // 3.  Проверка итоговых значений по всей форме
-            // todo как быть с проверками итоговых значений, если "Рассчитать" еще не нажимали и столбца ИТОГО нет?
-            def controlTotalAmount = getTotalAmount()
-            if ( ! row.get('amount').equals(controlTotalAmount)) {
-                formIsValid = false
-                logger.error('Итоговые значения рассчитаны неверно!')
             }
         }
     }
 
     return formIsValid
-}
-
-/**
- * Удалить строку.
- */
-def deleteRow() {
-    //todo (vsergeev) нумерация, после удаления строки из середины, должна обновляться?
-    formData.deleteDataRow(currentDataRow)
-}
-
-/**
- * @return получаем сумму всех сумм, исключая сумму в итоговом поле
- */
-private BigDecimal getTotalAmount() {
-    BigDecimal totalAmount = new BigDecimal(0)
-    for (def dataRow : formData.dataRows) {
-        if ( ! isTotalRow(dataRow) && dataRow.get('amount') != null)
-        totalAmount += dataRow.get('amount')
-    }
-
-    return totalAmount
-}
-
-boolean dateInReportPeriod(date, reportPeriod) {
-    //todo (vsergeev) узнать, как определить границы отчетного периода и дописать проверку. пока всегда возвращаем true
-    true
 }
 
 /**
@@ -163,16 +95,15 @@ boolean dateInReportPeriod(date, reportPeriod) {
  */
 boolean requiredColsFilled() {
     def formIsValid = true
-    def requiredCols = ['number', 'inventoryNumber', 'usefulDate', 'amount']
 
     for (def row : formData.dataRows) {
-        if ( ! isTotalRow(row)) {      //строку ИТОГО не проверяем
-            def fieldNumber = row.get('number')
-            for (def col : requiredCols) {
+        if ( ! isInTotalRowsAliases(row.getAlias())) {      //строку ИТОГО не проверяем
+            def fieldNumber = formData.dataRows.indexOf(row) + 1
+            for (def col : getEditableColsNames()) {
                 final def value = row.get(col)
                 if (isBlankOrNull(value)) {
                     formIsValid = false
-                    logger.error("Поле $fieldNumber не заполнено!")
+                    logger.error("Строка $fieldNumber не заполнена!")
                     break
                 }
             }
@@ -182,22 +113,190 @@ boolean requiredColsFilled() {
 }
 
 /**
- * @return массив имен ячеек, доступных для редактирования
+ * проверяем, что итоговые значения рассчитаны верно
  */
-def getEditableColsNames() {
-    return ['number']
+boolean checkTotalDataRowValid() {
+    def totalDataRow = formData.getDataRow(getTotalDataRowAlias())
+    if (totalDataRow == null) {
+        logger.error('Строка Итого не найдена')
+        return false
+    }
+    def controlTotalResultsForCols = getTotalResultsForCols()
+    for (def colName : getTotalColsAliases()) {
+        if (totalDataRow[colName] != controlTotalResultsForCols[colName]) {
+            logger.error('Строка Итого рассчитана неверно!')
+            return false
+        }
+    }
+
+    return true
 }
 
-def tryFindTotalAmountRow() {
-    return formData.dataRows.find {
-        isTotalRow(it)
+def getTotalDataRowAlias() {
+    return 'total'
+}
+
+/********************************   ОБЩИЕ ФУНКЦИИ   ********************************/
+
+/**
+ * false, если в строке нет символов или строка null
+ * true, если в строке есть символы
+ */
+boolean isBlankOrNull(value) {
+    return (value == null || value.equals(''))
+}
+
+/**
+ * возвращает список алиасов столбцов, доступных для редактирования
+ */
+def getEditableColsNames() {
+    return ['inventoryNumber', 'usefulDate', 'amount']
+}
+
+/**
+ * возвращает true, если в таблице выделен какой-нибудь столбце
+ * иначе возвращает false
+ */
+boolean isCurrentDataRowSelected() {
+    return (currentDataRow != null && formData.dataRows.indexOf(currentDataRow) >= 0)
+}
+
+/***********   ОБЩИЕ ФУНКЦИИ ДЛЯ ИТОГОВЫХ СТРОК   ***********/
+
+/**
+ * false, если алиас строки не входит в список алиасов итоговых строк
+ * true, если алиас строки входит в алиас итоговых строк
+ */
+boolean isInTotalRowsAliases(def alias){
+    return (totalRowsAliases.find {totalAlias -> alias == totalAlias} != null)
+}
+
+/**
+ * возвращает список алиасов для итоговых строк
+ */
+def getTotalRowsAliases() {
+    return ['total']
+}
+
+/***********   ОБЩИЕ ФУНКЦИИ ДЛЯ СТОЛБЦОВ, ПО КОТОРЫМ ПОДВОДЯТСЯ ИТОГИ   ***********/
+
+/**
+ * возвращает список алиасов для стобцов, по которым подводятся итоги
+ */
+def getTotalColsAliases() {
+    return ['amount']
+}
+
+/**
+ * находим для всех строк, кроме итоговых, суммы по столбцам, по которым подводят итоги
+ * возвращаем мапу вида алиас_столбца -> итоговое_значение
+ */
+def getTotalResultsForCols() {
+    def result = [:]
+    for (def colAlias : getTotalColsAliases()) {
+        result.put(colAlias, formData.dataRows.sum {row ->
+            if (! isInTotalRowsAliases(row.getAlias())) {    //строка не входит в итоговые
+                row[colAlias]
+            } else {
+                0
+            }
+        })
+    }
+    return result
+}
+
+/***********   ДОБАВЛЕНИЕ СТРОКИ В ТАБЛИЦУ С ФИКСИРОВАННЫМИ СТРОКАМИ ИТОГОВ   ***********/
+
+/**
+ * добавляет строку в таблицу с фиксированными строками итогов. строка добавляется перед выделенной
+ * строкой (если такая есть). если выделенной строки нет, то строка добавляется в конец таблицы перед
+ * последней итоговой строкой
+ */
+def addNewRow() {
+    def newRow = formData.createDataRow()
+
+    getEditableColsNames().each{ value ->
+        newRow.getCell(value).editable = true
+    }
+
+    int index = getNewRowIndex()
+
+    formData.dataRows.add(index, newRow)
+
+    return newRow
+}
+
+/**
+ * возвращает индекс для добавляемого столбца
+ * (находит строку, удовлетворяющую условиям addNewRow(). на ее место будет произведена вставка новой строки)
+ */
+int getNewRowIndex() {
+    def index
+
+    def isTotalRow = false
+    if (isCurrentDataRowSelected()) {
+        index = formData.dataRows.indexOf(currentDataRow)
+        if ( ! isBlankOrNull(currentDataRow.getAlias())) {
+            isTotalRow = true
+        }
+    } else {
+        index = formData.dataRows.size() - 1
+    }
+
+    index = goToTopAndGetMaxIndexOfRowWithoutAlias(index)
+
+    if (isTotalRow && index != null) {
+        index += 1
+    } else if (index == null) {
+        index = 0
+    }
+
+    return index
+}
+
+/**
+ * идем вверх по таблице, начиная со строки с индексом startIndex (включительно). находим первую неитоговую
+ * строку (алиас которой не помечен как итоговый в getTotalRowsAliases()).
+ *
+ * возвращает индекс этой строки.
+ */
+def goToTopAndGetMaxIndexOfRowWithoutAlias(def startIndex) {
+    for (int i = startIndex; i >= 0; i--) {
+        if (getTotalRowsAliases().find{ totalRowAlias ->
+            totalRowAlias == formData.dataRows[i].getAlias()
+            } == null) {
+            return i
+        }
+    }
+
+    return null
+}
+
+/***********   УДАЛЕНИЕ СТРОКИ ИЗ ТАБЛИЦЫ С ФИКСИРОВАННЫМИ СТРОКАМИ ИТОГОВ   ***********/
+
+/**
+ * удаляет выделенную строку, если она не является итоговой
+ * если выделенная строки является итоговой, то она не удаляется и выводится сообщение о критичесокй ошибке
+ */
+def deleteCurrentRow() {
+    if (isCurrentDataRowSelected() &&
+        totalRowsAliases.find { totalRowAlias ->
+        totalRowAlias == currentDataRow.getAlias()
+        } == null) {
+        formData.dataRows.remove(currentDataRow)
+    } else {
+        logger.error ('Невозможно удалить фиксированную строку!')
     }
 }
 
-boolean isTotalRow(dataRow) {
-    dataRow.getAlias().equals('total')
-}
+/***********   ДЛЯ РАБОТЫ С ОТЧЕТНЫМИ ПЕРИОДАМИ   ***********/
 
-boolean isBlankOrNull(value) {
-    value == null || value.equals('')
+/**
+ * возвращает диапазон Date..Date (включительно!) для текущего отчетного периода (с нарастающим итогом)
+ */
+def getReportPeriodRange() {
+    def periodStartsDate = reportPeriodService.getStartDate(formData.reportPeriodId).getTime()
+    def periodEndsDate = reportPeriodService.getEndDate(formData.reportPeriodId).getTime()
+
+    return periodStartsDate..periodEndsDate
 }
