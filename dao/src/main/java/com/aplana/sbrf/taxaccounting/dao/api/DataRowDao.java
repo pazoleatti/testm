0fc36ef87fee39746404a9c7aede9fa68342f06f
Sgoryachkin <Sgoryachkin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-06-10 13:26:26 +0000

    http://jira.aplana.com/browse/SBRFACCTAX-2756 Добавлены комментарии к наброскам Dao для работы со строками НФ
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@2246 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/api/DataRowDao.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/api/DataRowDao.java
@@ -6,60 +6,84 @@ import com.aplana.sbrf.taxaccounting.model.datarow.DataRowFilter;
 import com.aplana.sbrf.taxaccounting.model.datarow.DataRowRange;
 
 /**
- * DAO для работы со строкам НФ.
- * При редактировании состояние формы делится на 2 среза
- * - Сохранненый (Saved)
- * - Редактируемый (Edited)
+ * DAO для работы со строкам НФ. При редактировании состояние формы делится на 2
+ * среза - Сохранненый (Saved) - Редактируемый (Edited)
  * 
- * Все действия по изменению строк НФ происходят с редактируемым срезом.
- * После сохранения сохраненный срез удаляется, а редактируемый становится сохраненным
+ * Все действия по изменению строк НФ происходят с редактируемым срезом. После
+ * сохранения сохраненный срез удаляется, а редактируемый становится сохраненным
  * При отмене редактируемый срез удаляется
  * 
+ * Если форма в данный момент никем не редактируется (после save или cancel
+ * изменений небыло) значит редактируемое и сохраненное состояние совпадают.
+ * 
  * @author sgoryachkin
- *
+ * 
  */
 public interface DataRowDao {
-	
+
 	/*
-	 * 
 	 * Методы для работы с сохраненным срезом формы
-	 * 
 	 */
 
-	void getSavedRows(Long id, DataRowHandler handler, DataRowFilter filter, DataRowRange range);
-	
-	/*
+	/**
+	 * Метод получает строки сохранненого состояния
 	 * 
+	 * @param id
+	 * @param handler
+	 * @param filter
+	 * @param range
+	 */
+	void getSavedRows(Long id, DataRowHandler handler, DataRowFilter filter,
+			DataRowRange range);
+
+	/*
 	 * Методы для работы с редактируемым срезом формы
-	 * 
 	 */
-	
-	void getRows(Long id, DataRowHandler handler, DataRowFilter filter, DataRowRange range);
-	
-	void getRow(Long id, int index);
-	
+
+	/**
+	 * Метод получает строки редактируемого в данный момент состояния формы.
+	 * 
+	 * @param id
+	 * @param handler
+	 * @param filter
+	 * @param range
+	 */
+	void getRows(Long id, DataRowHandler handler, DataRowFilter filter,
+			DataRowRange range);
+
+	/**
+	 * Метод получает строку
+	 * 
+	 * @param id
+	 *            - идентификатор НФ
+	 * @param index
+	 *            - индекс строки в НФ
+	 * @return строка НФ
+	 */
+	DataRow<Cell> getRow(Long id, int index);
+
 	void updateRow(Long id, DataRow<Cell> row);
-	
+
 	void removeRow(Long id, DataRow<Cell> row);
-	
+
 	void removeRow(Long id, int index);
-	
+
 	DataRow<Cell> addRow(Long id, int index, DataRow<Cell> rowTemplate);
-	
+
 	DataRow<Cell> addRow(Long id, DataRow<Cell> rowTemplate);
-	
-	DataRow<Cell> addRowAfter(Long id, DataRow<Cell> afterRow, DataRow<Cell> rowTemplate);
-	
-	DataRow<Cell> addRowBefore(Long id, DataRow<Cell> beforeRow, DataRow<Cell> rowTemplate);
-	
+
+	DataRow<Cell> addRowAfter(Long id, DataRow<Cell> afterRow,
+			DataRow<Cell> rowTemplate);
+
+	DataRow<Cell> addRowBefore(Long id, DataRow<Cell> beforeRow,
+			DataRow<Cell> rowTemplate);
+
 	/*
-	 * 
 	 * Сохранение/отмена
-	 * 
 	 */
-	
+
 	void save(Long id);
-	
+
 	void cancel(Long id);
-		
+
 }
