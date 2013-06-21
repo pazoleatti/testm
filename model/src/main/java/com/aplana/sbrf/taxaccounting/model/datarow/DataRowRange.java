db2dd342fe0138106b6e7897da4e880d732a576b
Mfayzullin <Mfayzullin@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-06-21 11:59:11 +0000

    Добавил для удобства конструктор в DataRowRange
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@2285 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/datarow/DataRowRange.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/datarow/DataRowRange.java
@@ -15,21 +15,32 @@ public class DataRowRange implements Serializable{
 	 * Максимальное количество строк диапазона
 	 */
 	private int limit;
-	
+
 	/**
 	 * С какого индекса строки начинается диапазон (ндекс от 0)
 	 */
 	private int offset;
-	
+
+	public DataRowRange(){
+	}
+
+	public DataRowRange(int offset, int limit){
+		this.offset = offset;
+		this.limit = limit;
+	}
+
 	public int getLimit() {
 		return limit;
 	}
+
 	public void setLimit(int limit) {
 		this.limit = limit;
 	}
+
 	public int getOffset() {
 		return offset;
 	}
+
 	public void setOffset(int offset) {
 		this.offset = offset;
 	}
