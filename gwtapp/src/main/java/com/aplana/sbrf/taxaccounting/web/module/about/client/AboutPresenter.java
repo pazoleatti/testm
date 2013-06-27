9c63c523a5ceb386874205ac82d7c0ebe5096634
Ibukanov <Ibukanov@34aa7802-8fb0-764d-beb8-9b5b9d595954>
2013-06-27 12:44:59 +0000

    http://jira.aplana.com/browse/SBRFACCTAX-2537 Реализовать графический интерфейс «О программе»
    git-svn-id: https://horse.aplana.com/svn/SBRFACCTAX/trunk/taxaccounting@2307 34aa7802-8fb0-764d-beb8-9b5b9d595954

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/about/client/AboutPresenter.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/about/client/AboutPresenter.java
@@ -2,6 +2,7 @@ package com.aplana.sbrf.taxaccounting.web.module.about.client;
 
 import com.aplana.sbrf.taxaccounting.web.main.api.client.RevealContentTypeHolder;
 import com.aplana.sbrf.taxaccounting.web.main.api.client.event.TitleUpdateEvent;
+import com.aplana.sbrf.taxaccounting.web.widget.version.client.ProjectVersionPresenter;
 import com.google.inject.Inject;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.Presenter;
@@ -26,15 +27,26 @@ public class AboutPresenter extends
 	public interface MyView extends View {
 	}
 
+	private final ProjectVersionPresenter versionPresenter;
+	static final Object TYPE_ProjectVersionContent = new Object();
+
 	@Inject
 	public AboutPresenter(final EventBus eventBus, final MyView view,
-						  final MyProxy proxy) {
+						  final MyProxy proxy, ProjectVersionPresenter versionPresenter) {
 		super(eventBus, view, proxy, RevealContentTypeHolder.getMainContent());
+		this.versionPresenter = versionPresenter;
 	}
 
 	@Override
 	protected void onReveal() {
 		super.onReveal();
 		TitleUpdateEvent.fire(this, "О программе");
+		setInSlot(TYPE_ProjectVersionContent, versionPresenter);
+	}
+
+	@Override
+	protected void onHide() {
+		super.onHide();
+		clearSlot(TYPE_ProjectVersionContent);
 	}
 }
--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/about/client/AboutView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/about/client/AboutView.java
@@ -1,6 +1,9 @@
 package com.aplana.sbrf.taxaccounting.web.module.about.client;
 
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewImpl;
@@ -9,8 +12,19 @@ public class AboutView extends ViewImpl implements AboutPresenter.MyView {
 	interface Binder extends UiBinder<Widget, AboutView> {
 	}
 
+	@UiField
+	Panel version;
+
 	@Inject
 	public AboutView(final Binder binder) {
 		initWidget(binder.createAndBindUi(this));
+	}
+
+	@Override
+	public void setInSlot(Object slot, IsWidget content) {
+		version.clear();
+		if (content != null) {
+			version.add(content);
+		}
 	}
 }
