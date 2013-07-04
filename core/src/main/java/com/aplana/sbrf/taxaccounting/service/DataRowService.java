0caf6e30465f1ea965d330247d0028837d2dfb65
Semyon Goryachkin <sgoryachkin@aplana.com>
2013-07-04 11:06:52 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-2976 Добавлены комментарии к методам

--- core/src/main/java/com/aplana/sbrf/taxaccounting/service/DataRowService.java
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/service/DataRowService.java
@@ -10,10 +10,34 @@ import com.aplana.sbrf.taxaccounting.model.datarow.DataRowRange;
 
 public interface DataRowService {
 	
+	/**
+	 * Получение страныцы с набором строк НФ
+	 * 
+	 * @param userInfo
+	 * @param formDataId
+	 * @param range
+	 * @param saved
+	 * @return
+	 */
 	PaginatedSearchResult<DataRow<Cell>> getDataRows(TAUserInfo userInfo, long formDataId, DataRowRange range, boolean saved);
 	
+	/**
+	 * Получени количество строк НФ
+	 * 
+	 * @param userInfo
+	 * @param formDataId
+	 * @param saved
+	 * @return
+	 */
 	int getRowCount(TAUserInfo userInfo, long formDataId, boolean saved); 
 	
+	/**
+	 * Обновление набора строк во временном срезе НФ
+	 * 
+	 * @param userInfo
+	 * @param formDataId
+	 * @param dataRows
+	 */
 	void update(TAUserInfo userInfo, long formDataId, List<DataRow<Cell>> dataRows);
 
 }
