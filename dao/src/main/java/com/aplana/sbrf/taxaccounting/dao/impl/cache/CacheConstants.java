d1fe38d72956da335e753660390760b5d1a8a544
Alexander Vanteev <avanteev@aplana.com>
2013-07-15 14:38:08 +0400

    Разработка  http://jira.aplana.com/browse/SBRFACCTAX-3053 : Сделал кэширование через DataBlobsCache для всех отчетов.

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/cache/CacheConstants.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/cache/CacheConstants.java
@@ -6,7 +6,6 @@ public final class CacheConstants {
 	}
 	
 	public static final String DECLARATION_TEMPLATE = "DeclarationType";
-	public static final String DECLARATION_DATA_BLOB_PDF = "DeclarationDataBlobPdf";
-	public static final String DECLARATION_DATA_BLOB_XLSX = "DeclarationDataBlobXlsx";
+	public static final String DECLARATION_DATA_BLOB = "DataBlobsCache";
 	
 }
