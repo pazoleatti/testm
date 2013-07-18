d2be5e1d5894054ddbaf2eec3ce5288fe899e3d0
Eugene Stetsenko <estetsenko@aplana.com>
2013-07-18 13:45:02 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-3151 Делаем апдейт перед выполнением "Рассчитать" и "Проверить"

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdata/shared/RecalculateFormDataAction.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdata/shared/RecalculateFormDataAction.java
@@ -1,6 +1,10 @@
 package com.aplana.sbrf.taxaccounting.web.module.formdata.shared;
 
+import com.aplana.sbrf.taxaccounting.model.Cell;
+import com.aplana.sbrf.taxaccounting.model.DataRow;
 import com.aplana.sbrf.taxaccounting.web.main.api.shared.dispatch.ActionName;
+
+import java.util.List;
 
 /**
  * Пересчет формы.
@@ -9,6 +13,16 @@ import com.aplana.sbrf.taxaccounting.web.main.api.shared.dispatch.ActionName;
  * @author Vitalii Samolovskikh
  */
 public class RecalculateFormDataAction extends AbstractFormDataAction implements ActionName {
+
+	List<DataRow<Cell>> modifiedRows;
+
+	public List<DataRow<Cell>> getModifiedRows() {
+		return modifiedRows;
+	}
+
+	public void setModifiedRows(List<DataRow<Cell>> modifiedRows) {
+		this.modifiedRows = modifiedRows;
+	}
 
 	@Override
 	public String getName() {
