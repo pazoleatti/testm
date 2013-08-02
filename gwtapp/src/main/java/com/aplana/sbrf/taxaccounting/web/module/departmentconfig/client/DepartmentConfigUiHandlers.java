831ba69c153fa0508c2dcd82f159100a0791b6a6
Dmitriy Levykin <DLevykin@aplana.com>
2013-08-02 11:40:02 +0600

    Доработка формы настроек подразделений: зависимости компонентов, видимость, загрузка формы.

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/departmentconfig/client/DepartmentConfigUiHandlers.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/departmentconfig/client/DepartmentConfigUiHandlers.java
@@ -5,16 +5,41 @@ import com.aplana.sbrf.taxaccounting.model.TaxType;
 import com.aplana.sbrf.taxaccounting.web.module.departmentconfig.shared.DepartmentCombined;
 import com.gwtplatform.mvp.client.UiHandlers;
 
-import java.util.Date;
-
 /**
  * Хендлеры формы настройки подразделений
  *
  * @author Dmitriy Levykin
  */
 public interface DepartmentConfigUiHandlers extends UiHandlers {
+    /**
+     * Сохранение
+     * @param combinedDepartmentParam
+     */
     void save(DepartmentCombined combinedDepartmentParam);
+
+    /**
+     * Очистка формы
+     */
+    void clear();
+
+    /**
+     * Перезагрузка параметров подразделений на форме
+     * @param departmentId
+     * @param taxType
+     * @param reportPeriodId
+     */
     void reloadDepartmentParams(Integer departmentId, TaxType taxType, Integer reportPeriodId);
+
+    /**
+     * Перезагрузка налоговых периодов
+     * @param taxType
+     */
     void reloadTaxPeriods(TaxType taxType);
+
+    /**
+     * Обработка выбора налогового периода
+     * @param taxPeriod
+     * @param departmentId
+     */
     void onTaxPeriodSelected(TaxPeriod taxPeriod, Integer departmentId);
 }
