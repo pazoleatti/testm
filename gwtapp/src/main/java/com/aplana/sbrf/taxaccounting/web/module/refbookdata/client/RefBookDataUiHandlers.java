a4a713018c9eee756ac5efb33a8916e010e72925
Eugene Stetsenko <estetsenko@aplana.com>
2013-08-14 18:48:43 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-3124 Пэйджинг и кнопка "Сохранить" для редактора справочников

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/client/RefBookDataUiHandlers.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/client/RefBookDataUiHandlers.java
@@ -2,8 +2,11 @@ package com.aplana.sbrf.taxaccounting.web.module.refbookdata.client;
 
 import com.gwtplatform.mvp.client.UiHandlers;
 
+import java.util.Map;
+
 public interface RefBookDataUiHandlers extends UiHandlers {
 	void onCancelClicked();
 	void onAddRowClicked();
 	void onSelectionChanged(Long recordId);
+	void onSaveClicked(Map<String, Object > values);
 }
