ffa68fc177935ce023043d83253dc17bd43c6927
Elena Kabanova <elena.kabanova@aplana.com>
2013-08-15 13:41:07 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-3427: Удаление старых таблиц

--- src/main/resources/dml/transport_eco_class.sql
+++
@@ -1,6 +0,0 @@
-insert into transport_eco_class values (0, '怕形 0');
-insert into transport_eco_class values (1, '怕形 1');
-insert into transport_eco_class values (2, '怕形 2');
-insert into transport_eco_class values (3, '怕形 3');
-insert into transport_eco_class values (4, '怕形 4');
-insert into transport_eco_class values (5, '怕形 5');
--- src/main/resources/dml/transport_unit_code.sql
+++
@@ -1,4 +0,0 @@
-insert into transport_unit_code (code, name, convention) values (181,	'Брутто-регистровая тонна (2,8316 м3)',	'БРТ');
-insert into transport_unit_code (code, name, convention) values (251,	'Лошадиная сила',	'л.с.');
-insert into transport_unit_code (code, name, convention) values (796,	'Штука',	'Шт');
-insert into transport_unit_code (code, name, convention) values (999, 	'Килограмм силы',	'Кгс');
