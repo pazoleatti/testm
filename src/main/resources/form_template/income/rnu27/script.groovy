03aa12930507daa05446cc3c9a70fb50fa37b029
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2013-08-21 10:55:33 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-3494 РНУ-27. Ячейка "Валюта выпуска облигации" была недоступна для редактирования

--- src/main/resources/form_template/income/rnu27/script.groovy
+++ src/main/resources/form_template/income/rnu27/script.groovy
@@ -181,7 +181,7 @@ void logicalCheck() {
 
             // @author ivildanov
             // Арифметические проверки граф 5, 8, 11, 12, 13, 14, 15, 16, 17
-            List checks = ['currency', 'reserveCalcValuePrev', 'marketQuotation', 'rubCourse', 'marketQuotationInRub', 'costOnMarketQuotation', 'reserveCalcValue', 'reserveCreation', 'recovery']
+            List checks = ['reserveCalcValuePrev', 'marketQuotation', 'rubCourse', 'marketQuotationInRub', 'costOnMarketQuotation', 'reserveCalcValue', 'reserveCreation', 'recovery']
             Map<String, Object> value = [:]
             value.put('reserveCalcValuePrev', calc8(row))
             value.put('marketQuotation', calc11(row))
@@ -782,7 +782,7 @@ void addNewRowwarnrmData() {
         index = 0
     }
     [
-            'issuer', 'regNumber', 'tradeNumber', 'prev', 'current', 'reserveCalcValuePrev', 'cost', 'signSecurity',
+            'currency', 'issuer', 'regNumber', 'tradeNumber', 'prev', 'current', 'reserveCalcValuePrev', 'cost', 'signSecurity',
             'marketQuotation', 'rubCourse', 'costOnMarketQuotation', 'reserveCalcValue', 'reserveCreation', 'recovery'
     ].each {
         newRow.getCell(it).editable = true
