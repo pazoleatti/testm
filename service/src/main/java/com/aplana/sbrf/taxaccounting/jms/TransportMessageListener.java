cd754f019659f7d0e102597a400ab10eefe89215
Dmitriy Levykin <DLevykin@aplana.com>
2013-08-22 19:04:39 +0600

    Обработчик JMS-сообщений для транспортных файлов http://jira.aplana.com/browse/SBRFACCTAX-3180 Запуск dev-мода придется делать так: mvn activemq:run gwt:run

---
+++ service/src/main/java/com/aplana/sbrf/taxaccounting/jms/TransportMessageListener.java
@@ -0,0 +1,42 @@
+package com.aplana.sbrf.taxaccounting.jms;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+/**
+ * Слушатель JMS-сообщений при загрузке транспортных файлов
+ *
+ * @author Dmitriy Levykin
+ */
+public class TransportMessageListener implements MessageListener {
+
+    public static final String FILENAME_PROPERTY_NAME = "FILENAME";
+    public static final String DATA_PROPERTY_NAME = "DATA";
+
+    private static final Log logger = LogFactory.getLog(TransportMessageListener.class);
+
+    @Override
+    public void onMessage(Message message) {
+        logger.debug("onMessage: " + message);
+        if (message == null || !(message instanceof MapMessage)) {
+            return;
+        }
+
+        MapMessage mm = (MapMessage) message;
+
+        try {
+            String fileName = mm.getString(FILENAME_PROPERTY_NAME);
+            byte[] bodyFile = mm.getBytes(DATA_PROPERTY_NAME);
+
+            logger.debug("fileName = " + fileName);
+            logger.debug("bodyFile.length = " + bodyFile.length);
+        } catch (JMSException e) {
+            logger.error("Retrieving error message: " + e.getMessage(), e);
+        }
+    }
+}
