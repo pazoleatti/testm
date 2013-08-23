8995b2998966923ac97a6e09f44307112b6b3f80
Aydar Uldanov <Aydar.Uldanov@aplana.com>
2013-08-23 10:34:50 +0600

    Актуализация формы  (первичка по ТН)

--- src/main/resources/form_template/transport/nalog/script.groovy
+++ src/main/resources/form_template/transport/nalog/script.groovy
@@ -1,8 +1,4 @@
-import com.aplana.sbrf.taxaccounting.model.FormData
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
-import com.aplana.sbrf.taxaccounting.model.FormDataKind
-import com.aplana.sbrf.taxaccounting.model.log.LogLevel
-
 /**
  * Форма "Сведения о транспортных средствах, по которым уплачивается транспортный налог".
  * @author ivildanov
@@ -42,13 +38,14 @@ switch (formDataEvent) {
 
 // Инициирование Пользователем расчёта данных формы (нажатие на кнопку «Рассчитать»)
     case FormDataEvent.CALCULATE:
-        fillForm()
 
-        checkRequiredField()
-        checkNSI()
-        logicalChecks()
+        if (checkRequiredField()){
+            fillForm()
+            checkNSI()
+            logicalChecks()
+            sort()
+        }
 
-        sort()
         break
 
 // Инициирование Пользователем проверки данных формы
@@ -142,12 +139,13 @@ switch (formDataEvent) {
  */
 void addRow() {
     def newRow = formData.createDataRow()
-    formData.dataRows.add(newRow)
+
 
     ['codeOKATO', 'tsTypeCode', 'identNumber', 'model', 'ecoClass', 'regNumber', 'powerVal', 'baseUnit', 'year', 'regDate', 'regDateEnd', 'stealDateStart', 'stealDateEnd'].each { column ->
         newRow.getCell(column).editable = true
         newRow.getCell(column).setStyleAlias("Редактируемое поле")
     }
+    dataRowHelper.insert(newRow, getDataRows().size() ?: 1)
 }
 
 /**
@@ -155,7 +153,7 @@ void addRow() {
  */
 void setRowIndex() {
     def index = 0
-    for (def row : formData.dataRows) {
+    for (def row : getDataRows()) {
         row.rowNumber = index + 1
         index += 1
     }
@@ -165,7 +163,7 @@ void setRowIndex() {
  * Скрипт для проверки создания.
  */
 void checkBeforeCreate() {
-    def findForm = FormDataService.find(formData.formType.id, formData.kind, formData.departmentId, formData.reportPeriodId)
+    def findForm = formDataService.find(formData.formType.id, formData.kind, formData.departmentId, formData.reportPeriodId)
 
     if (findForm != null) {
         logger.error('Налоговая форма с заданными параметрами уже существует.')
@@ -176,20 +174,23 @@ void checkBeforeCreate() {
 /**
  * Проверка обязательных полей.
  */
-void checkRequiredField() {
-    for (def row : formData.dataRows) {
+def checkRequiredField() {
+    for (def row : getDataRows()) {
 
         def errorMsg = ''
 
         // 1-7,9-13
-        ['rowNumber', 'codeOKATO', 'regionName', 'tsTypeCode', 'tsType', 'identNumber', 'model', 'regNumber', 'powerVal', 'baseUnit', 'year', 'regDate'].each { column ->
+        ['codeOKATO', 'tsTypeCode', 'identNumber', 'model', 'ecoClass', 'regNumber', 'powerVal', 'baseUnit', 'year', 'regDate', 'regDateEnd', 'stealDateStart', 'stealDateEnd'].each { column ->
             if (row.getCell(column) != null && (row.getCell(column).getValue() == null || ''.equals(row.getCell(column).getValue()))) {
                 errorMsg += (!''.equals(errorMsg) ? ', ' : '') + '"' + row.getCell(column).getColumn().getName() + '"'
             }
         }
         if (!''.equals(errorMsg)) {
-            logger.error("В строке $row.rowNumber не заполнены поля : $errorMsg.")
+            logger.error("В строке "+row.getIndex()+" не заполнены поля : $errorMsg.")
+            return false;
         }
+
+        return true
     }
 }
 
@@ -200,7 +201,7 @@ void deleteRow() {
     def row = currentDataRow
     if (row != null) {
         // удаление строки
-        formData.deleteDataRow(row)
+        getDataRowHelper().delete(row)
     }
 }
 
@@ -208,7 +209,7 @@ void deleteRow() {
  * Скрипт логические проверки сводной формы.
  */
 void logicalChecks() {
-    for (def row : formData.dataRows) {
+    for (def row : getDataRows()) {
 
         // Проверка на соответствие дат при постановке (снятия) с учёта
         if (!(row.regDateEnd > row.regDate)) {
@@ -221,76 +222,58 @@ void logicalChecks() {
  * Скрипт для проверки соответствия НСИ.
  */
 void checkNSI() {
-    for (def row : formData.dataRows) {
+    for (def row : getDataRows()) {
 
-        // 1. Проверка ОКАТО
-        if (row.codeOKATO != null) {
-            // TODO переписать проверку ОКАТО. ОКАТО переехал в версионный справочник.
-            // if (!transportTaxDao.validateOkato(row.codeOKATO)) {
-            //    logger.error('Неверный код ОКАТО!')
-            //}
+        // Проверка ОКАТО
+        if (!checkOkato(row.codeOKATO)){
+            logger.error("Неверный код ОКАТО!");
         }
 
         // 2. Проверка муниципального образования
-        if (row.regionName != null) {
-            // TODO переписать проверку
-            // if (row.regionName != transportTaxDao.getRegionName(row.codeOKATO)) {
-            //     logger.error('Неверное наименование муниципального образования!')
-            // }
+        if (!checkRegionName(row.regionName)){
+            logger.error("Неверное наименование муниципального образования!!");
         }
 
         // 3. Проверка кода вида ТС
-        if (row.tsTypeCode != null) {
-            // TODO Переписать. Переехали в версионные справочники
-            // if (transportTaxDao.validateTransportTypeCode(row.tsTypeCode)) {
-
-                // 4. Проверка вида ТС
-                // TODO Переписать. Переехали в версионные справочники
-                // if (transportTaxDao.getTsTypeName(row.tsTypeCode) != row.tsType) {
-                //    logger.error('Неверный вид ТС!')
-                //}
-
-            // } else {
-            //     logger.error('Неверный код вида транспортного средства!')
-            // }
+        if (checkTsTypeCode(row.tsTypeCode)){
+            // 4. Проверка вида ТС
+            if (!checkTsType(row.tsTypeCode, row.tsType)){
+                logger.error('Неверный вид ТС!')
+            }
+        } else{
+            logger.error('Неверный код вида транспортного средства!')
         }
 
         // 5. Проверка кода экологического класса
-        if (row.ecoClass != null) {
-            // TODO Переписать. Экологические классы переехали в версионные справочники
-            // if (!transportTaxDao.validateEcoClass(row.ecoClass)) {
-            //    logger.error('Неверный код экологического класса!')
-            // }
+        if (!checkEcoClass(row.ecoClass)){
+            logger.error('Неверный код экологического класса!')
         }
 
         // 6. Проверка кода ед. измерения мощности
-        if (row.baseUnit != null) {
-            // TODO Переписать. Переехали в версионные справочники
-            // if (!transportTaxDao.validateTaxBaseUnit(row.baseUnit)) {
-            //    logger.error('Неверный код ед. измерения мощности!')
-            //}
+        if (!checkBaseUnit(row.baseUnit)){
+            logger.error('Неверный код ед. измерения мощности!')
         }
-    }
+   }
 }
 
 /**
  * Алгоритмы заполнения полей формы
  */
 void fillForm() {
-    formData.dataRows.each { row ->
+    getDataRows().each { row ->
 
         // заполнение графы 3 на основе графы 2
         if (row.codeOKATO != null) {
-            // TODO Переписать. Переехали в версионные справочники
-            //row.regionName = transportTaxDao.getRegionName(row.codeOKATO)
+            row.regionName = row.codeOKATO
         }
 
         // заполнение графы 5 на основе графы 4
         if (row.tsTypeCode != null) {
-            // TODO Переписать. Переехали в версионные справочники
-            // row.tsType = transportTaxDao.getTsTypeName(row.tsTypeCode)
+            row.tsType = row.tsTypeCode
         }
     }
+
+    getDataRowHelper().save(getDataRows())
 }
 
 /**
@@ -301,17 +284,24 @@ void sort() {
     // 1 - ОКАТО
     // 2 - Муниципальное образование
     // 3 - Код вида ТС
-    formData.dataRows.sort { a, b ->
-        int val = (a.codeOKATO ?: "").compareTo(b.codeOKATO ?: "")
+    getDataRows().sort { a, b ->
+        okatoA = getRefBookValue(3, a.codeOKATO, "OKATO")
+        okatoB = getRefBookValue(3, b.codeOKATO, "OKATO")
+        int val = okatoA.getStringValue().compareTo(okatoB.getStringValue())
         if (val == 0) {
-            val = (a.regionName ?: "").compareTo(b.regionName ?: "")
+            def regionA = getRefBookValue(3, a.regionName, "NAME")
+            def regionB = getRefBookValue(3, b.regionName, "NAME")
+            val = regionA.getStringValue().compareTo(regionB.getStringValue())
 
             if (val == 0) {
-                val = (a.tsTypeCode ?: "").compareTo(b.tsTypeCode ?: "")
+                def tsTypeCodeA = getRefBookValue(42, a.tsTypeCode, "CODE")
+                def tsTypeCodeB = getRefBookValue(42, b.tsTypeCode, "CODE")
+                val = tsTypeCodeA.getStringValue().compareTo(tsTypeCodeB.getStringValue())
             }
         }
         return val
     }
+    getDataRowHelper().save(getDataRows())
 }
 
 /**
@@ -320,7 +310,7 @@ void sort() {
 void consolidation() {
 
     // удаляем все строки
-    formData.dataRows.clear()
+    getDataRows.clear()
 
     // скопировать строки из источников в данную форму
     departmentFormTypeService.getFormSources(formDataDepartment.id, formData.getFormType().getId(), formData.getKind()).each {
@@ -334,7 +324,7 @@ void consolidation() {
                     if (isRowInDataRows(row)) {
                         logger.error('Идентичные строки')
                     } else {
-                        formData.dataRows.add(row)
+                        dataRowsHelper.insert(row)
                     }
                 }
             }
@@ -349,10 +339,116 @@ void consolidation() {
  * @param nrow строка
  */
 boolean isRowInDataRows(def nrow) {
-    formData.dataRows.each { row ->
+    getDataRows().each { row ->
         if (row.equals(nrow)) {
             return true
         }
     }
     return false
+}
+
+
+/**
+ * 1. Проверка ОКАТО
+ * В справочнике «Коды ОКАТО» должна быть строка, для которой выполняется условие:
+ * «графа 2» (поле «Код ОКАТО») текущей строки формы = «графа 1» (поле «Код ОКАТО») строки справочника
+ */
+boolean checkOkato(codeOKATO){
+    if (codeOKATO != null && getRefBookValue(3, codeOKATO, "OKATO") != null){
+            return true;
+    }
+    return false;
+}
+
+/**
+ * Проверка муниципального образования
+ * «Графа 3» (поле «Муниципальное образование, на территории которого зарегистрировано
+ * транспортное средство (ТС)») текущей строки формы = «графа 2» (поле «Наименование»)
+ * строки справочника «Коды ОКАТО» для указанного значения в «графе 2»
+ * (поле «Код ОКАТО») текущей формы
+ */
+def checkRegionName(regionName){
+    if (regionName != null && getRefBookValue(3, regionName, "NAME") != null){
+        return true;
+    }
+    return false
+}
+
+/**
+ * Проверка кода вида ТС
+ * В справочнике «Коды видов транспортных средств» должна быть строка, для которой выполняется условие:
+ * «графа 4» (поле «Код вида ТС») текущей строки формы = «графа 3» (поле «Код вида ТС») строки справочника
+ */
+def checkTsTypeCode(tsTypeCode){
+    if (tsTypeCode != null && getRefBookValue(42, tsTypeCode, "CODE") != null){
+        return true;
+    }
+    return false
+}
+
+/**
+ * Проверка вида ТС
+ *
+ * «Графа 5» (поле «Вид ТС») текущей строки формы = «графа 1»
+ * (поле «Наименование вида транспортного средства») строки справочника
+ * «Коды видов транспортных средств» для указанного значения в «графе 4»
+ * («Код вида ТС») текущей формы
+ */
+
+def checkTsType(tsTypeCode, tsType){
+    if (tsTypeCode != null && tsType != null && tsTypeCode == tsType){
+        return true;
+    }
+    return false
+}
+
+/**
+ * Проверка кода экологического класса
+ * В справочнике «Экологические классы» должна быть строка, для которой выполняется условие:
+ * «графа 8» (поле «Экологический класс») текущей строки формы = «графа 1» (поле «Код экологического класса») строки справочника
+ */
+def checkEcoClass(ecoClass){
+    if (ecoClass != null && getRefBookValue(40, ecoClass, "CODE") != null){
+        return true;
+    }
+    return false
+}
+
+/**
+ * В справочнике «Коды единиц измерения налоговой базы на основании ОКЕИ» должна быть строка, для которой выполняется условие:
+ *  «графа 11» (поле «Мощности (ед. измерения)») текущей строки формы = «графа 1» (поле «Код единицы измерения») строки справочника
+ *
+ *  Проверка кода ед. измерения мощности
+ */
+def checkBaseUnit(baseUnit){
+    if (baseUnit != null && getRefBookValue(12, baseUnit, "CODE") != null){
+        return true;
+    }
+    return false
+}
+
+
+/**
+ * Получение значения (разменовываение)
+ */
+def getRefBookValue(refBookID, recordId, alias){
+    //logger.info("refBookID, recordId, alias = "+refBookID+", "+ recordId+", "+alias)
+    def  refDataProvider = refBookFactory.getDataProvider(refBookID)
+    def record = refDataProvider.getRecordData(recordId)
+
+    return record != null ? record.get(alias) : null;
+}
+
+
+def getDataRows() {
+    dataRowHelper.getAllCached()
+}
+
+
+def getDataRowHelper () {
+    if (formData != null && formData.id != null) {
+        return formDataService.getDataRowHelper(formData)
+    } else {
+        throw new Exception("Ошибка получения dataRows")
+    }
 }
