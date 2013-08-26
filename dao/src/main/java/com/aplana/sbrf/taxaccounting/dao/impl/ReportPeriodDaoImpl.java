e4136c53be17f4bd3f8274d2822947aa79fcc66f
Eugene Kuvshinov <EKuvshinov@aplana.com>
2013-08-26 20:03:49 +0600

    Вынес ReportPeriodMapper для публичного использования

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/ReportPeriodDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/ReportPeriodDaoImpl.java
@@ -1,6 +1,7 @@
 package com.aplana.sbrf.taxaccounting.dao.impl;
 
 import com.aplana.sbrf.taxaccounting.dao.ReportPeriodDao;
+import com.aplana.sbrf.taxaccounting.dao.mapper.ReportPeriodMapper;
 import com.aplana.sbrf.taxaccounting.exception.DaoException;
 import com.aplana.sbrf.taxaccounting.model.ReportPeriod;
 import com.aplana.sbrf.taxaccounting.model.TaxType;
@@ -23,23 +24,6 @@ import java.util.List;
 @Repository
 @Transactional(readOnly = true)
 public class ReportPeriodDaoImpl extends AbstractDao implements ReportPeriodDao {
-
-	private static final class ReportPeriodMapper implements RowMapper<ReportPeriod> {
-		@Override
-		public ReportPeriod mapRow(ResultSet rs, int index) throws SQLException {
-			ReportPeriod reportPeriod = new ReportPeriod();
-			reportPeriod.setId(rs.getInt("id"));
-			reportPeriod.setName(rs.getString("name"));
-			reportPeriod.setActive(rs.getBoolean("is_active"));
-			reportPeriod.setMonths(rs.getInt("months"));
-			reportPeriod.setTaxPeriodId(rs.getInt("tax_period_id"));
-			reportPeriod.setOrder(rs.getInt("ord"));
-			reportPeriod.setBalancePeriod(rs.getBoolean("is_balance_period"));
-			reportPeriod.setDepartmentId(rs.getLong("department_id"));
-			reportPeriod.setDictTaxPeriodId(rs.getInt("dict_tax_period_id"));
-			return reportPeriod;
-		}
-	}
 
 	@Override
 	public ReportPeriod get(int periodId) {
