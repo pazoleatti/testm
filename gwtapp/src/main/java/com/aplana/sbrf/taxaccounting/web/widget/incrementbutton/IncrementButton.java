8a1aa1f09cc63f67f42c7f2459d53d1284833ff9
Eugene Stetsenko <estetsenko@aplana.com>
2013-08-29 16:00:07 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-3785

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/incrementbutton/IncrementButton.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/incrementbutton/IncrementButton.java
@@ -76,7 +76,11 @@ public class IncrementButton extends Composite implements IncrementButtonView, H
 
 	@Override
 	public Integer getValue() {
-		return Integer.parseInt(textBox.getValue().isEmpty() ? minValue.toString() : textBox.getValue());
+		try {
+			return textBox.getValue().isEmpty() ? minValue : Integer.parseInt(textBox.getValue());
+		} catch (NumberFormatException e) {
+			return null;
+		}
 	}
 
 	@Override
