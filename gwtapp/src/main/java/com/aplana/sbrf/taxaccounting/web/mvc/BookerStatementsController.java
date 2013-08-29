de4a310716fa39029e1160810e7628902f1671b1
Dmitriy Levykin <DLevykin@aplana.com>
2013-08-29 13:34:47 +0600

    Ограничение строк при чтении файлов на форме загрузки бух. отчетности. И правки при обработке ошибок.

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/mvc/BookerStatementsController.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/mvc/BookerStatementsController.java
@@ -7,9 +7,12 @@ import org.apache.commons.fileupload.FileItemFactory;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,6 +33,8 @@ public class BookerStatementsController {
     @Autowired
     BookerStatementsService service;
 
+    private static final Log logger = LogFactory.getLog(BookerStatementsController.class);
+
     @RequestMapping(value = "/bookerstatements/{departmentId}/{periodId}/{typeId}", method = RequestMethod.POST)
     public void processUpload(@PathVariable Integer departmentId, @PathVariable Integer periodId,  @PathVariable int typeId,
                               HttpServletRequest request, HttpServletResponse response) throws FileUploadException, IOException, ServiceException {
@@ -37,5 +42,17 @@ public class BookerStatementsController {
         ServletFileUpload upload = new ServletFileUpload(factory);
         List<FileItem> items = upload.parseRequest(request);
         service.importXML(items.get(0).getInputStream(), periodId, typeId, departmentId);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public void exceptionHandler(Exception e, final HttpServletResponse response) {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+        logger.warn(e.getLocalizedMessage(), e);
+        try {
+            response.getWriter().append("error ").append(e.getMessage()).close();
+        } catch (IOException ioException) {
+            logger.error(ioException.getMessage(), ioException);
+        }
     }
 }
