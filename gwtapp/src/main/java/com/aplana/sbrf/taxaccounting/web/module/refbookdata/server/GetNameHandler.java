27558c66a6446f36b9c07fe9e78d07b8e2b5d471
Eugene Stetsenko <estetsenko@aplana.com>
2013-08-29 14:25:22 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-3813

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/server/GetNameHandler.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/server/GetNameHandler.java
@@ -23,7 +23,7 @@ public class GetNameHandler extends AbstractActionHandler<GetNameAction, GetName
 
 	@Override
 	public GetNameResult execute(GetNameAction action, ExecutionContext executionContext) throws ActionException {
-		String name = refBookFactory.get(action.getAttributeId()).getName();
+		String name = refBookFactory.get(action.getRefbookId()).getName();
 		GetNameResult result = new GetNameResult();
 		result.setName(name);
 		return result;
