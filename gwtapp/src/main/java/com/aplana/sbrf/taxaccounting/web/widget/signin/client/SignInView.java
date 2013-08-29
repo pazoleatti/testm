9986742bbb7ccde9d56b07ed0d657d043ecb2110
Marat Fayzullin <mfayzullin@aplana.com>
2013-08-29 13:55:07 +0600

    Небольшой рефакторинг кода

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/signin/client/SignInView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/signin/client/SignInView.java
@@ -1,14 +1,8 @@
 package com.aplana.sbrf.taxaccounting.web.widget.signin.client;
 
 import com.aplana.sbrf.taxaccounting.web.widget.signin.client.SignInPresenter.MyView;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
-import com.google.gwt.user.client.Window;
-import com.google.gwt.user.client.ui.Anchor;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
