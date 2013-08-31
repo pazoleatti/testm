2c3814bbecececed50051e1208f27259c7da820c
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2013-08-31 12:48:48 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-3864 Ошибка исполнения [строка -1]: No signature of method: com.aplana.sbrf.tax...

--- src/main/resources/form_template/income/rnu51/script.groovy
+++ src/main/resources/form_template/income/rnu51/script.groovy
@@ -332,7 +332,7 @@ DataRow<Cell> getItogo() {
 DataRow<Cell> getPrevItogoKvartal() {
     FormData formPrev = getFormPrev()
     if (formPrev != null && formPrev.state == WorkflowState.ACCEPTED) {
-        return getData(formPrev).getDataRow(getData(formPrev).getAllCahced(),'itogoKvartal')
+        return getData(formPrev).getDataRow(getRows(getData(formPrev)),'itogoKvartal')
     }
     return null
 }
