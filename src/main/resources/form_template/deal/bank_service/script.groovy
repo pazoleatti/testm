f8472bc79196a14c58c71b337d964c3733eeceb8
Lenar Haziev <lhaziev@aplana.com>
2013-09-06 18:09:07 +0600

    Правки импорта отчета "Оказание банковских услуг"

--- src/main/resources/form_template/deal/bank_service/script.groovy
+++ src/main/resources/form_template/deal/bank_service/script.groovy
@@ -426,7 +426,7 @@ def getNumber(def value, int indexRow, int indexCell) {
     try {
         return new BigDecimal(tmp)
     } catch (Exception e) {
-        throw new Exception("Строка ${indexRow+14} столбец ${indexCell+2} содержит недопустимый тип данных!")
+        throw new Exception("Строка ${indexRow+2} столбец ${indexCell+2} содержит недопустимый тип данных!")
     }
 }
 
@@ -448,7 +448,7 @@ def getRecordId(def ref_id, String code, String value, Date date, def cache, int
         cache[ref_id][filter] = (records.get(0).record_id.toString() as Long)
         return cache[ref_id][filter]
     }
-    throw new Exception("Строка ${indexRow+14} столбец ${indexCell+2} содержит значение, отсутствующее в справочнике!")
+    throw new Exception("Строка ${indexRow+2} столбец ${indexCell+2} содержит значение, отсутствующее в справочнике!")
 }
 
 
