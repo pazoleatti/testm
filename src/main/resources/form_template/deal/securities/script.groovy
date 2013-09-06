8de9e47d556f44f5f0e77ea00796cc3a31ff77db
Alexander Ivanov <alivanov@aplana.com>
2013-09-06 17:26:23 +0600

    Доработка скрипта импорта http://jira.aplana.com/browse/SBRFACCTAX-3946

--- src/main/resources/form_template/deal/securities/script.groovy
+++ src/main/resources/form_template/deal/securities/script.groovy
@@ -364,7 +364,6 @@ def addData(def xml) {
     Date date = new Date()
 
     def cache = [:]
-    println(formData)
     def data = formDataService.getDataRowHelper(formData)
     data.clear()
 
@@ -383,29 +382,28 @@ def addData(def xml) {
 
         def newRow = formData.createDataRow()
         ['fullNamePerson', 'dealSign', 'incomeSum', 'outcomeSum', 'docNumber', 'docDate', 'okeiCode', 'count', 'dealDate'].each {
-            row.getCell(it).editable = true
-            row.getCell(it).setStyleAlias('Редактируемая')
+            newRow.getCell(it).editable = true
+            newRow.getCell(it).setStyleAlias('Редактируемая')
         }
 
         def indexCell = 0
         // графа 1
         newRow.rowNumber = indexRow - 2
-        indexCell++
 
         // графа 2
         newRow.fullNamePerson = getRecordId(9, 'NAME', row.cell[indexCell].text(), date, cache, indexRow, indexCell)
         indexCell++
 
         // графа 3
-//        newRow.inn =
+        //newRow.inn = getNumber(row.cell[indexCell].text(), indexRow, indexCell)
         indexCell++
 
         // графа 4
-//        newRow.countryCode =
+        //newRow.countryCode = getRecordId(10, 'CODE', row.cell[indexCell].text(), date, cache, indexRow, indexCell)
         indexCell++
 
         // графа 5
-        newRow.dealSign = getNumber(row.cell[indexCell].text(), indexRow, indexCell)
+        newRow.dealSign = getRecordId(36, 'SIGN', row.cell[indexCell].text(), date, cache, indexRow, indexCell)
         indexCell++
 
         // графа 6
@@ -425,11 +423,11 @@ def addData(def xml) {
         indexCell++
 
         // графа 10
-        newRow.count = getNumber(row.cell[indexCell].text(), indexRow, indexCell)
+        newRow.okeiCode = getRecordId(12, 'CODE', row.cell[indexCell].text(), date, cache, indexRow, indexCell)
         indexCell++
 
         // графа 11
-        newRow.okeiCode = getNumber(row.cell[indexCell].text(), indexRow, indexCell)
+        newRow.count = getNumber(row.cell[indexCell].text(), indexRow, indexCell)
         indexCell++
 
         // графа 12
@@ -465,7 +463,7 @@ def getNumber(def value, int indexRow, int indexCell) {
     try {
         return new BigDecimal(tmp)
     } catch (Exception e) {
-        throw new Exception("Строка ${indexRow + 14} столбец ${indexCell + 2} содержит недопустимый тип данных!")
+        throw new Exception("Строка ${indexRow - 2} столбец ${indexCell + 2} содержит недопустимый тип данных!")
     }
 }
 
@@ -475,7 +473,13 @@ def getNumber(def value, int indexRow, int indexCell) {
  * @param value
  */
 def getRecordId(def ref_id, String code, String value, Date date, def cache, int indexRow, int indexCell) {
-    String filter = code + "= '" + value + "'"
+
+    String filter;
+    if (value == null || value.equals("")) {
+        filter = code + " is null"
+    } else {
+        filter = code + "= '" + value + "'"
+    }
     if (cache[ref_id] != null) {
         if (cache[ref_id][filter] != null) return cache[ref_id][filter]
     } else {
@@ -487,7 +491,7 @@ def getRecordId(def ref_id, String code, String value, Date date, def cache, int
         cache[ref_id][filter] = (records.get(0).record_id.toString() as Long)
         return cache[ref_id][filter]
     }
-    throw new Exception("Строка ${indexRow + 2} столбец ${indexCell} содержит значение, отсутствующее в справочнике!")
+    throw new Exception("Строка ${indexRow - 2} столбец ${indexCell + 2} содержит значение, отсутствующее в справочнике!")
 }
 
 /**
@@ -501,6 +505,6 @@ def getDate(def value, int indexRow, int indexCell) {
     try {
         return format.parse(value)
     } catch (Exception e) {
-        throw new Exception("Строка ${indexRow + 14} столбец ${indexCell} содержит недопустимый тип данных!")
+        throw new Exception("Строка ${indexRow - 2} столбец ${indexCell + 2} содержит недопустимый тип данных!")
     }
 }
