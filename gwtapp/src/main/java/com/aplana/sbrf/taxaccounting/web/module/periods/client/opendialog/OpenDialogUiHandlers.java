379eb94b10e3d2508ef4ee3bbc2be8cfe392cc81
Semyon Goryachkin <sgoryachkin@aplana.com>
2013-09-13 11:42:41 +0400

    Улучшен внешний вид формы открытия периолдов http://jira.aplana.com/browse/SBRFACCTAX-3845

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/periods/client/opendialog/OpenDialogUiHandlers.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/periods/client/opendialog/OpenDialogUiHandlers.java
@@ -1,10 +1,8 @@
 package com.aplana.sbrf.taxaccounting.web.module.periods.client.opendialog;
 
-import com.aplana.sbrf.taxaccounting.model.TaxPeriod;
 import com.gwtplatform.mvp.client.UiHandlers;
 
 
 public interface OpenDialogUiHandlers extends UiHandlers {
 	void onContinue(OpenFilterData openFilterData);
-	void onTaxPeriodSelected(TaxPeriod taxPeriod);
 }
