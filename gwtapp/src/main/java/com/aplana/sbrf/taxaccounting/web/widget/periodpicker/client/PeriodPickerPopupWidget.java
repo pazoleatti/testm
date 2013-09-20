92db5c46557c382f877ec3c3667fe5ac89e5ed85
Dmitriy Levykin <DLevykin@aplana.com>
2013-09-20 20:07:41 +0600

    Откатил изменения http://jira.aplana.com/browse/SBRFACCTAX-4162, последний отчетный период беру как последний из списка, т.к. он уже отсортирован.

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/periodpicker/client/PeriodPickerPopupWidget.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/periodpicker/client/PeriodPickerPopupWidget.java
@@ -24,151 +24,143 @@ import com.google.gwt.user.client.ui.UIObject;
 import com.google.gwt.user.client.ui.Widget;
 
 public class PeriodPickerPopupWidget extends Composite implements
-		PeriodPickerPopup {
-	
-	interface Binder extends UiBinder<Widget, PeriodPickerPopupWidget> {
-	}
-	private static Binder binder = GWT.create(Binder.class);
-	
-	@UiField(provided = true)
-	PeriodPickerWidget periodPicker;
-	
-	@UiField
-	Button selectButton;
-	
-	@UiField
-	Button clearButton;
-	
-	@UiField
-	PopupPanel popupPanel;
-	
-	@UiField
-	Panel panel;
-	
-	@UiField
-	HasText text;
-	 
-	
-	private List<Integer> value;
-	private Map<Integer, String> dereferenceValue;
-	
-	public PeriodPickerPopupWidget(){
-		periodPicker = new PeriodPickerWidget();
-		initWidget(binder.createAndBindUi(this));
-	}
-	
-	public PeriodPickerPopupWidget(boolean multiselect){
-		periodPicker = new PeriodPickerWidget(multiselect);
-		initWidget(binder.createAndBindUi(this));
-	}
+        PeriodPickerPopup {
 
-    @Override
-    public void setPeriods(List<ReportPeriod> periods) {
-        setPeriods(periods, false);
+    interface Binder extends UiBinder<Widget, PeriodPickerPopupWidget> {
+    }
+    private static Binder binder = GWT.create(Binder.class);
+
+    @UiField(provided = true)
+    PeriodPickerWidget periodPicker;
+
+    @UiField
+    Button selectButton;
+
+    @UiField
+    Button clearButton;
+
+    @UiField
+    PopupPanel popupPanel;
+
+    @UiField
+    Panel panel;
+
+    @UiField
+    HasText text;
+
+
+    private List<Integer> value;
+    private Map<Integer, String> dereferenceValue;
+
+    public PeriodPickerPopupWidget(){
+        periodPicker = new PeriodPickerWidget();
+        initWidget(binder.createAndBindUi(this));
+    }
+
+    public PeriodPickerPopupWidget(boolean multiselect){
+        periodPicker = new PeriodPickerWidget(multiselect);
+        initWidget(binder.createAndBindUi(this));
     }
 
     @Override
-    public void setPeriods(List<ReportPeriod> periods, boolean setLastPeriodSelected) {
+    public void setPeriods(List<ReportPeriod> periods) {
         dereferenceValue = new HashMap<Integer, String>();
         for (ReportPeriod reportPeriod : periods) {
             dereferenceValue.put(reportPeriod.getId(), reportPeriod.getName());
         }
-        periodPicker.setPeriods(periods, setLastPeriodSelected);
-        if (setLastPeriodSelected) {
-            setValue(periodPicker.getValue(), true);
+        periodPicker.setPeriods(periods);
+    }
+
+    @Override
+    public void setAcceptableValues(Collection<List<Integer>> values) {
+        periodPicker.setAcceptableValues(values);
+    }
+
+    @Override
+    public List<Integer> getValue() {
+        return this.value;
+    }
+
+    @Override
+    public void setValue(List<Integer> value) {
+        setValue(value, false);
+    }
+
+    @Override
+    public void setValue(List<Integer> value, boolean fireEvents) {
+        this.value = value;
+        dereference(this.value);
+        if (fireEvents){
+            ValueChangeEvent.fire(this, this.value);
         }
     }
 
-	@Override
-	public void setAcceptableValues(Collection<List<Integer>> values) {
-		periodPicker.setAcceptableValues(values);
-	}
+    @Override
+    public HandlerRegistration addValueChangeHandler(
+            ValueChangeHandler<List<Integer>> handler) {
+        return addHandler(handler, ValueChangeEvent.getType());
+    }
 
-	@Override
-	public List<Integer> getValue() {
-		return this.value;
-	}
+    @Override
+    public boolean isEnabled() {
+        return selectButton.isEnabled();
+    }
 
-	@Override
-	public void setValue(List<Integer> value) {
-		setValue(value, false);
-	}
+    @Override
+    public void setEnabled(boolean enabled) {
+        selectButton.setEnabled(enabled);
+        clearButton.setEnabled(enabled);
+    }
 
-	@Override
-	public void setValue(List<Integer> value, boolean fireEvents) {
-		this.value = value;
-		dereference(this.value);
-		if (fireEvents){
-			ValueChangeEvent.fire(this, this.value);
-		}
-	}
+    @UiHandler("okButton")
+    public void onOkButtonClick(ClickEvent event){
+        this.setValue(periodPicker.getValue(), true);
+        popupPanel.hide();
+    }
 
-	@Override
-	public HandlerRegistration addValueChangeHandler(
-			ValueChangeHandler<List<Integer>> handler) {
-		return addHandler(handler, ValueChangeEvent.getType());
-	}
+    @UiHandler("selectButton")
+    public void onSelectClick(ClickEvent event){
+        periodPicker.setValue(this.value);
+        popupPanel.setPopupPosition(panel.getAbsoluteLeft(),
+                panel.getAbsoluteTop() + panel.getOffsetHeight());
+        popupPanel.show();
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return selectButton.isEnabled();
-	}
+    @UiHandler("clearButton")
+    public void onClearClick(ClickEvent event){
+        this.setValue(null, true);
+    }
 
-	@Override
-	public void setEnabled(boolean enabled) {
-		selectButton.setEnabled(enabled);
-		clearButton.setEnabled(enabled);
-	}
-	
-	@UiHandler("okButton")
-	public void onOkButtonClick(ClickEvent event){
-		this.setValue(periodPicker.getValue(), true); 
-		popupPanel.hide();
-	}
-	
-	@UiHandler("selectButton")
-	public void onSelectClick(ClickEvent event){
-		periodPicker.setValue(this.value); 
-		popupPanel.setPopupPosition(panel.getAbsoluteLeft(),
-				panel.getAbsoluteTop() + panel.getOffsetHeight());
-		popupPanel.show();
-	}
-	
-	@UiHandler("clearButton")
-	public void onClearClick(ClickEvent event){
-		this.setValue(null, true); 
-	}
+    @Override
+    public void setTaxType(String taxType) {
+        // Операция не поддерживается. Пока не нужна была.
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public void setTaxType(String taxType) {
-		// Операция не поддерживается. Пока не нужна была.
-		throw new UnsupportedOperationException();
-	}
-	
-	private void dereference(List<Integer> value){
-		Collection<String> strings = new ArrayList<String>();
-		if (value != null && dereferenceValue != null){
-			for (Integer val : value) {
-				String name = dereferenceValue.get(val);
-				if (name != null){
-					strings.add(name);
-				}
-			}
-		}
-		String txt = joinListToString(strings);
-		this.text.setText(txt);
-		((UIObject) this.text).setTitle(txt);
-	}
-	
-	private String joinListToString(Collection<String> strings) {
-		if ((strings == null) || strings.isEmpty()) {
-			return "";
-		}
-		StringBuilder text = new StringBuilder();
-		for (String name : strings) {
-			text.append(name + "; ");
-		}
-		return text.toString();
-	}
+    private void dereference(List<Integer> value){
+        Collection<String> strings = new ArrayList<String>();
+        if (value != null && dereferenceValue != null){
+            for (Integer val : value) {
+                String name = dereferenceValue.get(val);
+                if (name != null){
+                    strings.add(name);
+                }
+            }
+        }
+        String txt = joinListToString(strings);
+        this.text.setText(txt);
+        ((UIObject) this.text).setTitle(txt);
+    }
+
+    private String joinListToString(Collection<String> strings) {
+        if ((strings == null) || strings.isEmpty()) {
+            return "";
+        }
+        StringBuilder text = new StringBuilder();
+        for (String name : strings) {
+            text.append(name + "; ");
+        }
+        return text.toString();
+    }
 
 }
