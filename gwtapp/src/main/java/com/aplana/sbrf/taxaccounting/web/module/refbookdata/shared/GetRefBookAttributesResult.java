7cfb839c07c36ae31b389459605edb54a7306f6d
Eugene Stetsenko <estetsenko@aplana.com>
2013-09-23 17:41:37 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-4325

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/shared/GetRefBookAttributesResult.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/shared/GetRefBookAttributesResult.java
@@ -1,25 +1,17 @@
 package com.aplana.sbrf.taxaccounting.web.module.refbookdata.shared;
 
-import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttribute;
 import com.gwtplatform.dispatch.shared.Result;
 
 import java.util.List;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Comp-1
- * Date: 28.08.13
- * Time: 13:32
- * To change this template use File | Settings | File Templates.
- */
 public class GetRefBookAttributesResult implements Result {
-	List<RefBookAttribute> attributes;
+	List<RefBookColumn> columns;
 
-	public List<RefBookAttribute> getAttributes() {
-		return attributes;
+	public List<RefBookColumn> getColumns() {
+		return columns;
 	}
 
-	public void setAttributes(List<RefBookAttribute> attributes) {
-		this.attributes = attributes;
+	public void setColumns(List<RefBookColumn> columns) {
+		this.columns = columns;
 	}
 }
