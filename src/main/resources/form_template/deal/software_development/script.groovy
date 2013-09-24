747ccbc5bc3abd4283bca0b438bbf59eba58cf96
Lenar Haziev <lhaziev@aplana.com>
2013-09-24 13:41:51 +0600

    Обновил запросы к справочникам при импорте отчетов МУКС(теперь запросы выполняются без учета регистра).

--- src/main/resources/form_template/deal/software_development/script.groovy
+++ src/main/resources/form_template/deal/software_development/script.groovy
@@ -407,7 +407,7 @@ def getNumber(def value, int indexRow, int indexCell) {
  * @param value
  */
 def getRecordId(def ref_id, String alias, String value, Date date, def cache, int indexRow, int indexCell) {
-    String filter = alias + "= '" + value + "'"
+    String filter = "LOWER($alias) = LOWER('$value')"
     if (cache[ref_id] != null) {
         if (cache[ref_id][filter] != null) return cache[ref_id][filter]
     } else {
