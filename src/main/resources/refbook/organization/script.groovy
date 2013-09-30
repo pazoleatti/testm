5c61a147cf600cf5151a12c8471e2df2ca004072
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2013-09-30 11:51:57 +0600

    импорт справочника «Организации - участники контролируемых сделок» (http://jira.aplana.com/browse/SBRFACCTAX-4564)

--- src/main/resources/refbook/organization/script.groovy
+++ src/main/resources/refbook/organization/script.groovy
@@ -1,8 +1,10 @@
 package refbook.organization
 
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
-import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttributeType
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBook
 import com.aplana.sbrf.taxaccounting.model.refbook.RefBookValue
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttributeType
+
 /**
  * Cкрипт справочника «Организации - участники контролируемых сделок»
  *
@@ -15,7 +17,6 @@ switch (formDataEvent) {
 }
 
 void importFromXLS() {
-    println("importFromXLS")
 
     if (inputStream == null) {
         logger.error('Поток данных пуст')
@@ -27,8 +28,6 @@ void importFromXLS() {
         logger.error('Отсутствие значении после обработки потока данных')
         return
     }
-
-    // println(xmlString)
 
     def xml = new XmlSlurper().parseText(xmlString)
     if (xml == null) {
@@ -79,30 +78,69 @@ def checkTableHead(def xml) {
 def addData(def xml) {
 
     def List<Map<String, RefBookValue>> insertList = new LinkedList<Map<String, RefBookValue>>()
+    def List<Map<String, RefBookValue>> updateList = new ArrayList<Map<String, RefBookValue>>()
 
+    def cache = [:]
+    def date = new Date()
+    def indexRow = 0
     for (def row : xml.row) {
+        indexRow++
         // Шапка
         if (row == xml.row[0]) {
             continue
         }
-
         def Map recordsMap = new HashMap<String, RefBookValue>()
+        indexCell = 0
+        def recordID = getRecordId(9, 'NAME', row.cell[indexCell].text(), date, cache, 0, 0)
 
-//        recordsMap.put("NAME", new RefBookValue(RefBookAttributeType.STRING, row.cell[0].text()))
-//        recordsMap.put("COUNTRY", new RefBookValue(RefBookAttributeType.REFERENCE,))
-//        recordsMap.put("REG_NUM", new RefBookValue(RefBookAttributeType.STRING,))
-//        recordsMap.put("TAXPAYER_CODE", new RefBookValue(RefBookAttributeType.STRING,))
-//        recordsMap.put("ADDRESS", new RefBookValue(RefBookAttributeType.STRING,))
-//        recordsMap.put("INN_KIO", new RefBookValue(RefBookAttributeType.STRING,))
-//        recordsMap.put("KPP", new RefBookValue(RefBookAttributeType.NUMBER,))
-//        recordsMap.put("ORGANIZATION", new RefBookValue(RefBookAttributeType.NUMBER,))
-//        recordsMap.put("OFFSHORE", new RefBookValue(RefBookAttributeType.REFERENCE,))
-//        recordsMap.put("DOP_INFO", new RefBookValue(RefBookAttributeType.STRING,))
-//        recordsMap.put("SKOLKOVO", new RefBookValue(RefBookAttributeType.REFERENCE,))
+        recordsMap.put("NAME",          new RefBookValue(RefBookAttributeType.STRING,       row.cell[indexCell++].text()))
+        recordsMap.put("COUNTRY",       new RefBookValue(RefBookAttributeType.REFERENCE,    getRecordId(10, 'NAME', row.cell[indexCell].text(), date, cache, indexRow, indexCell++)))
+        recordsMap.put("REG_NUM",       new RefBookValue(RefBookAttributeType.STRING,       row.cell[indexCell++].text()))
+        recordsMap.put("TAXPAYER_CODE", new RefBookValue(RefBookAttributeType.STRING,       row.cell[indexCell++].text()))
+        recordsMap.put("ADDRESS",       new RefBookValue(RefBookAttributeType.STRING,       row.cell[indexCell++].text()))
+        recordsMap.put("INN_KIO",       new RefBookValue(RefBookAttributeType.STRING,       row.cell[indexCell++].text()))
+        recordsMap.put("KPP",           new RefBookValue(RefBookAttributeType.NUMBER,       getNumber(row.cell[indexCell++].text())))
+        recordsMap.put("ORGANIZATION",  new RefBookValue(RefBookAttributeType.NUMBER,       getNumber(row.cell[indexCell++].text())))
+        recordsMap.put("OFFSHORE",      new RefBookValue(RefBookAttributeType.REFERENCE,    getRecordId(38, 'CODE', row.cell[indexCell].text(), date, cache, indexRow, indexCell++)))
+        recordsMap.put("DOP_INFO",      new RefBookValue(RefBookAttributeType.STRING,       row.cell[indexCell++].text()))
+        recordsMap.put("SKOLKOVO",      new RefBookValue(RefBookAttributeType.REFERENCE,    getRecordId(38, 'CODE', row.cell[indexCell].text(), date, cache, indexRow, indexCell)))
 
-        insertList.add(recordsMap)
+        if (recordID != null) {
+            recordsMap.put(RefBook.RECORD_ID_ALIAS, new RefBookValue(RefBookAttributeType.NUMBER, recordID))
+            updateList.add(recordsMap)
+        } else {
+            insertList.add(recordsMap)
+        }
     }
 
-   //  dataProvider.updateRecords(new Date(), insertList)
+    def refDataProvider = refBookFactory.getDataProvider(9)
+    if (updateList.size() > 0)
+        refDataProvider.updateRecords(new Date(), updateList)
+    if (insertList.size() > 0)
+        refDataProvider.insertRecords(new Date(), insertList)
+}
 
+def getNumber(def value) {
+    if (value == null || "".equals(value.trim())) {
+        return null
+    }
+    return new BigDecimal(value.trim())
+}
+
+def getRecordId(def ref_id, String alias, String value, Date date, def cache, int indexRow, int indexCell) {
+    String filter = "LOWER($alias) = LOWER('$value')"
+    if (cache[ref_id] != null) {
+        if (cache[ref_id][filter] != null) return cache[ref_id][filter]
+    } else {
+        cache[ref_id] = [:]
+    }
+    def refDataProvider = refBookFactory.getDataProvider(ref_id)
+    def records = refDataProvider.getRecords(date, null, filter, null).getRecords()
+    if (records.size() == 1) {
+        cache[ref_id][filter] = (records.get(0).record_id.toString() as Long)
+        return cache[ref_id][filter]
+    }
+    if (indexRow != 0 && alias!=null && "".equals(alias.trim()))
+        throw new Exception("Строка ${indexRow -1} столбец ${indexCell+1} содержит значение, отсутствующее в справочнике!")
+    return null
 }
