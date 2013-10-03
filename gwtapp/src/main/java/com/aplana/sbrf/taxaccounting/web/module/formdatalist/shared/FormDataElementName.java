ed1e6bda77a2da45381b07d399bde03bcfb2f5b0
Eugene Stetsenko <estetsenko@aplana.com>
2013-10-03 13:11:29 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-3928

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdatalist/shared/FormDataElementName.java
@@ -0,0 +1,15 @@
+package com.aplana.sbrf.taxaccounting.web.module.formdatalist.shared;
+
+/**
+ * Имена полей фильтра на форме списка форм
+ */
+public enum FormDataElementName {
+	HEADER,
+	SUB_HEADER,
+	DEPARTMENT,
+	FORM_KIND,
+	FORM_TYPE,
+	STATUS,
+	RETURN,
+	REPORT_PERIOD
+}
