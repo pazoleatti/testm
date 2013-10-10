6c630e5e282978e82f8f969be62622263f300e59
Dmitriy Levykin <DLevykin@aplana.com>
2013-10-10 10:15:03 +0600

    Доработка РНУ-107 http://jira.aplana.com/browse/SBRFACCTAX-4590

--- src/main/resources/form_template/income/rnu107/script.groovy
+++ src/main/resources/form_template/income/rnu107/script.groovy
@@ -1,5 +1,7 @@
 package form_template.income.rnu107
 
+import com.aplana.sbrf.taxaccounting.model.Cell
+import com.aplana.sbrf.taxaccounting.model.DataRow
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
 /**
  * 502 - (РНУ-107) Регистр налогового учёта доходов, возникающих в связи с применением в сделках с Взаимозависимыми лицами и резидентами оффшорных зон тарифов, не соответствующих рыночному уровню
@@ -8,33 +10,121 @@ import com.aplana.sbrf.taxaccounting.model.FormDataEvent
  */
 switch (formDataEvent) {
     case FormDataEvent.CREATE:
-//        checkCreation()
+        checkCreation()
         break
     case FormDataEvent.CHECK:
-//        logicalCheck()
+        logicalCheck()
         break
     case FormDataEvent.CALCULATE:
-//        calc()
-//        logicalCheck()
+        calc()
+        logicalCheck()
         break
     case FormDataEvent.ADD_ROW:
-//        addNewRow()
+        addNewRow()
         break
     case FormDataEvent.DELETE_ROW:
-//        deleteRow()
+        deleteRow()
         break
 // После принятия из Утверждено
     case FormDataEvent.AFTER_MOVE_APPROVED_TO_ACCEPTED:
-//        logicalCheck()
+        logicalCheck()
         break
 // После принятия из Подготовлена
     case FormDataEvent.AFTER_MOVE_PREPARED_TO_ACCEPTED:
-//        logicalCheck()
+        logicalCheck()
         break
 // Консолидация
     case FormDataEvent.COMPOSE:
-//        consolidation()
-//        calc()
-//        logicalCheck()
+        consolidation()
+        calc()
+        logicalCheck()
         break
+}
+
+// графа 1  - number
+// графа 2  - interdependence
+// графа 3  - operationDate
+// графа 4  - taxCode
+// графа 5  - baseNumber
+// графа 6  - baseDate
+// графа 7  - operationSum
+// графа 8  - rateService
+// графа 9  - rateTax
+// графа 10 - incomeSumFact
+// графа 11 - incomeSumTax
+// графа 12 - incomeSumBefore
+
+// Проверки при создании формы
+void checkCreation() {
+    // отчётный период
+    def reportPeriod = reportPeriodService.get(formData.reportPeriodId)
+
+    // проверка периода ввода остатков
+    if (reportPeriod != null && reportPeriodService.isBalancePeriod(reportPeriod.id, formData.departmentId)) {
+        logger.error('Налоговая форма не может создаваться в периоде ввода остатков.')
+        return
+    }
+
+    def findForm = formDataService.find(formData.formType.id, formData.kind, formData.departmentId,
+            formData.reportPeriodId)
+
+    if (findForm != null) {
+        logger.error('Налоговая форма с заданными параметрами уже существует.')
+    }
+}
+
+// Добавить новую строку
+void addNewRow() {
+    def dataRowHelper = formDataService.getDataRowHelper(formData)
+    def row = formData.createDataRow()
+    def dataRows = dataRowHelper.getAllCached()
+    def size = dataRows.size()
+    def index = currentDataRow != null ? (currentDataRow.getIndex() + 1) : (size == 0 ? 1 : (size + 1))
+    ['interdependence', 'operationDate', 'taxCode', 'baseNumber', 'baseDate', 'operationSum', 'rateService', 'rateTax',
+            'incomeSumTax', 'incomeSumBefore'].each {
+        row.getCell(it).editable = true
+        row.getCell(it).setStyleAlias('Редактируемая')
+    }
+    dataRowHelper.insert(row, index)
+}
+
+// Удалить строку
+def deleteRow() {
+    def dataRowHelper = formDataService.getDataRowHelper(formData)
+    dataRowHelper.delete(currentDataRow)
+}
+
+// Расчеты. Алгоритмы заполнения полей формы.
+void calc() {
+    // TODO
+}
+
+// Логические проверки
+def logicalCheck() {
+    def dataRows = formDataService.getDataRowHelper(formData).getAllCached()
+
+    if (dataRows.isEmpty()) {
+        return true
+    }
+
+    // TODO
+    return true
+}
+
+// Консолидация
+void consolidation() {
+    def rows = new LinkedList<DataRow<Cell>>()
+    departmentFormTypeService.getFormSources(formDataDepartment.id, formData.getFormType().getId(), formData.getKind()).each {
+        if (it.formTypeId == formData.getFormType().getId()) {
+            def source = formDataService.find(it.formTypeId, it.kind, it.departmentId, formData.reportPeriodId)
+            if (source != null && source.state == WorkflowState.ACCEPTED) {
+                formDataService.getDataRowHelper(source).getAllCached().each { row ->
+                    if (row.getAlias() == null || row.getAlias() == '') {
+                        rows.add(row)
+                    }
+                }
+            }
+        }
+    }
+    formDataService.getDataRowHelper(formData).save(rows)
 }
