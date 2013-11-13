ded5934c0317c0c48852204913a713d894d0de60
Marat Fayzullin <mfayzullin@aplana.com>
2013-11-13 15:50:14 +0600

    Добавил маппинг шаблонов по транспорту и прибыли

--- templatesync/src/main/groovy/com/aplana/taxaccounting/Main.groovy
+++ templatesync/src/main/groovy/com/aplana/taxaccounting/Main.groovy
@@ -35,36 +35,101 @@ class Main {
     def static resourcesPath = 'C:/form_template'
 
     // Маппинг шаблонов
-    def static templates = [deal: [
-            380: 'auctions_property',
-            382: 'bank_service',
-            384: 'bonds_trade',
-            387: 'corporate_credit',
-            385: 'credit_contract',
-            390: 'foreign_currency',
-            391: 'forward_contracts',
-            388: 'guarantees',
-            389: 'interbank_credits',
-            386: 'letter_of_credit',
-            400: 'matrix',
-            392: 'nondeliverable',
-            410: 'organization_matching',
-            393: 'precious_metals_deliver',
-            394: 'precious_metals_trade',
-            376: 'rent_provision',
-            383: 'repo',
-            381: 'securities',
-            375: 'software_development',
-            377: 'tech_service',
-            379: 'trademark']
+    def static templates = [
+            deal:[
+                    380:'auctions_property',
+                    382:'bank_service',
+                    384:'bonds_trade',
+                    387:'corporate_credit',
+                    385:'credit_contract',
+                    390:'foreign_currency',
+                    391:'forward_contracts',
+                    388:'guarantees',
+                    389:'interbank_credits',
+                    386:'letter_of_credit',
+                    400:'matrix',
+                    392:'nondeliverable',
+                    410:'organization_matching',
+                    393:'precious_metals_deliver',
+                    394:'precious_metals_trade',
+                    376:'rent_provision',
+                    383:'repo',
+                    381:'securities',
+                    375:'software_development',
+                    377:'tech_service',
+                    379:'trademark'
+            ],
+            income:[
+                    500:'advanceDistribution',
+                    362:'f7_8',
+                    302:'income_complex',
+                    301:'income_simple',
+                    303:'outcome_complex',
+                    304:'outcome_simple',
+                    306:'output1',
+                    307:'output2',
+                    308:'output3',
+                    360:'rnu101',
+                    364:'rnu12',
+                    321:'rnu14',
+                    499:'rnu16',
+                    501:'rnu17',
+                    322:'rnu22',
+                    323:'rnu23',
+                    324:'rnu25',
+                    325:'rnu26',
+                    326:'rnu27',
+                    329:'rnu30',
+                    328:'rnu31',
+                    332:'rnu33',
+                    333:'rnu36_1',
+                    334:'rnu38_1',
+                    335:'rnu38_2',
+                    337:'rnu39_2',
+                    316:'rnu4',
+                    338:'rnu40_1',
+                    339:'rnu40_2',
+                    340:'rnu44',
+                    341:'rnu45',
+                    342:'rnu46',
+                    344:'rnu47',
+                    343:'rnu48_1',
+                    313:'rnu48_2',
+                    312:'rnu49',
+                    317:'rnu5',
+                    365:'rnu50',
+                    345:'rnu51',
+                    346:'rnu53',
+                    347:'rnu54',
+                    348:'rnu55',
+                    349:'rnu56',
+                    353:'rnu57',
+                    350:'rnu59',
+                    318:'rnu6',
+                    351:'rnu60',
+                    352:'rnu61',
+                    355:'rnu64',
+                    311:'rnu7',
+                    358:'rnu72',
+                    320:'rnu8'
+            ],
+            transport:[
+                    202:'benefit_vehicles',
+                    201:'vehicles',
+                    200:'summary',
+            ]
     ]
 
     public static void main(String[] args) {
         def api = SyncAPI.getInstance(paramsMap)
         api.login()
-        downloadAll(api)
-        // uploadAll(api)
-        api.close()
+        try {
+            downloadAll(api)
+            // uploadAll(api)
+        }
+        finally {
+            api.close()
+        }
     }
 
     private static void downloadAll(SyncAPI api) {
