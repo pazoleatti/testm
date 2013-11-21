151da5c623f16071d336e7b06c116f94e9b1e84e
Dmitriy Levykin <DLevykin@aplana.com>
2013-11-21 17:40:51 +0600

    Скрытие пагинатора в налоговых формах, влезающих в одну страницу. Пагинатор для области уведомлений.

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/log/LogEntriesWidget.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/log/LogEntriesWidget.java
@@ -1,18 +1,23 @@
 package com.aplana.sbrf.taxaccounting.web.widget.log;
 
-import java.util.List;
-
 import com.aplana.sbrf.taxaccounting.model.log.LogEntry;
 import com.aplana.sbrf.taxaccounting.web.widget.log.cell.LogEntryImageCell;
 import com.aplana.sbrf.taxaccounting.web.widget.log.cell.LogEntryIndexCell;
 import com.aplana.sbrf.taxaccounting.web.widget.log.cell.LogEntryMessageCell;
+import com.aplana.sbrf.taxaccounting.web.widget.pager.FlexiblePager;
+import com.aplana.sbrf.taxaccounting.web.widget.style.GenericCellTable;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.cellview.client.CellTable;
 import com.google.gwt.user.cellview.client.Column;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LogEntriesWidget extends Composite implements LogEntriesView {
 
@@ -22,9 +27,18 @@ public class LogEntriesWidget extends Composite implements LogEntriesView {
 	interface LogEntryWidgetUiBinder extends UiBinder<Widget, LogEntriesWidget> {
 	}
 
-
 	@UiField
-	CellTable<LogEntry> logCellTable;
+    CellTable<LogEntry> logCellTable;
+
+    @UiField
+    FlexiblePager pager;
+
+    @UiField
+    DockLayoutPanel dock;
+
+    private static final int PAGE_SIZE = 50;
+
+    private ListDataProvider<LogEntry> dataProvider;
 
 	public LogEntriesWidget() {
 		initWidget(uiBinder.createAndBindUi(this));
@@ -56,25 +70,31 @@ public class LogEntriesWidget extends Composite implements LogEntriesView {
 		logCellTable.addColumn(indexColumn);
 		logCellTable.setColumnWidth(indexColumn, "30px");
 		logCellTable.addColumn(imageColumn);
-		
 		logCellTable.addColumn(messageColumn);
-		setLogEntries(null);
+
+        dataProvider = new ListDataProvider<LogEntry>();
+        dataProvider.addDataDisplay(logCellTable);
+        pager.setDisplay(logCellTable);
+        logCellTable.setPageSize(PAGE_SIZE);
+
+        setLogEntries(null);
 	}
 
 	@Override
 	public void setLogEntries(List<LogEntry> entries) {
-		if (entries != null) {
-			logCellTable.setRowCount(entries.size());
-			logCellTable.setRowData(entries);
-		} else {
-			logCellTable.setRowCount(0);
-		}
-		logCellTable.redraw();
+        if (entries != null) {
+            dataProvider.setList(entries);
+        } else {
+            dataProvider.setList(new ArrayList<LogEntry>());
+        }
+        boolean isVisible= entries != null && entries.size() > PAGE_SIZE;
+        pager.setVisible(isVisible);
+        dock.setWidgetSize(pager, isVisible ? 30 : 0);
+        pager.setPage(0);
 	}
 
 	@Override
 	public void setTableElementId(String id) {
 		this.getElement().setId(id);
 	}
-
 }
