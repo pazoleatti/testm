97d387447f458cb0c3f2efa42ba5c43340f5f35c
Eugene Stetsenko <estetsenko@aplana.com>
2013-12-03 11:12:40 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-4870 Фикс после ревью

---
+++ dao/src/test/resources/data/TARole.sql
@@ -0,0 +1,3 @@
+insert into sec_role (id, alias, name) values (1, 'ROLE_CONTROL', 'Контролёр');
+insert into sec_role (id, alias, name) values (2, 'ROLE_OPER', 'Оператор');
+insert into sec_role (id, alias, name) values (3, 'ROLE_CONTROL_UNP', 'Контролёр УНП');
