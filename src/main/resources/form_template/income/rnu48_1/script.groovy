67628fc3db9f25d6593ad3a08f874379255c174c
Ramil Timerbaev <Rtimerbaev@aplana.com>
2013-12-04 18:42:00 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-5192 - Унифицировать код скриптов НФ (48.1).

--- src/main/resources/form_template/income/rnu48_1/script.groovy
+++ src/main/resources/form_template/income/rnu48_1/script.groovy
@@ -1,46 +1,39 @@
 package form_template.income.rnu48_1
 
-import com.aplana.sbrf.taxaccounting.model.Cell
-import com.aplana.sbrf.taxaccounting.model.DataRow
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
-import com.aplana.sbrf.taxaccounting.model.WorkflowState
-import com.aplana.sbrf.taxaccounting.model.log.LogLevel
-import com.aplana.sbrf.taxaccounting.service.script.api.DataRowHelper
+import groovy.transform.Field
 
 /**
- * Форма "3.1	(РНУ-48.1) Регистр налогового учёта «ведомость ввода в эксплуатацию инвентаря и принадлежностей
- * до 40 000 руб.  ".
+ * Форма "(РНУ-48.1) Регистр налогового учёта «ведомость ввода в эксплуатацию инвентаря и принадлежностей до 40 000 руб.".
  * formTemplateId=343
  *
- * Версия ЧТЗ: 64
- *
- * TODO:
- *          -   что за сквозная нумерация в рамках текущего отчетного года, как ее реализовывать?
- *
  * @author vsergeev
- *
- * Графы:
- * number           -   № пп
- * inventoryNumber  -   Инвентарный номер
- * usefulDate       -   Дата ввода в эксплуатацию
- * amount           -   Сумма, включаемая в состав материальных расходов
+ * @author rtimerbaev
  */
+
+// 1 - number          - № пп
+// 2 - inventoryNumber - Инвентарный номер
+// 3 - usefulDate      - Дата ввода в эксплуатацию
+// 4 - amount          - Сумма, включаемая в состав материальных расходов
+
 switch (formDataEvent) {
     case FormDataEvent.CREATE :
         checkCreation()
         break
     case FormDataEvent.CHECK :
-        allCheck()
+        logicCheck()
         break
     case FormDataEvent.CALCULATE :
         calc()
-        allCheck()
+        logicCheck()
         break
     case FormDataEvent.ADD_ROW :
-        addNewRow()
+        formDataService.addRow(formData, currentDataRow, editableColumns, null)
         break
     case FormDataEvent.DELETE_ROW :
-        deleteCurrentRow()
+        if (currentDataRow != null && currentDataRow.getAlias() == null) {
+            formDataService.getDataRowHelper(formData)?.delete(currentDataRow)
+        }
         break
     case FormDataEvent.MOVE_CREATED_TO_APPROVED :  // Утвердить из "Создана"
     case FormDataEvent.MOVE_APPROVED_TO_ACCEPTED : // Принять из "Утверждена"
@@ -48,359 +41,98 @@ switch (formDataEvent) {
     case FormDataEvent.MOVE_CREATED_TO_PREPARED :  // Подготовить из "Создана"
     case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED : // Принять из "Подготовлена"
     case FormDataEvent.MOVE_PREPARED_TO_APPROVED : // Утвердить из "Подготовлена"
-        allCheck()
+        logicCheck()
         break
-    // обобщить
     case FormDataEvent.COMPOSE :
-        consolidation()
+        formDataService.consolidationSimple(formData, formDataDepartment.id, logger)
         calc()
-        if (allCheck()) {
-            // для сохранения изменений приемников
-            data.commit()
-        }
+        logicCheck()
         break
 }
 
-def allCheck() {
-    return !hasError() && logicalCheck()
-}
+// Все атрибуты
+@Field
+def allColumns = ['number', 'inventoryNumber', 'usefulDate', 'amount']
 
-/**
- * Проверка при создании формы.
- */
+// Редактируемые атрибуты (графа 2..4)
+@Field
+def editableColumns = ['inventoryNumber', 'usefulDate', 'amount']
+
+// Группируемые атрибуты (графа 3, 2)
+@Field
+def groupColumns = ['usefulDate', 'inventoryNumber']
+
+// Проверяемые на пустые значения атрибуты (графа 1..4)
+@Field
+def nonEmptyColumns = ['number', 'inventoryNumber', 'usefulDate', 'amount']
+
+// Атрибуты итоговых строк для которых вычисляются суммы (графа )
+@Field
+def totalColumns = ['amount']
+
 void checkCreation() {
-    // отчётный период
-    def reportPeriod = reportPeriodService.get(formData.reportPeriodId)
-
     //проверка периода ввода остатков
-    if (reportPeriod != null && reportPeriodService.isBalancePeriod(reportPeriod.id, formData.departmentId)) {
+    if (reportPeriodService.isBalancePeriod(formData.reportPeriodId, formData.departmentId)) {
         logger.error('Налоговая форма не может создаваться в периоде ввода остатков.')
         return
     }
-
-    def findForm = formDataService.find(formData.formType.id,
-            formData.kind, formData.departmentId, formData.reportPeriodId)
-
-    if (findForm != null) {
-        logger.error('Налоговая форма с заданными параметрами уже существует.')
-    }
+    formDataService.checkUnique(formData, logger)
 }
 
-/**
- * Имеются ли фатальные ошибки.
- */
-def hasError() {
-    return logger.containsLevel(LogLevel.ERROR)
-}
-
-/**
- * Расчеты
- */
 void calc() {
-    def data = data
-    def requiredColumns = getEditableColsAliases()
+    def dataRowHelper = formDataService.getDataRowHelper(formData)
+    def dataRows = dataRowHelper.getAllCached()
 
-    // Обязательность заполнения полей граф (с 2 по 4)
-    for (def row : getRows(data)) {
-        if (!isTotal(row) && !checkRequiredColumns(row, requiredColumns)) {
-            return
-        }
+    // удалить строку "итого"
+    deleteAllAliased(dataRows)
+    // сортировка
+    sortRows(dataRows, groupColumns)
+
+    def rowNumber = formDataService.getPrevRowNumber(formData, formDataDepartment.id, 'number')
+    for (def row : dataRows) {
+        row.number = ++rowNumber
     }
-
-    def sortColumns = ['usefulDate', 'inventoryNumber']
-    getRows(data).sort({ DataRow a, DataRow b ->
-        sortRow(sortColumns, a, b)
-    })
-
-    i = 1
-    for (def row : getRows(data)) {
-        if (!isTotal(row)){
-            row.number = i++
-        }
-    }
-
     //расчитываем новые итоговые значения
-    def totalResults = getTotalResultsForCols()
-    def totalRow = data.getDataRow(getRows(data),getTotalDataRowAlias())
-    getTotalColsAliases().each { colName ->
-        totalRow[colName] = totalResults[colName]
-    }
-    data.save(getRows(data))
+    def totalRow = getTotalRow(dataRows)
+    dataRows.add(totalRow)
+    dataRowHelper.save(dataRows)
 }
 
-int sortRow(List<String> params, DataRow a, DataRow b) {
-    for (String param : params) {
-        def aD = a.getCell(param).value
-        def bD = b.getCell(param).value
-
-        if (!isTotal(a) && !isTotal(b) && aD != bD) {
-            return aD <=> bD
-        }
-    }
-    return 0
-}
-
-/**
- * Общие проверки
- */
-boolean logicalCheck(){
-    def data = data
-    def rows = getRows(data)
-    def requiredColumns = getRequiredColsAliases()
-    //  проверка, что в таблице есть хотя бы одна строка
-    if (rows.isEmpty()) {
-        logger.error ('В таблице отсутствуют заполненные строки!')
-        return false
-    }
-
-    // 1.  Обязательность заполнения поля графы (с 1 по 4)
-    for (def row : rows) {
-        if (!isTotal(row) && !checkRequiredColumns(row, requiredColumns)) {
-            return false
-        }
-    }
+void logicCheck() {
+    def dataRows = formDataService.getDataRowHelper(formData)?.getAllCached()
 
     def reportPeriodRange = getReportPeriodRange()
+    for (def row : dataRows) {
+        if (row.getAlias() != null) {
+            continue
+        }
+        // 1. Обязательность заполнения поля графы 1..4
+        checkNonEmptyColumns(row, row.getIndex(), nonEmptyColumns, logger, true)
 
-    //построчно проверяем данные формы
-    for (def row : rows) {
-        if ( ! isTotal(row)) {
-            // 2.  Проверка даты ввода в эксплуатацию и границ отчетного периода
-            // Дата начала отчётного периода ≤ графа 3 ≤ дата окончания отчётного периода
-            if (! reportPeriodRange.containsWithinBounds(row.usefulDate)){
-                def rowStart = getRowIndexString(row)
-                logger.error("${rowStart}дата ввода в эксплуатацию вне границ отчетного периода!")
-                return false
-            }
+        // 2. Проверка даты ввода в эксплуатацию и границ отчетного периода
+        if (! (row.usefulDate in reportPeriodRange)){
+            logger.error("Строка ${row.getIndex()}: Дата ввода в эксплуатацию вне границ отчетного периода!")
         }
     }
 
-    def totalDataRow = data.getDataRow(getRows(data),getTotalDataRowAlias())
-    if (totalDataRow == null) {
-        logger.error('Строка Итого не найдена')
-        return false
-    }
-    def controlTotalResultsForCols = getTotalResultsForCols()
-    for (def colName : getTotalColsAliases()) {
-        if (totalDataRow[colName] != controlTotalResultsForCols[colName]) {
-            logger.error('Строка Итого рассчитана неверно!')
-            return false
-        }
-    }
-    return true
+    // 3. Проверка итоговых значений по всей форме
+    checkTotalSum(dataRows, totalColumns, logger, true)
 }
 
-/**
- * Консолидация.
- */
-void consolidation() {
-    def data = data
-    // удалить все строки и собрать из источников их строки
-    data.clear()
-
-    departmentFormTypeService.getFormSources(formDataDepartment.id, formData.getFormType().getId(), formData.getKind()).each {
-        if (it.formTypeId == formData.getFormType().getId()) {
-            def source = formDataService.find(it.formTypeId, it.kind, it.departmentId, formData.reportPeriodId)
-            if (source != null && source.state == WorkflowState.ACCEPTED) {
-                getRows(getData(source)).each { row->
-                    if (row.getAlias() == null || row.getAlias() == '') {
-                        data.insert(row, getRows(data).size()+1)
-                    }
-                }
-            }
-        }
-    }
-    logger.info('Формирование консолидированной формы прошло успешно.')
-}
-
-/**
- * Проверить заполненость обязательных полей.
- *
- * @param row строка
- * @param columns список обязательных графов
- * @param useLog нужно ли записывать сообщения в лог
- * @return true - все хорошо, false - есть незаполненные поля
- */
-def checkRequiredColumns(DataRow row, Object columns) {
-    def colNames = []
-
-    columns.each { String col ->
-        if (row.getCell(col).getValue() == null || ''.equals(row.getCell(col).getValue())) {
-            def name = row.getCell(col).getColumn().getName().replace('%', '%%')
-            colNames.add('"' + name + '"')
-        }
-    }
-    if (!colNames.isEmpty()) {
-        def errorBegin = getRowIndexString(row)
-        def errorMsg = colNames.join(', ')
-        logger.error("${errorBegin}не заполнены колонки : $errorMsg.")
-        return false
-    }
-    return true
-}
-
-/**
- * Начало предупреждений/ошибок
- * @param row
- * @return
- */
-def String getRowIndexString(def DataRow row){
-    def index = row.number
-    if (index != null) {
-        return "В строке \"№ пп\" равной $index "
-    } else {
-        index = getIndex(row) + 1
-        return "В строке $index "
-    }
-}
-
-/**
- * Получить номер строки в таблице.
- */
-def getIndex(def row) {
-    getRows(data).indexOf(row)
-}
-
-def getTotalDataRowAlias() {
-    return 'total'
-}
-
-/********************************   ОБЩИЕ ФУНКЦИИ   ********************************/
-
-/**
- * возвращает список алиасов столбцов, доступных для редактирования
- */
-def getEditableColsAliases() {
-    return ['inventoryNumber', 'usefulDate', 'amount']
-}
-
-/**
- * возвращает список алиасов обязательных столбцов
- */
-def getRequiredColsAliases() {
-    return ['number', 'inventoryNumber', 'usefulDate', 'amount']
-}
-
-/***********   ОБЩИЕ ФУНКЦИИ ДЛЯ ИТОГОВЫХ СТРОК   ***********/
-
-/**
- * Проверка является ли строка итоговой.
- */
-def isTotal(def row) {
-    return row != null && row.getAlias() != null && (row.getAlias() in totalRowsAliases)
-}
-
-/**
- * возвращает список алиасов для итоговых строк
- */
-def getTotalRowsAliases() {
-    return ['total']
-}
-
-/***********   ОБЩИЕ ФУНКЦИИ ДЛЯ СТОЛБЦОВ, ПО КОТОРЫМ ПОДВОДЯТСЯ ИТОГИ   ***********/
-
-/**
- * возвращает список алиасов для стобцов, по которым подводятся итоги
- */
-def getTotalColsAliases() {
-    return ['amount']
-}
-
-/**
- * находим для всех строк, кроме итоговых, суммы по столбцам, по которым подводят итоги
- * возвращаем мапу вида алиас_столбца -> итоговое_значение
- */
-def getTotalResultsForCols() {
-    def data = data
-    def rows = getRows(data)
-    def result = [:]
-    for (def colAlias : getTotalColsAliases()) {
-        result.put(colAlias, rows.sum {row ->
-            if (! isTotal(row)) {    //строка не входит в итоговые
-                row[colAlias]
-            } else {
-                0
-            }
-        })
-    }
-    return result
-}
-
-/***********   ДОБАВЛЕНИЕ СТРОКИ В ТАБЛИЦУ С ФИКСИРОВАННЫМИ СТРОКАМИ ИТОГОВ   ***********/
-def addNewRow() {
-    def data = data
-    def rows = getRows(data)
+def getTotalRow(def dataRows) {
     def newRow = formData.createDataRow()
-
-    getEditableColsAliases().each{ value ->
-        newRow.getCell(value).editable = true
-        newRow.getCell(value).setStyleAlias('Редактируемая')
+    newRow.setAlias('total')
+    newRow.inventoryNumber = 'Итого'
+    allColumns.each {
+        newRow.getCell(it).setStyleAlias('Контрольные суммы')
     }
-    def index = 0
-    if (currentDataRow!=null){
-        index = currentDataRow.getIndex()
-        def row = currentDataRow
-        while(row.getAlias()!=null && index>0){
-            row = rows.get(--index)
-        }
-        if(index!=currentDataRow.getIndex() && rows.get(index).getAlias()==null){
-            index++
-        }
-    }else if (rows.size()>0) {
-        for(int i = rows.size()-1;i>=0;i--){
-            def row = rows.get(i)
-            if(row.getAlias()==null){
-                index = rows.indexOf(row)+1
-                break
-            }
-        }
-    }
-    data.insert(newRow,index+1)
+    calcTotalSum(dataRows, newRow, totalColumns)
+    return newRow
 }
 
-/**
- * Удалить строку.
- */
-def deleteCurrentRow() {
-    if (currentDataRow != null && currentDataRow.getAlias() == null) {
-        data.delete(currentDataRow)
-    }
-}
-
-/***********   ДЛЯ РАБОТЫ С ОТЧЕТНЫМИ ПЕРИОДАМИ   ***********/
-
-/**
- * возвращает диапазон Date..Date (включительно!) для текущего отчетного периода (с нарастающим итогом)
- */
 def getReportPeriodRange() {
-    def periodStartsDate = reportPeriodService.getStartDate(formData.reportPeriodId).getTime()
-    def periodEndsDate = reportPeriodService.getEndDate(formData.reportPeriodId).getTime()
+    def periodStartsDate = reportPeriodService.getStartDate(formData.reportPeriodId)?.time
+    def periodEndsDate = reportPeriodService.getEndDate(formData.reportPeriodId)?.time
 
     return periodStartsDate..periodEndsDate
 }
-
-/**
- * Получить данные формы.
- *
- * @param formData форма
- */
-def DataRowHelper getData(def formData) {
-    if (formData != null && formData.id != null) {
-        return formDataService.getDataRowHelper(formData)
-    }
-    return null
-}
-
-def DataRowHelper getData(){
-    return getData(formData)
-}
-
-/**
- * Получить строки формы.
- *
- * @param formData форма
- */
-def List<DataRow<Cell>> getRows(def DataRowHelper data) {
-    return data.getAllCached()
-}
-
