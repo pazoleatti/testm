d05e734f61cde4602b87a0fbb6a0e2df03dd0dec
Ozherebtsova <Ozherebtsova@aplana.com>
2013-12-05 10:30:41 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-5195 + алиасы колонок

--- src/main/resources/ddl/getRefBookData.sql
+++ src/main/resources/ddl/getRefBookData.sql
@@ -8,25 +8,25 @@ AS
    nCount          number:=1;
    sTYPE           varchar2(40);
 BEGIN
-  
+
 vSQLselect:='with t as (select max(version) version, record_id from ref_book_record
                          where ref_book_id = '||refBookId||' and version <= to_date('||''''||to_char(version,'dd.mm.yyyy')||''''||','||''''||'dd.mm.yyyy'||''''||')group by record_id)
-             select r.id as id';
-vSQLfrom:=' from ref_book_record r join t on (r.version = t.version and r.record_id = t.record_id)';             
+             select r.id as record_id';
+vSQLfrom:=' from ref_book_record r join t on (r.version = t.version and r.record_id = t.record_id)';
 vSQLwhere:= ' where r.ref_book_id = '||refBookId||' and r.status <> -1';
 
-for q in (select id, type
+for q in (select id, type, alias
             from ref_book_attribute
             where ref_book_id=refBookId
-            order by ord) 
-loop  
+            order by ord)
+loop
   case q.type when 1 then sTYPE:='.string_value';
               when 2 then sTYPE:='.number_value';
               when 3 then sTYPE:='.date_value';
               when 4 then sTYPE:='.reference_value';
-  end case; 
-   
-vSQLselect:=vSQLselect||', a'||nCOUNT||sTYPE;
+  end case;
+
+vSQLselect:=vSQLselect||', a'||nCOUNT||sTYPE||' as '||q.alias;
 vSQLfrom:=vSQLfrom||' left join ref_book_value a'||nCOUNT||' on a'||nCOUNT||'.record_id = r.id and a'||nCOUNT||'.attribute_id = '||q.id;
 nCount:=nCount+1;
 end loop;
