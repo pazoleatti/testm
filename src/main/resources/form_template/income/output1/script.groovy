a8d4e5c452f1765433738de16c4f7fb8f1f49c08
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2013-12-10 09:18:07 +0600

    Сведения для расчёта налога с доходов в виде дивидендов (http://jira.aplana.com/browse/SBRFACCTAX-5192)

--- src/main/resources/form_template/income/output1/script.groovy
+++ src/main/resources/form_template/income/output1/script.groovy
@@ -1,169 +1,169 @@
 package form_template.income.output1
 
-import com.aplana.sbrf.taxaccounting.model.FormData
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
 import com.aplana.sbrf.taxaccounting.model.FormDataKind
-import com.aplana.sbrf.taxaccounting.model.log.LogLevel
-import com.aplana.sbrf.taxaccounting.service.script.api.DataRowHelper
+import groovy.transform.Field
 
 /**
  * Сведения для расчёта налога с доходов в виде дивидендов (доходов от долевого участия в других организациях,
  * созданных на территории Российской Федерации)
  * formTemplateId=306
+ *
+ * http://conf.aplana.com/pages/viewpage.action?pageId=8784122
+ *
+ * @author Stanislav Yasinskiy
  */
-
-DataRowHelper getDataRowsHelper() {
-    DataRowHelper dataRowsHelper = null
-    if (formData.id != null) {
-        dataRowsHelper = formDataService.getDataRowHelper(formData)
-    }
-    return dataRowsHelper
-}
-
 switch (formDataEvent) {
     case FormDataEvent.CREATE:
-        checkUniq()
+        if (formData.kind != FormDataKind.ADDITIONAL) {
+            logger.error("Нельзя создавать форму с типом ${formData.kind?.name}")
+        }
+        formDataService.checkUnique(formData, logger)
         break
     case FormDataEvent.CALCULATE:
-        logicalCheck()
-        logicCheck()
         calc()
-        dataRowsHelper.save(dataRowsHelper.getAllCached());
+        logicCheck()
         break
     case FormDataEvent.CHECK:
-        logicalCheck()
         logicCheck()
-        break
-    case FormDataEvent.MOVE_CREATED_TO_PREPARED:
-        checkDecl()
-        logicalCheck()
-        logicCheck()
-        dataRowsHelper.save(dataRowsHelper.getAllCached());
-        break
-    case FormDataEvent.MOVE_PREPARED_TO_CREATED:
-        break
-    case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED:
-        checkDecl()
-        logicalCheck()
-        logicCheck()
-        dataRowsHelper.save(dataRowsHelper.getAllCached());
-        break
-    case FormDataEvent.MOVE_ACCEPTED_TO_PREPARED:
-        checkDecl()
         break
     case FormDataEvent.ADD_ROW:
-        addRow()
+        formDataService.addRow(formData, currentDataRow, editableColumns, null)
         break
     case FormDataEvent.DELETE_ROW:
-        deleteRow()
+        formDataService.getDataRowHelper(formData).delete(currentDataRow)
+        break
+    case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
+    case FormDataEvent.MOVE_CREATED_TO_APPROVED:  // Утвердить из "Создана"
+    case FormDataEvent.MOVE_CREATED_TO_ACCEPTED:  // Принять из "Создана"
+    case FormDataEvent.MOVE_PREPARED_TO_APPROVED: // Утвердить из "Подготовлена"
+    case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED: // Принять из "Подготовлена"
+    case FormDataEvent.MOVE_APPROVED_TO_ACCEPTED: // Принять из "Утверждена"
+        logicCheck()
         break
 }
 
-void deleteRow() {
-    if (currentDataRow != null) {
-        dataRowsHelper.delete(currentDataRow)
-    }
+//// Кэши и константы
+@Field
+def refBookCache = [:]
+
+@Field
+def editableColumns = ['financialYear', 'dividendSumRaspredPeriod', 'dividendForgeinOrgAll', 'dividendForgeinPersonalAll',
+        'dividendStavka0', 'dividendStavkaLess5', 'dividendStavkaMore5', 'dividendStavkaMore10',
+        'dividendRussianOrgStavka9', 'dividendRussianOrgStavka0', 'dividendPersonRussia', 'dividendMembersNotRussianTax',
+        'dividendAgentAll', 'dividendAgentWithStavka0', 'taxSum', 'taxSumFromPeriodAll']
+
+// Проверяемые на пустые значения атрибуты
+@Field
+def nonEmptyColumns = ['dividendType', 'taxPeriod', 'financialYear', 'dividendSumRaspredPeriod', 'dividendForgeinOrgAll',
+        'dividendForgeinPersonalAll', 'dividendStavka0', 'dividendStavkaLess5', 'dividendStavkaMore5',
+        'dividendStavkaMore10', 'dividendRussianMembersAll', 'dividendRussianOrgStavka9', 'dividendRussianOrgStavka0',
+        'dividendPersonRussia', 'dividendMembersNotRussianTax', 'dividendAgentAll', 'dividendAgentWithStavka0',
+        'dividendSumForTaxAll', 'dividendSumForTaxStavka9', 'dividendSumForTaxStavka0', 'taxSum', 'taxSumFromPeriod',
+        'taxSumFromPeriodAll']
+
+//// Обертки методов
+
+// Разыменование записи справочника
+def getRefBookValue(def long refBookId, def Long recordId) {
+    return formDataService.getRefBookValue(refBookId, recordId, refBookCache)
 }
 
-void addRow() {
-    row = formData.createDataRow()
-    row.taxPeriod = '34'
-    row.dividendType = '2'
-    for (alias in ['financialYear', 'dividendSumRaspredPeriod', 'dividendForgeinOrgAll', 'dividendForgeinPersonalAll',
-            'dividendStavka0', 'dividendStavkaLess5', 'dividendStavkaMore5', 'dividendStavkaMore10', 'dividendRussianOrgStavka9',
-            'dividendRussianOrgStavka0', 'dividendPersonRussia', 'dividendMembersNotRussianTax', 'dividendAgentAll',
-            'dividendAgentWithStavka0', 'taxSum', 'taxSumFromPeriodAll']) {
-        row.getCell(alias).editable = true
-        row.getCell(alias).setStyleAlias('Редактируемая')
-    }
-    dataRowsHelper.insert(row, dataRowsHelper.getAllCached().size() + 1)
+//// Кастомные методы
 
-}
-
-/**
- * Проверяет уникальность в отчётном периоде и вид
- */
-void checkUniq() {
-    FormData findForm = formDataService.find(formData.formType.id, formData.kind, formData.departmentId, formData.reportPeriodId)
-
-    if (findForm != null) {
-        logger.error('Налоговая форма с заданными параметрами уже существует.')
-    }
-    if (formData.kind != FormDataKind.ADDITIONAL) {
-        logger.error("Нельзя создавать форму с типом ${formData.kind?.name}")
-    }
-}
-
-/**
- * Проверка наличия декларации для текущего department
- */
-void checkDecl() {
-    declarationType = 2;    // Тип декларации которую проверяем(Налог на прибыль)
-    declaration = declarationService.find(declarationType, formData.getDepartmentId(), formData.getReportPeriodId())
-    if (declaration != null && declaration.isAccepted()) {
-        logger.error("Декларация банка находиться в статусе принята")
-    }
-}
-
-void logicalCheck() {
-    reportPeriodPrev = reportPeriodService.getPrevReportPeriod(formData.reportPeriodId)
-
-    formPrev = null
-    if (reportPeriodPrev != null) {
-        formPrev = formDataService.find(formData.formType.id, formData.kind, formData.departmentId, reportPeriodPrev.id)
-    }
-    if (formPrev == null) {
-        logger.warn('Форма за предыдущий отчётный период не создавалась!')
-    }
-}
-
-/**
- * Проверка полей которые обязательно надо заполнить пользователю
- */
-void logicCheck() {
-    def int index = 0
-    for (row in dataRowsHelper.getAllCached()) {
-        index++
-        for (alias in ['financialYear', 'dividendSumRaspredPeriod', 'dividendForgeinOrgAll', 'dividendForgeinPersonalAll',
-                'dividendStavka0', 'dividendStavkaLess5', 'dividendStavkaMore5', 'dividendStavkaMore10',
-                'dividendRussianOrgStavka9', 'dividendRussianOrgStavka0', 'dividendPersonRussia',
-                'dividendMembersNotRussianTax', 'dividendAgentAll', 'dividendAgentWithStavka0', 'taxSum', 'taxSumFromPeriodAll'
-        ]) {
-            if (row.getCell(alias).value == null) {
-                def msg = row.getCell(alias).column.name.replace('%', '%%')
-                logger.error("Строка $index: поле «$msg» не заполнено!")
-            }
-        }
-    }
-}
-
+// Алгоритмы заполнения полей формы
 void calc() {
-    if (!logger.containsLevel(LogLevel.ERROR)) {
-        for (row in dataRowsHelper.getAllCached()) {
+    def dataRowHelper = formDataService.getDataRowHelper(formData)
+    def dataRows = dataRowHelper.getAllCached()
+    if (!dataRows.isEmpty()) {
+
+        for (def row in dataRows) {
+            // графа 1
             row.dividendType = '2'
+            // графа 2
             row.taxPeriod = '34'
-            row.dividendRussianMembersAll = row.dividendSumRaspredPeriod - row.dividendForgeinOrgAll - row.dividendForgeinPersonalAll
-            row.dividendSumForTaxAll = (row.dividendRussianMembersAll ?: 0) - (row.dividendAgentWithStavka0 ?: 0)
-            row.dividendSumForTaxStavka9 = (row.dividendRussianOrgStavka9 ?: 0)
-            row.dividendSumForTaxStavka0 = (row.dividendRussianOrgStavka0 ?: 0)
-
-            // Подсчёт поля 22 Сумма налога, начисленная с дивидендов, выплаченных в предыдущие отчетные (налоговые) периоды
-            def period = reportPeriodService.getPrevReportPeriod(formData.reportPeriodId)
-            def result = 0
-
-            if (period != null) {
-                formPrev = formDataService.find(formData.formType.id, formData.kind, formData.departmentId, period.id)
-                if (formPrev != null) {
-                    for (rowPrev in formDataService.getDataRowHelper(formPrev).getAll()) {
-                        if (rowPrev.financialYear.format('yyyy') == row.financialYear.format('yyyy')) {
-                            result += rowPrev.taxSumFromPeriod + rowPrev.taxSumFromPeriodAll
-                        }
-                    }
-                }
-            }
-            row.taxSumFromPeriod = result
+            // графа 11
+            row.dividendRussianMembersAll = calc11(row)
+            // графа 18
+            row.dividendSumForTaxAll = calc18(row)
+            // графа 19
+            row.dividendSumForTaxStavka9 = calc19(row)
+            // графа 20
+            row.dividendSumForTaxStavka0 = calc20(row)
+            // графа 22
+            row.taxSumFromPeriod = calc22(row)
         }
-    } else {
-        logger.error('Не могу заполнить поля, есть ошибки')
+        dataRowHelper.update(dataRows);
+    }
+}
+
+def BigDecimal calc11(def row) {
+    if (row.dividendSumRaspredPeriod == null || row.dividendForgeinOrgAll == null || row.dividendForgeinPersonalAll == null) {
+        return null
+    }
+    return row.dividendSumRaspredPeriod - row.dividendForgeinOrgAll - row.dividendForgeinPersonalAll
+}
+
+def BigDecimal calc18(def row) {
+    if (row.dividendRussianMembersAll == null || row.dividendAgentWithStavka0 == null) {
+        return null
+    }
+    return (row.dividendRussianMembersAll ?: 0) - (row.dividendAgentWithStavka0 ?: 0)
+}
+
+def BigDecimal calc19(def row) {
+    return row.dividendRussianOrgStavka9
+}
+
+def BigDecimal calc20(def row) {
+    return row.dividendRussianOrgStavka0
+}
+
+def BigDecimal calc22(def row) {
+    // TODO сумма или одна строка? если одна строка - то закоментаренный вариант
+    // [15:10:41] Sariya Mustafina: в прошлом периоде должна быть одна строка
+    // [15:10:55] Sariya Mustafina: но это нигде не проверяется
+    // [15:10:58] Sariya Mustafina: спрошу
+    def result = 0
+    // --
+    formPrev = formDataService.getFormDataPrev(formData, formData.departmentId)
+    if (formPrev != null) {
+        for (rowPrev in formDataService.getDataRowHelper(formPrev).getAll()) {
+            if (rowPrev.financialYear.format('yyyy') == row.financialYear.format('yyyy')) {
+                result += rowPrev.taxSumFromPeriod ?: 0 + rowPrev.taxSumFromPeriodAll ?: 0
+                // return rowPrev.taxSumFromPeriod ?: 0 + rowPrev.taxSumFromPeriodAll ?: 0
+            }
+        }
+    }
+    return result
+    // return 0
+}
+
+def logicCheck() {
+    def dataRowHelper = formDataService.getDataRowHelper(formData)
+    def dataRows = dataRowHelper.getAllCached()
+
+    // Алиасы граф для арифметической проверки
+    def arithmeticCheckAlias = ['dividendRussianMembersAll', 'dividendSumForTaxAll', 'dividendSumForTaxStavka9',
+            'dividendSumForTaxStavka0', 'taxSumFromPeriod']
+    // Для хранения правильных значении и сравнения с имеющимися при арифметических проверках
+    def needValue = [:]
+
+    for (def row in dataRows) {
+        // 1. Проверка на заполнение поля
+        checkNonEmptyColumns(row, row.getIndex(), nonEmptyColumns, logger, true)
+
+        // Арифметические проверки расчета граф 11, 18, 19, 20, 22
+        needValue['dividendRussianMembersAll'] = calc11(row)
+        needValue['dividendSumForTaxAll'] = calc18(row)
+        needValue['dividendSumForTaxStavka9'] = calc19(row)
+        needValue['dividendSumForTaxStavka0'] = calc20(row)
+        needValue['taxSumFromPeriod'] = calc22(row)
+        checkCalc(row, arithmeticCheckAlias, needValue, logger, true)
+
+        // 2. Проверка наличия формы за предыдущий отчётный период
+        if (formDataService.getFormDataPrev(formData, formData.departmentId) == null) {
+            logger.warn('Форма за предыдущий отчётный период не создавалась!')
+        }
     }
 }
