2460bbce22e28f19a2488a11baa5d9a55dc3065a
Vyacheslav Petrov <Vyacheslav.Petrov@aplana.com>
2013-12-11 14:08:01 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-5163 функциональная часть готово, осталось подправить интерфейс

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/testpage/client/TestPageView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/testpage/client/TestPageView.java
@@ -3,13 +3,18 @@ package com.aplana.sbrf.taxaccounting.web.module.testpage.client;
 import com.aplana.gwt.client.*;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.text.shared.Renderer;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.gwtplatform.mvp.client.ViewWithUiHandlers;
+//import com.sun.java.swing.plaf.windows.resources.windows;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -32,12 +37,56 @@ public class TestPageView extends ViewWithUiHandlers<TestPageUiHandlers> impleme
     @UiField
     RefBookTextBox refbookTextBox;
 
-    @UiField
+    @UiField(provided = true)
     MultiListBox mlistbox;
+
+    @UiField
+    Label showResult;
+
 
     @Inject
     public TestPageView(final Binder uiBinder) {
+        List<String> itemList = new ArrayList<String>();
+
+        String s1 = "Пункт 1";
+        String s3 = "Пункт 3";
+        itemList.add("Пункт 0");
+        itemList.add(s1);
+        itemList.add("Пункт 2");
+        itemList.add(s3);
+        itemList.add("Пункт 4");
+        itemList.add("Пункт 5");
+        itemList.add("Пункт 6");
+        itemList.add("Пункт 7");
+
+        List<String> valueList = new ArrayList<String>();
+
+        valueList.add(s1);
+        valueList.add(s3);
+
+        mlistbox = new MultiListBox(new Renderer() {
+            @Override
+            public String render(Object object) {
+                return object.toString();
+            }
+
+            @Override
+            public void render(Object object, Appendable appendable) throws IOException {
+                object.toString();
+            }}, false, true);
+
+         mlistbox.setAvailableValues(itemList);
+         mlistbox.setValue(valueList);
+         mlistbox.addValueChangeHandler( new ValueChangeHandler<List>() {
+             @Override
+             public void onValueChange(ValueChangeEvent<List> event) {
+                 Window.alert("Значение поменялось!");
+             }
+         });
+
+
         initWidget(uiBinder.createAndBindUi(this));
+
         ibl.addClickHandler(new ClickHandler() {
             public void onClick(ClickEvent event) {
                 Window.alert("Привет!");
@@ -66,7 +115,7 @@ public class TestPageView extends ViewWithUiHandlers<TestPageUiHandlers> impleme
 
         refbookTextBox.addButtons(list);
 
-        mlistbox.setRenderer(new Renderer() {
+      /*  mlistbox.setRenderer(new Renderer() {
             @Override
             public String render(Object object) {
                 return object.toString();
@@ -77,36 +126,14 @@ public class TestPageView extends ViewWithUiHandlers<TestPageUiHandlers> impleme
                 object.toString();
             }
         });
+*/
 
+        List<String> getM = (List<String>)mlistbox.getValue();
+        String strCont = "";
+        for (String str : getM)
+            strCont = strCont + " | " + str;
 
-        List<String> itemList = new ArrayList<String>();
-
-        itemList.add("Пункт 0");
-        itemList.add("Пункт 1");
-        itemList.add("Пункт 2");
-        itemList.add("Пункт 3");
-        itemList.add("Пункт 4");
-        itemList.add("Пункт 5");
-        itemList.add("Пункт 6");
-        itemList.add("Пункт 7");
-
-        List<Boolean> valueList = new ArrayList<Boolean>();
-
-        valueList.add(true);
-        valueList.add(false);
-        valueList.add(true);
-        valueList.add(false);
-        valueList.add(true);
-        valueList.add(false);
-        valueList.add(true);
-        valueList.add(false);
-
-        mlistbox.setAvailableValues(itemList);
-        mlistbox.setSelectedValues(valueList);
-
-
-
-
+            showResult.setText(strCont);
 
     }
 
