b2c8d45bf49e221bfa9aced69c4de9f4c3ce6a74
Dmitriy Levykin <DLevykin@aplana.com>
2013-12-16 11:26:34 +0600

    Удалил лишний комментарий TODO к предыдущему коммиту.

--- mdb/src/main/java/com/aplana/sbrf/taxaccounting/mdb/RateMDB.java
+++ mdb/src/main/java/com/aplana/sbrf/taxaccounting/mdb/RateMDB.java
@@ -36,7 +36,6 @@ import java.util.Map;
  *
  * @author Dmitriy Levykin
  */
-// TODO: Levykin: закомментировал для поставки 0.3.3. После поставки раскомментировать.
 @MessageDriven(activationConfig = {
         @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
         @ActivationConfigProperty(propertyName = "destination", propertyValue = "jms/rateQueue")})
