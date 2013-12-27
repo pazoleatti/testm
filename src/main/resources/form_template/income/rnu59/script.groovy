eba5581314a2728fd0a8bee7eacbd830834410c9
Alexander Ivanov <alivanov@aplana.com>
2013-12-27 14:50:21 +0600

    59 и 60 рну. Поменял порядок заполнения доходов/расходов для миграции

--- src/main/resources/form_template/income/rnu59/script.groovy
+++ src/main/resources/form_template/income/rnu59/script.groovy
@@ -795,13 +795,25 @@ def addData(def xml, def fileName) {
             index++
         }
 
-        // графа 9
-        newRow.income = getNumber(getCellValue(row, index, type))
-        index++
+        if (formDataEvent == FormDataEvent.MIGRATION) {
 
-        // графа 10
-        newRow.outcome = getNumber(getCellValue(row, index, type))
-        index++
+            // графа 10
+            newRow.outcome = getNumber(getCellValue(row, index, type))
+            index++
+
+            // графа 9
+            newRow.income = getNumber(getCellValue(row, index, type))
+            index++
+
+        } else {
+            // графа 9
+            newRow.income = getNumber(getCellValue(row, index, type))
+            index++
+
+            // графа 10
+            newRow.outcome = getNumber(getCellValue(row, index, type))
+            index++
+        }
 
         // графа 11
         newRow.rateBR= getNumber(getCellValue(row, index, type))
@@ -841,11 +853,20 @@ def addData(def xml, def fileName) {
             totalRow.salePrice = getNumber(getCellValue(row, 7, type))
         }
 
-        // графа 9
-        totalRow.income = getNumber(getCellValue(row, 8, type))
+        if (formDataEvent == FormDataEvent.MIGRATION) {
 
-        // графа 10
-        totalRow.outcome = getNumber(getCellValue(row, 9, type))
+            // графа 9
+            totalRow.income = getNumber(getCellValue(row, 9, type))
+
+            // графа 10
+            totalRow.outcome = getNumber(getCellValue(row, 8, type))
+        } else {
+            // графа 9
+            totalRow.income = getNumber(getCellValue(row, 8, type))
+
+            // графа 10
+            totalRow.outcome = getNumber(getCellValue(row, 9, type))
+        }
 
         // графа 12
         totalRow.outcome269st = getNumber(getCellValue(row, 11, type))
