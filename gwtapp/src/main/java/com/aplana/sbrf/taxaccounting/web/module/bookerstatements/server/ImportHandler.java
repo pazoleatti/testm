a4d75fd0c4bf1f15032e7f0840464ed11b669bac
Dmitriy Levykin <DLevykin@aplana.com>
2014-01-17 12:43:20 +0600

    Меню приведено в соответствие с постановкой http://conf.aplana.com/pages/viewpage.action?pageId=11382816

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/bookerstatements/server/ImportHandler.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/bookerstatements/server/ImportHandler.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
  * User: ekuvshinov
  */
 @Service
-@PreAuthorize("hasAnyRole('ROLE_OPER', 'ROLE_CONTROL', 'ROLE_CONTROL_UNP', 'ROLE_CONTROL_NS')")
+@PreAuthorize("hasAnyRole('ROLE_CONTROL_UNP', 'ROLE_CONTROL_NS')")
 public class ImportHandler extends AbstractActionHandler<ImportAction, ImportResult> {
     @Autowired
     BookerStatementsService service;
