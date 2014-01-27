6983b734fb12b81aae6cc36cf56056e699084469
Aydar Uldanov <Aydar.Uldanov@aplana.com>
2014-01-27 13:14:57 +0600

    Исправил названия файлов

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/taxformnomination/client/formDestinationsDialog/FormDestinationsUiHandlers.java
@@ -0,0 +1,7 @@
+package com.aplana.sbrf.taxaccounting.web.module.taxformnomination.client.formDestinationsDialog;
+
+import com.gwtplatform.mvp.client.UiHandlers;
+
+public interface FormDestinationsUiHandlers extends UiHandlers {
+	void onConfirm();
+}
