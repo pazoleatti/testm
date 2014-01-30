66f31ae3f918427ae27e4bf0e2f94fe8caa281c1
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2014-01-30 13:10:14 +0600

    исправил обращения к  dataRows.size() в скриптах (http://jira.aplana.com/browse/SBRFACCTAX-5594)

--- src/main/resources/form_template/income/rnu75/script.groovy
+++ src/main/resources/form_template/income/rnu75/script.groovy
@@ -131,7 +131,7 @@ void calc() {
         }
     }
 
-    dataRowHelper.insert(calcTotalRow(dataRows), dataRows.size + 1)
+    dataRowHelper.insert(calcTotalRow(dataRows), dataRows.size() + 1)
 
     dataRowHelper.save(dataRows)
 }
