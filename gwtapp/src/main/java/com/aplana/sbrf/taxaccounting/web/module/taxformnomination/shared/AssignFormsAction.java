4a1e754b82e304148dcd13b0d78b753a21f22aba
Aydar Uldanov <Aydar.Uldanov@aplana.com>
2014-01-30 12:17:11 +0600

    Action+Result+Handler для функционала назначений нф

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/taxformnomination/shared/AssignFormsAction.java
@@ -0,0 +1,56 @@
+package com.aplana.sbrf.taxaccounting.web.module.taxformnomination.shared;
+
+import com.aplana.sbrf.taxaccounting.model.FormDataKind;
+import com.aplana.sbrf.taxaccounting.web.main.api.shared.dispatch.ActionName;
+import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
+
+import java.util.List;
+
+/**
+ * Действие Назначение форм подразделениям
+ * @author auldanov
+ */
+public class AssignFormsAction extends UnsecuredActionImpl<AssignFormsResult> implements ActionName {
+
+    private List<Integer> departments;
+    private FormDataKind formDataKind;
+    private Integer formTypeId;
+    private List<Integer> performers;
+
+    public List<Integer> getDepartments() {
+        return departments;
+    }
+
+    public void setDepartments(List<Integer> departments) {
+        this.departments = departments;
+    }
+
+    public FormDataKind getFormDataKind() {
+        return formDataKind;
+    }
+
+    public void setFormDataKind(FormDataKind formDataKind) {
+        this.formDataKind = formDataKind;
+    }
+
+    public Integer getFormTypeId() {
+        return formTypeId;
+    }
+
+    public void setFormTypeId(Integer formTypeId) {
+        this.formTypeId = formTypeId;
+    }
+
+    public List<Integer> getPerformers() {
+        return performers;
+    }
+
+    public void setPerformers(List<Integer> performers) {
+        this.performers = performers;
+    }
+
+    @Override
+    public String getName() {
+        return "Назначение форм подразделениям";
+    }
+}
