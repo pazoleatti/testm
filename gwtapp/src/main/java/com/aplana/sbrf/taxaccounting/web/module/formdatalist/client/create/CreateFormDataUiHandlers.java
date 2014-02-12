3b519e96531ff287b10cbe5193f850f14c848063
Fail Mukhametdinov <Fail.Muhametdinov@aplana.com>
2014-02-12 19:06:17 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-5796, http://jira.aplana.com/browse/SBRFACCTAX-5787, http://jira.aplana.com/browse/SBRFACCTAX-5788 Исправил

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdatalist/client/create/CreateFormDataUiHandlers.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdatalist/client/create/CreateFormDataUiHandlers.java
@@ -4,8 +4,9 @@ package com.aplana.sbrf.taxaccounting.web.module.formdatalist.client.create;
 import com.gwtplatform.mvp.client.UiHandlers;
 
 public interface CreateFormDataUiHandlers extends UiHandlers {
-	void onConfirm();
+    void onConfirm();
+
     void onReportPeriodChange();
-    void onDepartmentChange();
+
     void onFormKindChange();
 }
