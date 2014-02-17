aeb77b1a69234ffe6b94e602a05296bf78b79921
Alexander Vanteev <avanteev@aplana.com>
2014-02-17 18:13:57 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-5780 : Добавил логгирование взаимодействия с СУДИР в части подразделений.

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/sudir/ws/departmentendpoint/TaxAccountingSupportPortType.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/sudir/ws/departmentendpoint/TaxAccountingSupportPortType.java
@@ -1,6 +1,11 @@
 package com.aplana.sbrf.taxaccounting.web.module.sudir.ws.departmentendpoint;
 
+import com.aplana.sbrf.taxaccounting.model.FormDataEvent;
+import com.aplana.sbrf.taxaccounting.model.TAUser;
+import com.aplana.sbrf.taxaccounting.model.TAUserInfo;
+import com.aplana.sbrf.taxaccounting.service.AuditService;
 import com.aplana.sbrf.taxaccounting.service.DepartmentService;
+import com.aplana.sbrf.taxaccounting.service.TAUserService;
 import com.aplana.sbrf.taxaccounting.web.module.sudir.ws.assembler.GenericAccountInfoAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
@@ -18,11 +23,28 @@ public class TaxAccountingSupportPortType extends SpringBeanAutowiringSupport{
 
 	@Autowired
 	private DepartmentService departmentService;
+
+    @Autowired
+    private AuditService auditService;
+
+    @Autowired
+    private TAUserService userService;
 	
 	private GenericAccountInfoAssembler gais = new GenericAccountInfoAssembler(); 
 	
 	public List<TaxAccDepartment> getDepartments() {
-		return gais.desassembleDepartments(departmentService.getDepartmentForSudir());
+        List<TaxAccDepartment> taxAccDepartmentList = gais.desassembleDepartments(departmentService.getDepartmentForSudir());
+        TAUserInfo userInfo = getUserInfo();
+        auditService.add(FormDataEvent.EXTERNAL_INTERACTION, userInfo, userInfo.getUser().getDepartmentId(), null, null, null, null,
+                "Успешный обмен данными с вебсервисом СУДИР.");
+		return taxAccDepartmentList;
 	}
+
+    private TAUserInfo getUserInfo(){
+        TAUserInfo userInfo = new TAUserInfo();
+        userInfo.setIp("127.0.0.1");
+        userInfo.setUser(userService.getUser(TAUser.SYSTEM_USER_ID));
+        return userInfo;
+    }
 
 }
