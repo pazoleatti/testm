dadc5a60b8c6f5b8477de0493e772cc3868b0156
Fail Mukhametdinov <Fail.Muhametdinov@aplana.com>
2014-02-18 16:08:18 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-5898, http://jira.aplana.com/browse/SBRFACCTAX-5867 Добавил очистку полей при повторном использовании налоговой формы и убрал лишнее обращние к серверу

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdatalist/client/create/CreateFormDataView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdatalist/client/create/CreateFormDataView.java
@@ -1,11 +1,9 @@
 package com.aplana.sbrf.taxaccounting.web.module.formdatalist.client.create;
 
-import com.aplana.gwt.client.ListBoxWithTooltipWidget;
 import com.aplana.gwt.client.dialog.Dialog;
 import com.aplana.gwt.client.dialog.DialogHandler;
 import com.aplana.sbrf.taxaccounting.model.Department;
 import com.aplana.sbrf.taxaccounting.model.FormDataFilter;
-import com.aplana.sbrf.taxaccounting.model.FormType;
 import com.aplana.sbrf.taxaccounting.model.ReportPeriod;
 import com.aplana.sbrf.taxaccounting.web.widget.departmentpicker.DepartmentPickerPopupWidget;
 import com.aplana.sbrf.taxaccounting.web.widget.periodpicker.client.PeriodPickerPopupWidget;
@@ -14,7 +12,6 @@ import com.google.gwt.editor.client.Editor;
 import com.google.gwt.editor.client.SimpleBeanEditorDriver;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.text.shared.AbstractRenderer;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
@@ -55,8 +52,6 @@ public class CreateFormDataView extends PopupViewWithUiHandlers<CreateFormDataUi
 
     @UiField
     Button cancelButton;
-
-    private Map<Integer, String> formTypesMap = new LinkedHashMap<Integer, String>();
 
     @Inject
     public CreateFormDataView(Binder uiBinder, final MyDriver driver, EventBus eventBus) {
@@ -99,8 +94,10 @@ public class CreateFormDataView extends PopupViewWithUiHandlers<CreateFormDataUi
         // Сброс состояния формы
         reportPeriodIds.setValue(null);
         departmentPicker.setValue(null);
-        formDataKind.setValue(null, true);
-        formTypeId.setValue(null, true);
+        formDataKind.setValue(new ArrayList<Long>(), true);
+        formDataKind.setDereferenceValue(null);
+        formTypeId.setValue(new ArrayList<Long>(), true);
+        formTypeId.setDereferenceValue(null);
         updateEnabled();
     }
 
@@ -110,9 +107,9 @@ public class CreateFormDataView extends PopupViewWithUiHandlers<CreateFormDataUi
         // "Тип налоговой формы" недоступен если не выбрано подразделение
         formDataKind.setEnabled(departmentPicker.getValue() != null && !departmentPicker.getValue().isEmpty());
         // "Вид налоговой формы" недоступен если не выбран тип НФ
-        formTypeId.setEnabled(formDataKind.getValue() != null && !formDataKind.getValue().isEmpty());
+        formTypeId.setEnabled(formDataKind.getValue().size() != 0);
         // Кнопка "Создать" недоступна пока все не заполнено
-        continueButton.setEnabled(formTypeId.getValue() != null);
+        continueButton.setEnabled(formTypeId.getValue().size() != 0);
     }
 
 
@@ -124,18 +121,17 @@ public class CreateFormDataView extends PopupViewWithUiHandlers<CreateFormDataUi
 
     @UiHandler("departmentPicker")
     public void onDepartmentChange(ValueChangeEvent<List<Integer>> event) {
-        formDataKind.setValue(null, true);
+        formDataKind.setValue(new ArrayList<Long>(), true);
         formDataKind.setDereferenceValue(null);
-        formTypeId.setValue(null, true);
+        formTypeId.setValue(new ArrayList<Long>(), true);
         formTypeId.setDereferenceValue(null);
         updateEnabled();
     }
 
     @UiHandler("formDataKind")
     public void onDataKindChange(ValueChangeEvent<List<Long>> event) {
-        formTypeId.setValue(null, true);
+        formTypeId.setValue(new ArrayList<Long>(), true);
         formTypeId.setDereferenceValue(null);
-        getUiHandlers().onFormKindChange();
         updateEnabled();
     }
 
@@ -169,17 +165,6 @@ public class CreateFormDataView extends PopupViewWithUiHandlers<CreateFormDataUi
     public void setAcceptableDepartments(List<Department> list, Set<Integer> availableValues) {
         departmentPicker.setAvalibleValues(list, availableValues);
     }
-
-    @Override
-    public void setAcceptableFormTypeList(List<FormType> formTypes) {
-        formTypesMap.clear();
-        for (FormType formType : formTypes) {
-            formTypesMap.put(formType.getId(), formType.getName());
-        }
-
-        formTypeId.setValue(null, true);
-    }
-
 
     @Override
     public void setFilterData(FormDataFilter filter) {
