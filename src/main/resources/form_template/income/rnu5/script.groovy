0b9f00c7a205a72e5b24edbb458f23cfd2f3cbf8
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2014-02-21 15:10:54 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-5895 РНУ-5. Баги бизнес-логики

--- src/main/resources/form_template/income/rnu5/script.groovy
+++ src/main/resources/form_template/income/rnu5/script.groovy
@@ -1,7 +1,9 @@
 package form_template.income.rnu5
 
+import com.aplana.sbrf.taxaccounting.model.DataRow
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
 import com.aplana.sbrf.taxaccounting.model.FormDataKind
+import com.aplana.sbrf.taxaccounting.model.WorkflowState
 import com.aplana.sbrf.taxaccounting.model.exception.ServiceException
 import groovy.transform.Field
 
@@ -39,7 +41,7 @@ switch (formDataEvent) {
         logicCheck()
         break
     case FormDataEvent.COMPOSE: // Консолидация
-        formDataService.consolidationSimple(formData, formDataDepartment.id, logger)
+        consolidation()
         calc()
         logicCheck()
         break
@@ -229,4 +231,42 @@ def getNewRow(def alias, def sum) {
 
 def String getKnu(def code) {
     return getRefBookValue(27, code)?.CODE?.stringValue
+}
+
+/**
+ * Консолидация.
+ */
+void consolidation() {
+    def dataRowHelper = formDataService.getDataRowHelper(formData)
+    def dataRows = []
+
+    departmentFormTypeService.getFormSources(formDataDepartment.id, formData.getFormType().getId(), formData.getKind()).each {
+        if (it.formTypeId == formData.getFormType().getId()) {
+            def source = formDataService.find(it.formTypeId, it.kind, it.departmentId, formData.reportPeriodId)
+            if (source != null && source.state == WorkflowState.ACCEPTED) {
+                formDataService.getDataRowHelper(source).allCached.each { sRow ->
+                    if (sRow.getAlias() == null || sRow.getAlias() == '') {
+                        def isFind = false
+                        // строки приемника - искать совпадения, если совпадения есть, то суммировать графу 5
+                        for (def row : dataRows) {
+                            if (sRow.number == row.number) {
+                                isFind = true
+                                totalColumns.each { alias ->
+                                    def tmp = (row.getCell(alias).value ?: 0) + (sRow.getCell(alias).value ?: 0)
+                                    row.getCell(alias).setValue(tmp, null)
+                                }
+                                break
+                            }
+                        }
+                        // если совпадений нет, то просто добавить строку
+                        if (!isFind) {
+                            dataRows.add(sRow)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    dataRowHelper.save(dataRows)
+    logger.info('Формирование консолидированной формы прошло успешно.')
 }
