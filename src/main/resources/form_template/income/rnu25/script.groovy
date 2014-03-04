b5c3fd0c44626bb866d866ba3e09f9b12f35f82e
Ramil Timerbaev <Rtimerbaev@aplana.com>
2014-03-04 17:57:48 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-6123 - РНУ-25.При расчете выводится некорректные сообщения об ошибке.

--- src/main/resources/form_template/income/rnu25/script.groovy
+++ src/main/resources/form_template/income/rnu25/script.groovy
@@ -215,10 +215,7 @@ void calc() {
         return
     }
 
-    // обновить индексы строк
-    dataRows.eachWithIndex { row, i ->
-        row.setIndex(i + 1)
-    }
+    updateIndexes(dataRows)
     // итоговые значения по ГРН
     def i = 0
     for (def codeName : totalGroupsName) {
@@ -234,6 +231,7 @@ void calc() {
         dataRows.add(lastRowIndex, subTotalRow)
         i++
     }
+    updateIndexes(dataRows)
     dataRowHelper.save(dataRows)
 }
 
@@ -816,4 +814,11 @@ def getReportPeriodEndDate() {
         endDate = reportPeriodService.getEndDate(formData.reportPeriodId).time
     }
     return endDate
+}
+
+// обновить индексы строк
+def updateIndexes(def dataRows) {
+    dataRows.eachWithIndex { row, i ->
+        row.setIndex(i + 1)
+    }
 }
