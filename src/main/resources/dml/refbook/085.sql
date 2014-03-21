7a83f32005de9b584663b38adf049ea508352b9b
Marat Fayzullin <mfayzullin@aplana.com>
2014-03-21 11:59:47 +0600

    Поправил оформление скриптов для заполнения справочников

--- src/main/resources/dml/refbook/085.sql
+++ src/main/resources/dml/refbook/085.sql
@@ -1,8 +1,12 @@
---Справочник "Виды срочных сделок"
 /*
-1;Своп
-2;DOP DM
-3;DF PM
+Справочник:
+	Виды срочных сделок
+Список изменений:
+	Первоначальное заполнение данными
+Данные:	
+	1;Своп
+	2;DOP DM
+	3;DF PM
 */
 INSERT INTO ref_book_record(id, record_id, ref_book_id, version, status) VALUES
   (seq_ref_book_record.nextval, 1, 85, to_date('01.01.2012','dd.mm.yyyy'), 0);
@@ -24,3 +28,6 @@ INSERT INTO ref_book_value(record_id, attribute_id, string_value, number_value, 
 (seq_ref_book_record.currval, 818, null, 3, null, null);
 INSERT INTO ref_book_value(record_id, attribute_id, string_value, number_value, date_value, reference_value) VALUES
 (seq_ref_book_record.currval, 819, 'DF PM', null, null, null);
+
+commit;
+exit;
--- src/main/resources/dml/refbook/087.sql
+++ src/main/resources/dml/refbook/087.sql
@@ -1,9 +1,13 @@
---Справочник "Виды операций"
 /*
-Закрытие
-Открытие
-Погашение
-Продажа
+Справочник:
+	Виды операций
+Список изменений:
+	Первоначальное заполнение данными
+Данные:	
+	Закрытие
+	Открытие
+	Погашение
+	Продажа
 */
 INSERT INTO ref_book_record(id, record_id, ref_book_id, version, status) VALUES
   (seq_ref_book_record.nextval, 1, 87, to_date('01.01.2012','dd.mm.yyyy'), 0);
@@ -24,3 +28,6 @@ INSERT INTO ref_book_record(id, record_id, ref_book_id, version, status) VALUES
 (seq_ref_book_record.nextval, 4, 87, to_date('01.01.2012','dd.mm.yyyy'), 0);
 INSERT INTO ref_book_value(record_id, attribute_id, string_value, number_value, date_value, reference_value) VALUES
 (seq_ref_book_record.currval, 824, 'Продажа', null, null, null);
+
+commit;
+exit;
