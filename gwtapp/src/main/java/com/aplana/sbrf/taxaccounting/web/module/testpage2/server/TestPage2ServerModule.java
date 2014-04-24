a685ed6a60cf2147ce5cb7c6eb1df7385ca949cd
Ramil Timerbaev <Rtimerbaev@aplana.com>
2014-04-24 13:48:36 +0600

    Убрал форму для тестов testpage2

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/testpage2/server/TestPage2ServerModule.java
+++
@@ -1,9 +0,0 @@
-package com.aplana.sbrf.taxaccounting.web.module.testpage2.server;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackageClasses = TestPage2ServerModule.class)
-public class TestPage2ServerModule {
-}
