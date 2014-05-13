3d7d1373bf15ab6b89af361a759e4114dc6948a4
Dmitriy Levykin <DLevykin@aplana.com>
2014-05-13 11:49:11 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-7208 Заготовки скриптов для справочников

---
+++ src/main/resources/refbook/bond/script.groovy
@@ -0,0 +1,15 @@
+package refbook.bond
+
+import com.aplana.sbrf.taxaccounting.model.FormDataEvent
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttributeType
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookValue
+
+/**
+ * Cкрипт справочника «Ценные бумаги» (id = 84)
+ * Diasoft
+ * blob_data.id = ?
+ */
+switch (formDataEvent) {
+    case FormDataEvent.IMPORT:
+        break
+}
---
+++ src/main/resources/refbook/emitent/script.groovy
@@ -0,0 +1,15 @@
+package refbook.emitent
+
+import com.aplana.sbrf.taxaccounting.model.FormDataEvent
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttributeType
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookValue
+
+/**
+ * Cкрипт справочника «Эмитенты» (id = ?)
+ * Diasoft
+ * blob_data.id = ?
+ */
+switch (formDataEvent) {
+    case FormDataEvent.IMPORT:
+        break
+}
