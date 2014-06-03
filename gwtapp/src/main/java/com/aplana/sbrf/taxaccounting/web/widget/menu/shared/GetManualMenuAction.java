bf98bf3e3a5bb611fb7da8b8e6ef8946468062cb
Fail Mukhametdinov <Fail.Muhametdinov@aplana.com>
2014-06-03 15:39:52 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-7404 0.3.8 Изменения главного меню - отдельный пункт для руководства пользователя Настройщика

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/menu/shared/GetManualMenuAction.java
@@ -0,0 +1,11 @@
+package com.aplana.sbrf.taxaccounting.web.widget.menu.shared;
+
+import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
+
+/**
+ * Получить меню "Руководство пользователя"
+ *
+ * @author Fail Mukhametdinov
+ */
+public class GetManualMenuAction extends UnsecuredActionImpl<GetManualMenuResult> {
+}
