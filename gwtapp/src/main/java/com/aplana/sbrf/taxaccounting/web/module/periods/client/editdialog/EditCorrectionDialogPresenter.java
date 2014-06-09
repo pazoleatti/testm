92dad5b46790b4f51cd3926250a8aa470b47ef63
Eugene Stetsenko <estetsenko@aplana.com>
2014-06-09 12:26:48 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-7656

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/periods/client/editdialog/EditCorrectionDialogPresenter.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/periods/client/editdialog/EditCorrectionDialogPresenter.java
@@ -4,6 +4,7 @@ import com.aplana.gwt.client.dialog.Dialog;
 import com.aplana.sbrf.taxaccounting.model.*;
 import com.aplana.sbrf.taxaccounting.web.main.api.client.dispatch.AbstractCallback;
 import com.aplana.sbrf.taxaccounting.web.main.api.client.dispatch.CallbackUtils;
+import com.aplana.sbrf.taxaccounting.web.main.api.client.event.log.LogAddEvent;
 import com.aplana.sbrf.taxaccounting.web.module.periods.client.event.UpdateForm;
 import com.aplana.sbrf.taxaccounting.web.module.periods.shared.*;
 import com.google.inject.Inject;
@@ -60,10 +61,10 @@ public class EditCorrectionDialogPresenter extends PresenterWidget<EditCorrectio
         if ((data.getCorrectionDate() == null)
                 || (data.getReportPeriodId() == null)
                 || (data.getCorrectionDate() == null)) {
-            Dialog.errorMessage("Редактирование параметров", "Не заполнены следующие обязательные к заполнению поля: "
-                            + ((data.getDepartmentId() == null) ? "Подразделение " : "")
-                            + ((data.getReportPeriodId() == null) ? " Период корректировки " : "")
-                            + ((data.getCorrectionDate() == null) ? "Период сдачи корректировки " : "")
+            Dialog.errorMessage("Редактирование параметров", "Не заполнены следующие обязательные к заполнению поля:"
+                            + ((data.getDepartmentId() == null) ? " Подразделение" : "")
+                            + ((data.getReportPeriodId() == null) ? " Период корректировки" : "")
+                            + ((data.getCorrectionDate() == null) ? " Период сдачи корректировки" : "")
 
                             + "!"
             );
@@ -72,7 +73,7 @@ public class EditCorrectionDialogPresenter extends PresenterWidget<EditCorrectio
 
         if (data.getDepartmentId().equals(initData.getDepartmentId())
                 && data.getCorrectionDate().equals(initData.getCorrectionDate())
-                && data.getCorrectionReportPeriods().get(0).equals(initData.getCorrectionReportPeriods().get(0))) {
+                && data.getReportPeriodId().equals(initData.getCorrectionReportPeriods().get(0).getId())) {
             Dialog.errorMessage("Редактирование параметров", "Ни один параметр не был изменен!");
             return;
         }
@@ -95,13 +96,7 @@ public class EditCorrectionDialogPresenter extends PresenterWidget<EditCorrectio
     }
 
     private void edit(final EditDialogData data) {
-        EditPeriodAction action = new EditPeriodAction();
-        action.setTaxType(taxType);
-        action.setCorrectionDate(initData.getCorrectionDate());
-        action.setNewCorrectionDate(data.getCorrectionDate());
-        action.setDepartmentId(data.getDepartmentId());
-        action.setNewReportPeriodId(data.getReportPeriodId().intValue());
-        action.setReportPeriodId(initData.getReportPeriodId().intValue());
+
         CheckCorrectionPeriodStatusAction checkCorrectionPeriodStatusAction = new CheckCorrectionPeriodStatusAction();
         checkCorrectionPeriodStatusAction.setSelectedDepartments(Arrays.asList(data.getDepartmentId()));
         checkCorrectionPeriodStatusAction.setReportPeriodId(data.getReportPeriodId().intValue());
@@ -120,7 +115,24 @@ public class EditCorrectionDialogPresenter extends PresenterWidget<EditCorrectio
                             || (result.getStatus() == PeriodStatusBeforeOpen.CLOSE)) {
                         Dialog.errorMessage("Указанный период уже заведён в Системе!");
                     } else {
-                        UpdateForm.fire(EditCorrectionDialogPresenter.this);
+                        EditPeriodAction action = new EditPeriodAction();
+                        action.setTaxType(taxType);
+                        action.setCorrectionDate(initData.getCorrectionDate());
+                        action.setNewCorrectionDate(data.getCorrectionDate());
+                        action.setDepartmentId(data.getDepartmentId());
+                        action.setNewReportPeriodId(data.getReportPeriodId().intValue());
+                        action.setReportPeriodId(initData.getReportPeriodId().intValue());
+                        dispatcher.execute(action, CallbackUtils
+                                        .defaultCallback(new AbstractCallback<EditPeriodResult>() {
+                                            @Override
+                                            public void onSuccess(EditPeriodResult result) {
+                                                LogAddEvent.fire(EditCorrectionDialogPresenter.this, result.getUuid());
+                                                getView().hide();
+                                                UpdateForm.fire(EditCorrectionDialogPresenter.this);
+                                            }
+
+                                        }, EditCorrectionDialogPresenter.this)
+                        );
                     }
                 }
 
