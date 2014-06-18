d0849a6d32a5fa6f49cd6ba79ff6f1b522c0e1ac
Dmitriy Levykin <DLevykin@aplana.com>
2014-06-18 16:06:09 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-7813 События IMPORT_TRANSPORT_FILE и UPLOAD_TRANSPORT_FILE

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/FormDataEvent.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/FormDataEvent.java
@@ -52,7 +52,8 @@ public enum FormDataEvent implements Serializable {
 	DELETE_ROW(303, "Удалить строку"),
 	AFTER_LOAD(302, "Загрузка"),	/* Вызывается после того как форма была получена из БД. */
 
-	IMPORT_TRANSPORT_FILE(401, "Загрузка ТФ"),
+	IMPORT_TRANSPORT_FILE(401, "Импорт из транспортных файлов"),
+    UPLOAD_TRANSPORT_FILE(402, "Загрузка транспортных файлов в каталог загрузки"),
 
     LOGIN(501, "Вход пользователя в Систему"),
     LOGOUT(502, "Выход пользователя из Системы"),
