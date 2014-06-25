6d679b8a99eb529f544965b551d5af47d7b557fb
Marat Fayzullin <mfayzullin@aplana.com>
2014-06-25 15:24:10 +0600

    Исправил сообщение при разборе фильтра. "System msg = null"

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/filter/Filter.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/filter/Filter.java
@@ -37,7 +37,8 @@ public class Filter
                                 int line, int charPositionInLine,
                                 String msg,
                                 RecognitionException e){
-            throw new IllegalArgumentException("Ошибка в строке фильтра. (System msg = "+e.getMessage()+" )");
+
+            throw new IllegalArgumentException("Ошибка в строке фильтра.", e);
         }
     }
 
