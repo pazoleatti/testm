b2b34de573289cf2c5164e53e193ce1d612c5375
Eugene Stetsenko <estetsenko@aplana.com>
2014-06-27 14:27:51 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-7944

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/taxformnomination/client/TaxFormNominationPresenter.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/taxformnomination/client/TaxFormNominationPresenter.java
@@ -87,6 +87,8 @@ public class TaxFormNominationPresenter
         void onReveal();
 
         void clearFilter();
+
+        void clearFormFilter();
     }
 
     private TaxType taxType;
@@ -120,7 +122,7 @@ public class TaxFormNominationPresenter
         super.prepareFromRequest(request);
         LogCleanEvent.fire(this);
         LogShowEvent.fire(this, false);
-
+        getView().clearFormFilter();
         dispatcher.execute(new GetOpenDataAction(),
                 CallbackUtils.defaultCallback(
                         new AbstractCallback<GetOpenDataResult>() {
