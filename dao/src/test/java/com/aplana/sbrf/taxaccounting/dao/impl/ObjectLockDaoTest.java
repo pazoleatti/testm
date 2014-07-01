d2f4d944bb82ddcac35d40cc7f4de34d90fb975e
Eugene Stetsenko <estetsenko@aplana.com>
2014-07-01 13:55:33 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-7961

--- dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/ObjectLockDaoTest.java
+++ dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/ObjectLockDaoTest.java
@@ -174,13 +174,4 @@ public class ObjectLockDaoTest {
 	public void testtestRefreshLockLockedByDifferentUserButTimedOut() {
 		objectLockDao.refreshLock(35L, FormData.class, 1);
 	}
-
-    @Test
-    public void unlockIfOlderThan() {
-        objectLockDao.lockObject(5L, FormData.class, 1);
-        assertTrue(objectLockDao.isLockedByUser(5L, FormData.class, 1));
-        objectLockDao.unlockIfOlderThan(10);
-        assertFalse(objectLockDao.isLockedByUser(5L, FormData.class, 1));
-    }
-
 }
