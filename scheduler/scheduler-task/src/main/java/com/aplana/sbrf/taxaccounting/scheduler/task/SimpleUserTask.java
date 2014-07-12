932f36d42927c2f1184ab212131c495cca0680ba
Aydar Uldanov <Aydar.Uldanov@aplana.com>
2014-07-12 14:13:33 +0600

    Теперь задача на себя берет управление своими параметрами, включил метод в интерфейс UserTask

--- scheduler/scheduler-task/src/main/java/com/aplana/sbrf/taxaccounting/scheduler/task/SimpleUserTask.java
+++ scheduler/scheduler-task/src/main/java/com/aplana/sbrf/taxaccounting/scheduler/task/SimpleUserTask.java
@@ -1,5 +1,7 @@
 package com.aplana.sbrf.taxaccounting.scheduler.task;
 
+import com.aplana.sbrf.taxaccounting.scheduler.api.entity.TaskParamType;
+import com.aplana.sbrf.taxaccounting.scheduler.api.form.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import com.aplana.sbrf.taxaccounting.scheduler.api.entity.TaskParam;
@@ -12,7 +14,9 @@ import com.aplana.sbrf.taxaccounting.scheduler.api.task.UserTaskRemote;
 import javax.ejb.Local;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Local(UserTaskLocal.class)
@@ -43,5 +47,31 @@ public class SimpleUserTask implements UserTask {
     @Override
     public String getTaskClassName() {
         return SimpleUserTask.class.getSimpleName();
+    }
+
+    @Override
+    public List<FormElement> getParams() {
+        List<FormElement> params = new ArrayList<FormElement>();
+        FormElement element1 = new TextBox();
+        element1.setName("Имя");
+        element1.setType(TaskParamType.STRING);
+        params.add(element1);
+
+        // значение дата
+        FormElement element2 = new DateBox();
+        element2.setName("Дата");
+        element2.setType(TaskParamType.DATE);
+        params.add(element2);
+
+        // значение класс
+        SelectBox element3 = new SelectBox();
+        element3.setName("Какое то число");
+        element3.setType(TaskParamType.INT);
+        List<SelectBoxItem> selectBoxValues = new ArrayList<SelectBoxItem>();
+        selectBoxValues.add(new SelectBoxItem("ОДин", 1));
+        selectBoxValues.add(new SelectBoxItem("Два", 2));
+        element3.setValues(selectBoxValues);
+
+        return params;
     }
 }
