9ff79566ded1e21d91dcca57c577afee47ab0f5b
Aydar Uldanov <Aydar.Uldanov@aplana.com>
2014-07-12 14:16:24 +0600

    Добавил методы equals, hashCode и toString для моделу

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/TaskParamModel.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/TaskParamModel.java
@@ -61,4 +61,42 @@ public class TaskParamModel implements Serializable {
     public void setTaskParamDateValue(Date taskParamDateValue) {
         this.taskParamDateValue = taskParamDateValue;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TaskParamModel model = (TaskParamModel) o;
+
+        if (taskParamDateValue != null ? !taskParamDateValue.equals(model.taskParamDateValue) : model.taskParamDateValue != null)
+            return false;
+        if (taskParamName != null ? !taskParamName.equals(model.taskParamName) : model.taskParamName != null)
+            return false;
+        if (taskParamType != null ? !taskParamType.equals(model.taskParamType) : model.taskParamType != null)
+            return false;
+        if (taskParamValue != null ? !taskParamValue.equals(model.taskParamValue) : model.taskParamValue != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = taskParamName != null ? taskParamName.hashCode() : 0;
+        result = 31 * result + (taskParamType != null ? taskParamType.hashCode() : 0);
+        result = 31 * result + (taskParamValue != null ? taskParamValue.hashCode() : 0);
+        result = 31 * result + (taskParamDateValue != null ? taskParamDateValue.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskParamModel{" +
+                "taskParamName='" + taskParamName + '\'' +
+                ", taskParamType=" + taskParamType +
+                ", taskParamValue='" + taskParamValue + '\'' +
+                ", taskParamDateValue=" + taskParamDateValue +
+                '}';
+    }
 }
