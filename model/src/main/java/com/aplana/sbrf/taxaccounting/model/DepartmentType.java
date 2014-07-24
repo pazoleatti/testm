ce8314891b7902920a981e307f77c3a73cb72b84
Marat Fayzullin <mfayzullin@aplana.com>
2014-07-24 14:54:25 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-8212 Лишний атрибут в графе "Тип подразделения" справочника "Подразделения"

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/DepartmentType.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/DepartmentType.java
@@ -15,7 +15,7 @@ public enum DepartmentType implements Serializable {
 	/** 4 Управление - MANAGEMENT*/
     MANAGEMENT("Управление", 4),
 	/** 5 INTERNAL - не передается в СУДИР */
-    INTERNAL("Не передается в СУДИР", 5);
+    INTERNAL("", 5);
 
 	private static final long serialVersionUID = 1L;	
 
