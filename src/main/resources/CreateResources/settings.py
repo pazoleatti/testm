3fc6554b4aa37c0b6ef4234d01dcf9541a21f3b8
unknown <evasilev@evasilyev-pc.datateh.ru>
2014-08-04 16:00:46 +0600

    Update script

---
+++ src/main/resources/CreateResources/settings.py
@@ -0,0 +1,28 @@
+# prefix for resources
+suffixForResources          = '-0.4'
+
+# database info
+jassUserId                  = 'TAX_0_4'
+jassUserPass                = 'TAX'
+dataBaseHost                = 'nalog-db.aplana.local'
+dataBasePort                = '1521'
+dataBaseSvcName             = 'orcl.aplana.local'
+
+migrationJassUserId         = 'MIGRATION'
+migrationJassUserPass       = 'TAX'
+migrationDataBaseHost       = 'nalog-db.aplana.local'
+migrationDataBasePort       = '1521'
+migrationDataBaseSvcName    = 'orcl.aplana.local'
+
+jdbcDriverPath   			= 'C:/IBM_TEST/ojdbc6.jar'
+
+# MQ server info
+MQServerHost                = 'nalog-web.aplana.local'
+MQServerPort                = '1414'
+MQServerChanel              = 'Q_CH'
+MQServerQueueName           = 'Q_MDB_0.4'
+MQServerQueueManager        = 'QM_MDB'
+
+# server info
+serverName					= 'server1'
+
