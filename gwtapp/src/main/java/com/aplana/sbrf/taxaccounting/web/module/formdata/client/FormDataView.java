2eb8d486cbe0d24be8c6de613650c2cabbe9d611
Eugene Stetsenko <estetsenko@aplana.com>
2014-09-03 14:56:49 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-8719

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdata/client/FormDataView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formdata/client/FormDataView.java
@@ -712,9 +712,8 @@ public class FormDataView extends ViewWithUiHandlers<FormDataUiHandlers>
             this.taxType = taxType;
             pager.setType("formData" + taxType.getCode());
             formDataTable.setPageSize(pager.getPageSize());
-        } else {
-            updateData(0);
         }
+        updateData(0);
     }
 
     @Override
