f033e41150a023af25baaf575f385d9d3eb67d29
Denis Loshkarev <Denis.Loshkarev@aplana.com>
2014-09-10 12:26:40 +0400

    http://jira.aplana.com/browse/SBRFACCTAX-8486 добавил задачу планировщика для очистки BLOB_DATA

---
+++ scheduler/scheduler-task/src/main/java/com/aplana/sbrf/taxaccounting/scheduler/task/CleanBlobDataTask.java
@@ -0,0 +1,57 @@
+package com.aplana.sbrf.taxaccounting.scheduler.task;
+
+import com.aplana.sbrf.taxaccounting.model.TAUserInfo;
+import com.aplana.sbrf.taxaccounting.scheduler.api.entity.TaskParam;
+import com.aplana.sbrf.taxaccounting.scheduler.api.exception.TaskExecutionException;
+import com.aplana.sbrf.taxaccounting.scheduler.api.form.FormElement;
+import com.aplana.sbrf.taxaccounting.scheduler.api.task.UserTask;
+import com.aplana.sbrf.taxaccounting.scheduler.api.task.UserTaskLocal;
+import com.aplana.sbrf.taxaccounting.scheduler.api.task.UserTaskRemote;
+import com.aplana.sbrf.taxaccounting.service.BlobDataService;
+import com.aplana.sbrf.taxaccounting.service.PropertyLoader;
+import com.aplana.sbrf.taxaccounting.service.SchedulerInterceptor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.interceptor.Interceptors;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Задача для очистки BLOB_DATA по расписанию
+ * @author dloshkarev
+ */
+@Local(UserTaskLocal.class)
+@Remote(UserTaskRemote.class)
+@Stateless
+@Interceptors(SchedulerInterceptor.class)
+public class CleanBlobDataTask implements UserTask {
+    private final Log LOG = LogFactory.getLog(getClass());
+
+    @Autowired
+    BlobDataService blobDataService;
+
+    @Override
+    public void execute(Map<String, TaskParam> params, int userId) throws TaskExecutionException {
+        blobDataService.clean();
+    }
+
+    @Override
+    public String getTaskName() {
+        return "Очистка файлового хранилища "+ PropertyLoader.getVersion();
+    }
+
+    @Override
+    public String getTaskClassName() {
+        return CleanBlobDataTask.class.getSimpleName();
+    }
+
+    @Override
+    public List<FormElement> getParams(TAUserInfo userInfo) {
+        return null;
+    }
+}
