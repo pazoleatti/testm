626190fcca32342e96649d35b8573d60bdcf5f49
Dmitriy Levykin <DLevykin@aplana.com>
2014-09-23 11:37:23 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-8818 Корректирующие периоды в ядре. Изменения в скриптах.

--- src/main/resources/form_template/income/rnu46/v2012/script.groovy
+++ src/main/resources/form_template/income/rnu46/v2012/script.groovy
@@ -172,15 +172,15 @@ def getDataRowHelperPrev() {
 
 // Признак периода ввода остатков. Отчетный период является периодом ввода остатков и месяц первый в периоде.
 def isMonthBalance() {
-    if (isBalancePeriod == null) {
+    if (isBalance == null) {
         def departmentReportPeriod = departmentReportPeriodService.get(formData.departmentReportPeriodId)
         if (!departmentReportPeriod.isBalance() || formData.periodOrder == null) {
-            isBalancePeriod = false
+            isBalance = false
         } else {
-            isBalancePeriod = formData.periodOrder - 1 % 3 == 0
+            isBalance = formData.periodOrder - 1 % 3 == 0
         }
     }
-    return isBalancePeriod
+    return isBalance
 }
 
 // Алгоритмы заполнения полей формы
