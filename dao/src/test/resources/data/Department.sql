c89d426d885d031c6f70fbfbdef05c72ffc6fd28
Alexander Ivanov <Alexander.Ivanov@aplana.com>
2014-10-21 19:30:27 +0600

    http://jira.aplana.com/browse/SBRFACCTAX-9035 добавлена работа с полей garant_use

--- dao/src/test/resources/data/Department.sql
+++ dao/src/test/resources/data/Department.sql
@@ -4,15 +4,15 @@ INSERT INTO department_type (id, name) VALUES (3, 'ЦСКО, ПЦП');
 INSERT INTO department_type (id, name) VALUES (4, 'Управление');
 INSERT INTO department_type (id, name) VALUES (5, '');
 
-INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code)
-  VALUES (1, 'Банк', NULL, 1, NULL, NULL, '12', 1, 1);
-INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code)
-  VALUES (2, 'ТБ1', 1, 2, NULL, NULL, '23', 2, 2);
-INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code)
-  VALUES (3, 'ТБ2', 1, 2, NULL, NULL, NULL, 1, 3);
-INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code)
-  VALUES (4, 'ЦСКО 1', 3, 3, NULL, NULL, NULL, 2, 4);
-INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code)
-  VALUES (5, 'ЦСКО 1', 3, 3, NULL, NULL, NULL, 1, 5);
-INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code)
-  VALUES (6, 'ЦСКО 1', 2, 3, NULL, NULL, NULL, 2, 6);
+INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code, garant_use)
+  VALUES (1, 'Банк', NULL, 1, NULL, NULL, '12', 1, 1, 0);
+INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code, garant_use)
+  VALUES (2, 'ТБ1', 1, 2, NULL, NULL, '23', 2, 2, 0);
+INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code, garant_use)
+  VALUES (3, 'ТБ2', 1, 2, NULL, NULL, NULL, 1, 3, 0);
+INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code, garant_use)
+  VALUES (4, 'ЦСКО 1', 3, 3, NULL, NULL, NULL, 2, 4, 0);
+INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code, garant_use)
+  VALUES (5, 'ЦСКО 1', 3, 3, NULL, NULL, NULL, 1, 5, 0);
+INSERT INTO department (id, name, parent_id, type, shortname, tb_index, sbrf_code, region_id, code, garant_use)
+  VALUES (6, 'ЦСКО 1', 2, 3, NULL, NULL, NULL, 2, 6, 0);
