f30c517cc8334f47fbb7a70f8f19d80d3b44bdf7
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2014-11-07 11:45:42 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-9367 На форме создания декларации в корр.периоде должно быть "декларация" вместо "форма"

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/client/creation/DeclarationCreationView.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/client/creation/DeclarationCreationView.java
@@ -32,7 +32,7 @@ public class DeclarationCreationView extends PopupViewWithUiHandlers<Declaration
     public static final String DECLARATION_TITLE_D = "Создание уведомления";
     public static final String DECLARATION_TYPE_TITLE = "Вид декларации:";
     public static final String DECLARATION_TYPE_TITLE_D = "Вид:";
-    public static final String DECLARATION_CORRECTION = "Форма будет создана в корректирующем периоде, дата сдачи корректировки: ";
+    public static final String DECLARATION_CORRECTION = "Декларация будет создана в корректирующем периоде, дата сдачи корректировки: ";
     public static final String NOTIFICATION_CORRECTION = "Уведомление будет создано в корректирующем периоде, дата сдачи корректировки: ";
 
     @UiField
