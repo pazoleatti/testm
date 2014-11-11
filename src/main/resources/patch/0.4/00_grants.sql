be92486e790189972462f16ba6a90f72331a9f7a
Marat Fayzullin <mfayzullin@aplana.com>
2014-11-11 18:45:08 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-8471 Создать патч для 0.4: добавил назначение прав для TAX_RNU

--- src/main/resources/patch/0.4/00_grants.sql
+++ src/main/resources/patch/0.4/00_grants.sql
@@ -3,3 +3,8 @@ grant select on pending_trans$ to TAX;
 grant select on dba_2pc_pending to TAX;
 grant select on dba_pending_transactions to TAX;
 grant execute on dbms_xa to TAX; 
+
+grant select on pending_trans$ to TAX_RNU;
+grant select on dba_2pc_pending to TAX_RNU;
+grant select on dba_pending_transactions to TAX_RNU;
+grant execute on dbms_xa to TAX_RNU;
