be3640497bfc32d8ca952c271256286790852492
Marat Fayzullin <mfayzullin@aplana.com>
2014-11-15 00:48:51 +0500

    Добавил ограничение в data_cell Поправил form_template_id в templatesync

--- src/main/resources/ddl/create_constraint.sql
+++ src/main/resources/ddl/create_constraint.sql
@@ -117,7 +117,7 @@ alter table department_declaration_type add constraint dept_decl_type_uniq_decl 
 
 alter table declaration_template add constraint declaration_template_pk primary key (id);
 alter table declaration_template add constraint declaration_template_fk_dtype foreign key (declaration_type_id) references declaration_type (id);
-alter table declaration_template add constraint declaration_tem_fk_blob_data foreign key (XSD) references blob_data (id);
+alter table declaration_template add constraint declaration_tem_fk_blob_data foreign key (XSD) references blob_data(id);
 alter table declaration_template add constraint dec_tem_fk_blob_data_jrxml foreign key (jrxml) references blob_data(id);
 alter table declaration_template add constraint dec_template_check_status check (status in (-1, 0, 1, 2));
 
@@ -156,6 +156,7 @@ alter table data_row add constraint data_row_chk_manual check (manual in (0, 1))
 
 alter table data_cell add constraint data_cell_pk primary key (row_id, column_id);
 alter table data_cell add constraint data_cell_fk_column_id foreign key (column_id) references form_column(id);
+alter table data_cell add constraint data_cell_fk_style_id foreign key (style_id) references form_style(id);
 alter table data_cell add constraint data_cell_fk_data_row foreign key (row_id) references data_row(id) on delete cascade;
 alter table data_cell add constraint data_cell_chk_editable check (editable in (0, 1));
 
