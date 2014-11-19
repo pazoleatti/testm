31c9f950792ccbf9766f9ea4338d448043e025b6
Alexander Vanteev <avanteev@aplana.com>
2014-11-19 15:37:01 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-9483

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/client/editform/EditFormPresenter.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/client/editform/EditFormPresenter.java
@@ -321,7 +321,7 @@ public class EditFormPresenter extends PresenterWidget<EditFormPresenter.MyView>
                                     @Override
                                     public void onSuccess(SaveRefBookRowVersionResult result) {
                                         if (!result.isCheckRegion()) {
-                                            String title = "Редактирование записи справочника";
+                                            String title = "Создание записи справочника";
                                             String msg = "Отсутствуют права доступа на редактирование записи для указанного региона!";
                                             Dialog.errorMessage(title, msg);
                                             return;
@@ -377,7 +377,7 @@ public class EditFormPresenter extends PresenterWidget<EditFormPresenter.MyView>
 	@Override
 	public void onCancelClicked() {
         if (isFormModified) {
-            Dialog.confirmMessage("Редактирование версии", "Сохранить изменения?", new DialogHandler() {
+            Dialog.confirmMessage("Сохранение изменений", "Сохранить изменения?", new DialogHandler() {
                 @Override
                 public void yes() {
                     setIsFormModified(false);
