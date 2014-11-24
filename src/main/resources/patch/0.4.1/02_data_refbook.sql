4d6b6f9e97efb837c8c199300efbfec3c532fd73
Elena Kabanova <elena.kabanova@aplana.com>
2014-11-24 16:02:12 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-9316: актуализация наполнения данных для патча 0.4.1

--- src/main/resources/patch/0.4.1/02_data_refbook.sql
+++ src/main/resources/patch/0.4.1/02_data_refbook.sql
@@ -1,5 +1,20 @@
 -- http://jira.aplana.com/browse/SBRFACCTAX-9316: Доработки справочников в связи с изменениями в транспортном налоге (Наполнение справочников)
 
+--Справочник "Категории средней стоимости транспортных средств"
+insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,1,211,to_date('01.01.2012','dd.mm.yyyy'),0);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'0',null,null,null);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'От 3 до 5 млн. руб.',null,null,null);
+insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,2,211,to_date('01.01.2012','dd.mm.yyyy'),0);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'1',null,null,null);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'От 5 до 10 млн. руб.',null,null,null);
+insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,3,211,to_date('01.01.2012','dd.mm.yyyy'),0);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'2',null,null,null);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'От 10 до 15 млн. руб.',null,null,null);
+insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,4,211,to_date('01.01.2012','dd.mm.yyyy'),0);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'3',null,null,null);
+insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'Свыше 15 млн. руб.',null,null,null);
+commit;
+
 --Справочник "Средняя стоимость транспортных средств"
 insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,1,208,to_date('01.01.2012','dd.mm.yyyy'),0);
 insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) (select seq_ref_book_record.currval,2080,null,null,null,r.id from ref_book_record r join (select max(version) version, record_id from ref_book_record where ref_book_id = 211 and version <= to_date('01.01.2012','dd.mm.yy') group by record_id) t on (r.version = t.version and r.record_id = t.record_id) left join ref_book_value a1 on a1.record_id = r.id and a1.attribute_id = 2110 where r.ref_book_id = 211 and r.status <> -1 and trim(a1.string_value)='0');
@@ -855,18 +870,4 @@ insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, 
 insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2093,null,3,null,null);
 
 commit;
-
---Справочник "Категории средней стоимости транспортных средств"
-insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,1,211,to_date('01.01.2012','dd.mm.yyyy'),0);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'0',null,null,null);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'От 3 до 5 млн. руб.',null,null,null);
-insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,2,211,to_date('01.01.2012','dd.mm.yyyy'),0);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'1',null,null,null);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'От 5 до 10 млн. руб.',null,null,null);
-insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,3,211,to_date('01.01.2012','dd.mm.yyyy'),0);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'2',null,null,null);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'От 10 до 15 млн. руб.',null,null,null);
-insert into REF_BOOK_RECORD(ID, RECORD_ID, REF_BOOK_ID, VERSION, STATUS) values (seq_ref_book_record.nextval,4,211,to_date('01.01.2012','dd.mm.yyyy'),0);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2110,'3',null,null,null);
-insert into REF_BOOK_VALUE(RECORD_ID, ATTRIBUTE_ID, STRING_VALUE, NUMBER_VALUE, DATE_VALUE, REFERENCE_VALUE) values(seq_ref_book_record.currval,2111,'Свыше 15 млн. руб.',null,null,null);
-commit;
+exit;
