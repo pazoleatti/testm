b63c87700b5f131979924c04561f9f34041b0472
Eugene Stetsenko <estetsenko@aplana.com>
2014-12-05 09:57:18 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-9704

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/departmentconfigproperty/server/DeleteConfigPropertyHandler.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/departmentconfigproperty/server/DeleteConfigPropertyHandler.java
@@ -41,8 +41,8 @@ public class DeleteConfigPropertyHandler extends AbstractActionHandler<DeleteCon
     @Autowired
     LogEntryService logEntryService;
 
-    private static final String SUCCESS_INFO = "Были удалены настройки подразделения в период с %s по %s";
-    private static final String SUCCESS_INFO_SHORT = "Были удалены настройки подразделения в период с %s";
+    private static final String SUCCESS_INFO = "Настройки подразделения в период с %s по %s были удалены";
+    private static final String SUCCESS_INFO_SHORT = "Настройки подразделения в период с %s были удалены";
 
     private final static SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
 
