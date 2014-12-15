c673c1870289c0c00c23a571ca566b0fdcac3ddd
Denis Loshkarev <Denis.Loshkarev@aplana.com>
2014-12-15 11:08:58 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-9880 откатил фикс, т.к после него проект на сфере перестал работать

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/main/api/server/ExceptionHandlerAspect.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/main/api/server/ExceptionHandlerAspect.java
@@ -19,80 +19,78 @@ import java.io.StringWriter;
 /**
  * Обработчик исключений для ActionHandler. Преобразует сервисные исключения в
  * клиентские.
- * 
+ *
  * @author sgoryachkin
- * 
+ *
  */
 @Aspect
 @Component
 public class ExceptionHandlerAspect {
 
-	private static final String ERROR_MESSAGE = "Операция%s не выполнена. ";
+    private static final String ERROR_MESSAGE = "Операция%s не выполнена. ";
 
-	private static final Log LOG = LogFactory.getLog(ExceptionHandlerAspect.class);
+    private static final Log LOG = LogFactory.getLog(ExceptionHandlerAspect.class);
 
-	private String getErrorMessage(String errName) {
-		return String.format(ERROR_MESSAGE, errName.isEmpty() ? "" : " \"" + errName + "\"");
-	}
+    private String getErrorMessage(String errName) {
+        return String.format(ERROR_MESSAGE, errName.isEmpty() ? "" : " \"" + errName + "\"");
+    }
 
-	@AfterThrowing(pointcut = "target(com.gwtplatform.dispatch.server.actionhandler.ActionHandler) && args(action,..)", throwing = "e")
-	public void handleException(@SuppressWarnings("rawtypes") Action action, Throwable e) throws ActionException {
+    @AfterThrowing(pointcut = "target(com.gwtplatform.dispatch.server.actionhandler.ActionHandler) && args(action,..)", throwing = "e")
+    public void handleException(@SuppressWarnings("rawtypes") Action action, Throwable e) throws ActionException {
 
-		LOG.error(e);
+        LOG.error(e);
 
-		String actionName = "";
-		
-		if (action instanceof ActionName)
-			actionName = ((ActionName) action).getName();
-		
-		if (e instanceof TaActionException) {
-			throw (TaActionException)e;
-		} else if (e instanceof ServiceLoggerException) {
+        String actionName = "";
+
+        if (action instanceof ActionName)
+            actionName = ((ActionName) action).getName();
+
+        if (e instanceof TaActionException) {
+            throw (TaActionException)e;
+        } else if (e instanceof ServiceLoggerException) {
             TaActionException tae = new TaActionException(getErrorMessage(actionName) + (e.getLocalizedMessage() != null ? e.getLocalizedMessage() : ""));
             tae.setUuid(((ServiceLoggerException) e).getUuid());
             //Сделал на случай если все таки надо будет отображать стек трейс
             tae.setNeedStackTrace(false);
             throw tae;
         } else if (e instanceof AccessDeniedException) {
-			throw new TaActionException(getErrorMessage(actionName) + (e.getLocalizedMessage() != null ? e.getLocalizedMessage() : ""));
-		} else if (e instanceof org.springframework.security.access.AccessDeniedException) {
-			throw new TaActionException(getErrorMessage(actionName) + "Доступ запрещен");
-		} else if (e instanceof DaoException) {
-			throw new TaActionException(getErrorMessage(actionName), formatException(e));
-		} else {
-			throw new TaActionException(getErrorMessage(actionName) + (e.getLocalizedMessage() != null ? e.getLocalizedMessage() : ""), formatException(e));
-		}
-	}
+            throw new TaActionException(getErrorMessage(actionName) + (e.getLocalizedMessage() != null ? e.getLocalizedMessage() : ""));
+        } else if (e instanceof org.springframework.security.access.AccessDeniedException) {
+            throw new TaActionException(getErrorMessage(actionName) + "Доступ запрещен");
+        } else if (e instanceof DaoException) {
+            throw new TaActionException(getErrorMessage(actionName), formatException(e));
+        } else {
+            throw new TaActionException(getErrorMessage(actionName) + (e.getLocalizedMessage() != null ? e.getLocalizedMessage() : ""), formatException(e));
+        }
+    }
 
-	/**
-	 * Обернуть необработанные исключения в DaoException
-	 *
-	 * @param e брошенное исключение
-	 * @throws DaoException
-	 */
-	@AfterThrowing(pointcut = "execution(* com.aplana.sbrf.taxaccounting.dao..*.*(..)) " +
-			"&& !within(com.aplana.sbrf.taxaccounting.dao.impl.DBInfo)", throwing = "e")
-	public void handleDaoException(Throwable e) throws DaoException {
-		if (e instanceof DaoException) {
-			throw (DaoException) e;
-		} else {
-			throw new DaoException(e.getLocalizedMessage(), e);
-		}
-	}
+    /**
+     * Обернуть необработанные исключения в DaoException
+     * @param e
+     * @throws DaoException
+     */
+    @AfterThrowing(pointcut = "execution(* com.aplana.sbrf.taxaccounting.dao.*.*(..))", throwing = "e")
+    public void handleDaoException(Throwable e) throws DaoException {
+        if (e instanceof DaoException) {
+            throw (DaoException) e;
+        } else {
+            throw new DaoException(e.getLocalizedMessage(), e);
+        }
+    }
 
-	/**
-	 * Метод преобразующий стек трейс исключения в формат строки.
-	 *
-	 * @param e брошенное исключение
-	 * @return строка содержащая в себе стектрейс исключения
-	 */
-	private static String formatException(Throwable e) {
-		if (e != null) {
-			StringWriter sw = new StringWriter();
-			e.printStackTrace(new PrintWriter(sw));
-			return sw.getBuffer().toString();
-		} else {
-			return null;
-		}
-	}
+    /**
+     * Метод преобразующий стек трейс исключения в формат строки.
+     *
+     * @param e брошенное исключение
+     * @return строка содержащая в себе стектрейс исключения
+     */
+    private static String formatException(Throwable e) {
+        if (e != null) {
+            StringWriter sw = new StringWriter();
+            e.printStackTrace(new PrintWriter(sw));
+            return sw.getBuffer().toString();
+        } else {
+            return null;
+        }
+    }
 }
