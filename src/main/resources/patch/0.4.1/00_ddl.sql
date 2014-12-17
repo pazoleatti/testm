e4c7bcb0ee2069918d4b390fb9b2cceceecf1f62
Marat Fayzullin <mfayzullin@aplana.com>
2014-12-17 12:03:09 +0500

    Добавил commit; exit; в патч

--- src/main/resources/patch/0.4.1/00_ddl.sql
+++ src/main/resources/patch/0.4.1/00_ddl.sql
@@ -1,3 +1,6 @@
 -- http://jira.aplana.com/browse/SBRFACCTAX-9829: Переименование деклараций по ТН
 ALTER TABLE declaration_template MODIFY name VARCHAR2(1000);
 ALTER TABLE declaration_type MODIFY name VARCHAR2(1000);
+
+COMMIT;
+EXIT;
