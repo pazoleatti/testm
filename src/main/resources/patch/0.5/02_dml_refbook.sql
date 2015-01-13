1a2cb51da9449f3ce914758f3b52ca5c12f4ad9a
Elena Kabanova <elena.kabanova@aplana.com>
2015-01-13 16:19:36 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-9997: Новые атрибуты в справочник параметров подразделений по НДС

---
+++ src/main/resources/patch/0.5/02_dml_refbook.sql
@@ -0,0 +1,5 @@
+--http://jira.aplana.com/browse/SBRFACCTAX-9997: Новые атрибуты в справочник параметров подразделений по НДС
+UPDATE ref_book_attribute SET ord = ord + 3 WHERE ref_book_id = 98 and ord >= 12;
+INSERT INTO ref_book_attribute (id, ref_book_id, name, alias, type, ord, reference_id, attribute_id, visible, precision, width, required, is_unique, sort_order, format, read_only, max_length) VALUES (981, 98, 'Код формы реорганизации и ликвидации', 'REORG_FORM_CODE', 4, 12, 5, 13, 1, null, 10, 0, 0, null, null, 0, null);
+INSERT INTO ref_book_attribute (id, ref_book_id, name, alias, type, ord, reference_id, attribute_id, visible, precision, width, required, is_unique, sort_order, format, read_only, max_length) VALUES (982, 98, 'ИНН реорганизованного обособленного подразделения', 'REORG_INN', 1, 13, null, null, 1, null, 10, 0, 0, null, null, 0, 10);
+INSERT INTO ref_book_attribute (id, ref_book_id, name, alias, type, ord, reference_id, attribute_id, visible, precision, width, required, is_unique, sort_order, format, read_only, max_length) VALUES (983, 98, 'КПП реорганизованного обособленного подразделения', 'REORG_KPP', 1, 14, null, null, 1, null, 10, 0, 0, null, null, 0, 9);
