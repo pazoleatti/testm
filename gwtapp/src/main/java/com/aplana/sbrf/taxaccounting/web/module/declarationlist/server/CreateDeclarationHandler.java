6359bb2d94a671a296046d6a0837dc2ee8e7a5c0
Denis Loshkarev <Denis.Loshkarev@aplana.com>
2015-01-27 13:37:13 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-10225 добавил блокировки при создании нф и деклараций

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/server/CreateDeclarationHandler.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/server/CreateDeclarationHandler.java
@@ -1,14 +1,15 @@
 package com.aplana.sbrf.taxaccounting.web.module.declarationlist.server;
 
-import com.aplana.sbrf.taxaccounting.model.DeclarationData;
-import com.aplana.sbrf.taxaccounting.model.DeclarationType;
-import com.aplana.sbrf.taxaccounting.model.DepartmentReportPeriod;
-import com.aplana.sbrf.taxaccounting.model.TaxType;
+import com.aplana.sbrf.taxaccounting.core.api.LockDataService;
+import com.aplana.sbrf.taxaccounting.model.*;
+import com.aplana.sbrf.taxaccounting.model.exception.ServiceException;
+import com.aplana.sbrf.taxaccounting.model.exception.ServiceLoggerException;
 import com.aplana.sbrf.taxaccounting.model.log.Logger;
 import com.aplana.sbrf.taxaccounting.service.*;
 import com.aplana.sbrf.taxaccounting.web.main.api.server.SecurityService;
 import com.aplana.sbrf.taxaccounting.web.module.declarationlist.shared.CreateDeclaration;
 import com.aplana.sbrf.taxaccounting.web.module.declarationlist.shared.CreateDeclarationResult;
+import com.aplana.sbrf.taxaccounting.web.service.PropertyLoader;
 import com.gwtplatform.dispatch.server.ExecutionContext;
 import com.gwtplatform.dispatch.server.actionhandler.AbstractActionHandler;
 import com.gwtplatform.dispatch.shared.ActionException;
@@ -44,58 +45,82 @@ public class CreateDeclarationHandler extends AbstractActionHandler<CreateDeclar
     @Autowired
     private DepartmentReportPeriodService departmentReportPeriodService;
 
+    @Autowired
+    private LockDataService lockDataService;
+
 	@Override
 	public CreateDeclarationResult execute(CreateDeclaration command, ExecutionContext executionContext) throws ActionException {
 
         CreateDeclarationResult result = new CreateDeclarationResult();
         Logger logger = new Logger();
+        TAUserInfo userInfo = securityService.currentUserInfo();
+        String key = LockData.LockObjects.DECLARATION_CREATE.name() + "_" + command.getDeclarationTypeId() + "_" + command.getTaxType().getName() + "_" + command.getDepartmentId() + "_" + command.getReportPeriodId() + "_" + command.getTaxOrganKpp() + "_" + command.getTaxOrganCode();
 
+        if (lockDataService.lock(key, userInfo.getUser().getId(), LockData.STANDARD_LIFE_TIME) == null) {
+            //Если блокировка успешно установлена
+            try {
+                DepartmentReportPeriod departmentReportPeriod = departmentReportPeriodService.getLast(command.getDepartmentId(),
+                        command.getReportPeriodId());
+                Integer declarationTypeId = command.getDeclarationTypeId();
 
+                if (departmentReportPeriod == null) {
+                    throw new ActionException("Не удалось определить налоговый период.");
+                }
 
-        try {
-            DepartmentReportPeriod departmentReportPeriod = departmentReportPeriodService.getLast(command.getDepartmentId(),
-                    command.getReportPeriodId());
-            Integer declarationTypeId = command.getDeclarationTypeId();
+                if (command.getTaxType().equals(TaxType.DEAL)) {
+                    List<DeclarationType> declarationTypeList = declarationTypeService.getTypes(departmentReportPeriod.getDepartmentId(),
+                            departmentReportPeriod.getReportPeriod().getId(), TaxType.DEAL);
+                    if (declarationTypeList.size() == 1) {
+                        declarationTypeId = declarationTypeList.get(0).getId();
+                    } else {
+                        throw new ActionException("Не удалось определить шаблон для уведомления.");
+                    }
+                }
 
-            if (departmentReportPeriod == null) {
-                throw new ActionException("Не удалось определить налоговый период.");
-            }
+                DeclarationData declarationData = declarationDataService.find(declarationTypeId, departmentReportPeriod.getId(), command.getTaxOrganKpp(), command.getTaxOrganCode());
+                if (declarationData != null) {
+                    logger.error("Декларация с заданными параметрами уже существует");
+                    result.setDeclarationId(null);
+                    return result;
+                }
 
-            if (command.getTaxType().equals(TaxType.DEAL)) {
-                List<DeclarationType> declarationTypeList = declarationTypeService.getTypes(departmentReportPeriod.getDepartmentId(),
-                        departmentReportPeriod.getReportPeriod().getId(), TaxType.DEAL);
-                if (declarationTypeList.size() == 1) {
-                    declarationTypeId = declarationTypeList.get(0).getId();
+                /*if (declarationTemplateService.) {
+                    logger.error("Выбранный вид декларации не существует в выбранном периоде");
+                    return result;
+                }*/
+
+                int activeDeclarationTemplateId = declarationTemplateService.getActiveDeclarationTemplateId(declarationTypeId,
+                        departmentReportPeriod.getReportPeriod().getId());
+
+                long declarationId = declarationDataService.create(logger, activeDeclarationTemplateId,
+                        securityService.currentUserInfo(), departmentReportPeriod, command.getTaxOrganCode(),
+                        command.getTaxOrganKpp());
+
+                result.setDeclarationId(declarationId);
+
+                lockDataService.unlock(key, userInfo.getUser().getId());
+                return result;
+            } catch (Exception e) {
+                try {
+                    lockDataService.unlock(key, userInfo.getUser().getId());
+                } catch (ServiceException e2) {
+                    if (PropertyLoader.isProductionMode() || !(e instanceof RuntimeException)) { // в debug-режиме не выводим сообщение об отсутсвии блокировки, если оня снята при выбрасывании исключения
+                        throw new ActionException(e2);
+                    }
+                }
+                if (e instanceof ServiceLoggerException) {
+                    throw new ServiceLoggerException(e.getMessage(), ((ServiceLoggerException) e).getUuid());
                 } else {
-                    throw new ActionException("Не удалось определить шаблон для уведомления.");
+                    throw new ActionException(e);
+                }
+            } finally {
+                if (logger.getEntries().size() != 0){
+                    result.setUuid(logEntryService.save(logger.getEntries()));
                 }
             }
-
-            DeclarationData declarationData = declarationDataService.find(declarationTypeId, departmentReportPeriod.getId(), command.getTaxOrganKpp(), command.getTaxOrganCode());
-            if (declarationData != null) {
-                logger.error("Декларация с заданными параметрами уже существует");
-                result.setDeclarationId(null);
-                return result;
-            }
-/*
-            if (declarationTemplateService.) {
-                logger.error("Выбранный вид декларации не существует в выбранном периоде");
-                return result;
-            }*/
-
-            int activeDeclarationTemplateId = declarationTemplateService.getActiveDeclarationTemplateId(declarationTypeId,
-                    departmentReportPeriod.getReportPeriod().getId());
-
-            long declarationId = declarationDataService.create(logger, activeDeclarationTemplateId,
-                    securityService.currentUserInfo(), departmentReportPeriod, command.getTaxOrganCode(),
-                    command.getTaxOrganKpp());
-
-            result.setDeclarationId(declarationId);
-        } finally {
-            result.setUuid(logEntryService.save(logger.getEntries()));
+        } else {
+            throw new ActionException("Создание декларации с указанными параметрами уже выполняется!");
         }
-
-		return result;
 	}
 
 	@Override
