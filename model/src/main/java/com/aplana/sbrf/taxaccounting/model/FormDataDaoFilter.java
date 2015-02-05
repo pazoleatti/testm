ff291fe5b2a2934f5cda8fb73388794cbcadde13
Denis Loshkarev <Denis.Loshkarev@aplana.com>
2015-02-05 19:11:55 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-10348 переделал принудительную фильтрацию

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/FormDataDaoFilter.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/FormDataDaoFilter.java
@@ -10,44 +10,19 @@ import java.util.List;
  */
 public class FormDataDaoFilter implements Serializable {
 
-    /**
-	 * Способ фильтрации по правам доступа 
-	 * (используется, чтобы предотвратить попадание в поиск результатов, на которые у пользователя нет прав)
-	 */
-	public static enum AccessFilterType {
-        /**
-         * Все существующие формы (используется для Контролёра УНП)
-         */
-        ALL,
-		/**
-		 * Формы, относящиеся исключительно к перечисленным подразделениям (используется для контролеров)
-		 */
-		AVAILABLE_DEPARTMENTS,
-        /**
-         * Формы указанных типов, относящиеся исключительно к перечисленным подразделениям (используется для операторов)
-         */
-        AVAILABLE_DEPARTMENTS_WITH_KIND
-	}
-
 	private static final long serialVersionUID = 1L;
 
 	private List<Integer> reportPeriodIds;
 
 	private List<Integer> departmentIds;
 
-    private List<Integer> availableDepartmentIds;
-
 	private List<Long> formTypeIds;
 
 	private List<FormDataKind> formDataKinds;
 
-    private List<FormDataKind> availableFormDataKinds;
-
 	private List<WorkflowState> states;
 
 	private List<TaxType> taxTypes;
-
-	private AccessFilterType accessFilterType;
 
 	private Boolean returnState;
 
@@ -103,23 +78,6 @@ public class FormDataDaoFilter implements Serializable {
 		this.taxTypes = taxTypes;
 	}
 
-	/**
-	 * Получает способ, по которому будут отбрасываться записи, недоступные пользователю
-	 * Используется совместно с {@link #availableDepartmentIds} и {@link #availableFormDataKinds}
-	 * @return способ, по которому будут отбрасываться записи, недоступные пользователю
-	 */
-	public AccessFilterType getAccessFilterType() {
-		return accessFilterType;
-	}
-
-	/**
-	 * Задать способ, по которому будут отбрасываться записи, недоступные пользователю
-	 * @param accessFilter способ, по которому будут отбрасываться записи, недоступные пользователю
-	 */
-	public void setAccessFilterType(AccessFilterType accessFilter) {
-		this.accessFilterType = accessFilter;
-	}
-
 	public Boolean getReturnState() {
 		return returnState;
 	}
@@ -127,34 +85,6 @@ public class FormDataDaoFilter implements Serializable {
 	public void setReturnState(Boolean returnState) {
 		this.returnState = returnState;
 	}
-
-    /**
-     * Подразделения, доступные пользователю (работает только с AccessFilterType.AVAILABLE_DEPARTMENTS)
-     */
-    public List<Integer> getAvailableDepartmentIds() {
-        return availableDepartmentIds;
-    }
-
-    /**
-     * Подразделения, доступные пользователю (работает только с AccessFilterType.AVAILABLE_DEPARTMENTS)
-     */
-    public void setAvailableDepartmentIds(List<Integer> availableDepartmentIds) {
-        this.availableDepartmentIds = availableDepartmentIds;
-    }
-
-    /**
-     * Типы форм, доступные пользователю (работает только с AccessFilterType.AVAILABLE_DEPARTMENTS_WITH_KIND для оператора)
-     */
-    public List<FormDataKind> getAvailableFormDataKinds() {
-        return availableFormDataKinds;
-    }
-
-    /**
-     * Типы форм, доступные пользователю (работает только с AccessFilterType.AVAILABLE_DEPARTMENTS_WITH_KIND для оператора)
-     */
-    public void setAvailableFormDataKinds(List<FormDataKind> availableFormDataKinds) {
-        this.availableFormDataKinds = availableFormDataKinds;
-    }
 
     /**
      * Признак корректирующего периода
