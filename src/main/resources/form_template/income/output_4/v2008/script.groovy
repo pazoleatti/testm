5ce2849d3287188ebd300db4603608a4b354a4a9
Ramil Timerbaev <Rtimerbaev@aplana.com>
2015-02-12 13:47:03 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10409 0.5.1 Ошибка при импорте xls в выходную НФ "Сведения о суммах налога на прибыль, уплаченного Банком за рубежом".

--- src/main/resources/form_template/income/output_4/v2008/script.groovy
+++ src/main/resources/form_template/income/output_4/v2008/script.groovy
@@ -8,12 +8,13 @@ import groovy.transform.Field
  * Сведения о суммах налога на прибыль, уплаченного Банком за рубежом
  * formTemplateId=417
  *
- * графа 1 - rowNum
- * графа 2 - name
+ * графа 1 - rowNumber
+ * графа 2 - taxName
  * графа 3 - taxSum
  *
  * @author Bulat Kinzyabulatov
  */
+
 switch (formDataEvent) {
     case FormDataEvent.CREATE:
         if (formData.kind != FormDataKind.ADDITIONAL) {
@@ -23,6 +24,7 @@ switch (formDataEvent) {
         break
     case FormDataEvent.CHECK:
         logicCheck()
+        break
     case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
     case FormDataEvent.MOVE_CREATED_TO_APPROVED:  // Утвердить из "Создана"
     case FormDataEvent.MOVE_CREATED_TO_ACCEPTED:  // Принять из "Создана"
@@ -50,19 +52,19 @@ def logicCheck() {
 }
 
 void importData() {
-    def xml = getXML(ImportInputStream, importService, UploadFileName, '№ пп', null, 3, 4)
+    def tmpRow = formData.createDataRow()
+    def xml = getXML(ImportInputStream, importService, UploadFileName, getColumnName(tmpRow, 'rowNumber'), null, 3, 4)
 
     checkHeaderSize(xml.row[0].cell.size(), xml.row.size(), 3, 4)
 
-    def tmpRow = formData.createDataRow()
-
     def headerMapping = [
-            (xml.row[0].cell[0]) : getColumnName(tmpRow, 'rowNum'),
-            (xml.row[0].cell[1]) : getColumnName(tmpRow, 'name'),
+            (xml.row[0].cell[0]) : getColumnName(tmpRow, 'rowNumber'),
+            (xml.row[0].cell[1]) : getColumnName(tmpRow, 'taxName'),
             (xml.row[0].cell[2]) : getColumnName(tmpRow, 'taxSum'),
             (xml.row[1].cell[0]) : '1',
             (xml.row[1].cell[1]) : '2',
             (xml.row[1].cell[2]) : '3',
+
             (xml.row[2].cell[0]) : '1',
             (xml.row[2].cell[1]) : 'Сумма налога на прибыль, выплаченная за пределами Российской Федерации в отчётном периоде',
             (xml.row[3].cell[0]) : '2',
@@ -88,9 +90,8 @@ void addData(def xml, headRowCount) {
 
         dataRows[i - 1].setImportIndex(xlsIndexRow)
 
-        xmlIndexCol = 1
-        dataRows[i - 1].sum = getNumber(row.cell[xmlIndexCol].text(), xlsIndexRow, xmlIndexCol + colOffset)
+        xmlIndexCol = 2
+        dataRows[i - 1].taxSum = getNumber(row.cell[xmlIndexCol].text(), xlsIndexRow, xmlIndexCol + colOffset)
     }
     dataRowHelper.save(dataRows)
 }
-
