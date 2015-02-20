9a833f4445297204b2c1225b34b56acdb0ce78a5
Eugene Stetsenko <estetsenko@aplana.com>
2015-02-20 14:37:00 +0300

    Починил тесты

--- dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/SourceDaoTest.java
+++ dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/SourceDaoTest.java
@@ -1,24 +1,20 @@
 package com.aplana.sbrf.taxaccounting.dao.impl;
 
 import com.aplana.sbrf.taxaccounting.dao.SourceDao;
-import com.aplana.sbrf.taxaccounting.model.DepartmentFormType;
-import com.aplana.sbrf.taxaccounting.model.FormDataKind;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "SourceDaoTest.xml" })
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 @Transactional
 public class SourceDaoTest {
 
