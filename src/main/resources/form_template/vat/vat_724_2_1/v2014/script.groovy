50dda6112b7b9032b7154839da7a6ea08cd6b1e5
Ramil Timerbaev <Rtimerbaev@aplana.com>
2015-02-24 13:21:16 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10404 0.5.1 НДС. Доработать НФ 724.2.1.

--- src/main/resources/form_template/vat/vat_724_2_1/v2014/script.groovy
+++ src/main/resources/form_template/vat/vat_724_2_1/v2014/script.groovy
@@ -3,14 +3,12 @@ package form_template.vat.vat_724_2_1.v2014
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
 import com.aplana.sbrf.taxaccounting.model.TaxType
 import com.aplana.sbrf.taxaccounting.model.WorkflowState
-import com.aplana.sbrf.taxaccounting.model.exception.ServiceException
 import groovy.transform.Field
 
 /**
- * (724.2.1) Операции, не подлежащие налогообложению (освобождаемые от налогообложения), операции, не признаваемые объектом
+ * (724.2.1) Операции, не подлежащие налогообложению (освобождаемые от налогообложения), не признаваемые объектом
  * налогообложения, операции по реализации товаров (работ, услуг), местом реализации которых не признается территория
- * Российской Федерации, а также суммы оплаты, частичной оплаты в счет предстоящих поставок (выполнения работ,
- * оказания услуг), длительность производственного цикла изготовления которых составляет свыше шести месяцев
+ * Российской Федерации.
  *
  * formTemplateId=601
  *
@@ -28,12 +26,10 @@ switch (formDataEvent) {
         formDataService.checkUnique(formData, logger)
         break
     case FormDataEvent.CALCULATE:
-        prevCalcCheck()
         calc()
         logicCheck()
         break
     case FormDataEvent.CHECK:
-        prevCalcCheck()
         logicCheck()
         break
     case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
@@ -42,11 +38,9 @@ switch (formDataEvent) {
     case FormDataEvent.MOVE_PREPARED_TO_APPROVED: // Утвердить из "Подготовлена"
     case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED: // Принять из "Подготовлена"
     case FormDataEvent.MOVE_APPROVED_TO_ACCEPTED: // Принять из "Утверждена"
-        prevCalcCheck()
         logicCheck()
         break
     case FormDataEvent.COMPOSE:
-        prevCalcCheck()
         consolidation()
         calc()
         logicCheck()
@@ -61,11 +55,7 @@ switch (formDataEvent) {
         break
 }
 
-// Автозаполняемые атрибуты (графа 4, 5)
-@Field
-def autoFillColumns = ['realizeCost', 'obtainCost']
-
-// Проверяемые на пустые значения атрибуты (группа 1..5)
+// Проверяемые на пустые значения атрибуты (графа  1..5)
 @Field
 def nonEmptyColumns = ['code', 'name', 'realizeCost', 'obtainCost']
 
@@ -85,41 +75,13 @@ def calendarStartDate = null
 def endDate = null
 
 @Field
-def prevEndDate = null
-
-@Field
-def dateFormat = "dd.MM.yyyy"
-
-@Field
-def calcRowAlias4 = ['R1', 'R3', 'R4', 'R5', 'R8', 'R9', 'R10', 'R11', 'R13', 'R14', 'R18']
-
-@Field
-def calcRowAlias5 = ['R8', 'R9', 'R13']
-
-@Field
-def repordPeriod = null
-
-// Cправочник «Отчет о прибылях и убытках (Форма 0409102-СБ)»
-@Field
-def income102DataCache = [:]
+def editableStyle = 'Редактируемая'
 
 //// Кастомные методы
 
 void calc() {
     def dataRowHelper = formDataService.getDataRowHelper(formData)
     def dataRows = dataRowHelper.allCached
-
-    for (def row in dataRows) {
-        if (row.getAlias() == 'itog') {
-            continue
-        }
-
-        // графа 4
-        row.realizeCost = (row.getAlias() in calcRowAlias4 ? calc4(row) : row.realizeCost)
-
-        // графа 5
-        row.obtainCost = (row.getAlias() in calcRowAlias5 ? calc5(row) : null)
-    }
 
     // подсчет итогов
     def itogValues = calcItog(dataRows)
@@ -138,8 +100,11 @@ def logicCheck() {
             continue
         }
 
-        // 1. Проверка заполнения граф (по графе 5 обязательны тока строки 8, 9 и 13)
-        def columns = (row.getAlias() in calcRowAlias5 ? nonEmptyColumns : nonEmptyColumns - 'obtainCost')
+        // 1. Проверка заполнения граф (по графе 5 обязательны только редактируемые)
+        def columns = nonEmptyColumns
+        if (row.getCell('obtainCost')?.style?.alias != editableStyle) {
+            columns = nonEmptyColumns - 'obtainCost'
+        }
         checkNonEmptyColumns(row, row.getIndex(), columns, logger, true)
     }
 
@@ -174,74 +139,6 @@ def getReportPeriodEndDate() {
     return endDate
 }
 
-def getPrevReportPeriodEndDate() {
-    if (prevEndDate == null) {
-        prevEndDate = getCalendarStartDate() - 1
-    }
-    return prevEndDate
-}
-
-def getRepordPeriod() {
-    if (repordPeriod == null) {
-        repordPeriod = reportPeriodService.get(formData.reportPeriodId)
-    }
-    return repordPeriod
-}
-
-// Получение данных из справочника «Отчет о прибылях и убытках» для текужего подразделения и отчетного периода
-def getIncome102Data(def date) {
-    if(date==null){
-        return []
-    }
-    if (!income102DataCache.containsKey(date)) {
-        def records = bookerStatementService.getRecords(52L, formData.departmentId, date, null)
-        if (records == null) {
-            return []
-        }
-        income102DataCache.put(date, records)
-    }
-    return income102DataCache.get(date)
-}
-
-// Проверка наличия необходимых записей в справочнике «Отчет о прибылях и убытках»
-void checkIncome102() {
-    // формирование названия периода
-    def periodName = null
-    def period = null
-    def prevPeriod = null
-
-    // Наличие экземпляра Отчета о прибылях и убытках подразделения и периода, для которых сформирована текущая форма
-    if (getIncome102Data(getReportPeriodEndDate()) == []) {
-        period = getBookerStatementPeriod(getReportPeriodEndDate())
-    }
-    // Наличие экземпляра Отчета о прибылях и убытках подразделения и предыдущего периода
-    if (getRepordPeriod().order > 1 && getIncome102Data(getPrevReportPeriodEndDate()) == []) {
-        prevPeriod = getBookerStatementPeriod(getPrevReportPeriodEndDate())
-    }
-
-    if (prevPeriod && period) {
-        periodName = prevPeriod + " и " + period
-    } else if (prevPeriod || period) {
-        periodName = (prevPeriod ?: period)
-    }
-
-    if (periodName) {
-        throw new ServiceException("Экземпляр Отчета о прибылях и убытках за $periodName " +
-                "не существует (отсутствуют данные для расчета)! Расчеты не могут быть выполнены.")
-    }
-}
-
-void prevCalcCheck() {
-    // 1. Проверка наличия экземпляра «Отчета о прибылях и убытках» по соответствующему подразделению за соответствующий налоговый период
-    checkIncome102()
-
-    // 2. Проверка наличия в справочнике «Классификатор соответствия кодов операций налоговой формы 724.2.1 по НДС символам ОПУ» данных для графы 4 и 5
-    // эта проверка происходит при расчетах в методе getOpuCodes
-
-    // 3. Проверка наличия символов ОПУ в Экземпляре Отчета о прибылях и убытках, необходимых для заполнения «Графы 4» и «Графы 5»
-    // эта проверка происходит при расчетах в методе getSumByOpuCodes
-}
-
 // Консолидация
 void consolidation() {
     def dataRowHelper = formDataService.getDataRowHelper(formData)
@@ -266,93 +163,6 @@ void consolidation() {
     }
 
     dataRowHelper.update(dataRows)
-}
-
-def calc4(def row) {
-    return calc4or5(row, 0)
-}
-
-def calc5(def row) {
-    return calc4or5(row, 1)
-}
-
-/**
- * Посчитать значение для графы 4 или 5.
- *
- * @param row строка
- * @param columnFlag признак графы: 0 – Графа 4, 1 – Графа 5
- */
-def calc4or5(def row, def columnFlag) {
-    // список кодов ОПУ из справочника
-    def opuCodes = getOpuCodes(row.code, row, columnFlag)
-    // сумма кодов ОПУ из отчета 102
-    def sum = getSumByOpuCodes(opuCodes, row, columnFlag)
-    return roundValue(sum, 2)
-}
-
-/**
- * Получить список кодов ОПУ.
- *
- * @param code код операции
- * @param row строка
- * @param columnFlag признак графы: 0 – Графа 4, 1 – Графа 5
- */
-def getOpuCodes(def code, def row, def columnFlag) {
-    def columnAlias = (columnFlag == 0 ? 'realizeCost' : 'obtainCost')
-    // потом поправить фильтр и id справочника
-    def filter = "BOX_724_2_1 = $columnFlag AND CODE = '$code'"
-    def records = refBookFactory.getDataProvider(102L)?.getRecords(getReportPeriodEndDate(), null, filter, null)
-    if (records == null || records.isEmpty()) {
-        // условия выполнения расчетов
-        // 2. Проверка наличия в справочнике «Классификатор соответствия кодов операций налоговой формы 724.2.1 по НДС символам ОПУ» данных для графы 4 и 5
-        throw new ServiceException("Строка ${row.getIndex()}: В справочнике «%s» нет данных для заполнения графы «${getColumnName(row, columnAlias)}»! Расчеты не могут быть выполнены.",
-                refBookFactory.get(102L).name)
-    }
-    def opuCodes = []
-    records.each { record ->
-        opuCodes.add(record?.OPU?.value)
-    }
-
-    return opuCodes
-}
-
-/**
- * Посчитать сумму по кодам ОПУ.
- *
- * @param opuCodes список кодов ОПУ
- * @param row строка
- * @param columnFlag признак графы: 0 – Графа 4, 1 – Графа 5
- */
-def getSumByOpuCodes(def opuCodes, def row, def columnFlag) {
-    def columnAlias = (columnFlag == 0 ? 'realizeCost' : 'obtainCost')
-    def tmp = BigDecimal.ZERO
-    def hasData = false
-    // В отчете 102 данные хранятся по периодам как у прибыли (1 квартал, полгода, 9 месяцев, год)
-    // Для формы 724.2.1 нужны квартальные данные, поэтому для получения кваратальных значении из отчета 102
-    // сначало берутся данные за текущий периода, а потом вычитаются данные запредыдущий период (например: 9 месяцев - полгода)
-    for (def income102Row : getIncome102Data(getReportPeriodEndDate())) {
-        if (income102Row?.OPU_CODE?.value in opuCodes) {
-            tmp += (income102Row?.TOTAL_SUM?.value ?: 0)
-            hasData = true
-        }
-    }
-    if (!hasData) {
-        // условия выполнения расчетов
-        // 3. Проверка наличия символов ОПУ в Экземпляре Отчета о прибылях и убытках, необходимых для заполнения «Графы 4» и «Графы 5»
-        def start = getReportPeriodStartDate().format(dateFormat)
-        def end = getReportPeriodEndDate().format(dateFormat)
-        logger.warn("Строка ${row.getIndex()}: Экземпляр Отчета о прибылях и убытках за период $start - $end не содержит записей " +
-                "для заполнения графы «${getColumnName(row, columnAlias)}» по следующим символам ОПУ: «${opuCodes.join(', ')}»!")
-        return BigDecimal.ZERO
-    }
-    if (getRepordPeriod().order > 1) {
-        for (def income102Row : getIncome102Data(getPrevReportPeriodEndDate())) {
-            if (income102Row?.OPU_CODE?.value in opuCodes) {
-                tmp -= (income102Row?.TOTAL_SUM?.value ?: 0)
-            }
-        }
-    }
-    return tmp
 }
 
 def calcItog(def dataRows) {
@@ -538,14 +348,4 @@ void addTransportData(def xml) {
         }
     }
     dataRowHelper.save(dataRows)
-}
-
-/**
- * Получить название периода Бухалтерской отчетности по дате.
- *
- * @param date дата.
- */
-def getBookerStatementPeriod(def date) {
-    def name = bookerStatementService.getPeriodValue(date)?.NAME?.value
-    return name + " " + getRepordPeriod()?.taxPeriod?.year
 }
