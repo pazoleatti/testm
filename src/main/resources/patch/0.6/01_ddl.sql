fb82767f016961d4f6bb4e8067bfbc58138ca574
Elena Kabanova <elena.kabanova@aplana.com>
2015-02-25 14:34:50 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10493: Патч 0.6

---
+++ src/main/resources/patch/0.6/01_ddl.sql
@@ -0,0 +1,27 @@
+--http://jira.aplana.com/browse/SBRFACCTAX-10485: Таблица для отчетов по журналу аудита
+
+create table log_system_report
+(
+blob_data_id varchar(32) not null,
+type number(1) not null,
+sec_user_id number(9)
+);
+
+comment on table log_system_report is 'Выгрузки журнала аудита';
+comment on column log_system_report.blob_data_id is 'Идентификатор таблицы BLOB_DATA, в которой хранятся файлы';
+comment on column log_system_report.type is 'Тип выгрузки (0 - архивация журнала аудита, 1 - генерация zip-файла для журнала аудита)';
+comment on column log_system_report.sec_user_id is 'Идентификатор пользователя, инициировавшего выгрузку типа 1';
+
+alter table log_system_report add constraint log_system_report_fk_blob_data foreign key (blob_data_id) references blob_data (id);
+alter table log_system_report add constraint log_system_report_fk_sec_user foreign key (sec_user_id) references sec_user (id) on delete cascade;
+alter table log_system_report add constraint log_system_report_chk_type check (type in (0, 1));
+
+create index i_log_system_rep_blob_data_id on log_system_report(blob_data_id);
+
+---------------------------------------------------------------------------------------------
+--http://jira.aplana.com/browse/SBRFACCTAX-10492: Удаление временной таблицы
+drop table data_row_temp;
+
+---------------------------------------------------------------------------------------------
+commit;
+end;
