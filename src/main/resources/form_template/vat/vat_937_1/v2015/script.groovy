99167ac9b0ae51370c08ea3450f2c08409583405
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2015-03-05 13:40:54 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10496 изменения по загрузке НФ НДС из файлов Excel

--- src/main/resources/form_template/vat/vat_937_1/v2015/script.groovy
+++ src/main/resources/form_template/vat/vat_937_1/v2015/script.groovy
@@ -61,7 +61,6 @@ switch (formDataEvent) {
     case FormDataEvent.IMPORT:
         importData()
         calc()
-        logicCheck()
         break
     case FormDataEvent.IMPORT_TRANSPORT_FILE:
         importTransportData()
