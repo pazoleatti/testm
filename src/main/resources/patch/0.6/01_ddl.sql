5a508bd96c901a3f0d4f4f62186eda0827167acd
Elena Kabanova <elena.kabanova@aplana.com>
2015-03-12 15:43:23 +0500

    Вынесение check-констрейнтов на tax_type в отдельную сущность

--- src/main/resources/patch/0.6/01_ddl.sql
+++ src/main/resources/patch/0.6/01_ddl.sql
@@ -31,5 +31,37 @@ create index i_data_row_fdata_manual_type on data_row(form_data_id, manual, type
 drop index data_row_uniq_form_data_order;
 alter table data_row drop constraint data_row_uniq_form_data_order;
 
+---------------------------------------------------------------------------------------------
+--Вынесение check-констрейнтов на tax_type в отдельную сущность
+create table tax_type
+(
+id char(1) not null,
+name varchar2(256) not null
+);
+
+alter table tax_type add constraint tax_type_pk primary key(id);
+
+comment on table tax_type is 'Справочник типов налогов';
+comment on column tax_type.id is 'Символьный идентификатор типа налога';
+comment on column tax_type.name is 'Тип налога';
+
+insert all
+  into tax_type (id, name) values ('I', 'Прибыль')
+  into tax_type (id, name) values ('P', 'Имущество')
+  into tax_type (id, name) values ('T', 'Транспортный')
+  into tax_type (id, name) values ('V', 'НДС')
+  into tax_type (id, name) values ('D', 'ТЦО')
+select * from dual;
+
+alter table form_type drop constraint form_type_chk_taxtype;
+alter table tax_period drop constraint tax_period_chk_taxtype;
+alter table declaration_type drop constraint declaration_type_chk_tax_type;
+
+alter table form_type add constraint form_type_fk_taxtype foreign key (tax_type) references tax_type(id);
+alter table declaration_type add constraint declaration_type_fk_taxtype foreign key (tax_type) references tax_type(id);
+alter table tax_period add constraint tax_period_fk_taxtype foreign key (tax_type) references tax_type(id);
+
+---------------------------------------------------------------------------------------------
+
 commit;
 end;
