6cd9a5e59d662ab709e46d8b4b4bb2e00ed74390
Ramil Timerbaev <Rtimerbaev@aplana.com>
2015-03-12 18:28:27 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10644 Оптимизировать "Рассчитать".

--- src/main/resources/form_template/vat/vat_937_2/v2015/script.groovy
+++ src/main/resources/form_template/vat/vat_937_2/v2015/script.groovy
@@ -149,7 +149,7 @@ void calc() {
 
     calcTotalSum(dataRows, totalRow, totalSumColumns)
 
-    save(dataRows)
+    dataRowHelper.update(totalRow)
 
     // Сортировка групп и строк
     sortFormDataRows()
