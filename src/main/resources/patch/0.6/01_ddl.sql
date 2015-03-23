2d6dde874b841cbce9b1fc7cffc1ba994d87eecc
Elena Kabanova <elena.kabanova@aplana.com>
2015-03-23 17:09:59 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10763: Добавление признака возможности формирования pdf и xlsx в таблицу declaration_data

--- src/main/resources/patch/0.6/01_ddl.sql
+++ src/main/resources/patch/0.6/01_ddl.sql
@@ -1,5 +1,4 @@
 --http://jira.aplana.com/browse/SBRFACCTAX-10485: Таблица для отчетов по журналу аудита
-
 create table log_system_report
 (
 blob_data_id varchar2(36) not null,
@@ -62,6 +61,14 @@ alter table declaration_type add constraint declaration_type_fk_taxtype foreign 
 alter table tax_period add constraint tax_period_fk_taxtype foreign key (tax_type) references tax_type(id);
 
 ---------------------------------------------------------------------------------------------
+--http://jira.aplana.com/browse/SBRFACCTAX-10763: Добавление признака возможности формирования pdf и xlsx в таблицу declaration_data
 
+alter table declaration_data add is_show_report number(1) default 1 not null;
+comment on column declaration_data.is_show_report is 'Признак возможности формирования pdf и xlsx';
+
+alter table declaration_data add constraint declaration_data_chk_isshowrep check (is_show_report in (0,1));
+
+
+---------------------------------------------------------------------------------------------
 commit;
 end;
