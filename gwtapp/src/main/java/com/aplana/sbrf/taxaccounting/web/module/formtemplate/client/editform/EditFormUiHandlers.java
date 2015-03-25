58737c8f2266559882ac48c55d7887bcdf0aa7ee
Alexander Vanteev <avanteev@aplana.com>
2015-03-25 11:24:19 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-10569

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formtemplate/client/editform/EditFormUiHandlers.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/formtemplate/client/editform/EditFormUiHandlers.java
@@ -1,8 +1,10 @@
 package com.aplana.sbrf.taxaccounting.web.module.formtemplate.client.editform;
 
+import com.aplana.sbrf.taxaccounting.web.module.formtemplate.shared.FormTypeTemplate;
 import com.gwtplatform.mvp.client.UiHandlers;
 
 public interface EditFormUiHandlers extends UiHandlers {
     void onSave();
     void onCancel();
+    void setModel(FormTypeTemplate model);
 }
