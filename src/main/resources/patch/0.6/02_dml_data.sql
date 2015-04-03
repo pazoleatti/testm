3c950bad81de34185cc245295e30f7702acdcb23
Elena Kabanova <elena.kabanova@aplana.com>
2015-04-03 14:08:48 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10831: патч 0.6

--- src/main/resources/patch/0.6/02_dml_data.sql
+++ src/main/resources/patch/0.6/02_dml_data.sql
@@ -1,4 +1,6 @@
+--http://jira.aplana.com/browse/SBRFACCTAX-10831
 insert into configuration_lock (key, timeout) values ('FILE', 3600000);
+insert into configuration_lock (key, timeout) values ('CONFIGURATION_PARAMS', 86400000);
 
 -----------------------------------------------------------------------
 commit;
