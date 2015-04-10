88ce38d18c6603cd0c3aa1f00902fb07653171b5
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2015-04-10 13:07:21 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10962 Заказчик решил делать это только с версии 0.6

--- src/main/resources/form_template/deal/guarantees/v2013/script.groovy
+++ src/main/resources/form_template/deal/guarantees/v2013/script.groovy
@@ -176,12 +176,6 @@ void logicCheck() {
             def msg2 = dealDateCell.column.name
             rowError(logger, row, "Строка $rowNum: Значение графы «$msg1» должно быть не меньше значения графы «$msg2»!")
         }
-
-        // Проверка положительной суммы
-        if (sumCell.value <= 0) {
-            def msg1 = sumCell.column.name
-            rowWarning(logger, row, "Строка $rowNum: Значение графы «$msg1» должно быть больше «0»!")
-        }
     }
 
     if (formData.kind == FormDataKind.CONSOLIDATED) {
