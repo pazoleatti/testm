69fc492cb538c4e3451228096a1fe5aca75dfc11
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2015-04-27 09:02:44 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-11151

--- src/main/resources/form_template/income/declaration_bank_2/v2015/script.groovy
+++ src/main/resources/form_template/income/declaration_bank_2/v2015/script.groovy
@@ -1212,7 +1212,7 @@ void generateXML() {
                 // Приложение №2 к налоговой декларации - формирутеся только для периода "год"
                 if (isTaxPeriod) {
                     // сортируем по ФИО, потом по остальным полям
-                    if (isCFOApp2) {
+                    if (isCFOApp2 && dataRowsApp2 != null) {
                         def sortColumns = ['surname', 'name', 'patronymic', 'innRF', 'inn', 'taxRate']
                         sortRows(dataRowsApp2, sortColumns)
                     }
