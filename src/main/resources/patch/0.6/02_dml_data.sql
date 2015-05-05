cdf0a4281080cbdbe948ace43dc2b620186e2b81
Elena Kabanova <elena.kabanova@aplana.com>
2015-05-05 13:16:28 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10770: Очистка данных (несоответствие типа графы графы с типом данных)

--- src/main/resources/patch/0.6/02_dml_data.sql
+++ src/main/resources/patch/0.6/02_dml_data.sql
@@ -10,6 +10,37 @@ INSERT INTO ASYNC_TASK_TYPE (ID, NAME, HANDLER_JNDI) VALUES (13, 'Загрузка ТФ из
 INSERT INTO configuration_lock (key, timeout) VALUES ('XSD_VALIDATION', 3600000);
 
 -----------------------------------------------------------------------
+--http://jira.aplana.com/browse/SBRFACCTAX-10770: Очистка данных (несоответствие типа графы графы с типом данных)
+
+-- SET NULLS instead of invalid data
+update data_cell dc
+set nvalue = null
+where nvalue is not null and dc.column_id in (select id from form_column fc where fc.type in ('D', 'S', 'A')); 
+
+update data_cell dc
+set nvalue = null
+where nvalue is not null and dc.column_id in (select id from form_column fc where fc.type = 'R' and fc.parent_column_id is not null); 
+
+update data_cell dc
+set svalue = null
+where svalue is not null and dc.column_id in (select id from form_column fc where fc.type <> 'S'); 
+
+update data_cell dc
+set dvalue = null
+where dvalue is not null and dc.column_id in (select id from form_column fc where fc.type <> 'D'); 
+
+-- Impose martial law
+delete from data_cell 
+where svalue is null 
+	and dvalue is null 
+	and nvalue is null 
+	and style_id is null 
+	and colspan is null 
+	and rowspan is null 
+	and editable is null;
+
+-----------------------------------------------------------------------
+
 commit;
 exit;
 
