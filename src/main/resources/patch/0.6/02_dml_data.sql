8009bdbef9428b71cb3b6c339f2bdb199b43725f
Elena Kabanova <elena.kabanova@aplana.com>
2015-05-05 12:35:51 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-10965: Ограничить время XSD-валидации (Патч 0.6)

--- src/main/resources/patch/0.6/02_dml_data.sql
+++ src/main/resources/patch/0.6/02_dml_data.sql
@@ -6,6 +6,9 @@ INSERT INTO configuration_lock (key, timeout) VALUES ('LOAD_TRANSPORT_DATA', 864
 INSERT INTO ASYNC_TASK_TYPE (ID, NAME, HANDLER_JNDI) VALUES (12, 'Загрузка ТФ с локального компьютера', 'ejb/taxaccounting/async-task.jar/UploadTransportDataAsyncTask#com.aplana.sbrf.taxaccounting.async.task.AsyncTaskRemote');
 INSERT INTO ASYNC_TASK_TYPE (ID, NAME, HANDLER_JNDI) VALUES (13, 'Загрузка ТФ из каталога загрузки', 'ejb/taxaccounting/async-task.jar/LoadAllTransportDataAsyncTask#com.aplana.sbrf.taxaccounting.async.task.AsyncTaskRemote');
 
+--http://jira.aplana.com/browse/SBRFACCTAX-10965: Ограничить время XSD-валидации
+INSERT INTO configuration_lock (key, timeout) VALUES ('XSD_VALIDATION', 3600000);
+
 -----------------------------------------------------------------------
 commit;
 exit;
