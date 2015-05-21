b44a5f5889dbd1cc7f3953d6886c9dd2cb79e505
Lenar Haziev <lhaziev@aplana.com>
2015-05-21 15:13:59 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-11348 0.6 Ошибка при попытке создать декларацию

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/server/CreateDeclarationHandler.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/server/CreateDeclarationHandler.java
@@ -88,6 +88,7 @@ public class CreateDeclarationHandler extends AbstractActionHandler<CreateDeclar
         if (lockDataService.lock(key, userInfo.getUser().getId(),
                 String.format(LockData.DescriptionTemplate.DECLARATION_CREATE.getText(),
                         declarationType.getTaxType().getDeclarationShortName(),
+                        declarationType.getName(),
                         department.getName(),
                         reportPeriod.getName() + " " + reportPeriod.getTaxPeriod().getYear(),
                         departmentReportPeriod.getCorrectionDate() != null
