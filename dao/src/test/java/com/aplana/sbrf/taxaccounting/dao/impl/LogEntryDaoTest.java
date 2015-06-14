c8388a31597d2d2602313033936f1d17e5b991f2
Denis Loshkarev <Denis.Loshkarev@aplana.com>
2015-06-14 19:38:40 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-11638 сделал сохранение логов через файл для экономии памяти

--- dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/LogEntryDaoTest.java
+++ dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/LogEntryDaoTest.java
@@ -14,14 +14,15 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.*;
+import java.util.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"BlobDataDaoTest.xml"})
 @Transactional
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class LogEntryDaoTest {
+    private static final String FILE_NAME = "c:\\test\\test.txt";
 
     @Autowired
     LogEntryDao logEntryDao;
@@ -92,5 +93,61 @@ public class LogEntryDaoTest {
         Assert.assertEquals(list.size(), 6);
         Assert.assertEquals(list.get(4).getMessage(), "E3");
         Assert.assertEquals(list.get(5).getLevel(), LogLevel.WARNING);
+    }
+
+    //@Test
+    public void hugeLogsTest() {
+        int size = 1000000;
+        long start = System.nanoTime();
+        generateLogs(size);
+        System.out.println("Generating file..."+(double) (System.nanoTime() - start) / 1000000000.0);
+        start = System.nanoTime();
+        List<LogEntry> entries = loadLogs();
+        System.out.println("Reading file..."+(double) (System.nanoTime() - start) / 1000000000.0);
+        start = System.nanoTime();
+        String uuid = UUID.randomUUID().toString().toLowerCase();
+        logEntryDao.save(entries, uuid);
+        System.out.println("Saving logs..."+(double) (System.nanoTime() - start) / 1000000000.0);
+        start = System.nanoTime();
+        List<LogEntry> logs = logEntryDao.get(uuid);
+        System.out.println("Getting logs..."+(double) (System.nanoTime() - start) / 1000000000.0);
+        Assert.assertEquals(logs.size(), size + 1);
+        LogEntry testEntry = logs.get(0);
+        Assert.assertEquals(testEntry.getMessage(), "test");
+        Assert.assertEquals(testEntry.getLevel(), LogLevel.WARNING);
+    }
+
+    private static List<LogEntry> loadLogs() {
+        BufferedReader br = null;
+        try {
+            List<LogEntry> entries = new ArrayList<LogEntry>();
+            entries.add(new LogEntry(LogLevel.WARNING, "test"));
+            br = new BufferedReader(new FileReader(FILE_NAME));
+            String text;
+            while ((text = br.readLine()) != null) {
+                entries.add(new LogEntry(LogLevel.INFO, text));
+            }
+            return entries;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {}
+        }
+        return null;
+    }
+
+    private static void generateLogs(int count) {
+        try {
+            FileWriter writer = new FileWriter(new File(FILE_NAME));
+            Random random = new Random();
+            for (int i=0; i<count; i++) {
+                writer.write(random.nextInt(10) + String.valueOf(new Date().getTime()) + "\r\n");
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
