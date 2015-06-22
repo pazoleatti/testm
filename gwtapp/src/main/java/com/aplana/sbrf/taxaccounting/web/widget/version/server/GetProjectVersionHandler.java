a254c5bee9c5a4f96006f2f752c9ee8f67b8a9d6
Marat Fayzullin <mfayzullin@aplana.com>
2015-06-22 19:08:32 +0500

    Откатил обратно изменения по зависимости на библиотеку "ws-runtime". Информацию о сервере сейчас получаю через рефлексию.

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/version/server/GetProjectVersionHandler.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/version/server/GetProjectVersionHandler.java
@@ -5,16 +5,15 @@ import com.aplana.sbrf.taxaccounting.web.widget.version.shared.GetProjectVersion
 import com.gwtplatform.dispatch.server.ExecutionContext;
 import com.gwtplatform.dispatch.server.actionhandler.AbstractActionHandler;
 import com.gwtplatform.dispatch.shared.ActionException;
-import com.ibm.websphere.runtime.ServerName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Method;
 import java.util.Properties;
 
 @Service
-public class GetProjectVersionHandler extends
-		AbstractActionHandler<GetProjectVersion, GetProjectVersionResult> {
+public class GetProjectVersionHandler extends AbstractActionHandler<GetProjectVersion, GetProjectVersionResult> {
 
     @Autowired
     @Qualifier("versionInfoProperties")
@@ -25,19 +24,28 @@ public class GetProjectVersionHandler extends
 	}
 
 	@Override
-	public GetProjectVersionResult execute(GetProjectVersion action,
-			ExecutionContext executionContext) throws ActionException {
+	public GetProjectVersionResult execute(GetProjectVersion action, ExecutionContext executionContext) throws ActionException {
 		
 		String version = "?";
 		String revision = "?";
+		String serverName = "?";
 
         if (versionInfoProperties != null) {
             version = versionInfoProperties.getProperty("version");
             revision = versionInfoProperties.getProperty("revision");
         }
-		
+
+		// получаем информацию о сервере приложений
+		try {
+			Class clazz = Class.forName("com.ibm.websphere.runtime.ServerName");
+			Method method =	clazz.getMethod("getDisplayName", null);
+			serverName = (String) method.invoke(null, null);
+		} catch (Exception e) {
+			// do nothing (dev mode)
+		}
+
 		GetProjectVersionResult result = new GetProjectVersionResult();
-        result.setProjectVersion(String.format("Версия: %s, Ревизия: %s, Сервер: %s", version, revision, ServerName.getDisplayName()));
+		result.setProjectVersion(String.format("Версия: %s, Ревизия: %s, Сервер: %s", version, revision, serverName));
         return result;
 
 	}
