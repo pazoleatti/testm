881ffad3fdbdbb1c5adc52654ae4dc4fe2d28916
Denis Loshkarev <Denis.Loshkarev@aplana.com>
2015-06-22 15:57:12 +0300

    кэш

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/RefBookDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/RefBookDaoImpl.java
@@ -151,7 +151,6 @@ public class RefBookDaoImpl extends AbstractDao implements RefBookDao {
     }
 
     @Override
-    @Cacheable(value = "PermanentData", key = "'RefBook_attributes_'+#refBookId.toString()")
     public List<RefBookAttribute> getAttributes(Long refBookId) {
         try {
             return getJdbcTemplate().query(
