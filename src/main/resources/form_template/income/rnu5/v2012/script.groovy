ba53085c0ac10c6a31336db3768e1da265dc5406
Ramil Timerbaev <Rtimerbaev@aplana.com>
2015-07-14 12:33:04 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-12000 Дополнить тесты для скриптов (рну 5).

--- src/main/resources/form_template/income/rnu5/v2012/script.groovy
+++ src/main/resources/form_template/income/rnu5/v2012/script.groovy
@@ -445,6 +445,10 @@ String pure(String cell) {
     return StringUtils.cleanString(cell)?.intern()
 }
 
+boolean isEmptyCells(def rowCells) {
+    return rowCells.length == 1 && rowCells[0] == ''
+}
+
 void importData() {
     int COLUMN_COUNT = 5
     int HEADER_ROW_COUNT = 3
