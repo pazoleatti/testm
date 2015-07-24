3d645bb6666ea3c4a081b4eb67045efd89ff0c2d
Elena Kabanova <elena.kabanova@aplana.com>
2015-07-24 12:45:44 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-12131 Новый вид налога E

--- src/main/resources/patch/0.7/01_dml_data.sql
+++ src/main/resources/patch/0.7/01_dml_data.sql
@@ -1,5 +1,8 @@
 --http://jira.aplana.com/browse/SBRFACCTAX-11977: В справочнике "Тип подразделений" значение "Пустой" переименовать в "Прочие"
 update department_type set name='Прочие' where id = 5;
 
+--http://jira.aplana.com/browse/SBRFACCTAX-12131: Новый вид налога E
+insert into tax_type (id, name) values ('E', 'Эффективная налоговая ставка');
+
 COMMIT;
 EXIT;
