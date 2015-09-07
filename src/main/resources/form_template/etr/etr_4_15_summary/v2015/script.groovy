cd55d19fd5c85baa0243435576f1df4d2a44ee4f
Ramil Timerbaev <Rtimerbaev@aplana.com>
2015-09-07 19:31:23 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-12595 0.7.1 ЭНС. Реализовать расчет "Прил.4-15" (сводный).

---
+++ src/main/resources/form_template/etr/etr_4_15_summary/v2015/script.groovy
@@ -0,0 +1,393 @@
+package form_template.etr.etr_4_15_summary.v2015
+
+import com.aplana.sbrf.taxaccounting.model.FormDataEvent
+import com.aplana.sbrf.taxaccounting.model.WorkflowState
+import com.aplana.sbrf.taxaccounting.model.exception.ServiceException
+import com.aplana.sbrf.taxaccounting.model.log.LogLevel
+import groovy.transform.Field
+
+/**
+ * Приложение 4-15. Анализ структуры налога на добавленную стоимость (НДС) (сводная).
+ * formTemplateId = 7150
+ */
+
+// графа    - fix
+// графа 1  - department	        Подразделение Банка
+// графа 2  - comparePeriod			Период сравнения. НДС всего
+// графа 3  - comparePeriodIgnore	Период сравнения. В том числе НДС не учитываемый
+// графа 4  - comparePeriodPercent	Период сравнения. Доля НДС не учитываемый, %
+// графа 5  - currentPeriod			Период. НДС всего
+// графа 6  - currentPeriodIgnore	Период. В том числе НДС не учитываемый
+// графа 7  - currentPeriodPercent	Период. Доля НДС не учитываемый, %
+// графа 8  - delta					Изменения за период. НДС всего
+// графа 9  - deltaIgnore			Изменения за период. В том числе НДС не учитываемый
+// графа 10 - deltaPercent			Изменения за период. Доля НДС не учитываемый, %
+
+switch (formDataEvent) {
+    case FormDataEvent.CREATE:
+        formDataService.checkUnique(formData, logger)
+        break
+    case FormDataEvent.CALCULATE:
+        calc()
+        logicCheck()
+        formDataService.saveCachedDataRows(formData, logger)
+        break
+    case FormDataEvent.CHECK:
+        logicCheck()
+        break
+    case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
+    case FormDataEvent.MOVE_CREATED_TO_APPROVED:  // Утвердить из "Создана"
+    case FormDataEvent.MOVE_CREATED_TO_ACCEPTED:  // Принять из "Создана"
+    case FormDataEvent.MOVE_PREPARED_TO_APPROVED: // Утвердить из "Подготовлена"
+    case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED: // Принять из "Подготовлена"
+    case FormDataEvent.MOVE_APPROVED_TO_ACCEPTED: // Принять из "Утверждена"
+        logicCheck()
+        break
+    case FormDataEvent.IMPORT:
+        importData()
+        formDataService.saveCachedDataRows(formData, logger)
+        break
+    case FormDataEvent.COMPOSE:
+        consolidation()
+        calc()
+        logicCheck()
+        formDataService.saveCachedDataRows(formData, logger)
+        break
+}
+
+@Field
+def providerCache = [:]
+@Field
+def recordCache = [:]
+
+@Field
+def allColumns = ['department', 'comparePeriod', 'comparePeriodIgnore', 'comparePeriodPercent', 'currentPeriod',
+        'currentPeriodIgnore', 'currentPeriodPercent', 'delta', 'deltaIgnore', 'deltaPercent']
+
+// общие графы у сводной и первичной/консолидированой 4-15 (графа 2..10)
+@Field
+def commonColumns = ['comparePeriod', 'comparePeriodIgnore', 'comparePeriodPercent', 'currentPeriod',
+        'currentPeriodIgnore', 'currentPeriodPercent', 'delta', 'deltaIgnore', 'deltaPercent']
+
+@Field
+def nonEmptyColumns = allColumns
+
+// итоговые графы (графа 2, 3, 5, 6, 8, 9)
+@Field
+def totalColumns = ['comparePeriod', 'comparePeriodIgnore', 'currentPeriod', 'currentPeriodIgnore', 'delta', 'deltaIgnore']
+
+@Field
+def startDateMap = [:]
+
+@Field
+def endDateMap = [:]
+
+def getStartDate(int reportPeriodId) {
+    if (startDateMap[reportPeriodId] == null) {
+        startDateMap[reportPeriodId] = reportPeriodService.getStartDate(reportPeriodId)?.time
+    }
+    return startDateMap[reportPeriodId]
+}
+
+def getEndDate(int reportPeriodId) {
+    if (endDateMap[reportPeriodId] == null) {
+        endDateMap[reportPeriodId] = reportPeriodService.getEndDate(reportPeriodId)?.time
+    }
+    return endDateMap[reportPeriodId]
+}
+
+// Поиск записи в справочнике по значению (для импорта)
+def getRecordIdImport(def Long refBookId, def String alias, def String value, def int rowIndex, def int colIndex,
+                      def boolean required = true) {
+    if (value == null || value.trim().isEmpty()) {
+        return null
+    }
+    return formDataService.getRefBookRecordIdImport(refBookId, recordCache, providerCache, alias, value,
+            getEndDate(formData.reportPeriodId), rowIndex, colIndex, logger, required)
+}
+
+void calc() {
+    def dataRows = formDataService.getDataRowHelper(formData).allCached
+    calcValues(dataRows, dataRows)
+}
+
+void logicCheck() {
+    def dataRows = formDataService.getDataRowHelper(formData).allCached
+    for (def row : dataRows) {
+        // 1. Проверка заполнения ячеек
+        def columns = (row.getAlias() ? commonColumns : nonEmptyColumns)
+        checkNonEmptyColumns(row, row.getIndex(), columns, logger, true)
+    }
+
+    // получить строки из шаблона
+    def formTemplate = formDataService.getFormTemplate(formData.formType.id, formData.reportPeriodId)
+    def tempRows = formTemplate.rows
+    updateIndexes(tempRows)
+    // 2. Проверка графы 3 при расчете графы 4
+    // 3. Проверка графы 6 при расчете графы 7
+    calcValues(tempRows, dataRows)
+}
+
+/**
+ * Посчитать значения.
+ *
+ * @param resultRows строки в которые запишутся результаты
+ * @param sourceRows строки, данные которых будут использоваться в расчетах
+ */
+void calcValues(def resultRows, def sourceRows) {
+    def resultRow = getDataRow(resultRows, 'total')
+    def sourceRow = getDataRow(sourceRows, 'total')
+
+    // признак показывать ли сообщение логической проверки 3
+    def needShowMsg = resultRows != sourceRows
+
+    // графа 2, 3, 5, 6, 8, 9
+    calcTotalSum(sourceRows, resultRow, totalColumns)
+
+    // графа 4
+    resultRow.comparePeriodPercent = calc4(sourceRow, needShowMsg)
+    // графа 7
+    resultRow.currentPeriodPercent = calc7(sourceRow, needShowMsg)
+    // графа 10
+    resultRow.deltaPercent = calc10(resultRow)
+}
+
+def calc4(def row, def needShowMsg) {
+    return calc4or7(row, 'comparePeriodIgnore', 'comparePeriod', needShowMsg, 'comparePeriodPercent')
+}
+
+def calc7(def row, def needShowMsg) {
+    return calc4or7(row, 'currentPeriodIgnore', 'currentPeriod', needShowMsg, 'currentPeriodPercent')
+}
+
+/**
+ * Расчитать значение для графы.
+ *
+ * @param row строка для расчета
+ * @param dividendAlias алиас графы числителя
+ * @param dividerAlias алиас графы знаменателя
+ * @param needShowMsg выводить ли сообщение логической проверки 3
+ * @param resultAlias алиас графы для которой производится расчет (нужен для вывода сообщения)
+ */
+def calc4or7(def row, def dividendAlias, def dividerAlias, def needShowMsg, def resultAlias) {
+    def result = 0
+    def dividend = (row[dividendAlias] ?: 0)
+    def divider = row[dividerAlias]
+    // проверка делителя на 0 или null
+    if (divider) {
+        // расчет
+        result = dividend * 100 / divider
+    } else if (needShowMsg) {
+        // Логическая проверка 2. Проверка графы 3 при расчете графы 4
+        // Логическая проверка 3. Проверка графы 6 при расчете графы 7
+        def msg = String.format("Строка %s: Графа «%s» не может быть заполнена. Выполнение расчета невозможно, так как в результате проверки получен нулевой знаменатель (деление на ноль невозможно). Ячейка будет заполнена значением «0».",
+                row.getIndex(), getColumnName(row, resultAlias))
+        rowWarning(logger, row, msg)
+    }
+    return result
+}
+
+def calc10(def row) {
+    if (row.currentPeriodPercent == null || row.comparePeriodPercent == null) {
+        return null
+    }
+    return row.currentPeriodPercent - row.comparePeriodPercent
+}
+
+void consolidation() {
+    def dataRows = formDataService.getDataRowHelper(formData).allCached
+    def totalRow = getDataRow(dataRows, 'total')
+    dataRows = []
+    def sourceFormTypeId = 715
+    departmentFormTypeService.getFormSources(formData.departmentId, formData.formType.id, formData.kind,
+            getStartDate(formData.reportPeriodId), getEndDate(formData.reportPeriodId)).each {
+        if (it.formTypeId == sourceFormTypeId) {
+            def source = formDataService.getLast(it.formTypeId, it.kind, it.departmentId, formData.reportPeriodId, formData.periodOrder)
+            if (source != null && source.state == WorkflowState.ACCEPTED) {
+                def sourceRows = formDataService.getDataRowHelper(source)?.allSaved
+                def row = getDataRow(sourceRows, 'R1')
+                def newRow = formData.createDataRow()
+                // графа 1
+                newRow.department = it.departmentId
+                // графа 2..10
+                commonColumns.each { column ->
+                    newRow[column] = row[column]
+                }
+                dataRows.add(newRow)
+            }
+        }
+    }
+    // сортируем по наименованию подразделения
+    dataRows.sort { departmentService.get(it.department as Integer).name }
+    dataRows.add(totalRow)
+    updateIndexes(dataRows)
+    formDataService.getDataRowHelper(formData).allCached = dataRows
+}
+
+void importData() {
+    int COLUMN_COUNT = 10
+    int HEADER_ROW_COUNT = 3
+    String TABLE_START_VALUE = 'Подразделение Банка'
+    String TABLE_END_VALUE = null
+    int INDEX_FOR_SKIP = 0
+
+    def allValues = []      // значения формы
+    def headerValues = []   // значения шапки
+    def paramsMap = ['rowOffset': 0, 'colOffset': 0]  // мапа с параметрами (отступы сверху и слева)
+
+    checkAndReadFile(ImportInputStream, UploadFileName, allValues, headerValues, TABLE_START_VALUE, TABLE_END_VALUE, HEADER_ROW_COUNT, paramsMap)
+
+    // проверка шапки
+    checkHeaderXls(headerValues, COLUMN_COUNT, HEADER_ROW_COUNT)
+    if (logger.containsLevel(LogLevel.ERROR)) {
+        return
+    }
+    // освобождение ресурсов для экономии памяти
+    headerValues.clear()
+    headerValues = null
+
+    def fileRowIndex = paramsMap.rowOffset
+    def colOffset = paramsMap.colOffset
+    paramsMap.clear()
+    paramsMap = null
+
+    def rowIndex = 0
+    def rows = []
+    def allValuesCount = allValues.size()
+    def totalRowFromFile = null
+
+    // формирвание строк нф
+    for (def i = 0; i < allValuesCount; i++) {
+        def rowValues = allValues[0]
+        fileRowIndex++
+        // все строки пустые - выход
+        if (!rowValues) {
+            allValues.remove(rowValues)
+            rowValues.clear()
+            break
+        }
+        // Пропуск итоговых строк
+        if (rowValues[INDEX_FOR_SKIP] == "В целом по Банку") {
+            totalRowFromFile = getNewRowFromXls(rowValues, colOffset, fileRowIndex, rowIndex, true)
+            allValues.remove(rowValues)
+            rowValues.clear()
+            continue
+        }
+        // простая строка
+        def newRow = getNewRowFromXls(rowValues, colOffset, fileRowIndex, rowIndex)
+        rows.add(newRow)
+        // освободить ненужные данные - иначе не хватит памяти
+        allValues.remove(rowValues)
+        rowValues.clear()
+    }
+
+    // получить строки из шаблона
+    def formTemplate = formDataService.getFormTemplate(formData.formType.id, formData.reportPeriodId)
+    def templateRows = formTemplate.rows
+
+    // итоговая строка
+    def totalRow = getDataRow(templateRows, 'total')
+    rows.add(totalRow)
+    updateIndexes(rows)
+
+    if (totalRowFromFile) {
+        commonColumns.each {
+            totalRow[it] = totalRowFromFile[it]
+        }
+        // подсчет итогов
+        totalRowFromFile.setAlias('total')
+        calcValues([totalRowFromFile], rows)
+        // сравнение итогов
+        compareTotalValues(totalRow, totalRowFromFile, commonColumns, logger, false);
+    }
+
+    showMessages(rows, logger)
+    if (!logger.containsLevel(LogLevel.ERROR)) {
+        formDataService.getDataRowHelper(formData).allCached = rows
+    }
+}
+
+/**
+ * Проверить шапку таблицы
+ *
+ * @param headerRows строки шапки
+ * @param colCount количество колонок в таблице
+ * @param rowCount количество строк в таблице
+ */
+void checkHeaderXls(def headerRows, def colCount, rowCount) {
+    if (headerRows.isEmpty()) {
+        throw new ServiceException(WRONG_HEADER_ROW_SIZE)
+    }
+    checkHeaderSize(headerRows[headerRows.size() - 1].size(), headerRows.size(), colCount, rowCount)
+
+    def headerMapping = [
+            (headerRows[0][0]) : 'Подразделение Банка',
+            (headerRows[0][2]) : 'Период сравнения',
+            (headerRows[0][5]) : 'Период',
+            (headerRows[0][8]) : 'Изменения за период',
+            (headerRows[1][2]) : 'НДС всего',
+            (headerRows[1][3]) : 'В том числе НДС не учитываемый',
+            (headerRows[1][4]) : 'Доля НДС не учитываемый, %',
+            (headerRows[1][5]) : 'НДС всего',
+            (headerRows[1][6]) : 'В том числе НДС не учитываемый',
+            (headerRows[1][7]) : 'Доля НДС не учитываемый, %',
+            (headerRows[1][8]) : 'НДС всего',
+            (headerRows[1][9]) : 'В том числе НДС не учитываемый',
+            (headerRows[1][10]): 'Доля НДС не учитываемый, %',
+            '1' : '1'
+    ]
+    (2..10).each { index ->
+        headerMapping.put((headerRows[2][index]), index.toString())
+    }
+    checkHeaderEquals(headerMapping, logger)
+}
+
+/**
+ * Заполняет заданную строку нф значениями из экселя.
+ *
+ * @param row строка нф
+ * @param values список строк со значениями
+ * @param fileRowIndex номер строки в тф
+ * @param rowIndex номер строки в нф
+ * @param colOffset отступ по столбцам
+ */
+def fillRowFromXls(def row, def values, def colOffset, def fileRowIndex, def rowIndex) {
+    row.setIndex(rowIndex)
+    row.setImportIndex(fileRowIndex)
+    // графа 1..10
+    def colIndex = -1
+    allColumns.each { alias ->
+        colIndex++
+        row[alias] = parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+    }
+    return row
+}
+
+/**
+ * Получить новую строку нф по значениям из экселя.
+ *
+ * @param values список строк со значениями
+ * @param colOffset отступ в колонках
+ * @param fileRowIndex номер строки в тф
+ * @param rowIndex строка в нф
+ * @param isTotal признак того что строка итоговая
+ */
+def getNewRowFromXls(def values, def colOffset, def fileRowIndex, def rowIndex, def isTotal = false) {
+    def newRow = formData.createStoreMessagingDataRow()
+    newRow.setIndex(rowIndex)
+    newRow.setImportIndex(fileRowIndex)
+
+    // графа 1
+    def colIndex = 1
+    if (!isTotal) {
+        newRow.department = getRecordIdImport(30, 'NAME', values[colIndex], fileRowIndex, colIndex + colOffset, false)
+    }
+
+    // графа 2..10
+    colIndex = 1
+    commonColumns.each { alias ->
+        colIndex++
+        newRow[alias] = parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+    }
+    return newRow
+}
