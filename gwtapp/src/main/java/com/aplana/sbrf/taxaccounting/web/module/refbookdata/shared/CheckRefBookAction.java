afda6d03318f47d47cf8274595d8b9f01c813380
Alexander Vanteev <avanteev@aplana.com>
2015-09-24 11:04:27 +0300

    http://jira.aplana.com/browse/SBRFACCTAX-9384 Некорректное введение адреса справочника.

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/shared/CheckRefBookAction.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/refbookdata/shared/CheckRefBookAction.java
@@ -3,6 +3,9 @@ package com.aplana.sbrf.taxaccounting.web.module.refbookdata.shared;
 import com.aplana.sbrf.taxaccounting.web.main.api.shared.dispatch.ActionName;
 import com.gwtplatform.dispatch.shared.UnsecuredActionImpl;
 
+/**
+ * Проверка доступа к справочнику
+ */
 public class CheckRefBookAction extends UnsecuredActionImpl<CheckRefBookResult> implements ActionName {
 
     private long refBookId;
