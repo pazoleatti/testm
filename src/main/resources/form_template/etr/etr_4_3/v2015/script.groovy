25dbb1f1611205e363cbc6ffdf0dee6440d94df1
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2015-10-05 16:14:49 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-12780 0.7.2 ЭНС. Реализовать расчет "Прил.4-3" (первичный, консолидированный)

--- src/main/resources/form_template/etr/etr_4_3/v2015/script.groovy
+++ src/main/resources/form_template/etr/etr_4_3/v2015/script.groovy
@@ -1,4 +1,4 @@
-package form_template.etr.etr_4_2.v2015
+package form_template.etr.etr_4_3.v2015
 
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
 import com.aplana.sbrf.taxaccounting.model.FormDataKind
