6001e7821a4a7b31985637b9a5f3aa0e8f0061bc
Ramil Timerbaev <Rtimerbaev@aplana.com>
2015-10-14 11:50:54 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-12966 Расходы сложные. Отсутствует ЛП №1.

--- src/main/resources/form_template/income/outcome_simple_1/v2015/script.groovy
+++ src/main/resources/form_template/income/outcome_simple_1/v2015/script.groovy
@@ -221,8 +221,14 @@ void logicCheck() {
         logger.warn("Cтроки ${rowIndexes102.join(', ')}: Отсутствуют данные бухгалтерской отчетности в форме \"Отчет о прибылях и убытках\"")
     }
 
-    dataRows.each {row ->
-         checkRequiredColumns(row, nonEmptyColumns)
+    for (def row : dataRows) {
+        def columns = []
+        nonEmptyColumns.each { alias ->
+            if (row.getCell(alias)?.style?.alias == editableStyle) {
+                columns.add(alias)
+            }
+        }
+        checkNonEmptyColumns(row, row.getIndex(), columns, logger, true)
     }
 
     def row50001 = getDataRow(dataRows, total1Alias)
@@ -622,22 +628,6 @@ boolean isEqualNum(String accNum, def balance) {
     def a = accNum?.replace('.', '')
     def b = (balance ? getBalanceValue(balance)?.replace('.', '') : null)
     return a == b
-}
-
-/** Проверить заполненость обязательных полей. */
-void checkRequiredColumns(def row, def columns) {
-    def colNames = []
-    columns.each {
-        def cell = row.getCell(it)
-        if (cell?.style?.alias == editableStyle && (cell.getValue() == null || row[it] == '')) {
-            colNames.add('"' + getColumnName(row, it) + '"')
-        }
-    }
-    if (!colNames.isEmpty()) {
-        def index = row.getIndex()
-        def errorMsg = colNames.join(', ')
-        logger.error("В строке $index не заполнены колонки : $errorMsg.")
-    }
 }
 
 // для уроня Банка:	проверка наличия и принятия РНУ-14
