b4735fa303c73275994c4dcc8e316cd3bd2e06b0
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2015-10-29 10:35:25 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-13161 0.8 ТЦО. Реализовать макет формы Приложение 4.1 (6 месяцев). Только макет

---
+++ script-test/src/test/java/com/aplana/sbrf/taxaccounting/form_template/deal/app_4_1/v2015/App_4_1Test.java
@@ -0,0 +1,69 @@
+package com.aplana.sbrf.taxaccounting.form_template.deal.app_4_1.v2015;
+
+import com.aplana.sbrf.taxaccounting.model.*;
+import com.aplana.sbrf.taxaccounting.util.ScriptTestBase;
+import com.aplana.sbrf.taxaccounting.util.TestScriptHelper;
+import com.aplana.sbrf.taxaccounting.util.mock.ScriptTestMockHelper;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Приложение 4.1. (6 месяцев) Отчет в отношении доходов и расходов Банка по сделкам с российскими ВЗЛ, применяющими Общий режим налогообложения
+ */
+public class App_4_1Test extends ScriptTestBase {
+    private static final int TYPE_ID = 801;
+    private static final int DEPARTMENT_ID = 1;
+    private static final int REPORT_PERIOD_ID = 1;
+    private static final int DEPARTMENT_PERIOD_ID = 1;
+    private static final FormDataKind KIND = FormDataKind.PRIMARY;
+
+    @Override
+    protected FormData getFormData() {
+        FormData formData = new FormData();
+        FormType formType = new FormType();
+        formType.setId(TYPE_ID);
+        formData.setId(TestScriptHelper.CURRENT_FORM_DATA_ID);
+        formData.setFormType(formType);
+        formData.setFormTemplateId(TYPE_ID);
+        formData.setKind(KIND);
+        formData.setState(WorkflowState.CREATED);
+        formData.setDepartmentId(DEPARTMENT_ID);
+        formData.setDepartmentReportPeriodId(DEPARTMENT_PERIOD_ID);
+        formData.setReportPeriodId(REPORT_PERIOD_ID);
+        formData.setPeriodOrder(1);
+        return formData;
+    }
+
+    @Override
+    protected ScriptTestMockHelper getMockHelper() {
+        return getDefaultScriptTestMockHelper(App_4_1Test.class);
+    }
+
+    @Before
+    public void mockServices() {
+    }
+
+    @Test
+    public void create() {
+        testHelper.execute(FormDataEvent.CREATE);
+        Assert.assertEquals(testHelper.getFormTemplate().getRows().size(), testHelper.getDataRowHelper().getAll().size());
+        checkLogger();
+    }
+
+    // Проверка пустой
+    @Test
+    public void checkTest() {
+        testHelper.execute(FormDataEvent.CHECK);
+        checkLogger();
+    }
+
+    // Расчет пустой
+    @Test
+    public void calcTest() {
+        testHelper.execute(FormDataEvent.CALCULATE);
+        checkLogger();
+    }
+
+    // TODO добавить тесты
+}
