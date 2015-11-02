fc3fd4380587f9478011997214421aacfdc6e2c6
Marat Fayzullin <mfayzullin@aplana.com>
2015-11-02 20:17:15 +0500

    Юнит-тест для DBInfo :)

--- dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/DBInfoTest.java
+++ dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/DBInfoTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"ContextTest.xml"})
 @Transactional
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class DBInfoTest {
