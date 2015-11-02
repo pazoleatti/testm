ee5d0afe08cb5e63a6f83f6ec356c8f8d5ca3730
Marat Fayzullin <mfayzullin@aplana.com>
2015-11-02 20:01:55 +0500

    Юнит-тест для DBInfo :)

---
+++ dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/DBInfoTest.java
@@ -0,0 +1,31 @@
+package com.aplana.sbrf.taxaccounting.dao.impl;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author <a href="mailto:Marat.Fayzullin@aplana.com">Файзуллин Марат</a>
+ * @since 02.11.2015 19:54
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class DBInfoTest {
+
+	@Autowired
+	DBInfo dbInfo;
+
+	@Test
+	public void test() {
+		Assert.assertFalse(dbInfo.isSupportOver());
+		Assert.assertTrue(dbInfo.isWithRecursive());
+		Assert.assertFalse(dbInfo.isDateDiffNumber());
+	}
+
+}
