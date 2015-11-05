1a26b67a236c32cc4569d793b37d18c6d2c66a57
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2015-11-05 19:11:23 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-13185 0.8 ТЦО. Реализовать форму Приложение 4.2.

--- src/main/resources/form_template/deal/app_4_2/v2015/script.groovy
+++ src/main/resources/form_template/deal/app_4_2/v2015/script.groovy
@@ -20,14 +20,15 @@ import groovy.transform.Field
 // sum53            - Сделки купли-продажи драгоценных металлов
 // sum54            - Сделки, отраженные в Журнале взаиморасчетов
 // sum55            - Сделки с лицами, информация о которых не  отражена в Журнале взаиморасчетов
+// sum56            - Сумма дополнительно начисленных налогооблагаемых расходов
 // sum61            - Сделки с ценными бумагами
 // sum62            - Сделки купли-продажи иностранной валюты
 // sum63            - Сделки купли-продажи драгоценных металлов
 // sum64            - Сделки, отраженные в Журнале взаиморасчетов
 // sum65            - Сделки с лицами, информация о которых не  отражена в Журнале взаиморасчетов
-// sum7             - Сумма дополнительно начисленных налогооблагаемых доходов, тыс. руб.
-// sum8             - Итого объем доходов и расходов, тыс. руб.
-// thresholdValue   - Применимое Пороговое значение, тыс. руб.
+// sum66            - Сумма дополнительно начисленных налогооблагаемых расходов
+// sum7             - Итого объем доходов и расходов, руб.
+// thresholdValue   - Применимое Пороговое значение, руб.
 // sign             - Признак наличия Контролируемых сделок (Да / Нет)
 // categoryRevised  - Пересмотренная Категория юридического лица по состоянию на 1 апреля следующего Налогового периода
 
@@ -44,10 +45,6 @@ switch (formDataEvent) {
         logicCheck()
         break
     case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
-        calc()
-        logicCheck()
-        formDataService.saveCachedDataRows(formData, logger)
-        break
     case FormDataEvent.MOVE_CREATED_TO_APPROVED:  // Утвердить из "Создана"
     case FormDataEvent.MOVE_PREPARED_TO_APPROVED: // Утвердить из "Подготовлена"
     case FormDataEvent.MOVE_CREATED_TO_ACCEPTED:  // Принять из "Создана"
@@ -56,7 +53,7 @@ switch (formDataEvent) {
         logicCheck()
         break
     case FormDataEvent.COMPOSE:
-        formDataService.consolidationSimple(formData, logger)
+        consolidation()
         calc()
         logicCheck()
         formDataService.saveCachedDataRows(formData, logger)
@@ -80,11 +77,13 @@ def editableColumns = []
 
 // Автозаполняемые атрибуты
 @Field
-def autoFillColumns = []
+def autoFillColumns = ['name', 'group', 'sum51', 'sum52', 'sum53', 'sum54', 'sum55', 'sum56', 'sum61', 'sum62', 'sum63',
+                       'sum64', 'sum65', 'sum66', 'sum7', 'thresholdValue', 'sign', 'categoryRevised']
 
 // Проверяемые на пустые значения атрибуты
 @Field
-def nonEmptyColumns = []
+def nonEmptyColumns = ['name', 'group', 'sum51', 'sum52', 'sum53', 'sum54', 'sum55', 'sum56', 'sum61', 'sum62', 'sum63',
+                       'sum64', 'sum65', 'sum66', 'sum7', 'thresholdValue', 'sign', 'categoryRevised']
 
 // Дата начала отчетного периода
 @Field
@@ -106,6 +105,26 @@ def getReportPeriodStartDate() {
         startDate = reportPeriodService.getStartDate(formData.reportPeriodId).time
     }
     return startDate
+}
+
+// "Да"
+@Field
+def Long recYesId
+
+def Long getRecYesId() {
+    if (recYesId == null)
+        recYesId = getRecordId(38, 'CODE', '1')
+    return recYesId
+}
+
+// "Нет"
+@Field
+def Long recNoId
+
+def Long getRecNoId() {
+    if (recNoId == null)
+        recNoId = getRecordId(38, 'CODE', '0')
+    return recNoId
 }
 
 //// Обертки методов
@@ -138,14 +157,66 @@ def getRefBookValue(def long refBookId, def Long recordId) {
 void calc() {
     def dataRows = formDataService.getDataRowHelper(formData).allCached
     for (def row : dataRows) {
+        row.group = calc4(row)
+        row.sum7 = calc17(row)
+        row.sum8 = calc18(row)
+        row.sign = calc19(row)
+        row.categoryRevised = calc20(row)
     }
+}
+
+def Long calc4(def row) {
+    // TODO
+    return 0
+}
+
+def BigDecimal calc17(def row) {
+    // Графа 17 = сумма значений в графах 5-16
+    def value = 0
+    ['group', 'sum51', 'sum52', 'sum53', 'sum54', 'sum55', 'sum56', 'sum61', 'sum62', 'sum63', 'sum64', 'sum65', 'sum66',
+     'sum7', 'thresholdValue', 'sign', 'categoryRevised'].each {
+        value += row[it] ?: 0
+    }
+    return value
+}
+
+def BigDecimal calc18(def row) {
+    // TODO
+    return 0
+}
+
+def Long calc19(def row) {
+    if (row.sum7 >= row.sum8) {
+        return getRecYesId()
+    }
+    return getRecNoId()
+}
+
+def Long calc20(def row) {
+    // TODO
+    return 0
 }
 
 void logicCheck() {
     def dataRows = formDataService.getDataRowHelper(formData).allCached
     for (def row : dataRows) {
+        if (row.getAlias() != null) {
+            continue
+        }
+        def rowNum = row.getIndex()
+
         // 1. Проверка заполнения обязательных полей
         checkNonEmptyColumns(row, row.getIndex(), nonEmptyColumns, logger, true)
+
+        // 2. Отсутствие нулевых значений
+        if (calc17(row) == 0) {
+            logger.error("Строка $rowNum: Объем доходов и расходов по всем сделкам не может быть нулевым!")
+        }
+
+        // TODO
+        // 3. Наличие порогового значения
+        // 4. Наличие правила назначения категории
+        // 5. Проверка соответствия категории пороговым значениям
     }
 }
 
@@ -153,7 +224,7 @@ void logicCheck() {
 void sortFormDataRows(def saveInDB = true) {
     def dataRowHelper = formDataService.getDataRowHelper(formData)
     def dataRows = dataRowHelper.allCached
-    sortRows(refBookService, logger, dataRows, getSubTotalRows(dataRows), null, true)
+    sortRows(refBookService, logger, dataRows, null, null, null)
     if (saveInDB) {
         dataRowHelper.saveSort()
     } else {
@@ -161,7 +232,6 @@ void sortFormDataRows(def saveInDB = true) {
     }
 }
 
-// Получение подитоговых строк
-def getSubTotalRows(def dataRows) {
-    return dataRows.findAll { it.getAlias() != null}
+def consolidation() {
+    // TODO
 }
