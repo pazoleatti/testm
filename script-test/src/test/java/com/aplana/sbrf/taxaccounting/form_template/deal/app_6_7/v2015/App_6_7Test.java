426d75ed1d6ac879596ca4118e9e61463f5d16eb
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2015-11-19 09:41:12 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-13490 откатил изменения

--- script-test/src/test/java/com/aplana/sbrf/taxaccounting/form_template/deal/app_6_7/v2015/App_6_7Test.java
+++ script-test/src/test/java/com/aplana/sbrf/taxaccounting/form_template/deal/app_6_7/v2015/App_6_7Test.java
@@ -136,7 +136,7 @@ public class App_6_7Test extends ScriptTestBase {
         Assert.assertEquals("Строка 1: Значение графы «Дата совершения сделки» должно быть не меньше значения графы «Дата договора»!", entries.get(i++).getMessage());
         Assert.assertEquals("Строка 1: Год, указанный по графе «Дата совершения сделки» (2989), должен относиться к календарному году текущей формы (2014)!", entries.get(i++).getMessage());
         Assert.assertEquals("Строка 1: Значение даты атрибута «Дата договора» должно принимать значение из следующего диапазона: 01.01.1900 - 31.12.2099", entries.get(i++).getMessage());
-        Assert.assertEquals("Строка 1: Значение графы «Сумма доходов Банка, руб.» должно быть больше или равно «0»!", entries.get(i++).getMessage());
+        Assert.assertEquals("Строка 1: Значение графы «Сумма доходов Банка, руб.» должно быть больше значения «0»!", entries.get(i++).getMessage());
         Assert.assertEquals(i, testHelper.getLogger().getEntries().size());
         testHelper.getLogger().clear();
 
