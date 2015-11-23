a465fbeb54aa9c89225f8a88d5352948a45b7fbb
Lenar Haziev <lhaziev@aplana.com>
2015-11-23 18:22:02 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-13452 0.8.1 Реализовать аналитический отчет «Сведения о начисленных и уплачиваемых налогах, сборах и взносах, отнесенных на расходы» (ЦА)

--- src/main/resources/form_template/etr/etr_4_17/v2015/script.groovy
+++ src/main/resources/form_template/etr/etr_4_17/v2015/script.groovy
@@ -1,6 +1,7 @@
 package form_template.etr.etr_4_17.v2015
 
 import com.aplana.sbrf.taxaccounting.model.FormDataEvent
+import com.aplana.sbrf.taxaccounting.model.log.LogLevel
 import com.aplana.sbrf.taxaccounting.model.script.range.ColumnRange
 import groovy.transform.Field
 
@@ -16,13 +17,24 @@ switch (formDataEvent) {
     case FormDataEvent.CREATE:
         formDataService.checkUnique(formData, logger)
         break
+    case FormDataEvent.ADD_ROW:
+        formDataService.addRow(formData, currentDataRow, editableColumns, autoFillColumns)
+        break
+    case FormDataEvent.DELETE_ROW:
+        formDataService.getDataRowHelper(formData).delete(currentDataRow)
+        break
     case FormDataEvent.CALCULATE:
         preCalcCheck()
+        if (logger.containsLevel(LogLevel.ERROR))
+            return
         calc()
         logicCheck()
         formDataService.saveCachedDataRows(formData, logger)
         break
     case FormDataEvent.CHECK:
+        preCalcCheck()
+        if (logger.containsLevel(LogLevel.ERROR))
+            return
         logicCheck()
         break
     case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
@@ -31,15 +43,25 @@ switch (formDataEvent) {
     case FormDataEvent.MOVE_PREPARED_TO_APPROVED: // Утвердить из "Подготовлена"
     case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED: // Принять из "Подготовлена"
     case FormDataEvent.MOVE_APPROVED_TO_ACCEPTED: // Принять из "Утверждена"
+        preCalcCheck()
         logicCheck()
         break
 }
 
+// Редактируемые атрибуты
+@Field
+def editableColumns = ['department']
+
+// Автозаполняемые атрибуты
+@Field
+def autoFillColumns = ['parentTB', 'tax26411_01', 'tax26411_02', 'sum34', 'rate5', 'tax26411_03', 'rate7', 'tax26411_13', 'rate9', 'tax26411_12', 'rate11', 'tax26412', 'rate13', 'tax26410_09', 'rate15', 'sum', 'rate']
+
+// Атрибуты по которым рассчитываются итоги
 @Field
 def totalColumns = ['tax26411_01', 'tax26411_02', 'sum34', 'tax26411_03', 'tax26411_13', 'tax26411_12', 'tax26412', 'tax26410_09', 'sum']
 
 @Field
-def nonEmptyColumns = ['tax26411_01', 'tax26411_02', 'sum34', 'rate5', 'tax26411_03', 'rate7', 'rate9', 'tax26411_13', 'rate11', 'tax26411_12', 'rate13', 'tax26412', 'rate15', 'tax26410_09', 'sum', 'rate']
+def nonEmptyColumns = ['department', 'tax26411_01', 'tax26411_02', 'sum34', 'rate5', 'tax26411_03', 'rate7', 'tax26411_13', 'rate9', 'tax26411_12', 'rate11', 'tax26412', 'rate13', 'tax26410_09', 'rate15', 'sum', 'rate']
 
 // alias -> opu code
 @Field
@@ -62,26 +84,6 @@ def rateMap = ['rate5': 'sum34',
 
 @Field
 def providerCache = [:]
-
-@Field
-def departmentMap = [R1: 4,
-                     R2: 8,
-                     R3: 16,
-                     R4: 20,
-                     R5: 27,
-                     R6: 32,
-                     R7: 44,
-                     R8: 52,
-                     R9: 64,
-                     R10: 72,
-                     R11: 82,
-                     R12: 88,
-                     R13: 97,
-                     R14: 102,
-                     R15: 109,
-                     R16: 37,
-                     R17: 113
-                    ]
 
 @Field
 def periodMap = [:]
@@ -140,6 +142,26 @@ def getPeriodNameBO(def date) {
     return periodNameBOMap[date]
 }
 
+@Field
+def parentTBMap = [:]
+
+@Field
+def departmentMap = [:]
+
+def getParentTBId(def id) {
+    if (id != null && parentTBMap[id] == null) {
+        parentTBMap[id] = departmentService.getParentTBId(id)
+    }
+    return parentTBMap[id]
+}
+
+def getDepartment(Integer id) {
+    if (id != null && departmentMap[id] == null) {
+        departmentMap[id] = departmentService.get(id)
+    }
+    return departmentMap[id]
+}
+
 void checkOpuCodes(def department, def date) {
     def opuCodes = opuMap.values()
     def accountPeriodId = bookerStatementService.getAccountPeriodId(department.id, date)
@@ -173,18 +195,39 @@ void checkOpuCodes(def department, def date) {
         def missedCodes = opuCodes.findAll { !recordOpuCodes.contains(it) }
         if (!missedCodes.isEmpty()) {
             logger.warn('Форма 102 бухгалтерской отчетности: Период: %s %s, Подразделение: %s. ' +
-                    'Отсутствуют значения по следующим символам: %s! При заполнении граф формы значения по данным символам будут приняты за нулевые.',
+                    'Отсутствуют значения по следующим символам: %s! При заполнении формы, значения по данным символам будут приняты за нулевые.',
                     getPeriodNameBO(date), date.format('yyyy'), department.name, missedCodes.join(', '))
         }
     }
 }
 
 void preCalcCheck() {
+    def dataRows = formDataService.getDataRowHelper(formData).allCached
     // находим записи для текущего периода и подразделений по строкам
     def reportPeriod = getReportPeriod(formData.reportPeriodId)
     def date = getEndDate(reportPeriod?.taxPeriod?.year, reportPeriod?.order)
-    departmentMap.values().each{departmentId ->
-        checkOpuCodes(departmentService.get(departmentId), date)
+    def prevDate
+    if (!formData.accruing && reportPeriod.order != 1) {
+        prevDate = getEndDate(reportPeriod?.taxPeriod?.year, reportPeriod?.order - 1)
+    }
+    for(def row : dataRows) {
+        if (row.getAlias())
+            continue
+
+        if (row.department != null) {
+            def parentTB = getParentTBId(row.department.intValue())
+            if (parentTB == 113) {
+                if (!formData.accruing && reportPeriod.order != 1) {
+                    checkOpuCodes(getDepartment(row.department.intValue()), prevDate)
+                }
+                checkOpuCodes(getDepartment(row.department.intValue()), date)
+            } else {
+                logger.error("Строка %s: Графа «%s»: Выбранное подразделение не является дочерним подразделением для ЦА!",
+                        row.getIndex(), getColumnName(row, 'department'))
+            }
+        } else {
+            checkNonEmptyColumns(row, row.getIndex(), ['department'], logger, true)
+        }
     }
 }
 
@@ -194,48 +237,72 @@ def get102(def departmentId, def date) {
     // справочник "Отчет о прибылях и убытках (Форма 0409102-СБ)"
     def records = bookerStatementService.getRecords(52L, departmentId, date, filter)
     if (records == null || records.isEmpty()) {
-        return [:]
+        return [[:], false]
     }
-    return records.collect{
+    return [records.collect{
         def record = [:]
         record[it.OPU_CODE.stringValue] = it.TOTAL_SUM.numberValue / 1000
         return record
-    }.sum()
+    }.sum(), true]
+}
+
+def calcBO(def departmentId) {
+    def result = [:]
+    def reportPeriod = getReportPeriod(formData.reportPeriodId)
+    def date = getEndDate(reportPeriod?.taxPeriod?.year, reportPeriod?.order)
+    def pair = get102(departmentId, date)
+    opuMap.each { k, v ->
+        result[k] = pair[1] ? (pair[0][v]?pair[0][v]:0) : 0
+    }
+    if (!formData.accruing && reportPeriod.order != 1 && pair[1]) {
+        def prevDate = getEndDate(reportPeriod?.taxPeriod?.year, reportPeriod?.order - 1)
+        pair = get102(departmentId, prevDate)
+        opuMap.each { k, v ->
+            result[k] = result[k] - (pair[1] ? (pair[0][v]?:0) : 0)
+        }
+    }
+    return result
 }
 
 void calc() {
     def dataRows = formDataService.getDataRowHelper(formData).allCached
-    def reportPeriod = getReportPeriod(formData.reportPeriodId)
-    def date = getEndDate(reportPeriod?.taxPeriod?.year, reportPeriod?.order)
     // получение данных из БО
-    for (def alias in departmentMap.keySet()) {
-        def row = getDataRow(dataRows, alias)
-        def records = get102(departmentMap.get(alias), date)
-        opuMap.each{k,v ->
-            def value = records?records[v]:0
-            row[k] = value
+    for(def row : dataRows) {
+        if (row.getAlias())
+            continue
+
+        row.parentTB = row.department ? getParentTBId(row.department.intValue()) : null
+        def records = calcBO(row.department?.intValue())
+        opuMap.each { k, v ->
+            row[k] = records[k]
         }
         row.sum34 = row.tax26411_01 + row.tax26411_02
         row.sum = row.sum34 + row.tax26411_01 + row.tax26411_02 + row.tax26411_03 + row.tax26411_13 + row.tax26411_12 +
-                        row.tax26412 + row.tax26410_09
+                row.tax26412 + row.tax26410_09
     }
 
-    //расчет итоговой строки
+    // расчет итоговой строки
     def totalRow = getDataRow(dataRows, 'total')
     totalColumns.each { alias ->
-        totalRow[alias] = summ(formData, dataRows, new ColumnRange(alias, getDataRow(dataRows, 'R1').getIndex() - 1, getDataRow(dataRows, 'R17').getIndex() - 1))
+        totalRow[alias] = summ(formData, dataRows, new ColumnRange(alias, 0, dataRows.size() - 2))
         def rateAlias = rateMap.find{ it.value==alias }?.key
         if (rateAlias)
             totalRow[rateAlias] = totalRow[alias] > 0 ? 100 : 0
     }
 
-    //расчет процентов
-    for (def alias in departmentMap.keySet()) {
-        def row = getDataRow(dataRows, alias)
+    // расчет процентов
+    for(def row : dataRows) {
+        if (row.getAlias())
+            continue
+
         rateMap.each { k, v ->
             row[k] = calcRate(row, totalRow, v, k)
         }
     }
+
+    dataRows.remove(totalRow)
+    dataRows.sort { getDepartment(it.department?.intValue())?.name }
+    dataRows.add(totalRow)
 }
 
 def calcRate(def row, def totalRow, def alias, def resultAlias) {
@@ -247,16 +314,30 @@ def calcRate(def row, def totalRow, def alias, def resultAlias) {
         // расчет
         result = dividend * 100 / divider
     } else {
-        def msg = String.format("Строка %s: Графа «%s»: выполнение расчета невозможно, так как в результате проверки получен нулевой знаменатель (деление на ноль невозможно)». Ячейка будет заполнена значением «0».",
+        logger.warn("Строка %s: Графа «%s»: выполнение расчета невозможно, так как в результате проверки получен нулевой знаменатель (деление на ноль невозможно)». Ячейка будет заполнена значением «0».",
                 row.getIndex(), getColumnName(row, resultAlias))
-        logger.warn(msg)
     }
     return result
 }
 
 void logicCheck() {
     def dataRows = formDataService.getDataRowHelper(formData).allCached
-    for(int i = 0; i < dataRows.size(); i++){
-        checkNonEmptyColumns(dataRows[i], dataRows[i].getIndex(), nonEmptyColumns, logger, true)
+    for(def row : dataRows) {
+        if (row.getAlias())
+            continue
+        checkNonEmptyColumns(row, row.getIndex(), nonEmptyColumns, logger, true)
+
+        def needValue = formData.createDataRow()
+        needValue.parentTB = row.department ? getParentTBId(row.department.intValue()) : null
+
+        def records = calcBO(row.department?.intValue())
+        opuMap.each { k, v ->
+            needValue[k] = records[k]
+        }
+        needValue.sum34 = needValue.tax26411_01 + needValue.tax26411_02
+        needValue.sum = needValue.sum34 + needValue.tax26411_01 + needValue.tax26411_02 + needValue.tax26411_03 + needValue.tax26411_13 + needValue.tax26411_12 +
+                needValue.tax26412 + needValue.tax26410_09
+        checkCalc(row, totalColumns, needValue, logger, true)
     }
+    checkTotalSum(dataRows, totalColumns, logger, true)
 }
