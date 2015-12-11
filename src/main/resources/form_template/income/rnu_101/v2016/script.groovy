cd5d2e363e246420686345cec0959f41c180220a
Ellina Mamedova <Emamedova@aplana.com>
2015-12-11 13:46:03 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-13855 0.9.ТЦО Реализовать РНУ-101

---
+++ src/main/resources/form_template/income/rnu_101/v2016/script.groovy
@@ -0,0 +1,378 @@
+package form_template.income.rnu_101.v2016
+
+import com.aplana.sbrf.taxaccounting.model.FormDataEvent
+import com.aplana.sbrf.taxaccounting.model.exception.ServiceException
+import com.aplana.sbrf.taxaccounting.model.log.LogLevel
+import groovy.transform.Field
+
+/**
+ * РНУ 101. Регистр налогового учёта сумм доначисления доходов по прочим операциям при применении в сделках с
+ * Взаимозависимыми лицами и резидентами оффшорных зон цен, не соответствующих рыночному уровню
+ *
+ * formTemplateId=818
+ *
+ * @author Emamedova
+ */
+
+// fix
+// rowNumber    		(1) -  № пп
+// name         		(2) -  Наименование Взаимозависимого лица (резидента оффшорной зоны)
+// iksr					(3) -  Идентификационный номер
+// countryName  		(4) -  Страна регистрации
+// transDoneDate        (5) -  Дата совершения операции
+// course 				(6) -  Курс Банка России
+// incomeCode 			(7) -  Код классификации дохода
+// reasonNumber 		(8) -  номер
+// reasonDate   		(9) -  дата
+// count 				(10) - Количество услуг/работ (ед./шт.)
+// dealPrice			(11) - Цена за оказанные услуги согласно условиям договора
+// taxPrice				(12) - Цена, признаваемая рыночной для целей налогообложения
+// sum1					(13) - Сумма фактически начисленного дохода (руб.)
+// incomeRate			(14) - Коэффициент корректировки доходов
+// sum2					(15) - Сумма дохода, соответствующая рыночному уровню (руб.)
+// sum3					(16) - Сумма доначисления дохода до рыночного уровня процентной ставки (руб.)
+
+switch (formDataEvent) {
+    case FormDataEvent.CREATE:
+        formDataService.checkUnique(formData, logger)
+        break
+    case FormDataEvent.CALCULATE:
+        calc()
+        logicCheck()
+        formDataService.saveCachedDataRows(formData, logger, formDataEvent)
+        break
+    case FormDataEvent.CHECK:
+        logicCheck()
+        break
+    case FormDataEvent.ADD_ROW:
+        formDataService.addRow(formData, currentDataRow, editableColumns, autoFillColumns)
+        break
+    case FormDataEvent.DELETE_ROW:
+        formDataService.getDataRowHelper(formData).delete(currentDataRow)
+        break
+    case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
+    case FormDataEvent.MOVE_CREATED_TO_APPROVED:  // Утвердить из "Создана"
+    case FormDataEvent.MOVE_PREPARED_TO_APPROVED: // Утвердить из "Подготовлена"
+    case FormDataEvent.MOVE_CREATED_TO_ACCEPTED:  // Принять из "Создана"
+    case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED: // Принять из "Подготовлена"
+    case FormDataEvent.MOVE_APPROVED_TO_ACCEPTED: // Принять из "Утверждена"
+        logicCheck()
+        break
+    case FormDataEvent.COMPOSE: // Консолидация
+        formDataService.consolidationSimple(formData, logger, userInfo)
+        calc()
+        logicCheck()
+        formDataService.saveCachedDataRows(formData, logger, formDataEvent)
+        break
+    case FormDataEvent.IMPORT:
+        importData()
+        formDataService.saveCachedDataRows(formData, logger, formDataEvent)
+        break
+    case FormDataEvent.SORT_ROWS:
+        sortFormDataRows()
+        break
+}
+
+//// Кэши и константы
+@Field
+def providerCache = [:]
+@Field
+def recordCache = [:]
+@Field
+def refBookCache = [:]
+
+@Field
+def allColumns = ['fix', 'rowNumber', 'name', 'iksr', 'countryName', 'transDoneDate', 'course', 'incomeCode',
+                  'reasonNumber', 'reasonDate', 'count', 'dealPrice', 'taxPrice', 'sum1', 'incomeRate', 'sum2','sum3']
+
+// Редактируемые атрибуты
+@Field
+def editableColumns = ['name', 'transDoneDate', 'course', 'incomeCode','reasonNumber', 'reasonDate', 'count', 'dealPrice', 'taxPrice', 'sum1', 'sum2']
+
+// Автозаполняемые атрибуты
+@Field
+def autoFillColumns = ['iksr', 'countryName', 'sum3']
+
+// Проверяемые на пустые значения атрибуты
+@Field
+def nonEmptyColumns = ['name', 'transDoneDate', 'course', 'incomeCode','reasonNumber', 'reasonDate', 'count', 'dealPrice', 'taxPrice', 'sum1', 'sum2']
+
+@Field
+def totalColumns = ['sum1', 'sum2', 'sum3']
+
+// Дата окончания отчетного периода
+@Field
+def endDate = null
+
+def getReportPeriodEndDate() {
+    if (endDate == null) {
+        endDate = reportPeriodService.getEndDate(formData.reportPeriodId).time
+    }
+    return endDate
+}
+
+//// Обертки методов
+
+// Поиск записи в справочнике по значению (для импорта)
+def getRecordIdImport(def Long refBookId, def String alias, def String value, def int rowIndex, def int colIndex,
+                      def boolean required = false) {
+    if (value == null || value.trim().isEmpty()) {
+        return null
+    }
+    return formDataService.getRefBookRecordIdImport(refBookId, recordCache, providerCache, alias, value,
+            getReportPeriodEndDate(), rowIndex, colIndex, logger, required)
+}
+
+// Разыменование записи справочника
+def getRefBookValue(def long refBookId, def Long recordId) {
+    return formDataService.getRefBookValue(refBookId, recordId, refBookCache)
+}
+
+//// Кастомные методы
+
+// Логические проверки
+void logicCheck() {
+}
+
+
+// Алгоритмы заполнения полей формы
+void calc() {
+}
+
+def calcTotalRow(def dataRows) {
+    def totalRow = (formDataEvent in [FormDataEvent.IMPORT, FormDataEvent.IMPORT_TRANSPORT_FILE]) ? formData.createStoreMessagingDataRow() : formData.createDataRow()
+    totalRow.setAlias('total')
+    totalRow.fix = 'Итого'
+    totalRow.getCell('fix').colSpan = 3
+    allColumns.each {
+        totalRow.getCell(it).setStyleAlias('Контрольные суммы')
+    }
+    calcTotalSum(dataRows, totalRow, totalColumns)
+    return totalRow
+}
+
+// Получение импортируемых данных
+void importData() {
+    def tmpRow = formData.createDataRow()
+    int COLUMN_COUNT = 16
+    int HEADER_ROW_COUNT = 3
+    String TABLE_START_VALUE = '№ пп'
+    String TABLE_END_VALUE = null
+    int INDEX_FOR_SKIP = 0
+
+    def allValues = []      // значения формы
+    def headerValues = []   // значения шапки
+    def paramsMap = ['rowOffset': 0, 'colOffset': 0]  // мапа с параметрами (отступы сверху и слева)
+
+    checkAndReadFile(ImportInputStream, UploadFileName, allValues, headerValues, TABLE_START_VALUE, TABLE_END_VALUE, HEADER_ROW_COUNT, paramsMap)
+
+    // проверка шапки
+    checkHeaderXls(headerValues, COLUMN_COUNT, HEADER_ROW_COUNT, tmpRow)
+    if (logger.containsLevel(LogLevel.ERROR)) {
+        return
+    }
+    // освобождение ресурсов для экономии памяти
+    headerValues.clear()
+    headerValues = null
+
+    def fileRowIndex = paramsMap.rowOffset
+    def colOffset = paramsMap.colOffset
+    paramsMap.clear()
+    paramsMap = null
+
+    def rowIndex = 0
+    def rows = []
+    def allValuesCount = allValues.size()
+    def totalRowFromFile = null
+
+    // формирвание строк нф
+    for (def i = 0; i < allValuesCount; i++) {
+        rowValues = allValues[0]
+        fileRowIndex++
+        // все строки пустые - выход
+        if (!rowValues) {
+            allValues.remove(rowValues)
+            rowValues.clear()
+            break
+        }
+        rowIndex++
+        // Пропуск итоговых строк
+        if (rowValues[INDEX_FOR_SKIP] == "Итого") {
+            totalRowFromFile = getNewTotalFromXls(rowValues, colOffset, fileRowIndex, rowIndex)
+
+            allValues.remove(rowValues)
+            rowValues.clear()
+            continue
+        }
+        // простая строка
+        def newRow = getNewRowFromXls(rowValues, colOffset, fileRowIndex, rowIndex)
+        rows.add(newRow)
+        // освободить ненужные данные - иначе не хватит памяти
+        allValues.remove(rowValues)
+        rowValues.clear()
+    }
+
+    // сравнение итогов
+    def totalRow = calcTotalRow(rows)
+    rows.add(totalRow)
+    updateIndexes(rows)
+    if (totalRowFromFile) {
+        compareSimpleTotalValues(totalRow, totalRowFromFile, rows, totalColumns, formData, logger, false)
+    }
+
+    showMessages(rows, logger)
+    if (!logger.containsLevel(LogLevel.ERROR)) {
+        updateIndexes(rows)
+        formDataService.getDataRowHelper(formData).allCached = rows
+    }
+}
+
+/**
+ * Проверить шапку таблицы
+ *
+ * @param headerRows строки шапки
+ * @param colCount количество колонок в таблице
+ * @param rowCount количество строк в таблице
+ * @param tmpRow вспомогательная строка для получения названии графов
+ */
+void checkHeaderXls(def headerRows, def colCount, rowCount, def tmpRow) {
+    if (headerRows.isEmpty()) {
+        throw new ServiceException(WRONG_HEADER_ROW_SIZE)
+    }
+    checkHeaderSize(headerRows[headerRows.size() - 1].size(), headerRows.size(), colCount, rowCount)
+    def headerMapping = [
+            ([(headerRows[0][8]): 'Основание для совершения операции']),
+            ([(headerRows[1][2]): getColumnName(tmpRow, 'name')]),
+            ([(headerRows[1][3]): getColumnName(tmpRow, 'iksr')]),
+            ([(headerRows[1][4]): getColumnName(tmpRow, 'countryName')]),
+            ([(headerRows[1][5]): getColumnName(tmpRow, 'transDoneDate')]),
+            ([(headerRows[1][6]): getColumnName(tmpRow, 'course')]),
+            ([(headerRows[1][7]): getColumnName(tmpRow, 'incomeCode')]),
+            ([(headerRows[1][8]): 'номер']),
+            ([(headerRows[1][9]): 'дата']),
+            ([(headerRows[1][10]): getColumnName(tmpRow, 'count')]),
+            ([(headerRows[1][11]): getColumnName(tmpRow, 'dealPrice')]),
+            ([(headerRows[1][12]): getColumnName(tmpRow, 'taxPrice')]),
+            ([(headerRows[1][13]): getColumnName(tmpRow, 'sum1')]),
+            ([(headerRows[1][14]): getColumnName(tmpRow, 'incomeRate')]),
+            ([(headerRows[1][15]): getColumnName(tmpRow, 'sum2')]),
+            ([(headerRows[1][16]): getColumnName(tmpRow, 'sum3')])
+    ]
+    (1..16).each {
+        headerMapping.add([(headerRows[2][it]): it.toString()])
+    }
+    checkHeaderEquals(headerMapping, logger)
+}
+
+/**
+ * Получить новую строку нф по значениям из экселя.
+ *
+ * @param values список строк со значениями
+ * @param colOffset отступ в колонках
+ * @param fileRowIndex номер строки в тф
+ * @param rowIndex строка в нф
+ */
+def getNewRowFromXls(def values, def colOffset, def fileRowIndex, def rowIndex) {
+    def newRow = formData.createStoreMessagingDataRow()
+    newRow.setIndex(rowIndex)
+    newRow.setImportIndex(fileRowIndex)
+    editableColumns.each {
+        newRow.getCell(it).editable = true
+        newRow.getCell(it).setStyleAlias('Редактируемая')
+    }
+    autoFillColumns.each {
+        newRow.getCell(it).setStyleAlias('Автозаполняемая')
+    }
+    def String iksrName = getColumnName(newRow, 'iksr')
+    def nameFromFile = values[2]
+
+    def int colIndex = 2
+
+    def recordId = getTcoRecordId(nameFromFile, values[4], iksrName, fileRowIndex, colIndex, getReportPeriodEndDate(), true, logger, refBookFactory, recordCache)
+    def map = getRefBookValue(520, recordId)
+
+    // графа 2
+    newRow.name = recordId
+    colIndex++
+
+    // графа 3
+    if (map != null) {
+        formDataService.checkReferenceValue(520, values[colIndex], map.IKSR?.stringValue, fileRowIndex, colIndex + colOffset, logger, false)
+    }
+    colIndex++
+
+    // графа 4
+    if (map != null) {
+        map = getRefBookValue(10, map.COUNTRY_CODE?.referenceValue)
+        if (map != null) {
+            formDataService.checkReferenceValue(10, values[colIndex], map.NAME?.stringValue, fileRowIndex, colIndex + colOffset, logger, false)
+        }
+    }
+    colIndex++
+
+    // графа 5
+    newRow.transDoneDate = parseDate(values[colIndex], "dd.MM.yyyy", fileRowIndex, colIndex + colOffset, logger, true)
+    colIndex++
+
+    // графа 6
+    newRow.course = parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+    colIndex++
+
+    // графа 7
+    newRow.incomeCode = values[colIndex]
+    colIndex++
+
+    // графа 8
+    newRow.reasonNumber = values[colIndex]
+    colIndex++
+
+    // графа 9
+    newRow.reasonDate = parseDate(values[colIndex], "dd.MM.yyyy", fileRowIndex, colIndex + colOffset, logger, true)
+    colIndex++
+
+    // графы 10-16
+    ['count', 'dealPrice', 'taxPrice', 'sum1', 'incomeRate', 'sum2','sum3'].each{
+        newRow[it]= parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+        colIndex++
+    }
+
+    return newRow
+}
+
+/**
+ * Получить итоговую строку нф по значениям из экселя.
+ *
+ * @param values список строк со значениями
+ * @param colOffset отступ в колонках
+ * @param fileRowIndex номер строки в тф
+ * @param rowIndex строка в нф
+ */
+def getNewTotalFromXls(def values, def colOffset, def fileRowIndex, def rowIndex) {
+    def newRow = formData.createStoreMessagingDataRow()
+    newRow.setIndex(rowIndex)
+    newRow.setImportIndex(fileRowIndex)
+
+    // графа 13
+    def colIndex = 13
+    newRow.sum1 = parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+    // графа 15
+    colIndex = 15
+    newRow.sum2 = parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+    // графа 17
+    colIndex = 16
+    newRow.sum3 = parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+
+    return newRow
+}
+
+// Сортировка групп и строк
+void sortFormDataRows(def saveInDB = true) {
+    def dataRowHelper = formDataService.getDataRowHelper(formData)
+    def dataRows = dataRowHelper.allCached
+    sortRows(refBookService, logger, dataRows, null, dataRows.find { it.getAlias() == 'total' }, true)
+    if (saveInDB) {
+        dataRowHelper.saveSort()
+    } else {
+        updateIndexes(dataRows)
+    }
+}
