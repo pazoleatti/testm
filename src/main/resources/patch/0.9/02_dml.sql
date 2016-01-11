0b9a213c9c18fb47870707e77c02a4b786c4c24a
Elena Kabanova <elena.kabanova@aplana.com>
2016-01-11 18:00:11 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-14070: 0.9. ТЦО. Справочник "Признаки физической поставки". Добавить запись "Поставочная сделка" (Патч 0.9)

--- src/main/resources/patch/0.9/02_dml.sql
+++ src/main/resources/patch/0.9/02_dml.sql
@@ -1,7 +1,27 @@
 --http://jira.aplana.com/browse/SBRFACCTAX-13954: Добавить новую асинхронную задачу "Формирование специфичного отчета НФ"
-insert into async_task_type (id, name, handler_jndi, limit_kind, dev_mode) values (22, 'Формирование специфичного отчета налоговой формы', 'ejb/taxaccounting/async-task.jar/SpecificReportFormDataAsyncTaskImpl#com.aplana.sbrf.taxaccounting.async.task.AsyncTaskRemote', 'Количество ячеек таблицы формы = Количество строк * Количество граф', 0);
+INSERT INTO async_task_type (id, name, handler_jndi, limit_kind, dev_mode) values (22, 'Формирование специфичного отчета налоговой формы', 'ejb/taxaccounting/async-task.jar/SpecificReportFormDataAsyncTaskImpl#com.aplana.sbrf.taxaccounting.async.task.AsyncTaskRemote', 'Количество ячеек таблицы формы = Количество строк * Количество граф', 0);
+COMMIT;
 
+-----------------------------------------------------------------------------------------
+--http://jira.aplana.com/browse/SBRFACCTAX-14070: 0.9. ТЦО. Справочник "Признаки физической поставки". Добавить запись "Поставочная сделка"
+SET serveroutput ON SIZE 10000;
+BEGIN
+INSERT ALL
+	INTO ref_book_record (id, ref_book_id, record_id, status, version) VALUES (seq_ref_book_record.nextval, 18, 3, 0, to_date('01.01.2012', 'dd.mm.yyyy'))
+	INTO ref_book_value (record_id, attribute_id, number_value) VALUES (seq_ref_book_record.currval, 43, 3)
+	INTO ref_book_value (record_id, attribute_id, string_value) VALUES (seq_ref_book_record.currval, 44, 'Поставочная сделка')
+SELECT * FROM dual;
 
+dbms_output.put_line(SQL%ROWCOUNT || ' rows inserted (SBRFACCTAX-14070)');
 
+EXCEPTION
+	WHEN OTHERS THEN
+		dbms_output.put_line('Insert failed (' || SQLERRM || ' ). Block will be rollbacked. (SBRFACCTAX-14070)');
+		ROLLBACK;
+END;
+/	
+COMMIT;
+
+-----------------------------------------------------------------------------------------
 COMMIT;
 EXIT;
