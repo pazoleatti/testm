855f832d737758859980dfb61b10a56ee8b48049
Ramil Timerbaev <Rtimerbaev@aplana.com>
2016-02-11 11:17:15 +0500

    http://jira.aplana.com/browse/SBRFACCTAX-14653 0.9.2 РНУ-101. Не загружается файл XLSM (рну-101, рну-102).

--- src/main/resources/form_template/income/rnu_101/v2015/script.groovy
+++ src/main/resources/form_template/income/rnu_101/v2015/script.groovy
@@ -386,7 +386,7 @@ void importData() {
     int HEADER_ROW_COUNT = 3
     String TABLE_START_VALUE = '№ пп'
     String TABLE_END_VALUE = null
-    int INDEX_FOR_SKIP = 0
+    int INDEX_FOR_SKIP = 1
 
     def allValues = []      // значения формы
     def headerValues = []   // значения шапки
