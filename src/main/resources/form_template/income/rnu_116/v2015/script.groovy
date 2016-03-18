43ea621d43729a7246a4c8327152f61e4004ba47
Ramil Timerbaev <Rtimerbaev@aplana.com>
2016-03-18 11:58:27 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-14751 0.9.2 ПСИ. УКС 6-16. При загрузке организация распозналась, но почему-то Система выдала нефатальное сообщение, что в справочнике не найдена организация с заданным значением КИО.

--- src/main/resources/form_template/income/rnu_116/v2015/script.groovy
+++ src/main/resources/form_template/income/rnu_116/v2015/script.groovy
@@ -647,7 +647,9 @@ def getNewRowFromXls(def values, def colOffset, def fileRowIndex, def rowIndex) 
 
     // графа 6
     if (map != null) {
-        formDataService.checkReferenceValue(520, values[colIndex], map.IKSR?.stringValue, fileRowIndex, colIndex + colOffset, logger, false)
+        def expectedValues = [ map.INN?.value, map.REG_NUM?.value, map.TAX_CODE_INCORPORATION?.value, map.SWIFT?.value, map.KIO?.value ]
+        expectedValues = expectedValues.unique().findAll{ it != null && it != '' }
+        formDataService.checkReferenceValue(values[colIndex], expectedValues, getColumnName(newRow, 'iksr'), map.NAME.value, fileRowIndex, colIndex + colOffset, logger, false)
     }
     colIndex++
 
