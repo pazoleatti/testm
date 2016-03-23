a36b03c165bb16696b63044ee1436863b85eb3c4
Lenar Haziev <lhaziev@aplana.com>
2016-03-23 13:41:59 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-13751 0.8 ПСИ. В форме 4.2 в графе 8 сделать разбивку цифр по разрядам

--- model/src/test/java/com/aplana/sbrf/taxaccounting/model/NumericColumnTest.java
+++ model/src/test/java/com/aplana/sbrf/taxaccounting/model/NumericColumnTest.java
@@ -7,18 +7,17 @@ public class NumericColumnTest {
 	@Test
 	public void testFormatter() {
 		NumericColumn numericColumn = new NumericColumn();
-        ColumnFormatter formatter = numericColumn.getFormatter();
 
-		Assert.assertEquals("12 345", formatter.format("12345"));
-		Assert.assertEquals("123 456 789 012 345", formatter.format("123456789012345"));
+		Assert.assertEquals("12 345", numericColumn.getFormatter().format("12345"));
+		Assert.assertEquals("123 456 789 012 345", numericColumn.getFormatter().format("123456789012345"));
 
 		numericColumn.setPrecision(6);
-		Assert.assertEquals("1 122 345 678 901.234500", formatter.format("1122345678901.2345"));
-		Assert.assertEquals("-1 122 345 678 901.234500", formatter.format("-1122345678901.2345"));
-		Assert.assertEquals("-122 345 678 901.234500", formatter.format("-122345678901.2345"));
-		Assert.assertEquals("-122 345 678 901.234500", formatter.format("- 122345678901.2345"));
+		Assert.assertEquals("1 122 345 678 901.234500", numericColumn.getFormatter().format("1122345678901.2345"));
+		Assert.assertEquals("-1 122 345 678 901.234500", numericColumn.getFormatter().format("-1122345678901.2345"));
+		Assert.assertEquals("-122 345 678 901.234500", numericColumn.getFormatter().format("-122345678901.2345"));
+		Assert.assertEquals("-122 345 678 901.234500", numericColumn.getFormatter().format("- 122345678901.2345"));
 
 		numericColumn.setPrecision(2);
-		Assert.assertEquals("-122 345 678 901.24", formatter.format("- 122345678901.2365"));
+		Assert.assertEquals("-122 345 678 901.24", numericColumn.getFormatter().format("- 122345678901.2365"));
 	}
 }
