84c1d9e0fc4ad7aab98c36dacd5b65de932bc879
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2016-03-30 15:38:46 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-14902 0.9.4 ТЦО. Доработать форму 6.7

--- src/main/resources/form_template/deal/app_6_7/v2015/script.groovy
+++ src/main/resources/form_template/deal/app_6_7/v2015/script.groovy
@@ -154,8 +154,17 @@ void logicCheck() {
         // 1. Проверка на заполнение графы
         checkNonEmptyColumns(row, rowNum, nonEmptyColumns, logger, true)
 
-        // 2. Проверка возможности заполнения цены и стоимости
-        if (row.sum==null) {
+        // 2. Проверка суммы доходов
+        if (row.sum != null && row.sum < 0) {
+            def msg = row.getCell('sum').column.name
+            logger.error("Строка $rowNum: Значение графы «$msg» должно быть больше или равно «0»!")
+        }
+
+        // 3. Проверка корректности даты договора
+        checkDatePeriod(logger, row, 'docDate', Date.parse('dd.MM.yyyy', '01.01.1991'), getReportPeriodEndDate(), true)
+
+        // 4. Проверка возможности заполнения цены и стоимости
+        if (row.sum == null) {
             def msg1 = row.getCell('price').column.name
             def msg2 = row.getCell('cost').column.name
             def msg3 = row.getCell('sum').column.name
@@ -163,31 +172,41 @@ void logicCheck() {
                     "используемая в расчете графа «$msg3»!")
         }
 
-        // 3. Проверка цены
+        // 5. Проверка цены
         if (row.sum!=null && row.price != row.sum) {
             def msg1 = row.getCell('price').column.name
             def msg2 = row.getCell('sum').column.name
             logger.error("Строка $rowNum: Значение графы «$msg1» должно быть равно значению графы «$msg2»!")
         }
 
-        // 4. Проверка стоимости
+        // 6. Проверка стоимости
         if (row.sum!=null && row.cost != row.sum) {
             def msg1 = row.getCell('cost').column.name
             def msg2 = row.getCell('sum').column.name
             logger.error("Строка $rowNum: Значение графы «$msg1» должно быть равно значению графы «$msg2»!")
         }
 
-        // 5. Проверка корректности даты договора
-        checkDatePeriod(logger, row, 'docDate', Date.parse('dd.MM.yyyy', '01.01.1991'), getReportPeriodEndDate(), true)
+        // 7. Проверка корректности даты совершения сделки
+        // TODO (SBRFACCTAX-15094) заменить на checkDatePeriodExt
+        checkDatePeriodExtLocal(logger, row, 'dealDoneDate', 'docDate', Date.parse('dd.MM.yyyy', '01.01.' + getReportPeriodEndDate().format('yyyy')), getReportPeriodEndDate(), true)
+    }
+}
 
-        // 6. Проверка корректности даты совершения сделки
-        checkDatePeriod(logger, row, 'dealDoneDate', 'docDate', getReportPeriodEndDate(), true)
+// TODO (SBRFACCTAX-15094) удалить
+void checkDatePeriodExtLocal(logger, row, String alias, String startAlias, Date yearStartDate, Date endDate, boolean fatal) {
+    // дата проверяемой графы
+    Date docDate = row.getCell(alias).getDateValue();
+    // дата другой графы
+    Date startDate = row.getCell(startAlias).getDateValue();
 
-        // 7. Проверка положительной суммы доходов
-        if (row.sum!=null && row.sum < 0) {
-            def msg = row.getCell('sum').column.name
-            logger.error("Строка $rowNum: Значение графы «$msg» должно быть больше или равно «0»!")
-        }
+    if (docDate != null && startDate != null && (docDate.before(yearStartDate) || docDate.after(endDate) || docDate.before(startDate))) {
+        logger.error(String.format("Строка %d: Дата по графе «%s» должна принимать значение из диапазона %s - %s и быть больше либо равна дате по графе «%s»!",
+                row.getIndex(),
+                getColumnName(row, alias),
+                formatDate(yearStartDate, "dd.MM.yyyy"),
+                formatDate(endDate, "dd.MM.yyyy"),
+                getColumnName(row, startAlias)
+        ));
     }
 }
 
