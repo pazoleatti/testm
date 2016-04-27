49ab310c7ac152f2d4cbfc61811348b3ce83555e
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2016-04-27 16:07:10 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-14033 1.0 Загрузка ТФ выдается ошибочное сообщение если в файле есть только заголовок и пустая строка (ТЦО)

--- src/main/resources/form_template/deal/app_6_10_2/v2015/script.groovy
+++ src/main/resources/form_template/deal/app_6_10_2/v2015/script.groovy
@@ -381,16 +381,9 @@ void importTransportData() {
     InputStreamReader isr = new InputStreamReader(ImportInputStream, DEFAULT_CHARSET)
     CSVReader reader = new CSVReader(isr, SEPARATOR, QUOTE)
     try {
-        // пропускаем заголовок
-        rowCells = reader.readNext()
-        if (isEmptyCells(rowCells)) {
-            logger.error('Первой строкой должен идти заголовок, а не пустая строка')
-        }
-        // пропускаем пустую строку
-        rowCells = reader.readNext()
-        if (rowCells == null || !isEmptyCells(rowCells)) {
-            logger.error('Вторая строка должна быть пустой')
-        }
+        // проверить первые строки тф - заголовок и пустая строка
+        checkFirstRowsTF(reader, logger)
+
         // грузим основные данные
         while ((rowCells = reader.readNext()) != null) {
             fileRowIndex++
@@ -491,10 +484,6 @@ def getNewRow(String[] rowCells, def columnCount, def fileRowIndex, def rowIndex
 
 String pure(String cell) {
     return StringUtils.cleanString(cell)?.intern()
-}
-
-boolean isEmptyCells(def rowCells) {
-    return rowCells.length == 1 && rowCells[0] == ''
 }
 
 // Сортировка групп и строк
