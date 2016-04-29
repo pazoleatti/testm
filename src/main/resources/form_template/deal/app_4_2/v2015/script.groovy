ae48961c74a82d6d0a0917d89175e4fd0a1d6b52
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2016-04-29 12:38:32 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-14988 1.0. ТЦО. Реализовать спец.отчет "Контролируемые лица". Вернул функционал.

--- src/main/resources/form_template/deal/app_4_2/v2015/script.groovy
+++ src/main/resources/form_template/deal/app_4_2/v2015/script.groovy
@@ -80,7 +80,7 @@ switch (formDataEvent) {
         sortFormDataRows()
         break
     case FormDataEvent.GET_SPECIFIC_REPORT_TYPES:
-        //specificReportType.add("Контролируемые лица") TODO вернуть для 1.0
+        specificReportType.add("Контролируемые лица")
         break
     case FormDataEvent.CREATE_SPECIFIC_REPORT:
         createSpecificReport()
