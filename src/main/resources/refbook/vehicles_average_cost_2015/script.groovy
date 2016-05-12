c30a3b1582f08a60a8d073f52262133a5de68059
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2016-05-12 16:18:39 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-15726 1.0 Транспорт. Доработать справочник "Средняя стоимость транспортных средств (с 2015)" (БЛ)

--- src/main/resources/refbook/vehicles_average_cost_2015/script.groovy
+++ src/main/resources/refbook/vehicles_average_cost_2015/script.groovy
@@ -5,22 +5,16 @@ import com.aplana.sbrf.taxaccounting.model.FormDataEvent
 /**
  * blob_data.id = '54def165-b118-4d1e-a52c-be429ebd832e'
  *
- * Скрипт справочника "Средняя стоимость транспортных средств (2015)" (id = 218)
+ * Скрипт справочника "Средняя стоимость транспортных средств (с 2015)" (id = 218)
  *
  * @author Lhaziev
  */
-switch (formDataEvent) {
+/*switch (formDataEvent) {
     case FormDataEvent.SAVE:
         save()
         break
 }
 
 void save() {
-    saveRecords.each {
-        def Long yearFrom = it.YEAR_FROM?.numberValue.longValue()
-        def Long yearTo = it.YEAR_TO?.numberValue.longValue()
-        if (yearFrom >= yearTo) {
-            logger.error("Поле «Количество лет, прошедших с года выпуска ТС, до» должно быть больше поля «Количество лет, прошедших с года выпуска ТС, от»!")
-        }
-    }
 }
+*/
