1b29ebed901a1686029165578cd7b8ff4493562d
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2016-05-30 15:52:56 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-15074 перенести проверку формата rnu в ScriptUtils. НДС

--- src/main/resources/form_template/vat/vat_937_3/v2015/script.groovy
+++ src/main/resources/form_template/vat/vat_937_3/v2015/script.groovy
@@ -656,17 +656,8 @@ def getReportPeriod() {
     return reportPeriod
 }
 
-// TODO (SBRFACCTAX-15074) убрать
-void checkTFLocal(BufferedInputStream inputStream, String fileName) {
-    checkBeforeGetXml(inputStream, fileName);
-    if (fileName != null && !fileName.toLowerCase().endsWith(".rnu")) {
-        throw new ServiceException("Выбранный файл не соответствует формату rnu!");
-    }
-}
-
 void importTransportData() {
-    // TODO (SBRFACCTAX-15074) заменить на "ScriptUtils.checkTF(ImportInputStream, UploadFileName)"
-    checkTFLocal(ImportInputStream, UploadFileName)
+    checkTF(ImportInputStream, UploadFileName)
 
     int COLUMN_COUNT = 19
     def DEFAULT_CHARSET = "cp866"
