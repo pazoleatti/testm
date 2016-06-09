8279beb7b48af7624854d9055ad05ddd9512fcb7
Elena Kabanova <elena.kabanova@aplana.com>
2016-06-09 13:04:20 +0500

    Подготовительные действия перед накатом патча - удаление временного среза и пересбор статистики

--- src/main/resources/patch/1.0/01_ddl.sql
+++ src/main/resources/patch/1.0/01_ddl.sql
@@ -1,3 +1,35 @@
+--Подготовительные действия перед накатом патча
+--Удаление временного среза
+set serveroutput on size 1000000;
+declare ifTableExists varchar2(256);
+begin
+  for x in (select * from form_template ft where status <> 2 order by id) loop
+  
+       select coalesce(max(ifExists), 0) into ifTableExists from (
+          select 1 as ifExists
+            from dual
+            where exists (select 1 from user_tables where table_name = 'FORM_DATA_'||x.id));
+            
+       if ifTableExists = 1 then
+          execute immediate 'delete from form_data_'||x.id||' where temporary=1';
+				if sql%rowcount <> 0 then
+					dbms_output.put_line('form_data_'||x.id||': '||sql%rowcount||' row(s) deleted.');
+				end if;
+		end if;     
+  end loop;
+end;
+/
+commit;
+
+--Пересбор статистики
+declare running_user varchar2(30);
+begin
+	select user into running_user from dual;
+	dbms_stats.gather_schema_stats(running_user); 
+end;
+/
+
+----------------------------------------------------------------------------------------------------------------
 --http://jira.aplana.com/browse/SBRFACCTAX-13720: 1.0 Добавить в ЖА поле "Сервер" по аналогии с блокировками
 alter table log_system add server varchar2(200);
 comment on column log_system.server is 'Сервер'; 
