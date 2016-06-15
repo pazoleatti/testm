a62bcba331643518f18804f7e79cca71455e1180
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2016-06-15 17:49:07 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-15869 1.0 Справочник "Средняя стоимость транспортных средств (с 2015)". Реализовать возможность загрузки данных из XML файла. Пробросил методы из дао

--- script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/RefBookService.java
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/RefBookService.java
@@ -3,10 +3,9 @@ package com.aplana.sbrf.taxaccounting.service.script;
 import com.aplana.sbrf.taxaccounting.model.Cell;
 import com.aplana.sbrf.taxaccounting.model.Column;
 import com.aplana.sbrf.taxaccounting.model.DataRow;
+import com.aplana.sbrf.taxaccounting.model.FormLink;
 import com.aplana.sbrf.taxaccounting.model.log.Logger;
-import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttribute;
-import com.aplana.sbrf.taxaccounting.model.refbook.RefBookRecord;
-import com.aplana.sbrf.taxaccounting.model.refbook.RefBookValue;
+import com.aplana.sbrf.taxaccounting.model.refbook.*;
 import com.aplana.sbrf.taxaccounting.model.util.Pair;
 import com.aplana.sbrf.taxaccounting.util.ScriptExposed;
 import com.aplana.sbrf.taxaccounting.util.TransactionLogic;
@@ -66,4 +65,137 @@ public interface RefBookService {
      * @return список пар идентификатор записи-имя атрибута, у которых совпали значения уникальных атрибутов
      */
     List<Pair<String,String>> getMatchedRecordsByUniqueAttributes(Long recordId, List<RefBookAttribute> attributes, List<RefBookRecord> records);
+
+    /**
+     * Возвращает данные о версии следующей за указанной
+     * @param refBookId идентификатор справочника
+     * @param recordId идентификатор записи справочника (без учета версий)
+     * @param versionFrom дата начала актуальности версии текущей версии, после которой будет выполняться поиск следующей версии
+     * @return данные версии
+     */
+    RefBookRecordVersion getNextVersion(Long refBookId, Long recordId, Date versionFrom);
+
+    /**
+     * Поиск существующих версий, которые могут пересекаться с новой версией
+     * @param refBookId идентификатор справочника
+     * @param recordId идентификатор записи справочника (без учета версий)
+     * @param versionFrom дата начала актуальности новой версии
+     * @param versionTo дата окончания актуальности новой версии
+     * @param excludedRecordId идентификатор версии записи справочника, которая исключается из проверки пересечения. Используется только при редактировании
+     * @return результат проверки по каждой версии, с которой есть пересечение
+     */
+    List<CheckCrossVersionsResult> checkCrossVersions(Long refBookId, Long recordId,
+                                                      Date versionFrom, Date versionTo,
+                                                      Long excludedRecordId);
+
+    /**
+     * Изменение периода актуальности для указанной версии
+     *
+     * @param tableName      название таблицы
+     * @param uniqueRecordId уникальный идентификатор версии записи
+     * @param version        новая дата начала актуальности
+     */
+    void updateVersionRelevancePeriod(String tableName, Long uniqueRecordId, Date version);
+
+    /**
+     * Удаляет указанные версии записи из справочника
+     *
+     * @param uniqueRecordIds список идентификаторов версий записей, которые будут удалены
+     */
+    void deleteRecordVersions(String tableName, List<Long> uniqueRecordIds);
+
+    /**
+     * Проверка использования записи в налоговых формах
+     * @param refBookId идентификатор справочника
+     * @param uniqueRecordIds список уникальных идентификаторов записей справочника
+     * @param versionFrom дата начала периода
+     * @param versionTo дата конца периода
+     * @param restrictPeriod
+     *      false - возвращает ссылки-использования, период которых НЕ пересекается с указанным периодом
+     *      true - возвращает ссылки-использования, период которых пересекается с указанным периодом
+     *      null - возвращает все ссылки-использования на указанную запись справочника, без учета периода
+     * @return результаты проверки. Сообщения об ошибках
+     */
+    List<FormLink> isVersionUsedInForms(Long refBookId, List<Long> uniqueRecordIds, Date versionFrom, Date versionTo,
+                                        Boolean restrictPeriod);
+
+    /**
+     * Проверка использования записи в справочниках
+     * @param refBookId идентификатор справочника
+     * @param uniqueRecordIds список уникальных идентификаторов записей справочника
+     * @param versionFrom дата начала периода
+     * @param versionTo дата конца периода
+     * @param restrictPeriod
+     *      false - возвращает ссылки-использования, период которых НЕ пересекается с указанным периодом
+     *      true - возвращает ссылки-использования, период которых пересекается с указанным периодом
+     *      null - возвращает все ссылки-использования на указанную запись справочника, без учета периода
+     * @param excludeUseCheck идентификаторы справочников, которые игнорируются при проверке использования
+     * @return результаты проверки. Сообщения об ошибках
+     */
+    List<String> isVersionUsedInRefBooks(Long refBookId, List<Long> uniqueRecordIds, Date versionFrom, Date versionTo,
+                                         Boolean restrictPeriod, List<Long> excludeUseCheck);
+
+    /**
+     * Проверяет есть ли ссылки на запись справочника в настройках подразделений
+     * @param uniqueRecordIds список уникальных идентификаторов записей справочника
+     * @param versionFrom дата начала периода
+     * @param versionTo дата конца периода
+     * @param restrictPeriod
+     *      false - возвращает ссылки-использования, период которых НЕ пересекается с указанным периодом
+     *      true - возвращает ссылки-использования, период которых пересекается с указанным периодом
+     *      null - возвращает все ссылки-использования на указанную запись справочника, без учета периода
+     * @param excludeUseCheck идентификаторы справочников, которые игнорируются при проверке использования
+     */
+    List<String> isVersionUsedInDepartmentConfigs(Long refBookId, List<Long> uniqueRecordIds, Date versionFrom, Date versionTo, Boolean restrictPeriod, List<Long> excludeUseCheck);
+
+    /**
+     * Возвращает данные о версии следующей до указанной
+     * @param refBookId идентификатор справочника
+     * @param recordId идентификатор записи справочника (без учета версий)
+     * @param versionFrom дата начала актуальности версии текущей версии, после которой будет выполняться поиск следующей версии
+     * @return данные версии
+     */
+    RefBookRecordVersion getPreviousVersion(Long refBookId, Long recordId, Date versionFrom);
+
+    /**
+     * Возвращает уникальный идентификатор записи, удовлетворяющей указанным условиям
+     * @param refBookId идентификатор справочника
+     * @param recordId идентификатор записи справочника (без учета версий)
+     * @param version дата
+     * @return уникальный идентификатор записи, удовлетворяющей указанным условиям
+     */
+    Long findRecord(Long refBookId, Long recordId, Date version);
+
+    /**
+     * Возвращает идентификаторы фиктивных версии, являющихся окончанием указанных версии.
+     * Без привязки ко входным параметрам, т.к метод используется просто для удаления по id
+     * @param uniqueRecordIds идентификаторы версии записи справочника
+     * @return идентификаторы фиктивных версии
+     */
+    List<Long> getRelatedVersions(List<Long> uniqueRecordIds);
+
+    /**
+     * Проверяет существование версий записи справочника
+     * @param refBookId идентификатор справочника
+     * @param recordIds идентификаторы записей справочника без учета версий
+     * @param version версия записи справочника
+     * @return
+     */
+    boolean isVersionsExist(Long refBookId, List<Long> recordIds, Date version);
+
+    /**
+     * Создает фиктивную запись, являющуюся датой окончания периода актуальности какой то версии
+     * @param refBookId код справочника
+     * @param recordId идентификатор записи справочника без учета версий
+     * @param version версия записи справочника
+     */
+    void createFakeRecordVersion(Long refBookId, Long recordId, Date version);
+
+    /**
+     * Обновляет значения атрибутов у указанной версии
+     * @param refBookId код справочника
+     * @param uniqueRecordId уникальный идентификатор версии записи справочника
+     * @param records список значений атрибутов
+     */
+    void updateRecordVersion(Long refBookId, Long uniqueRecordId, Map<String, RefBookValue> records);
 }
