02f4906664c119e0c2fbd61d5e28b05d1392a1d4
Bulat Kinzyabulatov <bkinzyabulatov@aplana.com>
2016-06-15 14:00:28 +0500

    Переименование. Шаг 2

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/widget/historytemplatechanges/shared/GetRefBookHistoryResult1.java
+++
@@ -1,8 +0,0 @@
-package com.aplana.sbrf.taxaccounting.web.widget.historytemplatechanges.shared;
-
-/**
- * @author lhaziev
- */
-public class GetRefBookHistoryResult extends TemplateHistoryResult {
-    private static final long serialVersionUID = 1564668134994631543L;
-}
