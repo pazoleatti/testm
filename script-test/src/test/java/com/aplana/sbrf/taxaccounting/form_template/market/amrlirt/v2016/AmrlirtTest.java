e951a3c3d24bb0376503ba71737c88e95773955d
Ramil Timerbaev <Rtimerbaev@aplana.com>
2016-06-29 18:47:53 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-16007 1.1 РнРнтб. Реализовать форму "Данные АМРЛИРТ".

---
+++ script-test/src/test/java/com/aplana/sbrf/taxaccounting/form_template/market/amrlirt/v2016/AmrlirtTest.java
@@ -0,0 +1,280 @@
+package com.aplana.sbrf.taxaccounting.form_template.market.amrlirt.v2016;
+
+import com.aplana.sbrf.taxaccounting.model.*;
+import com.aplana.sbrf.taxaccounting.model.log.LogEntry;
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttribute;
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookValue;
+import com.aplana.sbrf.taxaccounting.refbook.RefBookDataProvider;
+import com.aplana.sbrf.taxaccounting.refbook.RefBookFactory;
+import com.aplana.sbrf.taxaccounting.service.script.util.ScriptUtils;
+import com.aplana.sbrf.taxaccounting.util.ScriptTestBase;
+import com.aplana.sbrf.taxaccounting.util.TestScriptHelper;
+import com.aplana.sbrf.taxaccounting.util.mock.ScriptTestMockHelper;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Matchers.*;
+import static org.mockito.Matchers.anyMapOf;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.when;
+
+/**
+ * Данные АМРЛИРТ.
+ */
+public class AmrlirtTest extends ScriptTestBase {
+    private static final int TYPE_ID = 902;
+    private static final int DEPARTMENT_ID = 1;
+    private static final int REPORT_PERIOD_ID = 1;
+    private static final int DEPARTMENT_PERIOD_ID = 1;
+    private static final FormDataKind KIND = FormDataKind.PRIMARY;
+
+    @Override
+    protected FormData getFormData() {
+        FormData formData = new FormData();
+        FormType formType = new FormType();
+        formType.setId(TYPE_ID);
+        formData.setId(TestScriptHelper.CURRENT_FORM_DATA_ID);
+        formData.setFormType(formType);
+        formData.setFormTemplateId(TYPE_ID);
+        formData.setKind(KIND);
+        formData.setState(WorkflowState.CREATED);
+        formData.setDepartmentId(DEPARTMENT_ID);
+        formData.setDepartmentReportPeriodId(DEPARTMENT_PERIOD_ID);
+        formData.setReportPeriodId(REPORT_PERIOD_ID);
+        return formData;
+    }
+
+    @Override
+    protected ScriptTestMockHelper getMockHelper() {
+        return getDefaultScriptTestMockHelper(AmrlirtTest.class);
+    }
+
+    @Before
+    public void mockServices() {
+        // макет нф
+        when(testHelper.getFormDataService().getFormTemplate(anyInt(), anyInt())).thenReturn(testHelper.getFormTemplate());
+    }
+
+    @After
+    public void resetMock() {
+        reset(testHelper.getRefBookFactory());
+        testHelper.reset();
+    }
+
+    @Test
+    public void create() {
+        testHelper.execute(FormDataEvent.CREATE);
+        Assert.assertEquals(testHelper.getFormTemplate().getRows().size(), testHelper.getDataRowHelper().getAll().size());
+        checkLogger();
+    }
+
+    // Проверка пустой
+    @Test
+    public void checkTest() {
+        testHelper.execute(FormDataEvent.CHECK);
+        // ошибок быть не должно
+        checkLogger();
+    }
+
+    // Расчет пустой
+    @Test
+    public void calcTest() {
+        testHelper.execute(FormDataEvent.CALCULATE);
+        // ошибок быть не должно
+        checkLogger();
+    }
+
+    @Test
+    public void addDelRowTest() {
+        int expected = testHelper.getDataRowHelper().getAll().size() + 1;
+
+        // Добавление
+        testHelper.execute(FormDataEvent.ADD_ROW);
+        // ошибок быть не должно
+        checkLogger();
+        // Количество строк должно увеличиться на 1
+        Assert.assertEquals("Add new row", expected, testHelper.getDataRowHelper().getAll().size());
+
+        // Удаление
+        DataRow<Cell> addDataRow = null;
+        for (DataRow<Cell> dataRow : testHelper.getDataRowHelper().getAll()) {
+            if (dataRow.getAlias() == null) {
+                addDataRow = dataRow;
+                break;
+            }
+        }
+        // Количество строк должно уменьшиться на 1
+        Assert.assertNotNull(addDataRow);
+        testHelper.setCurrentDataRow(addDataRow);
+        testHelper.execute(FormDataEvent.DELETE_ROW);
+        expected--;
+        // Количество строк должно уменьшиться на 1
+        Assert.assertEquals("Delete row", expected, testHelper.getDataRowHelper().getAll().size());
+        checkLogger();
+    }
+
+    @Test
+    public void calc1Test() {
+        // провайдер для справочника 520
+        when(testHelper.getFormDataService().getRefBookProvider(any(RefBookFactory.class), anyLong(),
+                anyMapOf(Long.class, RefBookDataProvider.class))).thenReturn(testHelper.getRefBookDataProvider());
+
+        // записи для справочника 520
+        when(testHelper.getRefBookDataProvider().getRecords(any(Date.class), any(PagingParams.class), anyString(),
+                any(RefBookAttribute.class))).thenAnswer(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                // ищет среди записей справочника запись соответствующую коду из фильтра
+                // в фильтре: INN = 'test' OR KIO = 'test'
+                String filter = (String) invocation.getArguments()[2];
+                String before = "INN = '";
+                String after = "' OR KIO = '";
+                int beforeIndex = filter.indexOf(before) + before.length();
+                int afterIndex = filter.indexOf(after);
+                String findValue = filter.substring(beforeIndex, afterIndex);
+                if (findValue == null) {
+                    return new PagingResult<Map<String, RefBookValue>>();
+                }
+                final Map<Long, Map<String, RefBookValue>> records = testHelper.getRefBookAllRecords(520L);
+                for (Map<String, RefBookValue> record : records.values()) {
+                    if (findValue.equals(record.get("INN").getStringValue()) || findValue.equals(record.get("KIO").getStringValue())) {
+                        List<Map<String, RefBookValue>> tmpRecords = Arrays.asList(record);
+                        return new PagingResult<Map<String, RefBookValue>>(tmpRecords);
+                    }
+                }
+                return new PagingResult<Map<String, RefBookValue>>();
+            }
+        });
+
+        FormData formData = getFormData();
+        formData.initFormTemplateParams(testHelper.getTemplate("..//src/main//resources//form_template//market//amrlirt//v2016//"));
+        List<DataRow<Cell>> dataRows = testHelper.getDataRowHelper().getAll();
+        // строка
+        DataRow<Cell> row = formData.createDataRow();
+        row.setIndex(1);
+        dataRows.add(row);
+        setDefaultValue(row);
+
+        testHelper.execute(FormDataEvent.CALCULATE);
+
+        String value2 = (String) row.getCell("name").getValue();
+        String expected = "name";
+        Assert.assertEquals(expected, value2);
+
+        checkLogger();
+    }
+
+    @Test
+    public void chec1kTest() {
+        FormData formData = getFormData();
+        formData.initFormTemplateParams(testHelper.getTemplate("..//src/main//resources//form_template//market//amrlirt//v2016//"));
+        List<DataRow<Cell>> dataRows = testHelper.getDataRowHelper().getAll();
+        List<LogEntry> entries = testHelper.getLogger().getEntries();
+        String msg;
+        int i;
+
+        // строка
+        DataRow<Cell> row = formData.createDataRow();
+        row.setIndex(1);
+        dataRows.add(row);
+
+        // нет ошибок
+        i = 0;
+        setDefaultValue(row);
+        testHelper.execute(FormDataEvent.CHECK);
+        Assert.assertEquals(i, entries.size());
+        testHelper.getLogger().clear();
+
+        // 1. Проверка заполнения обязательных полей
+        i = 0;
+        String [] nonEmptyColumns = { "crmId", "name", "inn", "code", "lgd" };
+        for (String alias : nonEmptyColumns) {
+            row.getCell(alias).setValue(null, null);
+        }
+        testHelper.execute(FormDataEvent.CHECK);
+        for (String alias : nonEmptyColumns) {
+            msg = String.format(ScriptUtils.WRONG_NON_EMPTY, row.getIndex(), row.getCell(alias).getColumn().getName());
+            Assert.assertEquals(msg, entries.get(i++).getMessage());
+        }
+        Assert.assertEquals(i, entries.size());
+        testHelper.getLogger().clear();
+
+        // 2. Неотрицательность графы
+        i = 0;
+        setDefaultValue(row);
+        row.getCell("lgd").setValue(-1L, null);
+        testHelper.execute(FormDataEvent.CHECK);
+        msg = String.format("Строка %s: Значение графы «%s» должно быть больше либо равно 0!",
+                row.getIndex(), row.getCell("lgd").getColumn().getName());
+        Assert.assertEquals(msg, entries.get(i++).getMessage());
+        Assert.assertEquals(i, entries.size());
+        testHelper.getLogger().clear();
+
+        // 3. Проверка на отсутствие нескольких записей по одном и тому же кредитному договору
+        i = 0;
+        DataRow<Cell> row2 = formData.createDataRow();
+        row2.setIndex(2);
+        dataRows.add(row2);
+        setDefaultValue(row);
+        setDefaultValue(row2);
+        testHelper.execute(FormDataEvent.CHECK);
+        msg = String.format("Строка %s: На форме уже существует строка со значением графы «%s» = «%s»!",
+                row2.getIndex(), row2.getCell("crmId").getColumn().getName(), row2.getCell("crmId").getValue());
+        Assert.assertEquals(msg, entries.get(i++).getMessage());
+        Assert.assertEquals(i, entries.size());
+        testHelper.getLogger().clear();
+        dataRows.remove(row2);
+    }
+
+    @Test
+    public void importExcelTest() {
+        int expected = 2; // в файле 2 строки
+        testHelper.setImportFileInputStream(getImportXlsInputStream());
+        testHelper.execute(FormDataEvent.IMPORT);
+        printLog();
+        Assert.assertEquals(expected, testHelper.getDataRowHelper().getAll().size());
+        checkLoadData(testHelper.getDataRowHelper().getAll());
+        checkLogger();
+    }
+
+    // Проверить загруженные данные
+    void checkLoadData(List<DataRow<Cell>> dataRows) {
+        // графа 1
+        Assert.assertEquals("test11", dataRows.get(0).getCell("crmId").getStringValue());
+        Assert.assertEquals("test21", dataRows.get(1).getCell("crmId").getStringValue());
+
+        // графа 2
+        Assert.assertEquals("test12", dataRows.get(0).getCell("name").getStringValue());
+        Assert.assertEquals("test22", dataRows.get(1).getCell("name").getStringValue());
+
+        // графа 3
+        Assert.assertEquals("test13", dataRows.get(0).getCell("inn").getStringValue());
+        Assert.assertEquals("test23", dataRows.get(1).getCell("inn").getStringValue());
+
+        // графа 4
+        Assert.assertEquals("test14", dataRows.get(0).getCell("code").getStringValue());
+        Assert.assertEquals("test24", dataRows.get(1).getCell("code").getStringValue());
+
+        // графа 5
+        Assert.assertEquals(11L, dataRows.get(0).getCell("lgd").getNumericValue().longValue());
+        Assert.assertEquals(22L, dataRows.get(1).getCell("lgd").getNumericValue().longValue());
+    }
+
+    private void setDefaultValue(DataRow<Cell> dataRow) {
+        String testValue = "test";
+        dataRow.getCell("crmId").setValue(testValue, null);
+        dataRow.getCell("name").setValue(testValue, null);
+        dataRow.getCell("inn").setValue(testValue, null);
+        dataRow.getCell("code").setValue(testValue, null);
+        dataRow.getCell("lgd").setValue(1L, null);
+    }
+}
---
+++ src/main/resources/form_template/market/amrlirt/v2016/script.groovy
@@ -0,0 +1,285 @@
+package form_template.market.amrlirt.v2016
+
+import com.aplana.sbrf.taxaccounting.model.FormDataEvent
+import com.aplana.sbrf.taxaccounting.model.log.LogLevel
+import groovy.transform.Field
+
+/**
+ * Данные АМРЛИРТ.
+ *
+ * formTemplateId = 902
+ * formType = 902
+ */
+
+// графа 1 - crmId
+// графа 2 - name
+// графа 3 - inn
+// графа 4 - code
+// графа 5 - lgd
+
+switch (formDataEvent) {
+    case FormDataEvent.CREATE:
+        formDataService.checkUnique(formData, logger)
+        break
+    case FormDataEvent.CHECK:
+        logicCheck()
+        break
+    case FormDataEvent.CALCULATE:
+        calc()
+        logicCheck()
+        formDataService.saveCachedDataRows(formData, logger)
+        break
+    case FormDataEvent.ADD_ROW:
+        formDataService.addRow(formData, currentDataRow, editableColumns, autoFillColumns)
+        break
+    case FormDataEvent.DELETE_ROW:
+        if (currentDataRow != null) {
+            formDataService.getDataRowHelper(formData)?.delete(currentDataRow)
+        }
+        break
+    case FormDataEvent.MOVE_CREATED_TO_PREPARED:  // Подготовить из "Создана"
+    case FormDataEvent.MOVE_CREATED_TO_APPROVED:  // Утвердить из "Создана"
+    case FormDataEvent.MOVE_CREATED_TO_ACCEPTED:  // Принять из "Создана"
+    case FormDataEvent.MOVE_PREPARED_TO_APPROVED: // Утвердить из "Подготовлена"
+    case FormDataEvent.MOVE_PREPARED_TO_ACCEPTED: // Принять из "Подготовлена"
+    case FormDataEvent.MOVE_APPROVED_TO_ACCEPTED: // Принять из "Утверждена"
+        logicCheck()
+        break
+    case FormDataEvent.IMPORT:
+        importData()
+        formDataService.saveCachedDataRows(formData, logger)
+        break
+}
+
+@Field
+def allColumns = ['crmId', 'name', 'inn', 'code', 'lgd']
+
+@Field
+def editableColumns = allColumns
+
+@Field
+def autoFillColumns = []
+
+// Проверяемые на пустые значения атрибуты
+@Field
+def nonEmptyColumns = allColumns
+
+@Field
+def startDate = null
+
+@Field
+def endDate = null
+
+def getReportPeriodStartDate() {
+    if (startDate == null) {
+        startDate = reportPeriodService.getStartDate(formData.reportPeriodId).time
+    }
+    return startDate
+}
+
+def getReportPeriodEndDate() {
+    if (endDate == null) {
+        endDate = reportPeriodService.getEndDate(formData.reportPeriodId).time
+    }
+    return endDate
+}
+
+@Field
+def refBookCache = [:]
+
+def getRefBookValue(def long refBookId, def recordId) {
+    return formDataService.getRefBookValue(refBookId, recordId, refBookCache)
+}
+
+// Поиск записи в справочнике по значению (для импорта)
+def getRecordIdImport(def Long refBookId, def String alias, def String value, def int rowIndex, def int colIndex,
+                      def boolean required = true) {
+    if (value == null || value.trim().isEmpty()) {
+        return null
+    }
+    return formDataService.getRefBookRecordIdImport(refBookId, recordCache, providerCache, alias, value,
+            getReportPeriodEndDate(), rowIndex, colIndex, logger, required)
+}
+
+void logicCheck() {
+    def dataRows = formDataService.getDataRowHelper(formData).allCached
+
+    def rowsMap = [:]
+    for (def row : dataRows) {
+        // 1. Проверка заполнения обязательных полей
+        checkNonEmptyColumns(row, row.getIndex(), nonEmptyColumns, logger, true)
+
+        // 2. Неотрицательность графы
+        if (row.lgd != null && row.lgd < 0) {
+            logger.error("Строка %s: Значение графы «%s» должно быть больше либо равно 0!",
+                    row.getIndex(), getColumnName(row, 'lgd'))
+        }
+
+        // 3. Проверка на отсутствие нескольких записей по одном и тому же кредитному договору
+        def key = getKey(row)
+        if (rowsMap[key] == null) {
+            rowsMap[key] = []
+        } else {
+            logger.error("Строка %s: На форме уже существует строка со значением графы «%s» = «%s»!",
+                    row.getIndex(), getColumnName(row, 'crmId'), row.crmId)
+        }
+        rowsMap[key].add(row)
+    }
+}
+
+String getKey(def row) {
+    return row.crmId?.trim()?.toLowerCase()
+}
+
+@Field
+def recordCache = [:]
+
+@Field
+def providerCache = [:]
+
+def getRecords(def inn) {
+    def filter = 'INN = \'' + inn + '\' OR KIO = \'' + inn + '\''
+    def provider = formDataService.getRefBookProvider(refBookFactory, 520L, providerCache)
+    if (recordCache[filter] == null) {
+        recordCache.put(filter, provider.getRecords(getReportPeriodEndDate(), null, filter, null))
+    }
+    return recordCache[filter]
+}
+
+void calc() {
+    def dataRows = formDataService.getDataRowHelper(formData).allCached
+
+    for (def row : dataRows) {
+        // графа 2
+        row.name = calc2(row)
+    }
+}
+
+def calc2(def row) {
+    if (row.inn == null) {
+        return row.name
+    }
+    def records = getRecords(row.inn)
+    if (records?.size() == 1) {
+        row.name = records.get(0).NAME.value
+    }
+    return row.name
+}
+
+void importData() {
+    def tmpRow = formData.createDataRow()
+    int COLUMN_COUNT = 5
+    int HEADER_ROW_COUNT = 2
+    String TABLE_START_VALUE = getColumnName(tmpRow, 'crmId')
+    String TABLE_END_VALUE = null
+
+    def allValues = []      // значения формы
+    def headerValues = []   // значения шапки
+    def paramsMap = ['rowOffset' : 0, 'colOffset' : 0]  // мапа с параметрами (отступы сверху и слева)
+
+    checkAndReadFile(ImportInputStream, UploadFileName, allValues, headerValues, TABLE_START_VALUE, TABLE_END_VALUE, HEADER_ROW_COUNT, paramsMap)
+
+    // проверка шапки
+    checkHeaderXls(headerValues, COLUMN_COUNT, HEADER_ROW_COUNT)
+    if (logger.containsLevel(LogLevel.ERROR)) {
+        return;
+    }
+    // освобождение ресурсов для экономии памяти
+    headerValues.clear()
+    headerValues = null
+
+    def fileRowIndex = paramsMap.rowOffset
+    def colOffset = paramsMap.colOffset
+    paramsMap.clear()
+    paramsMap = null
+
+    def rowIndex = 0
+    def rows = []
+    def allValuesCount = allValues.size()
+
+    // формирование строк нф
+    for (def i = 0; i < allValuesCount; i++) {
+        rowValues = allValues[0]
+        fileRowIndex++
+        // все строки пустые - выход
+        if (!rowValues) {
+            allValues.remove(rowValues)
+            rowValues.clear()
+            break
+        }
+        // простая строка
+        rowIndex++
+        def newRow = getNewRowFromXls(rowValues, colOffset, fileRowIndex, rowIndex)
+        rows.add(newRow)
+        // освободить ненужные данные - иначе не хватит памяти
+        allValues.remove(rowValues)
+        rowValues.clear()
+    }
+
+    showMessages(rows, logger)
+    if (!logger.containsLevel(LogLevel.ERROR)) {
+        updateIndexes(rows)
+        formDataService.getDataRowHelper(formData).save(rows)
+    }
+}
+
+/**
+ * Проверить шапку таблицы
+ *
+ * @param headerRows строки шапки
+ * @param colCount количество колонок в таблице
+ * @param rowCount количество строк в таблице
+ */
+void checkHeaderXls(def headerRows, def colCount, rowCount) {
+    checkHeaderSize(headerRows, colCount, rowCount)
+
+    def headers = formDataService.getFormTemplate(formData.formType.id, formData.reportPeriodId).headers
+    def headerMapping =[[:]]
+    def index = 0
+    allColumns.each { alias ->
+        headerMapping.add(([(headerRows[0][index]): headers[0][alias]]))
+        headerMapping.add(([(headerRows[1][index]): (index + 1).toString()]))
+        index++
+    }
+    checkHeaderEquals(headerMapping, logger)
+}
+
+/**
+ * Получить новую строку нф по значениям из экселя.
+ *
+ * @param values список строк со значениями
+ * @param colOffset отступ в колонках
+ * @param fileRowIndex номер строки в тф
+ * @param rowIndex строка в нф
+ */
+def getNewRowFromXls(def values, def colOffset, def fileRowIndex, def rowIndex) {
+    def newRow = getNewRow()
+    newRow.setIndex(rowIndex)
+    newRow.setImportIndex(fileRowIndex)
+
+    def colIndex = -1
+
+    // графа 1..4
+    ['crmId', 'name', 'inn', 'code'].each { alias ->
+        colIndex++
+        newRow[alias] = values[colIndex]
+    }
+
+    // графа 5
+    colIndex++
+    newRow.lgd = parseNumber(values[colIndex], fileRowIndex, colIndex + colOffset, logger, true)
+
+    return newRow
+}
+
+def getNewRow() {
+    def newRow = formData.createStoreMessagingDataRow()
+    editableColumns.each {
+        newRow.getCell(it).editable = true
+        newRow.getCell(it).setStyleAlias('Редактируемая')
+    }
+    autoFillColumns.each {
+        newRow.getCell(it).setStyleAlias('Автозаполняемая')
+    }
+    return newRow
+}
