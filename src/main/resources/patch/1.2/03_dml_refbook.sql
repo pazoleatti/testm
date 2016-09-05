67c8e0f2b27d2d385d2d3b96b99541fd66ab41ea
Elena Kabanova <elena.kabanova@aplana.com>
2016-09-05 17:48:50 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-16814: 1.2. ЗемНалог. Изменить перечень периодов в справочнике "Коды, определяющие налоговый (отчетный) период"

--- src/main/resources/patch/1.2/03_dml_refbook.sql
+++ src/main/resources/patch/1.2/03_dml_refbook.sql
@@ -54,5 +54,28 @@ end;
 COMMIT;
 
 ---------------------------------------------------------------------------
+--https://jira.aplana.com/browse/SBRFACCTAX-16814: 1.2. ЗемНалог. Изменить перечень периодов в справочнике "Коды, определяющие налоговый (отчетный) период"
+declare l_task_name varchar2(128) := 'RefBook Block #3 (SBRFACCTAX-16814 - Report period for ''L'' changes))';
+begin
+	
+	merge into ref_book_value tgt
+		using (
+		  select rbr.id as record_id, 3002 as attribute_id, case when rbv.string_value in ('21', '22', '23', '34') then 1 else 0 end as number_value 
+		  from ref_book_record rbr
+		  join ref_book_value rbv on rbv.record_id = rbr.id and rbv.attribute_id = 25) src
+		on (tgt.record_id = src.record_id and tgt.attribute_id = src.attribute_id) 
+		when matched then
+			 update set tgt.number_value = src.number_value where tgt.number_value <> src.number_value; 		
+			 
+	dbms_output.put_line(l_task_name||'[INFO]: '||sql%rowcount||' row(s) merged into ref_book_value');	
+	
+EXCEPTION
+	when OTHERS then
+		dbms_output.put_line(l_task_name||'[FATAL]: '||sqlerrm);
+        ROLLBACK;
+end;
+/
+commit;	
+---------------------------------------------------------------------------
 COMMIT;
 EXIT;
