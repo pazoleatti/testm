f74388086dbc5467af667e1dd676fa8834de16e9
Stanislav Yasinskiy <Stanislav.Yasinskiy@aplana.com>
2016-09-23 09:25:20 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-17006 1.2 РнРнтб. Первичка 2.1. Убрать сортировку

--- src/main/resources/form_template/market/market_2_1/v2016/script.groovy
+++ src/main/resources/form_template/market/market_2_1/v2016/script.groovy
@@ -89,9 +89,6 @@ switch (formDataEvent) {
         importData()
         formDataService.saveCachedDataRows(formData, logger)
         break
-    case FormDataEvent.SORT_ROWS:
-        sortFormDataRows()
-        break
 }
 
 @Field
@@ -474,16 +471,4 @@ def getNewRow() {
         newRow.getCell(it).setStyleAlias('Автозаполняемая')
     }
     return newRow
-}
-
-// Сортировка групп и строк
-void sortFormDataRows(def saveInDB = true) {
-    def dataRowHelper = formDataService.getDataRowHelper(formData)
-    def dataRows = dataRowHelper.allCached
-    sortRows(refBookService, logger, dataRows, null, null, null)
-    if (saveInDB) {
-        dataRowHelper.saveSort()
-    } else {
-        updateIndexes(dataRows)
-    }
 }
