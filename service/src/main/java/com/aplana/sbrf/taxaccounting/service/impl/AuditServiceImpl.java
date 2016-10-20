21c64be8f1a10075268df1827aca15329f4a4153
Denis Novikov <Denis.Novikov@aplana.com>
2016-10-20 11:02:24 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-17159 Сервис логирования (мелкие исправления)

--- service/src/main/java/com/aplana/sbrf/taxaccounting/service/impl/AuditServiceImpl.java
+++ service/src/main/java/com/aplana/sbrf/taxaccounting/service/impl/AuditServiceImpl.java
@@ -266,7 +266,7 @@ public class AuditServiceImpl implements AuditService {
         LogSystem log = new LogSystem();
         log.setIp(userInfo.getIp());
         log.setEventId(event.getCode());
-		log.setUserLogin(userInfo.getUser().getName() + " (" + userInfo.getUser().getLogin() + ")");
+		log.setUserLogin((userInfo.getUser().getName() != null ? userInfo.getUser().getName() + " " : "") + "(" + userInfo.getUser().getLogin() + ")");
 
         StringBuilder roles = new StringBuilder();
         List<TARole> taRoles = userInfo.getUser().getRoles();
