bec6d533c103d4078a3a4c386fdfee39f5bc8ecb
Elena Kabanova <elena.kabanova@aplana.com>
2016-10-25 16:01:18 +0500

    https://jira.aplana.com/browse/SBRFACCTAX-17344: 1.25 Прибыль. Реализовать справочник "Типы операций по сделкам"

--- src/main/resources/patch/1.25/01_dml_refbook.sql
+++ src/main/resources/patch/1.25/01_dml_refbook.sql
@@ -61,5 +61,32 @@ end;
 
 COMMIT;
 ---------------------------------------------------------------------------
+--https://jira.aplana.com/browse/SBRFACCTAX-17344: 1.25 Прибыль. Реализовать справочник "Типы операций по сделкам"
+declare l_task_name varchar2(128) := 'RefBook Block #3 (SBRFACCTAX-17344) - new ref_book(I))';
+begin
+	
+	INSERT INTO ref_book (id, name, visible, type, read_only, region_attribute_id) VALUES (802,'Типы операций по сделкам',1,0,0,null);
+		INSERT INTO ref_book_attribute (id, ref_book_id, name, alias, type, ord, reference_id, attribute_id, visible, precision, width, required, is_unique, sort_order, format, read_only, max_length) VALUES (8021, 802, 'Тип операции', 'TYPE',1,1,null,null,1,null,30,1,1,null,null,0,256);
+	
+	INSERT INTO ref_book_record (id, record_id, ref_book_id, version, status) values (seq_ref_book_record.nextval, 1, 802, to_date('01.01.2017', 'DD.MM.YYYY'), 0);
+		INSERT INTO ref_book_value (record_id, attribute_id, string_value) values (seq_ref_book_record.currval, 8021, 'Уступка (новация)');		
+
+	INSERT INTO ref_book_record (id, record_id, ref_book_id, version, status) values (seq_ref_book_record.nextval, 2, 802, to_date('01.01.2017', 'DD.MM.YYYY'), 0);
+		INSERT INTO ref_book_value (record_id, attribute_id, string_value) values (seq_ref_book_record.currval, 8021, 'Досрочное прекращение');	
+	
+	dbms_output.put_line(l_task_name||'[INFO]: Success');	
+	
+EXCEPTION
+	when DUP_VAL_ON_INDEX then
+		dbms_output.put_line(l_task_name||'[ERROR]: ref_book or its attributes already exist ('||sqlerrm||')');
+		ROLLBACK;
+	when OTHERS then
+		dbms_output.put_line(l_task_name||'[FATAL]: '||sqlerrm);
+        ROLLBACK;
+end;
+/
+
+COMMIT;
+---------------------------------------------------------------------------
 COMMIT;
 EXIT;
