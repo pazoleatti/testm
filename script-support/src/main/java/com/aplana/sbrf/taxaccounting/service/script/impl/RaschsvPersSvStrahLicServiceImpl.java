87f46df559dd7e39fa9d4925f49049f707b11a56
Oleg Shelepaev <OShelepaev@aplana.com>
2016-12-28 17:28:19 +0500

    https://jira.aplana.com/browse/SBRFNDFL-83 - СБОРЫ. Реализовать логику разбиения xml по таблицам / Убрано дублирование в сервисе + вынесена генерация идентификаторов за пределы batchUpdate

--- script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/impl/RaschsvPersSvStrahLicServiceImpl.java
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/impl/RaschsvPersSvStrahLicServiceImpl.java
@@ -4,13 +4,15 @@ import com.aplana.sbrf.taxaccounting.dao.raschsv.RaschsvPersSvStrahLicDao;
 import com.aplana.sbrf.taxaccounting.model.raschsv.RaschsvPersSvStrahLic;
 import com.aplana.sbrf.taxaccounting.service.script.RaschsvPersSvStrahLicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("raschsvPersSvStrahLicService")
 public class RaschsvPersSvStrahLicServiceImpl implements RaschsvPersSvStrahLicService {
 
     @Autowired
-    RaschsvPersSvStrahLicDao raschsvPersSvStrahLicDao;
+    private RaschsvPersSvStrahLicDao raschsvPersSvStrahLicDao;
 
     public Integer insert(List<RaschsvPersSvStrahLic> raschsvPersSvStrahLic) {
         return raschsvPersSvStrahLicDao.insert(raschsvPersSvStrahLic);
