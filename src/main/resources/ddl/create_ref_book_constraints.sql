6e176616d2eb96f69775dd1447770b89ffbfd7d9
Марина Еремеева <Marina.Eremeeva@aplana.com>
2017-01-12 18:04:50 +0200

    https://jira.aplana.com/browse/SBRFNDFL-141 : создана таблица для справочника "Коды видов вычетов" и соответствующие последовательности

--- src/main/resources/ddl/create_ref_book_constraints.sql
+++ src/main/resources/ddl/create_ref_book_constraints.sql
@@ -3,6 +3,9 @@
 -----------------------------------------------------------------------------------------------------------------------------
 -- Коды видов дохода
 alter table ref_book_income_type add constraint pk_ref_book_income_type primary key (id);
-alter table ref_book_income_type add constraint chk_ref_book_income_type_ST check (status in (-1,0,1,2));
+alter table ref_book_income_type add constraint chk_ref_book_income_type_st check (status in (-1,0,1,2));
+-- Коды видов вычетов
+alter table ref_book_deduction_type add constraint pk_ref_book_deduction_type primary key (id);
+alter table ref_book_deduction_type add constraint chk_ref_book_deduc_type_status check (status in (-1,0,1,2));
 
 -----------------------------------------------------------------------------------------------------------------------------
--- src/main/resources/ddl/create_ref_book_tables.sql
+++ src/main/resources/ddl/create_ref_book_tables.sql
@@ -20,9 +20,26 @@ comment on column ref_book_income_type.status is 'Статус записи (0 -
 comment on column ref_book_income_type.code is 'Код';
 comment on column ref_book_income_type.name is 'Наименование дохода';
 
---alter table ref_book_income_type add constraint pk_ref_book_income_type primary key (id);
+create sequence seq_ref_book_income_type start with 21 increment by 1;
 
---alter table ref_book_income_type add constraint chk_ref_book_income_type_ST check (status in (-1,0,1,2));
+-- Коды видов вычетов
+create table ref_book_deduction_type
+(
+  id number(18) not null, 
+	record_id number(9) not null, 
+	version date not null, 
+	status number(1) default 0 not null, 
+	code varchar2(3 char) not null, 
+	name varchar2(2000 char) not null
+);
 
---create sequence seq_ref_book_income_type start with 21 increment by 1;
+comment on table ref_book_deduction_type is 'Коды видов вычетов';
+comment on column ref_book_deduction_type.id is 'Уникальный идентификатор';
+comment on column ref_book_deduction_type.record_id is 'Идентификатор строки справочника. Может повторяться у разных версий';
+comment on column ref_book_deduction_type.version is 'Версия. Дата актуальности записи';
+comment on column ref_book_deduction_type.status is 'Статус записи (0 - обычная запись, -1 - удаленная, 1 - черновик, 2 - фиктивная)';
+comment on column ref_book_deduction_type.code is 'Код';
+comment on column ref_book_deduction_type.name is 'Наименование вычета';
+
+create sequence seq_ref_book_deduction_type start with 106 increment by 1;
 -----------------------------------------------------------------------------------------------------------------------------
