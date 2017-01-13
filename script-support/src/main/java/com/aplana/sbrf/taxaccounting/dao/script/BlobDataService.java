1be9179568d359a8e2b333b0ae49659ec4e3a236
YTrofimov <Yuriy.Trofimov@aplana.com>
2017-01-13 14:13:47 +0300

    Иентерфейс получения бинарных данных

--- script-support/src/main/java/com/aplana/sbrf/taxaccounting/dao/script/BlobDataService.java
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/dao/script/BlobDataService.java
@@ -1,4 +1,4 @@
-package com.aplana.sbrf.taxaccounting.service.script;
+package com.aplana.sbrf.taxaccounting.dao.script;
 
 import com.aplana.sbrf.taxaccounting.model.BlobData;
 import com.aplana.sbrf.taxaccounting.util.ScriptExposed;
