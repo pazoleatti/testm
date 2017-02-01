dc58417cb71953290b56fb96dab88e21143056eb
Marina Eremeeva <Marina.Eremeeva@aplana.com>
2017-02-01 11:44:31 +0200

    https://jira.aplana.com/browse/SBRFNDFL-282 - НДФЛ. Заполнить таблицу для справочника "Налоговые инспекции"

--- src/main/resources/ddl/create_ref_book_constraints.sql
+++ src/main/resources/ddl/create_ref_book_constraints.sql
@@ -65,6 +65,9 @@ alter table ref_book_income_kind add constraint pk_ref_book_income_kind primary 
 --Категории прикрепленных файлов
 alter table ref_book_attach_file_type add constraint pk_ref_book_attach_file_type primary key(id);
 
+--Налоговые инспекции
+alter table ref_book_tax_inspection add constraint pk_ref_book_tax_inspection primary key(id);
+
 -- Параметры подразделения по НДФЛ
 alter table ref_book_ndfl add constraint pk_ref_book_ndfl primary key(id);
 alter table ref_book_ndfl_detail add constraint pk_ref_book_ndfl_detail primary key(id);
