089cff6fdc460e19827ff0ae838074858f6dfdfc
Pavel Makarov <Pavel.Makarov@aplana.com>
2017-02-20 00:43:08 +0500

    Merge branch 'master' of https://scm.aplana.com/git/sbrfndfl
    Conflicts:
    	script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/DeclarationService.java
    	script-support/src/main/java/com/aplana/sbrf/taxaccounting/service/script/impl/DeclarationServiceImpl.java

--- script-support/src/main/java/com/aplana/sbrf/taxaccounting/dao/script/impl/BlobDataServiceImpl.java
+++ script-support/src/main/java/com/aplana/sbrf/taxaccounting/dao/script/impl/BlobDataServiceImpl.java
@@ -3,20 +3,20 @@ package com.aplana.sbrf.taxaccounting.dao.script.impl;
 import com.aplana.sbrf.taxaccounting.dao.BlobDataDao;
 import com.aplana.sbrf.taxaccounting.dao.script.BlobDataService;
 import com.aplana.sbrf.taxaccounting.model.BlobData;
+import com.aplana.sbrf.taxaccounting.model.exception.ServiceException;
+import org.apache.poi.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
+import java.io.*;
 import java.util.Date;
+import java.util.UUID;
 
 @Service("blobDataServiceDaoImpl")
 public class BlobDataServiceImpl implements BlobDataService {
 
     @Autowired
     private BlobDataDao blobDataDao;
-
-    @Autowired
-    private com.aplana.sbrf.taxaccounting.service.BlobDataService blobDataService;
 
     @Override
     public BlobData get(String uuid) {
@@ -25,6 +25,29 @@ public class BlobDataServiceImpl implements BlobDataService {
 
     @Override
     public String create(File file, String name, Date createDate) {
-        return blobDataService.create(file, name, createDate);
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+            BlobData data = initBlob("", fileInputStream, name, createDate);
+            return blobDataDao.createWithDate(data);
+        } catch (FileNotFoundException e) {
+            throw new ServiceException("", e);
+        } finally {
+            IOUtils.closeQuietly(fileInputStream);
+        }
+    }
+
+    private static BlobData initBlob(String blobId, InputStream is, String name, Date date){
+        BlobData blobData = new BlobData();
+        blobData.setName(name);
+        try {
+            is.available();
+        } catch (IOException e) {
+            throw new ServiceException("Ошибка при получении данных", e);
+        }
+        blobData.setInputStream(is);
+        blobData.setCreationDate(date);
+        blobData.setUuid(blobId.isEmpty() ? UUID.randomUUID().toString().toLowerCase() : blobId);
+        return blobData;
     }
 }
