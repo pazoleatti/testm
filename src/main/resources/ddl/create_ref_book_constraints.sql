deb052a494b8dbe342fbf2ce5fc71bee2b0ca886
Marina Eremeeva <Marina.Eremeeva@aplana.com>
2017-02-20 17:47:22 +0200

    https://jira.aplana.com/browse/SBRFNDFL-479 - Заменить dml-скрипт для таблицы department: обновила скрипт https://jira.aplana.com/browse/SBRFNDFL-481 - Заменить dml-файлы таблицы REF_BOOK_OKTMO: удалила ../dml/refbook/ref_book_oktmo

--- src/main/resources/ddl/create_ref_book_constraints.sql
+++ src/main/resources/ddl/create_ref_book_constraints.sql
@@ -51,9 +51,8 @@ alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_oblig forei
 alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_type foreign key(type) references ref_book_make_calc(id);
 alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_re_code foreign key(reorg_form_code) references ref_book_reorganization(id);
 alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_signatory foreign key(signatory_id) references ref_book_signatory_mark(id);
-alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_oktmo foreign key(oktmo) references ref_book_oktmo(id);
-alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_dept foreign key(department_id) references department(id);
-alter table ref_book_fond add constraint fk_ref_book_fond_depart foreign key(department_id) references department(id);
+alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_depart foreign key (department_id) references department(id);
+alter table ref_book_ndfl_detail add constraint fk_ref_book_ndfl_det_oktmo foreign key (oktmo) references ref_book_oktmo(id);
 alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_parent foreign key(ref_book_fond_id) references ref_book_fond(id) on delete cascade;
 alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_pres_pl foreign key(present_place) references ref_book_present_place(id);
 alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_okved foreign key(okved) references ref_book_okved(id);
@@ -62,8 +61,9 @@ alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_oblig forei
 alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_type foreign key(type) references ref_book_make_calc(id);
 alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_re_code foreign key(reorg_form_code) references ref_book_reorganization(id);
 alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_signatory foreign key(signatory_id) references ref_book_signatory_mark(id);
-alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_oktmo foreign key(oktmo) references ref_book_oktmo(id);
-alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_dept foreign key(department_id) references department(id);
+alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_depart foreign key (department_id) references department(id);
+alter table ref_book_fond_detail add constraint fk_ref_book_fond_det_oktmo foreign key (oktmo) references ref_book_oktmo(id);
+alter table ref_book_fond add constraint fk_ref_book_fond_depart foreign key(department_id) references department(id);
 alter table declaration_data add constraint declaration_data_fk_asnu_id foreign key (asnu_id) references ref_book_asnu(id);
 alter table declaration_template add constraint fk_declaration_template_fkind foreign key(form_kind) references declaration_kind(id);
 alter table declaration_template add constraint fk_declaration_template_ftype foreign key(form_type) references ref_book_form_type(id);
@@ -73,6 +73,7 @@ alter table ref_book_deduction_type add constraint fk_ref_book_deduc_type_mark f
 alter table sec_user_asnu add constraint fk_sec_user_asnu_user foreign key(user_id) references sec_user(id) on delete cascade;
 alter table sec_user_asnu add constraint fk_sec_user_asnu_asnu foreign key(asnu_id) references ref_book_asnu(id);
 ------
+alter table department add constraint fk_department_region_id foreign key (region_id) references ref_book_region(id);
 alter table ref_book_address add constraint fk_ref_book_address_country foreign key (country_id) references ref_book_country(id);
 alter table ref_book_person add constraint fk_ref_book_person_citizenship foreign key (citizenship) references ref_book_country(id);
 alter table ref_book_id_doc add constraint fk_ref_book_id_doc_doc_id foreign key (doc_id) references ref_book_doc_type(id);
