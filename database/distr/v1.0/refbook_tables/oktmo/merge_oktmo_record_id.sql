ea784ba4d1de2da330eed347a5a3e2a2b0fb0715
Marina Eremeeva <Marina.Eremeeva@aplana.com>
2017-02-22 11:29:51 +0200

    исправлен патч на БД по результатам выполнения на ndfl_next2

--- database/distr/v1.0/refbook_tables/oktmo/merge_oktmo_record_id.sql
+++ database/distr/v1.0/refbook_tables/oktmo/merge_oktmo_record_id.sql
@@ -5,6 +5,12 @@ using (select distinct t.code,first_value(t.record_id) over(partition by t.code 
  on (r.code=nr.code)
 when matched then update set r.record_id=nr.first_rec_id;
 
+-- исправление дублей по record_id и version
+update ref_book_oktmo set record_id=seq_ref_book_record_row_id.nextval
+where (record_id,version) in (select record_id,version
+                              from ref_book_oktmo
+                              group by record_id,version having count(*)>1);
+
 update ref_book_oktmo set code=substr(code,1,8) where razd=1;
 
 commit;
