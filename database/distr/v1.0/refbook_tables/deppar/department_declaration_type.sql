112ac08576d506c5c558c7298dcd19d3a10e1233
Marina Eremeeva <Marina.Eremeeva@aplana.com>
2017-02-27 11:29:35 +0200

    https://jira.aplana.com/browse/SBRFNDFL-475 - Учесть новые подразделения для 1151111 при установке нового Справочника подразделений: добавлены dml-скрипты на заполнение назначения форм по фондам и сборам для новых подразделений

---
+++ database/distr/v1.0/refbook_tables/deppar/department_declaration_type.sql
@@ -0,0 +1,5 @@
+insert into department_declaration_type (id,department_id,declaration_type_id) values (seq_dept_declaration_type.nextval,(select id from department where code='59'),200);
+insert into department_declaration_type (id,department_id,declaration_type_id) values (seq_dept_declaration_type.nextval,(select id from department where code='999992'),200);
+insert into department_declaration_type (id,department_id,declaration_type_id) values (seq_dept_declaration_type.nextval,(select id from department where code='999991'),200);
+insert into department_declaration_type (id,department_id,declaration_type_id) values (seq_dept_declaration_type.nextval,(select id from department where code='999993'),200);
+insert into department_declaration_type (id,department_id,declaration_type_id) values (seq_dept_declaration_type.nextval,(select id from department where code='8'),200);
