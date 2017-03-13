37df20d0a71baa63d4771bbbe22eb62f4b623b10
Marina Eremeeva <Marina.Eremeeva@aplana.com>
2017-03-13 21:16:53 +0200

    синхронизированы скрипты по заполнению настроек подразделений с патчем

--- src/main/resources/dml/fill_dep_par/fill_ref_dep_params.sql
+++ src/main/resources/dml/fill_dep_par/fill_ref_dep_params.sql
@@ -1,33 +1,46 @@
-insert into ref_book_ndfl(department_id, inn, version, record_id, status, id)
-select tab.*,rownum,0,rownum
-  from (select d.dep_id,first_value(t.inn) over(partition by t.depcode) inn,
-               trunc(sysdate,'y')
-          from tmp_dep_params t join tmp_depart d on (d.code=t.depcode)
-         where d.dep_id is not null
-        group by d.dep_id,t.depcode,t.inn) tab;
- 
-insert into ref_book_ndfl_detail(department_id, id, ref_book_ndfl_id,record_id, version, status, row_ord,
-          tax_organ_code, kpp, present_place, name, oktmo, phone, 
-          signatory_id, signatory_surname, signatory_firstname, signatory_lastname, approve_doc_name, approve_org_name, reorg_form_code)
-select d.dep_id, seq_ref_book_ndfl_detail.nextval,n.id ndfl_id,seq_ref_book_ndfl_detail.currval,trunc(sysdate,'y'),0,row_number() over (partition by t.depcode order by t.rowid) rn,
-       t.tax_end,t.kpp,pp.id place_id,t.titname,
-              o.id oktmo_id,t.phone,s.id sign_id,t.surname,t.name,t.lastname,t.docname,t.orgname,ro.id reorg_id 
-          from tmp_dep_params t join tmp_depart d on (d.code=t.depcode)
-                                left join ref_book_ndfl n on (n.department_id=d.dep_id and n.version=trunc(sysdate,'y'))
-                                left join ref_book_present_place pp on (pp.code=t.place and trunc(sysdate,'y') between pp.version and trunc(sysdate,'y'))
-                                left join ref_book_oktmo o on (o.code=t.oktmo and trunc(sysdate,'y') between o.version and trunc(sysdate,'y'))
-                                left join (select rec.id,
-                                                  rec.record_id,
-                                                  rec.version,
-                                                  rec.status,
-                                                  vl.code,vl.name
-                                            from (select record_id,
-                                                          max(decode(v.attribute_id,212,v.number_value)) code,
-                                                          max(decode(v.attribute_id,213,v.string_value)) name
-                                                     from ref_book_value v
-                                                    where v.attribute_id in (212,213)
-                                                    group by record_id) vl left join ref_book_record rec on (rec.id=vl.record_id and trunc(sysdate,'y') between rec.version and trunc(sysdate,'y'))
-                                            order by rec.id) s on (s.code=t.sign and trunc(sysdate,'y') between s.version and trunc(sysdate,'y'))
-                                left join ref_book_reorganization ro on (ro.code=t.reorgcode and trunc(sysdate,'y') between ro.version and trunc(sysdate,'y'))
-/*where t.depcode='ЦЧБ'*/;     
+declare
+  v_version date:=to_date('01.01.2016','dd.mm.yyyy');
+begin
+      for dep in (select depcode,count(*) cnt,max(row_num) last_num
+                    from tmp_dep_params
+                    group by depcode
+                    minus
+                    select td.code,count(*) cnt,max(row_ord) last_num
+                    from ref_book_ndfl n join department d on (d.id=n.department_id)
+                                         join ref_book_ndfl_detail p on (p.ref_book_ndfl_id=n.id)
+                                         join tmp_depart td on (td.dep_id=d.id)
+                    group by td.code 
+                    order by 1) loop
 
+    delete from ref_book_ndfl_detail where department_id in (select dep_id from tmp_depart where code=dep.depcode);
+    delete from ref_book_ndfl where department_id in (select dep_id from tmp_depart where code=dep.depcode);
+    
+    insert into ref_book_ndfl(department_id, version, record_id, status, id)
+    select tab.*,seq_ref_book_record_row_id.nextval,0,seq_ref_book_record.nextval
+      from (select d.dep_id,
+                   v_version
+              from tmp_dep_params t join tmp_depart d on (d.code=t.depcode)
+             where d.dep_id is not null
+               and t.depcode=dep.depcode
+            group by d.dep_id,t.depcode,t.inn) tab;
+            
+    insert into ref_book_ndfl_detail(department_id, id, ref_book_ndfl_id,record_id, version, status, row_ord,
+              tax_organ_code, kpp, present_place, name, oktmo, phone, 
+              signatory_id, signatory_surname, signatory_firstname, signatory_lastname, approve_doc_name, approve_org_name, reorg_form_code)
+    select d.dep_id, seq_ref_book_record.nextval,n.id ndfl_id,seq_ref_book_record_row_id.nextval,v_version,0,row_num,
+           t.tax_end,t.kpp,pp.id place_id,t.titname,
+                  o.mid oktmo_id,t.phone,s.id sign_id,t.surname,t.name,t.lastname,t.docname,t.orgname,ro.id reorg_id 
+              from tmp_dep_params t join tmp_depart d on (d.code=t.depcode)
+                                             left join ref_book_ndfl n on (n.department_id=d.dep_id and n.version=v_version)
+                                             left join ref_book_present_place pp on (pp.code=t.place and v_version between pp.version and v_version)
+                                             left join (select code,max(id) mid from ref_book_oktmo group by code) o on (o.code=t.oktmo)
+                                             left join ref_book_signatory_mark s on (s.code=t.sign and v_version between s.version and v_version)
+                                             left join ref_book_reorganization ro on (ro.code=t.reorgcode and v_version between ro.version and v_version)
+    where t.depcode=dep.depcode;
+  commit;
+  end loop;
+end;
+/
+
+commit;
+--exit;
