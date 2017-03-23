0b5ca9da8f196390187efb9faae1ae6743b32a07
Marat Fayzullin <mfayzullin@aplana.com>
2017-03-23 22:50:36 +0500

    https://jira.aplana.com/browse/SBRFNDFL-191 Обновил патч

--- database/distr/v1.0/templates/data/ndfl/primary_rnu_ndfl/v2016/script.groovy
+++ database/distr/v1.0/templates/data/ndfl/primary_rnu_ndfl/v2016/script.groovy
@@ -2084,6 +2084,7 @@ def getRefIncomeCode() {
     def refBookMap = getRefBook(REF_BOOK_INCOME_CODE_ID)
     refBookMap.each { refBook ->
         mapResult.put(refBook?.id?.numberValue, refBook?.CODE?.stringValue)
+//        logger.info("getRefIncomeCode ${refBook?.id?.numberValue} ${refBook?.CODE?.stringValue}")
     }
     return mapResult;
 }
@@ -2094,15 +2095,16 @@ def getRefIncomeCode() {
  */
 def getRefIncomeType() {
     // Map<REF_BOOK_INCOME_KIND.MARK, List<REF_BOOK_INCOME_KIND.INCOME_TYPE_ID>>
-    def mapResult = [:]
+    Map<String, List<Long>> mapResult = [:]
     def refBookList = getRefBook(REF_BOOK_INCOME_KIND_ID)
     refBookList.each { refBook ->
         String mark = refBook?.MARK?.stringValue
-        List<String> incomeTypeIdList = mapResult.get(mark)
+        List<Long> incomeTypeIdList = mapResult.get(mark)
         if (incomeTypeIdList == null) {
             incomeTypeIdList = []
         }
         incomeTypeIdList.add(refBook?.INCOME_TYPE_ID?.referenceValue)
+//        logger.info("getRefIncomeType $mark ${refBook?.INCOME_TYPE_ID?.referenceValue}")
         mapResult.put(mark, incomeTypeIdList)
     }
     return mapResult
@@ -2218,7 +2220,7 @@ Map<Long, Map<String, RefBookValue>> getActualRefDulByDeclarationDataId() {
  */
 def getRefBook(def long refBookId) {
     // Передаем как аргумент только срок действия версии справочника
-    def refBookList = getProvider(refBookId).getRecords(getReportPeriodEndDate() - 1, null, null, null)
+    def refBookList = getProvider(refBookId).getRecordsVersion(getReportPeriodStartDate(), getReportPeriodEndDate(), null, null)
 
     if (refBookList == null || refBookList.size() == 0) {
         throw new Exception("Ошибка при получении записей справочника " + refBookId)
@@ -2234,7 +2236,7 @@ def getRefBook(def long refBookId) {
  */
 List<Map<String, RefBookValue>> getRefBookByFilter(def long refBookId, def filter) {
     // Передаем как аргумент только срок действия версии справочника
-    List<Map<String, RefBookValue>> refBookList = getProvider(refBookId).getRecords(getReportPeriodEndDate() - 1, null, filter, null)
+    List<Map<String, RefBookValue>> refBookList = getProvider(refBookId).getRecordsVersion(getReportPeriodStartDate(), getReportPeriodEndDate(), null, filter)
     return refBookList
 }
 
