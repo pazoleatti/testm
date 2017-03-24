8c4e3adf8b88204bc21d2388112dc7f78d2fbf75
Marina Eremeeva <Marina.Eremeeva@aplana.com>
2017-03-24 11:51:27 +0200

    внешний ключ NDFL_PERSON_I_FK_S сделан обнуляемым при удалении родительской записи (on delete set null)

--- database/distr/v1.0/create_constraint.sql
+++ database/distr/v1.0/create_constraint.sql
@@ -413,7 +413,7 @@ alter table ndfl_person_prepayment add constraint ndfl_pp_pk primary key (id);
 alter table ndfl_person add constraint ndfl_person_fk_d foreign key (declaration_data_id) references declaration_data(id) on delete cascade;
 alter table ndfl_person add constraint ndfl_person_fk_person_id foreign key (person_id) references ref_book_person(id);
 alter table ndfl_person_income add constraint ndfl_person_i_fk_np foreign key (ndfl_person_id) references ndfl_person(id) on delete cascade;
-alter table ndfl_person_income add constraint ndfl_person_i_fk_s foreign key (source_id) references ndfl_person_income(id);
+alter table ndfl_person_income add constraint ndfl_person_i_fk_s foreign key (source_id) references ndfl_person_income(id) on delete set null;
 alter table ndfl_person_deduction add constraint ndfl_pd_fk_np foreign key (ndfl_person_id) references ndfl_person(id) on delete cascade;
 alter table ndfl_person_deduction add constraint ndfl_pd_fk_s foreign key (source_id) references ndfl_person_deduction(id);
 alter table ndfl_person_prepayment add constraint ndfl_pp_fk_np foreign key (ndfl_person_id) references ndfl_person(id) on delete cascade;
