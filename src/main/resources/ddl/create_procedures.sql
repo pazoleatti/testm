489136e8d70d1095835c31c2047761e6854b8f60
MEremeeva <MEremeeva@aplana.com>
2017-04-21 07:13:59 +0200

    https://jira.aplana.com/browse/SBRFNDFL-871 - НДФЛ. Расчет формы. Справочник по ФЛ. Не заполняется поле город при расчете новых ФЛ: дополнен пакет fias_pkg

--- src/main/resources/ddl/create_procedures.sql
+++ src/main/resources/ddl/create_procedures.sql
@@ -1571,7 +1571,7 @@ as
                                    left join mv_fias_city_act city on (city.aoid=nvl2(c_locality,locality.parentguid,street.parentguid) 
                                                                        and city.regioncode=c_region 
                                                                        and city.fname=c_city
-                                                                       and city.ftype=c_city_type
+                                                                       and city.ftype=nvl(c_city_type,'г')
                                                                       )
                                    left join mv_fias_area_act area on (area.aoid=nvl(nvl(locality.parentguid,city.parentguid),street.parentguid) 
                                                                        and area.regioncode=c_region 
@@ -1690,9 +1690,13 @@ as
   -- возвращается курсор на таблицу типа TTblCheckExistsAddrByFias
   function CheckExistsAddrByFias(p_declaration number,p_check_type number default 0) return ref_cursor;
   
+  -- Обновление мат. представлений
+  procedure RefreshViews;
+  
 end fias_pkg;
 /
 show errors;
+
 create or replace package body fias_pkg as
   
   cursor ndfl_rec(c_ndfl number) is
@@ -1737,7 +1741,7 @@ create or replace package body fias_pkg as
       from ndfl_person n
      where n.id=c_ndfl;
 
-    v_check_path boolean:=true;
+    v_check_path boolean:=false;
   --------------------------------------------------------------------------------------------------------------
   -- внутренние функции
   --------------------------------------------------------------------------------------------------------------
@@ -1896,10 +1900,11 @@ begin
            and a.has_child=decode(p_leaf,1,0,1);
       elsif (p_check_type='CITY') then
         select decode(count(*),0,0,1) into v_result
-          from mv_fias_city_act c join mv_fias_area_act a on (a.parentguid=c.aoid)
+          from mv_fias_city_act c left join mv_fias_area_act a on (a.parentguid=c.aoid)
          where c.regioncode=p_region
            and c.fname=nvl(replace(lower(p_check_element),' ',''),'-')
-           and a.fname=nvl(replace(lower(p_parent_element),' ',''),'-')
+           and (nvl(replace(lower(p_parent_element),' ',''),'-')='-' or 
+                a.fname=nvl(replace(lower(p_parent_element),' ',''),'-'))
            and c.has_child=decode(p_leaf,1,0,1);
       elsif (p_check_type='LOCALITY') then
         select decode(sum(cnt),0,0,1) into v_result
@@ -2186,6 +2191,18 @@ begin
                where n.declaration_data_id=p_declaration) tab;
 
     return v_ref;
+  end;
+
+  -------------------------------------------------------------------------------------------------------------
+  -- Обновление мат. представлений
+  -------------------------------------------------------------------------------------------------------------
+  procedure RefreshViews
+  is
+  begin
+    dbms_mview.refresh('MV_FIAS_AREA_ACT', 'C');
+    dbms_mview.refresh('MV_FIAS_CITY_ACT', 'C');
+    dbms_mview.refresh('MV_FIAS_LOCALITY_ACT', 'C');
+    dbms_mview.refresh('MV_FIAS_STREET_ACT', 'C');
   end;
 
 end fias_pkg;
