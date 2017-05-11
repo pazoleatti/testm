0e89bd7449faf58985082efdd9d12d03cd64beab
Lenar Haziev <lhaziev@aplana.com>
2017-05-11 16:28:38 +0500

    https://jira.aplana.com/browse/SBRFNDFL-924 НДФЛ. Разбор ответа от ФНС. В реестре справок не отображаются не все ошибки от ФНС, загруженные через протокол

--- src/main/resources/refbook/declaration_type/script.groovy
+++ src/main/resources/refbook/declaration_type/script.groovy
@@ -1058,10 +1058,16 @@ def importNdflResponse() {
                         } else {
                             def ndflRef = ndflRefProvider.getRecordData(ndflRefIds.get(0))
 
-                            if (entry.valid) {
-                                ndflRef.ERRTEXT.value = "Текст ошибки от ФНС: \"${entry.addressBefore}\"; (Адрес признан верным (ИФНСМЖ - ${entry.valid}))".toString()
+                            String errtext;
+                            if (ndflRef.ERRTEXT.value == null || ndflRef.ERRTEXT.value.isEmpty()) {
+                                errtext = ""
                             } else {
-                                ndflRef.ERRTEXT.value = "Текст ошибки от ФНС: \"${entry.addressBefore}\" ДО исправления; (\"${entry.addressAfter}\" ПОСЛЕ исправления)".toString()
+                                errtext = ndflRef.ERRTEXT.value + "; "
+                            }
+                            if (entry.valid) {
+                                ndflRef.ERRTEXT.value = errtext + "Текст ошибки от ФНС: \"${entry.addressBefore}\"; (Адрес признан верным (ИФНСМЖ - ${entry.valid}))".toString()
+                            } else {
+                                ndflRef.ERRTEXT.value = errtext + "Текст ошибки от ФНС: \"${entry.addressBefore}\" ДО исправления; (\"${entry.addressAfter}\" ПОСЛЕ исправления)".toString()
                             }
                             ndflRefProvider.updateRecordVersion(logger, ndflRefIds.get(0), null, null, ndflRef)
                         }
