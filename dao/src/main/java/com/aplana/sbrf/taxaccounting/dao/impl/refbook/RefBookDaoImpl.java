5d87a6aea8101fcc113743fcfb076d3b99712994
ytrofimov <Yuriy.Trofimov@aplana.com>
2017-05-12 12:41:52 +0300

    https://jira.aplana.com/browse/SBRFNDFL-862 НДФЛ. Справочник "Виды дохода". Ошибка при нажатии кнопку "Найти". Поправлена некорректная проверка справочника.

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/RefBookDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/RefBookDaoImpl.java
@@ -74,6 +74,8 @@ public class RefBookDaoImpl extends AbstractDao implements RefBookDao {
     private static final String FORM_LINK_MSG = "Существует экземпляр %sформы%s, который содержит ссылку на запись! Тип: \"%s\", Вид: \"%s\", Подразделение: \"%s\", Период: \"%s\"%s%s%s%s.";
     private static final String REF_BOOK_LINK_MSG = "Существует ссылка на запись справочника. Справочник \"%s\", запись: \"%s\"%s.";
 
+    private static final Long REF_BOOK_CALENDAR_ID = 945L;
+
 	@Autowired
     private ApplicationContext applicationContext;
 
@@ -2501,7 +2503,7 @@ public class RefBookDaoImpl extends AbstractDao implements RefBookDao {
         String orderBy = "";
         PreparedStatementData ps = new PreparedStatementData();
         ps.appendQuery("SELECT row_number_over");
-        if (refBook.hasAttribute("ID")) {
+        if (!refBook.getId().equals(REF_BOOK_CALENDAR_ID)) {
             ps.appendQuery(", ");
             ps.appendQuery("id ");
             ps.appendQuery(RefBook.RECORD_ID_ALIAS);
@@ -2801,7 +2803,7 @@ public class RefBookDaoImpl extends AbstractDao implements RefBookDao {
     public List<Map<String, RefBookValue>> getRecordsData(PreparedStatementData ps, RefBook refBook) {
         if (!ps.getParams().isEmpty()) {
             RowMapper<Map<String, RefBookValue>> rowMapper;
-            if (!refBook.hasAttribute("ID")) {
+            if (refBook.getId().equals(REF_BOOK_CALENDAR_ID)) {
                 rowMapper = new RefBookCalendarValueMapper(refBook);
                 return getJdbcTemplate().query(ps.getQuery().toString(), ps.getParams().toArray(), rowMapper);
             }
