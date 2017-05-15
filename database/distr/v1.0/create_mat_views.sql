1e3e699a1f0f3c0fd19590fecd9de87b7389350c
MEremeeva <MEremeeva@aplana.com>
2017-05-15 17:13:10 +0200

    оптимизация пакета FIAS_PKG

--- database/distr/v1.0/create_mat_views.sql
+++ database/distr/v1.0/create_mat_views.sql
@@ -4,6 +4,7 @@ drop materialized view mv_fias_city_act;
 drop materialized view mv_fias_locality_act;
 drop materialized view mv_fias_street_act;
 */
+
 create materialized view mv_fias_area_act
 refresh complete on demand
 as
