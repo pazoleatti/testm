dd85e6c88c46ec95e9fc8a29a62aa0370bdd6f6f
MEremeeva <MEremeeva@aplana.com>
2017-06-08 15:16:58 +0200

    https://jira.aplana.com/browse/SBRFNDFL-1129 - НДФЛ. Адаптация идентфикации ФЛ под НДФЛ: значение параметра LIMIT_IDENT(порог схожести ФЛ) установлен = 0.65

--- database/distr/v1.0/dml/configuration.sql
+++ database/distr/v1.0/dml/configuration.sql
@@ -1,4 +1,4 @@
-insert into configuration(code,department_id,value) values ('LIMIT_IDENT',0,'0.7');
+insert into configuration(code,department_id,value) values ('LIMIT_IDENT',0,'0.65');
 insert into configuration(code,department_id,value) values ('NO_CODE',0,'9979');
 insert into configuration(code,department_id,value) values ('SBERBANK_INN',0,'7707083893');
 insert into configuration(code,department_id,value) values ('SHOW_TIMING',0,'0');
