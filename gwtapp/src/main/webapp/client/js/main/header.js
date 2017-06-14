95c5bd5b7e2f8f91a6d0a5a825d2349ccfc189b0
Anastasiya Kuleshova <Anastasiya.Kuleshova@aplana.com>
2017-06-14 16:09:42 +0500

    https://jira.aplana.com/browse/SBRFNDFL-1206. Доработала названия в главном меню

--- gwtapp/src/main/webapp/client/js/main/header.js
+++ gwtapp/src/main/webapp/client/js/main/header.js
@@ -17,8 +17,8 @@
             };
         })
         .controller('MainMenuController', [
-            '$scope', '$translate', '$http', 'USER_DATA', '$rootScope', 'deviceDetector',
-            function ($scope, $translate, $http, USER_DATA, $rootScope, deviceDetector) {
+            '$scope', '$translate', '$http', 'USER_DATA', '$rootScope', 'deviceDetector', '$filter',
+            function ($scope, $translate, $http, USER_DATA, $rootScope, deviceDetector, $filter) {
                 /**
                  * @description Обновляет информацию о текущем пользователе
                  */
@@ -40,79 +40,79 @@
 
                         //Задаем ссылки для главного меню
                         $scope.treeTaxes = [{
-                            name: "НДФЛ",
+                            name: $filter('translate')('menu.taxes.ndfl'),
                             subtree: [{
-                                name: "Формы",
+                                name: $filter('translate')('menu.taxes.ndfl.forms'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!declarationList;nType=NDFL"
                             }, {
-                                name: "Ведение периодов",
+                                name: $filter('translate')('menu.taxes.ndfl.maintenanceOfPeriods'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!periods;nType=NDFL"
                             }, {
-                                name: "Настройки подразделений",
+                                name: $filter('translate')('menu.taxes.ndfl.settingsUnits'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!departmentConfigProperty;nType=NDFL"
                             }, {
-                                name: "Назначение форм",
+                                name: $filter('translate')('menu.taxes.ndfl.formAssignment'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!destination;nType=NDFL;isForm=false"
                             }, {
-                                name: "Отчетность",
+                                name: $filter('translate')('menu.taxes.ndfl.accountability'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!declarationList;nType=NDFL;isReports=true"
                             }]
                         }, {
-                            name: "Сервис",
+                            name: $filter('translate')('menu.taxes.service'),
                             subtree: [{
-                                name: "Загрузить файлы",
+                                name: $filter('translate')('menu.taxes.service.loadFiles'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!uploadTransportData"
                             }]
                         }, {
-                            name: "Общие параметры",
+                            name: $filter('translate')('menu.taxes.commonParameters'),
                             href: "Main.jsp" + $scope.gwtMode + "#!commonParameter"
                         }];
 
                         $scope.treeNsi = [{
-                            name: "Справочники",
+                            name: $filter('translate')('menu.nsi.refbooks'),
                             href: "Main.jsp" + $scope.gwtMode + "#!refbooklist"
                         }];
 
                         $scope.treeAdministration = [{
-                            name: "Список блокировок",
+                            name: $filter('translate')('menu.administration.blockList'),
                             href: "Main.jsp" + $scope.gwtMode + "#!lockList"
                         }, {
-                            name: "Журнал аудита",
+                            name: $filter('translate')('menu.administration.auditLog'),
                             href: "Main.jsp" + $scope.gwtMode + "#!audit"
                         }, {
-                            name: "Список пользователей",
+                            name: $filter('translate')('menu.administration.usersList'),
                             href: "Main.jsp" + $scope.gwtMode + "#!members"
                         }, {
-                            name: "Конфигурационные параметры",
+                            name: $filter('translate')('menu.administration.configParams'),
                             href: "Main.jsp" + $scope.gwtMode + "#!configuration"
                         }, {
-                            name: "Планировщик задач",
+                            name: $filter('translate')('menu.administration.taskManager'),
                             href: "Main.jsp" + $scope.gwtMode + "#!taskList"
                         }, {
-                            name: "Настройки",
+                            name: $filter('translate')('menu.administration.settings'),
                             subtree: [{
-                                name: "Макеты налоговых форм",
+                                name: $filter('translate')('menu.administration.settings.mockOfTaxForms'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!declarationTemplateList"
                             }, {
-                                name: "Справочники",
+                                name: $filter('translate')('menu.administration.settings.refbooks'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!refbooklistadmin"
                             }, {
-                                name: "Сбросить кэш",
+                                name: $filter('translate')('menu.administration.settings.resetCache'),
                                 href: "controller/actions/cache/clear-cache"
                             }, {
-                                name: "Экспорт макетов",
+                                name: $filter('translate')('menu.administration.settings.exportLayouts'),
                                 href: "controller/actions/formTemplate/downloadAll"
                             }, {
-                                name: "Импорт скриптов",
+                                name: $filter('translate')('menu.administration.settings.importingScripts'),
                                 href: "Main.jsp" + $scope.gwtMode + "#!scriptsImport"
                             }]
                         }];
 
                         $scope.treeManual = [{
-                            name: "Руководство пользователя",
+                            name: $filter('translate')('menu.manuals.manualUser'),
                             href: "resources/help_ndfl.pdf"
                         }, {
-                            name: "Руководство настройщика макета",
+                            name: $filter('translate')('menu.manuals.manualLayoutDesigner'),
                             href: "resources/help_conf.pdf"
                         }];
                     }
