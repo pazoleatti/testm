f9e5161350c1d841a739e3bd964191b247127f68
MEremeeva <MEremeeva@aplana.com>
2017-06-22 16:12:07 +0200

    отредактированы скрипты для справочников declaration_type.sql и ref_book_form_type.sql

--- src/main/resources/dml/declaration_type.sql
+++ src/main/resources/dml/declaration_type.sql
@@ -1,6 +1,6 @@
-insert into declaration_type (id,tax_type,name,status,is_ifrs,ifrs_name) values (104,'N','2-НДФЛ (2)',0,0,null);
-insert into declaration_type (id,tax_type,name,status,is_ifrs,ifrs_name) values (100,'N','РНУ_НДФЛ (первичная)',0,0,null);
-insert into declaration_type (id,tax_type,name,status,is_ifrs,ifrs_name) values (101,'N','РНУ_НДФЛ (консолидированная)',0,0,null);
-insert into declaration_type (id,tax_type,name,status,is_ifrs,ifrs_name) values (102,'N','2-НДФЛ (1)',0,0,null);
-insert into declaration_type (id,tax_type,name,status,is_ifrs,ifrs_name) values (103,'N','6-НДФЛ',0,0,null);
-insert into declaration_type (id,tax_type,name,status,is_ifrs,ifrs_name) values (200,'F','1151111 (первичная)',0,0,null);
+insert into declaration_type (id,name,status,is_ifrs,ifrs_name) values (104,'2-НДФЛ (2)',0,0,null);
+insert into declaration_type (id,name,status,is_ifrs,ifrs_name) values (100,'РНУ_НДФЛ (первичная)',0,0,null);
+insert into declaration_type (id,name,status,is_ifrs,ifrs_name) values (101,'РНУ_НДФЛ (консолидированная)',0,0,null);
+insert into declaration_type (id,name,status,is_ifrs,ifrs_name) values (102,'2-НДФЛ (1)',0,0,null);
+insert into declaration_type (id,name,status,is_ifrs,ifrs_name) values (103,'6-НДФЛ',0,0,null);
+insert into declaration_type (id,name,status,is_ifrs,ifrs_name) values (200,'1151111 (первичная)',0,0,null);
