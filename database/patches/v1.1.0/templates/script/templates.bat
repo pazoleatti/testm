40a3b8b547d9fc9c3263b4a3051922308de0c739
Lenar Haziev <lhaziev@aplana.com>
2017-07-26 17:27:31 +0500

    https://jira.aplana.com/browse/SBRFNDFL-191 Обновил патч: актуализировал патч 1.1, удалил фонды

--- database/patches/v1.1.0/templates/script/templates.bat
+++ database/patches/v1.1.0/templates/script/templates.bat
@@ -2,9 +2,11 @@
 IF "%MODE%"=="" (
 REM url for connect to oracle database
 REM for example: user_name/password@host:port/service_name
-SET AUTH=ndfl_psi/ndfl_psi@172.19.214.46:1521/orcl.aplana.local
+REM SET AUTH=ndfl_psi/ndfl_psi@172.19.214.46:1521/orcl.aplana.local
+SET AUTH=ndfl_prod/P2pzLA35@172.19.214.46:1521/orcl.aplana.local
 REM path to oracle bin folder: ORACLE_HOME\BIN
-SET ORA_BIN=C:\app\oracle\product\11.2.0\dbhome_1\BIN
+REM SET ORA_BIN=C:\app\oracle\product\11.2.0\dbhome_1\BIN
+SET ORA_BIN=D:\dev\instantclient_12_2
 SET LOG_DIR=_logs
 SET BAD_DIR=_bad
 SET nls_lang=AMERICAN_AMERICA.AL32UTF8
@@ -16,12 +18,6 @@ ECHO #################################
 ECHO ## template: refbook
 ECHO #################################
 CD refbook
-CALL update.bat
-CD ..
-ECHO #################################
-ECHO ## template: fond
-ECHO #################################
-CD fond
 CALL update.bat
 CD ..
 ECHO #################################
