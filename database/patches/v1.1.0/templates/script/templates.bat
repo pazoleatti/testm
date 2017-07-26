941dd23a1e7e34ec1836df9e24f78e1b6f997736
Lenar Haziev <lhaziev@aplana.com>
2017-07-26 14:37:44 +0500

    https://jira.aplana.com/browse/SBRFNDFL-191 Обновил патч: актуализировал патч 1.1

--- database/patches/v1.1.0/templates/script/templates.bat
+++ database/patches/v1.1.0/templates/script/templates.bat
@@ -2,9 +2,9 @@
 IF "%MODE%"=="" (
 REM url for connect to oracle database
 REM for example: user_name/password@host:port/service_name
-REM SET AUTH=ndfl_psi/ndfl_psi@172.19.214.46:1521/orcl.aplana.local
+SET AUTH=ndfl_psi/ndfl_psi@172.19.214.46:1521/orcl.aplana.local
 REM path to oracle bin folder: ORACLE_HOME\BIN
-REM SET ORA_BIN=C:\app\oracle\product\11.2.0\dbhome_1\BIN
+SET ORA_BIN=C:\app\oracle\product\11.2.0\dbhome_1\BIN
 SET LOG_DIR=_logs
 SET BAD_DIR=_bad
 SET nls_lang=AMERICAN_AMERICA.AL32UTF8
