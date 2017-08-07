2a07439dcb33c9a1e0a077618804259c5324fcab
Ruslan Alizade <Ruslan.AliZade@aplana.com>
2017-08-07 16:23:17 +0300

    https://jira.aplana.com/browse/SBRFNDFL-1660 Next. Ошибка при проверке отчетных форм. Работа с патчем версии 1.1.1. Поиск и очистка данных не позволяющих активировать внешние ключи.

--- database/patches/v1.1.1/templates/script/ndfl/clean.sql
+++ database/patches/v1.1.1/templates/script/ndfl/clean.sql
@@ -39,5 +39,10 @@ DELETE FROM declaration_template_file WHERE declaration_template_id             
 DELETE FROM declaration_subreport WHERE declaration_template_id                  		IN (100, 101, 102, 103, 104);
 DELETE FROM declaration_template  WHERE id                                       		IN (100, 101, 102, 103, 104);
 
+DELETE FROM DECLARATION_REPORT WHERE SUBREPORT_ID in (1022, 1031, 1042); 
+
+Update notification 
+Set report_id = NULL where report_id IN (Select report_id from notification where report_id NOT IN (select id from blob_data));
+
 COMMIT;
 EXIT;
