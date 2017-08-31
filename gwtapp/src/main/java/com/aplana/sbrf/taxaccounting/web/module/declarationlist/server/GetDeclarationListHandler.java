fca351bfc2b3888ea8c1f1e00d157b9e1af039a8
Lenar Haziev <lhaziev@aplana.com>
2017-08-31 13:32:16 +0500

    Исправил конфликты после мержа с 1.2

--- gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/server/GetDeclarationListHandler.java
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/declarationlist/server/GetDeclarationListHandler.java
@@ -17,10 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 @PreAuthorize("hasAnyRole('N_ROLE_OPER', 'N_ROLE_CONTROL_UNP', 'N_ROLE_CONTROL_NS', 'F_ROLE_OPER', 'F_ROLE_CONTROL_UNP', 'F_ROLE_CONTROL_NS')")
