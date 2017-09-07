aaa7ec01bd78a730274a31375ef5242e2005134b
RNurgaleev <Rinat.Nurgaleev@aplana.com>
2017-09-07 17:50:45 +0500

    В grants.sql не было grant execute on &2..add_log_system_ndfl TO &1;

--- database/database-1.4/grants.sql
+++ database/database-1.4/grants.sql
@@ -7,6 +7,7 @@ grant select on &2..sec_user to &1;
 grant references on &2..sec_user to &1;
 grant select on &2..sec_user_role to &1;
 grant references on &2..sec_user_role to &1;
+grant execute on &2..add_log_system_ndfl TO &1;
 
 commit;
 exit;
