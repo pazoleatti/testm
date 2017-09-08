33d9e112b840c299faef6d7a1553204f1557d68e
Ishevchuk <Igor.Shevchuk@aplana.com>
2017-09-08 15:54:08 +0300

    Изменена версия приложения на 1.2

--- database/database-1.2/templates/script/templates.bat
+++ database/database-1.2/templates/script/templates.bat
@@ -6,7 +6,7 @@ SET AUTH=ndfl_psi/ndfl_psi@172.19.214.45:1521/orcl.aplana.local
 rem SET AUTH=ndfl_prod/P2pzLA35@172.19.214.45:1521/orcl.aplana.local
 REM path to oracle bin folder: ORACLE_HOME\BIN
 REM SET ORA_BIN=C:\app\oracle\product\11.2.0\dbhome_1\BIN
-SET ORA_BIN=D:\dev\instantclient_12_2
+SET ORA_BIN=c:\tools\oracle\product\11.2.0\client_1\BIN\
 SET LOG_DIR=_logs
 SET BAD_DIR=_bad
 SET nls_lang=AMERICAN_AMERICA.AL32UTF8
