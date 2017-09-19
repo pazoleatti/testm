a1053ea5537498a2b1668cf9d2a0192bdccf0a80
RNurgaleev <Rinat.Nurgaleev@aplana.com>
2017-09-19 12:55:10 +0500

    https://jira.aplana.com/browse/SBRFNDFL-2056 - Изменения БД связанные с запуском скриптов для различных событий

--- database/database-2.1.0/create_sequences.sql
+++ database/database-2.1.0/create_sequences.sql
@@ -1,9 +1,14 @@
 declare 
   v_count number;
 begin
-	select count(1) into v_count from user_sequences where sequence_name='SEQ_ASYNC_TASK';
+	select count(1) into v_count from user_sequences where lower(sequence_name)='seq_async_task';
 	IF v_count=0 THEN
 		EXECUTE IMMEDIATE 'create sequence seq_async_task start with 100000 increment by 100';
+	END IF; 
+	
+	select count(1) into v_count from user_sequences where lower(sequence_name)='seq_decl_template_event_script';
+	IF v_count=0 THEN
+		EXECUTE IMMEDIATE 'create sequence seq_decl_template_event_script start with 1 increment by 1';
 	END IF; 
 end;
 /
