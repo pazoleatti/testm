b8b991cc7b6d433c1d4e6bb913b0a44ae749735a
Alexander Okunev <AOkunev@aplana.com>
2017-09-26 12:35:11 +0500

    https://jira.aplana.com/browse/SBRFNDFL-2094 Исправлены константы для состояния формы

--- gwtapp/src/main/webapp/client/app/common/constants.js
+++ gwtapp/src/main/webapp/client/app/common/constants.js
@@ -58,7 +58,7 @@
                 RETURN_TO_CREATED: 1 << 6,
                 EDIT_ASSIGNMENT: 1 << 7
             },
-            NDFL_STATE: {
+            STATE: {
                 CREATED: {id: 1, name: "Создана"},
                 PREPARED: {id: 2, name: "Подготовлена"},
                 ACCEPTED: {id: 3, name: "Принята"},
