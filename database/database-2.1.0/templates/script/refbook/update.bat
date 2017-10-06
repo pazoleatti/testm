92773aeb80199dccd1c46ce197aabf65bed09283
RNurgaleev <Rinat.Nurgaleev@aplana.com>
2017-10-06 12:12:48 +0500

    https://jira.aplana.com/browse/SBRFNDFL-2197 Переделать обновление макетов в патче БД

--- database/database-2.1.0/templates/script/refbook/update.bat
+++ database/database-2.1.0/templates/script/refbook/update.bat
@@ -9,5 +9,3 @@ CD %SOURCE_DIR%
 ECHO -- blob
 "%ORA_BIN%\sqlldr" userid=%AUTH% control="%BASE_DIR%/blob_data.ldr" log="%LOG_DIR_REL%/%PREFIX%_2_blob_data.txt" bad="%BAD_DIR_REL%/%PREFIX%_blob_data.txt"
 CD %BASE_DIR%
-ECHO -- script
-"%ORA_BIN%\sqlplus" %AUTH% @"ref_book_script.sql" > "%LOG_DIR_REL%/%PREFIX%_3_ref_book_script.txt"
