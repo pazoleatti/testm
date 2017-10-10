13e5e390d251bcbff9d424890988d5703119f897
ytrofimov <Yuriy.Trofimov@aplana.com>
2017-10-10 14:17:37 +0300

    https://jira.aplana.com/browse/SBRFNDFL-2247 Рефакторинг: константы для кэшей

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/cache/CacheConstants.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/cache/CacheConstants.java
@@ -1,9 +1,18 @@
 package com.aplana.sbrf.taxaccounting.dao.impl.cache;
 
+/**
+ * Класс содержит перечень констант использующихся при кэшировании dao-запросов
+ */
 public final class CacheConstants {
 
-    public static final String DECLARATION_TEMPLATE = "DeclarationTemplate";
-    public static final String DECLARATION_TEMPLATE_EVENT_SCRIPT = "DeclarationTemplateEventScript";
+	/** Макет налоговой формы */
+	public static final String DECLARATION_TEMPLATE = "DeclarationTemplate";
+	/** Скрипт для конкретного события */
+	public static final String DECLARATION_TEMPLATE_EVENT_SCRIPT = "DeclarationTemplateEventScript";
+	/** Вид налоговой формы */
+	public static final String DECLARATION_TYPE = "DeclarationType";
+	/** Справочники */
+	public static final String PERMANENT_DATA = "PermanentData";
 
     private CacheConstants() {
     }
