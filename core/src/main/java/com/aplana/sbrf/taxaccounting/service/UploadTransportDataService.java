0d273882e1dc5c56527f7d303035b83a68619a00
Vyacheslav Lednev <vyacheslav.lednev@aplana.com>
2017-10-10 13:38:25 +0300

    https://jira.aplana.com/browse/SBRFNDFL-2114 Вынос бизнес логики из TransportDataController

--- core/src/main/java/com/aplana/sbrf/taxaccounting/service/UploadTransportDataService.java
+++ core/src/main/java/com/aplana/sbrf/taxaccounting/service/UploadTransportDataService.java
@@ -3,6 +3,7 @@ package com.aplana.sbrf.taxaccounting.service;
 import com.aplana.sbrf.taxaccounting.model.TAUserInfo;
 import com.aplana.sbrf.taxaccounting.model.UploadResult;
 import com.aplana.sbrf.taxaccounting.model.log.Logger;
+import com.aplana.sbrf.taxaccounting.model.result.ActionResult;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,11 +17,33 @@ public interface UploadTransportDataService {
     /**
      * Загрузка ТФ в каталог загрузки. Загружаются ТФ НФ и ТФС.
      *
-     * @param userInfo Пользователь
-     * @param fileName Имя ТФ
+     * @param userInfo    Пользователь
+     * @param fileName    Имя ТФ
      * @param inputStream Поток ТФ
-     * @param logger Логгер для области уведомлений
+     * @param logger      Логгер для области уведомлений
      * @return true — был загружен хотя бы один файл
      */
     UploadResult uploadFile(TAUserInfo userInfo, String fileName, InputStream inputStream, Logger logger) throws IOException;
+
+    /**
+     * Загрузка выбранного пользователем ТФ
+     *
+     * @param userInfo    Пользователь
+     * @param fileName    Имя ТФ
+     * @param inputStream Поток ТФ
+     * @param logger      Логгер для области уведомлений
+     * @return ActionResult результат выполнения операции с UUID группы сообщений в журнале
+     */
+    ActionResult upload(TAUserInfo userInfo, String fileName, InputStream inputStream, Logger logger);
+
+    /**
+     * Загрузка всех файлов из каталога загрузки
+     *
+     * @param userInfo Пользователь
+     * @param logger   Логгер для области уведомлений
+     * @return ActionResult результат выполнения операции с UUID группы сообщений в журнале
+     */
+    ActionResult uploadAll(TAUserInfo userInfo, Logger logger);
+
+
 }
