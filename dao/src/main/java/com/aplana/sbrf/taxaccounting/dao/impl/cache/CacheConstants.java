17844f74e51d7cd0ae438943b92536b1c52fe061
Ishevchuk <Igor.Shevchuk@aplana.com>
2017-10-10 07:13:20 +0300

    https://jira.aplana.com/browse/SBRFNDFL-2188 Убрать из pom-файлов проекта лишние зависимости и навести там порядок

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/cache/CacheConstants.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/cache/CacheConstants.java
@@ -2,11 +2,9 @@ package com.aplana.sbrf.taxaccounting.dao.impl.cache;
 
 public final class CacheConstants {
 
-	public static final String DEPARTMENT = "Department";
-	public static final String DECLARATION_TEMPLATE = "DeclarationTemplate";
-	public static final String DECLARATION_TEMPLATE_EVENT_SCRIPT = "DeclarationTemplateEventScript";
+    public static final String DECLARATION_TEMPLATE = "DeclarationTemplate";
+    public static final String DECLARATION_TEMPLATE_EVENT_SCRIPT = "DeclarationTemplateEventScript";
 
-	private CacheConstants(){
-	}
-
+    private CacheConstants() {
+    }
 }
