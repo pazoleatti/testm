be654ea285f87d8fa3e9c4fe8c2e912ea66b99ca
RNurgaleev <Rinat.Nurgaleev@aplana.com>
2017-10-25 17:46:44 +0500

    https://jira.aplana.com/browse/SBRFNDFL-2407 Подготовить скрипты для миграции данных пользователей из схемы 1.1 в 1.2.1 Переделал скрипты и выкинул из них все ненужное

--- newProject/db/other_scripts/grants_next.sql
+++
@@ -1,6 +0,0 @@
-﻿
-
-grant select,insert,update,delete on TAX.department to NDFL_NEXT;
-grant select,insert,update,delete on TAX.sec_role to NDFL_NEXT;
-grant select,insert,update,delete on TAX.sec_user to NDFL_NEXT;
-grant select,insert,update,delete on TAX.sec_user_role to NDFL_NEXT;
