3ff9662ad335c17ec0f199251ab5873172f84025
ytrofimov <Yuriy.Trofimov@aplana.com>
2017-11-02 09:35:27 +0300

    https://jira.aplana.com/browse/SBRFNDFL-2486 Доработать утилиту модификации данных физлиц в ТФ

--- generatorTF/src/main/java/com.aplana.generatorTF/ReaderAndGeneratorXMLFiles.java
+++ generatorTF/src/main/java/com.aplana.generatorTF/ReaderAndGeneratorXMLFiles.java
@@ -15,34 +15,47 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
-import java.util.Date;
-import java.util.Random;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
-/**
- * Класс для генерации ТФ РНУ
- */
 public class ReaderAndGeneratorXMLFiles {
 
+    private static List<String> lastnameDictionary;
+
+    private static List<String> firstnameDictionary;
+
+    private static List<String> middlenameDictionary;
+
+    private static List<String> regionsDictionary;
+
+    /**
+     * Максимальная возможная дата для генераци даты рождения
+     */
+    private static final long dateLimit = 946684799000L;
 
     public static void main(String[] args) {
         try {
-            if (args.length < 2){
+            if (args.length != 2) {
                 System.out.print("Вы ввели неверное количество аргументов");
             } else {
+                initDictionaries();
                 String path = args[0];
                 Integer numberOfFiles = Integer.parseInt(args[1]);
 
                 File sourceFile = new File(path);
                 File destFile;
 
-                for (int i = 0; i < numberOfFiles; i++){
-                    String tmpPath = path.replaceAll(path.substring(path.length() - 36, path.length()-4),
-                            Integer.toString(i+1).length() == 1 ?
-                                    RandomStringUtils.randomAlphanumeric(30) + "-" + Integer.toString(i+1)
-                                    : RandomStringUtils.randomAlphanumeric(29) + "-" + Integer.toString(i+1));
+                for (int i = 0; i < numberOfFiles; i++) {
+                    String tmpPath = path.replaceAll(path.substring(path.length() - 36, path.length() - 4),
+                            Integer.toString(i + 1).length() == 1 ?
+                                    RandomStringUtils.randomAlphanumeric(30) + "-" + Integer.toString(i + 1)
+                                    : RandomStringUtils.randomAlphanumeric(29) + "-" + Integer.toString(i + 1));
                     destFile = new File(tmpPath);
                     FileUtils.copyFile(sourceFile, destFile);
-                    changeXmlFile(destFile, tmpPath.substring(tmpPath.lastIndexOf("/") + 1, tmpPath.length() - 4), args.length == 2 ? 0 : Integer.parseInt(args[2]));
+                    changeXmlFile(destFile, tmpPath.substring(tmpPath.lastIndexOf("/") + 1, tmpPath.length() - 4));
                 }
             }
 
@@ -51,40 +64,25 @@ public class ReaderAndGeneratorXMLFiles {
         }
     }
 
-    private static void changeXmlFile (File fXmlFile, String fileName, int countTF) {
+    private static void changeXmlFile(File fXmlFile, String fileName) {
         try {
-            System.out.println("Время начала: " + new Date());
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(fXmlFile);
 
             doc.getDocumentElement().normalize();
 
-            if (fileName.length() > 59){
+            if (fileName.length() > 59) {
                 fileName = fileName.substring(fileName.lastIndexOf("\\") + 1);
             }
 
             Node slPeace = doc.getElementsByTagName("СлЧасть").item(0);
             if (slPeace.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) slPeace;
-                    element.setAttribute("ИдФайл", fileName);
+                Element element = (Element) slPeace;
+                element.setAttribute("ИдФайл", fileName);
             }
 
-
-            if (countTF != 0 && countTF > doc.getElementsByTagName("ИнфЧасть").getLength()){
-                Node infoPiece = doc.getElementsByTagName("ИнфЧасть").item(0);
-                int countNewNode = countTF - doc.getElementsByTagName("ИнфЧасть").getLength();
-                System.out.println("Количество новых объектов = " + countNewNode);
-                Node nodeForInsert;
-                for (int i = 0; i < countNewNode; i++){
-                    nodeForInsert = infoPiece.cloneNode(true);
-                    infoPiece.getParentNode().insertBefore(nodeForInsert, infoPiece);
-                }
-            }
-
-            System.out.println("Началось изменение атрибутов " + new Date());
             NodeList nList = doc.getElementsByTagName("ПолучДох");
-            System.out.println("Количество ПолучДох = " + nList.getLength());
 
             Random r = new Random(System.currentTimeMillis());
             String alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
@@ -104,14 +102,20 @@ public class ReaderAndGeneratorXMLFiles {
                     eElement.setAttribute("СНИЛС", generateSnils(r));
 
                     //генерация ФИО
-                    eElement.setAttribute("ФамФЛ", String.valueOf(alph.charAt(r.nextInt(33))));
-                    eElement.setAttribute("ИмяФЛ", String.valueOf(alph.charAt(r.nextInt(33))));
-                    eElement.setAttribute("ОтчФЛ", String.valueOf(alph.charAt(r.nextInt(33))));
+                    eElement.setAttribute("ФамФЛ", lastnameDictionary.get(r.nextInt(lastnameDictionary.size())));
+                    eElement.setAttribute("ИмяФЛ", firstnameDictionary.get(r.nextInt(firstnameDictionary.size())));
+                    eElement.setAttribute("ОтчФЛ", middlenameDictionary.get(r.nextInt(middlenameDictionary.size())));
 
                     //генерация номера уд.лич.
-                    if (eElement.getAttribute("УдЛичнФЛКод").equals("21")){
+                    if (eElement.getAttribute("УдЛичнФЛКод").equals("21")) {
                         eElement.setAttribute("УдЛичнФЛНом", generateNumberDul(r));
                     }
+
+                    // генерация даты рождения
+                    eElement.setAttribute("ДатаРожд", generateDate(r));
+
+                    // генерация ИНН
+                    eElement.setAttribute("ИННФЛ", generateInn(r));
                 }
             }
             Transformer transformer = TransformerFactory.newInstance().newTransformer();
@@ -119,13 +123,12 @@ public class ReaderAndGeneratorXMLFiles {
             DOMSource source = new DOMSource(doc);
             StreamResult result = new StreamResult(fXmlFile);
             transformer.transform(source, result);
-            System.out.println("Время окончания: " + new Date());
         } catch (Exception e) {
-             e.printStackTrace();
+            e.printStackTrace();
         }
     }
 
-    private static String generateSnils(Random r){
+    private static String generateSnils(Random r) {
         String snils = "";
         int controlSum = 0;
         int tmp = 0;
@@ -139,11 +142,11 @@ public class ReaderAndGeneratorXMLFiles {
         }
 
         tmp = 100 + r.nextInt(899);
-        controlSum += 3 * tmp/100 + 2 * (tmp/10 % 10) + tmp % 100;
+        controlSum += 3 * tmp / 100 + 2 * (tmp / 10 % 10) + tmp % 100;
         snils += tmp + "-";
 
         String strControlSum = String.valueOf(controlSum % 101);
-        if (strControlSum.length() == 1){
+        if (strControlSum.length() == 1) {
             snils += "0" + strControlSum;
         } else {
             snils += strControlSum.charAt(strControlSum.length() - 2);
@@ -153,11 +156,123 @@ public class ReaderAndGeneratorXMLFiles {
         return snils;
     }
 
-    private static String generateNumberDul (Random r){
+    private static String generateNumberDul(Random r) {
         String numberDul = "";
 
         numberDul += (10 + r.nextInt(89)) + " " + (10 + r.nextInt(89)) + " " + (100000 + r.nextInt(899999));
 
         return numberDul;
+    }
+
+    /**
+     * @param filePath
+     * @return
+     */
+    private static List<String> initDictionary(String filePath) {
+        List<String> toReturn = new ArrayList<>();
+        try (InputStream is = ReaderAndGeneratorXMLFiles.class.getResourceAsStream(filePath)) {
+            Scanner scanner = new Scanner(is);
+            while (scanner.hasNextLine()) {
+                toReturn.add(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return toReturn;
+    }
+
+    /**
+     * Инициализирует словари
+     */
+    private static void initDictionaries() {
+        firstnameDictionary = initDictionary("/firstnames.txt");
+        lastnameDictionary = initDictionary("/lastnames.txt");
+        middlenameDictionary = initDictionary("/middlenames.txt");
+        regionsDictionary = initDictionary("/regions.txt");
+    }
+
+    /**
+     * Генерирует дату
+     *
+     * @param r
+     * @return
+     */
+    private static String generateDate(Random r) {
+        Calendar calendar = Calendar.getInstance();
+        int year = randomBetween(1970, 2000, r);
+        int dayOfYear = randomBetween(1, calendar.getActualMaximum(Calendar.DAY_OF_YEAR), r);
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.DAY_OF_YEAR, dayOfYear);
+        DateFormat formatter = new SimpleDateFormat("dd.MM.yyyy");
+        return formatter.format(calendar.getTime());
+    }
+
+    /**
+     * Генерирует случайное целое число для интервала
+     *
+     * @param start
+     * @param end
+     * @param r
+     * @return
+     */
+    private static int randomBetween(int start, int end, Random r) {
+        int difference = end - start;
+        return r.nextInt(difference) + start;
+    }
+
+    /**
+     * генерирует ИНН
+     *
+     * @param r
+     * @return
+     */
+    private static String generateInn(Random r) {
+        StringBuilder builder = new StringBuilder(regionsDictionary.get(r.nextInt(regionsDictionary.size())));
+        for (int i = 0; i < 8; i++) {
+            builder.append(generateDigit(r));
+        }
+        builder.append(computeControlDigit(builder.toString(), new int[]{7, 2, 4, 10, 3, 5, 9, 4, 6, 8}));
+        builder.append(computeControlDigit(builder.toString(), new int[]{3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8}));
+        return builder.toString();
+    }
+
+    /**
+     * Генерирует цифру
+     *
+     * @param r
+     * @return
+     */
+    private static char generateDigit(Random r) {
+        String digits = "0123456789";
+        return digits.charAt(r.nextInt(10));
+    }
+
+    /**
+     * Расчет контрольной цифры для ИНН.
+     *
+     * @param inn
+     * @param weights коэффициенты весов
+     * @return
+     */
+    private static int computeControlDigit(String inn, int[] weights) {
+        int key = 0;
+        // Замечено что до jdk 8 использование String#split с параметром "" - возвращает массив где первый элемент ""
+        // поэтому вводится дополнительная проверка на длину строки и массива весовых коэффициентов чтобы в случае
+        // наличия убрать лишний символ ""
+        String[] innSplited = inn.split("");
+        String[] innAsArray = new String[weights.length];
+        if (innSplited.length == weights.length) {
+            innAsArray = innSplited;
+        } else {
+            for (int i = 0; i < innSplited.length; i++) {
+                if (!innSplited[i].equals("")) {
+                    innAsArray[i-1] = innSplited[i];
+                }
+            }
+        }
+        for (int i = 0; i < weights.length; i++) {
+            key += Integer.valueOf(innAsArray[i]) * weights[i];
+        }
+        return key % 11 % 10;
     }
 }
