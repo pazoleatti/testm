1ab4c03453fdffd2e846cc738b594510cd40b30c
Denis Novikov <Denis.Novikov@aplana.com>
2017-11-16 18:38:31 +0500

    https://jira.aplana.com/browse/SBRFNDFL-2344 Вернул список пользователей

---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/members/server/GetFilterValuesHandler.java
@@ -0,0 +1,75 @@
+package com.aplana.sbrf.taxaccounting.web.module.members.server;
+
+import com.aplana.sbrf.taxaccounting.model.Department;
+import com.aplana.sbrf.taxaccounting.model.TARole;
+import com.aplana.sbrf.taxaccounting.model.TAUser;
+import com.aplana.sbrf.taxaccounting.service.DepartmentService;
+import com.aplana.sbrf.taxaccounting.service.SourceService;
+import com.aplana.sbrf.taxaccounting.service.TARoleService;
+import com.aplana.sbrf.taxaccounting.web.main.api.server.SecurityService;
+import com.aplana.sbrf.taxaccounting.web.module.members.shared.FilterValues;
+import com.aplana.sbrf.taxaccounting.web.module.members.shared.GetFilterValues;
+import com.gwtplatform.dispatch.server.ExecutionContext;
+import com.gwtplatform.dispatch.server.actionhandler.AbstractActionHandler;
+import com.gwtplatform.dispatch.shared.ActionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Component
+@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'N_ROLE_CONTROL_UNP', 'N_ROLE_CONTROL_NS', 'F_ROLE_CONTROL_UNP', 'F_ROLE_CONTROL_NS')")
+public class GetFilterValuesHandler extends AbstractActionHandler<GetFilterValues, FilterValues> {
+
+    @Autowired
+    TARoleService taRoleService;
+    @Autowired
+    DepartmentService departmentService;
+    @Autowired
+    SecurityService securityService;
+    @Autowired
+    SourceService departmentFormTypService;
+
+    public GetFilterValuesHandler() {
+        super(GetFilterValues.class);
+    }
+
+    @Override
+    public FilterValues execute(GetFilterValues action, ExecutionContext context) throws ActionException {
+        FilterValues result = new FilterValues();
+
+        TAUser currentUser = securityService.currentUserInfo().getUser();
+
+        result.setCanEdit(false);
+
+        List<TARole> allRoles = taRoleService.getAll();
+
+        result.setRoles(allRoles);
+
+        Set<Integer> depIds = new HashSet<Integer>();
+
+        if (currentUser.hasRole(TARole.ROLE_ADMIN)) {
+            depIds.addAll(departmentService.listIdAll());
+        } else {
+            depIds.addAll(departmentService.getBADepartmentIds(currentUser));
+        }
+        result.setDepartments(new ArrayList<Department>(departmentService.getRequiredForTreeDepartments(depIds).values()));
+
+        Set<Integer> userDepIds = new HashSet<Integer>();
+        for (Department department : departmentService.getDepartmentForSudir()) {
+            userDepIds.add(department.getId());
+        }
+        result.setUserDepIds(userDepIds);
+        result.setUserDepartments(new ArrayList<Department>(departmentService.getRequiredForTreeDepartments(userDepIds).values()));
+        return result;
+    }
+
+    @Override
+    public void undo(GetFilterValues action, FilterValues result, ExecutionContext context) throws ActionException {
+
+    }
+}
---
+++ gwtapp/src/main/java/com/aplana/sbrf/taxaccounting/web/module/members/server/PrintHandler.java
@@ -0,0 +1,48 @@
+package com.aplana.sbrf.taxaccounting.web.module.members.server;
+
+import com.aplana.sbrf.taxaccounting.model.PagingResult;
+import com.aplana.sbrf.taxaccounting.model.TAUserView;
+import com.aplana.sbrf.taxaccounting.service.BlobDataService;
+import com.aplana.sbrf.taxaccounting.service.DepartmentService;
+import com.aplana.sbrf.taxaccounting.service.PrintingService;
+import com.aplana.sbrf.taxaccounting.service.TAUserService;
+import com.aplana.sbrf.taxaccounting.web.module.members.shared.PrintAction;
+import com.aplana.sbrf.taxaccounting.web.module.members.shared.PrintResult;
+import com.gwtplatform.dispatch.server.ExecutionContext;
+import com.gwtplatform.dispatch.server.actionhandler.AbstractActionHandler;
+import com.gwtplatform.dispatch.shared.ActionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+@Component
+@PreAuthorize("hasAnyRole('ROLE_ADMIN', 'N_ROLE_CONTROL_UNP', 'N_ROLE_CONTROL_NS', 'F_ROLE_CONTROL_UNP', 'F_ROLE_CONTROL_NS')")
+public class PrintHandler extends AbstractActionHandler<PrintAction, PrintResult> {
+
+    @Autowired
+    PrintingService printingService;
+    @Autowired
+    TAUserService taUserService;
+    @Autowired
+    BlobDataService blobDataService;
+    @Autowired
+    DepartmentService departmentService;
+
+    public PrintHandler() {
+        super(PrintAction.class);
+    }
+
+    @Override
+    public PrintResult execute(PrintAction printAction, ExecutionContext executionContext) throws ActionException {
+
+        PagingResult<TAUserView> usersByFilter = taUserService.getUsersByFilter(printAction.getMembersFilterData());
+
+        PrintResult result = new PrintResult();
+        result.setUuid(printingService.generateExcelUsers(usersByFilter));
+        return result;
+    }
+
+    @Override
+    public void undo(PrintAction printAction, PrintResult printResult, ExecutionContext executionContext) throws ActionException {
+    }
+}
