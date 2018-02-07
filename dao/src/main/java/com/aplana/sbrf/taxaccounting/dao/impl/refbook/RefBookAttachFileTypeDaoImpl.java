c842cd772604f663e91f8553185a8f224da3752f
ytrofimov <Yuriy.Trofimov@aplana.com>
2018-02-07 16:58:53 +0300

    https://jira.aplana.com/browse/SBRFNDFL-3549 Выпилить QueryDSL из RefBookAttachFileTypeDao

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/RefBookAttachFileTypeDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/RefBookAttachFileTypeDaoImpl.java
@@ -1,29 +1,21 @@
 package com.aplana.sbrf.taxaccounting.dao.impl.refbook;
 
+import com.aplana.sbrf.taxaccounting.dao.impl.AbstractDao;
+import com.aplana.sbrf.taxaccounting.dao.impl.util.SqlUtils;
 import com.aplana.sbrf.taxaccounting.dao.refbook.RefBookAttachFileTypeDao;
 import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttachFileType;
-import com.querydsl.core.types.QBean;
-import com.querydsl.sql.SQLQueryFactory;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
-
-import static com.aplana.sbrf.taxaccounting.model.querydsl.QRefBookAttachFileType.refBookAttachFileType;
-import static com.querydsl.core.types.Projections.bean;
 
 /**
  * Реализация дао для работы со справочником Категории прикрепленных файлов
  */
 @Repository
-public class RefBookAttachFileTypeDaoImpl implements RefBookAttachFileTypeDao {
-
-    final private SQLQueryFactory sqlQueryFactory;
-
-    public RefBookAttachFileTypeDaoImpl(SQLQueryFactory sqlQueryFactory) {
-        this.sqlQueryFactory = sqlQueryFactory;
-    }
-
-    final private QBean<RefBookAttachFileType> refBookAttachFileTypeBean = bean(RefBookAttachFileType.class, refBookAttachFileType.all());
+public class RefBookAttachFileTypeDaoImpl extends AbstractDao implements RefBookAttachFileTypeDao {
 
     /**
      * Получение всех значений справочника
@@ -32,10 +24,20 @@ public class RefBookAttachFileTypeDaoImpl implements RefBookAttachFileTypeDao {
      */
     @Override
     public List<RefBookAttachFileType> fetchAll() {
-        return sqlQueryFactory
-                .select(refBookAttachFileTypeBean)
-                .from(refBookAttachFileType)
-                .where(refBookAttachFileType.code.gt(0))
-                .fetch();
+        return getJdbcTemplate().query("select aft.id, aft.code, aft.name " +
+                        "from ref_book_attach_file_type aft " +
+                        "where aft.code > 0",
+                new RefBookAttachFileTypeRowMapper());
+    }
+
+    private static final class RefBookAttachFileTypeRowMapper implements RowMapper<RefBookAttachFileType> {
+        @Override
+        public RefBookAttachFileType mapRow(ResultSet resultSet, int i) throws SQLException {
+            RefBookAttachFileType refBookAttachFileType = new RefBookAttachFileType();
+            refBookAttachFileType.setId(SqlUtils.getLong(resultSet, "id"));
+            refBookAttachFileType.setCode(resultSet.getByte("code"));
+            refBookAttachFileType.setName(resultSet.getString("name"));
+            return refBookAttachFileType;
+        }
     }
 }
