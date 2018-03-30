21b8827221e90ddd5e7613147feeb0f1ad285ea4
RNurgaleev <Rinat.Nurgaleev@aplana.com>
2018-03-30 15:18:50 +0500

    Исправление ошибки в патче бд 2.2

---
+++ db/other_scripts/correct_2_2/run.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+REM ����� ��������� ��� � ������ ��������� ����� � ����� ���� ������
+REM ������: user_name/password@host:port/service_name
+SET AUTH=ndfl_psi_test/ndfl_psi_test@sbrf_un
+REM ����� ��������� ���� � ����� ORACLE_HOME\BIN
+SET ORA_BIN=D:\app\client\RNurgaleev\product\12.1.0\client_1\BIN
+SET LOG_DIR=_logs
+SET BAD_DIR=_bad
+SET nls_lang=AMERICAN_AMERICA.AL32UTF8
+
+ECHO ## DB: %AUTH%
+
+SET MODE=1
+MKDIR %LOG_DIR%
+MKDIR %BAD_DIR%
+
+DEL /s /q /f %LOG_DIR%\*.txt
+DEL /s /q /f %BAD_DIR%\*.*
+
+
+ECHO ## dml
+"%ORA_BIN%\sqlplus" %AUTH% @"update_dml.sql" > "%LOG_DIR%/update_dml.txt"
+
+PAUSE
---
+++ db/other_scripts/correct_2_2/update_dml.sql
@@ -0,0 +1,7 @@
+set serveroutput on;
+
+prompt fill ref_book
+@@ref_book.sql;
+
+commit;
+exit;
