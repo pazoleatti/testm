b012def63ac3bca948f77d9d7cb990730f064424
RNurgaleev <Rinat.Nurgaleev@aplana.com>
2018-06-13 15:53:07 +0500

    https://jira.aplana.com/browse/SBRFNDFL-4876 Написать диагностический скрипт, проверяющий наличие внешних ключей, ссылающихся на схему УН

---
+++ db/other_scripts/check_keys_SBRFNDFL-4876/run.bat
@@ -0,0 +1,25 @@
+@ECHO OFF
+REM ����� ��������� ��� � ������ ��������� ����� � ����� ���� ������
+REM ������: user_name/password@host:port/service_name
+SET AUTH=ndfl/ndfl@172.19.214.45:1521/orcl.aplana.local
+REM ����� ��������� ���� � ����� ORACLE_HOME\BIN
+SET ORA_BIN=D:\app\client\RNurgaleev\product\12.1.0\client_1\BIN
+SET LOG_DIR=_logs
+SET BAD_DIR=_bad
+SET nls_lang=AMERICAN_AMERICA.AL32UTF8
+
+ECHO ## DB: %AUTH%
+
+SET MODE=1
+MKDIR %LOG_DIR%
+MKDIR %BAD_DIR%
+
+DEL /s /q /f %LOG_DIR%\*.txt
+DEL /s /q /f %BAD_DIR%\*.*
+
+
+ECHO ## check keys
+"%ORA_BIN%\sqlplus" %AUTH% @"checks.sql" %LOG_DIR%/checks.csv
+
+
+PAUSE
