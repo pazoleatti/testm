55319c405a29c15900b353096d295a77388239e0
ytrofimov <Yuriy.Trofimov@aplana.com>
2018-07-05 19:44:40 +0300

    https://jira.aplana.com/browse/SBRFNDFL-4895 Оптимизация запроса для получения адресов из справочника

---
+++ model/src/test/java/com/aplana/sbrf/taxaccounting/model/QueryBuilderTest.java
@@ -0,0 +1,20 @@
+package com.aplana.sbrf.taxaccounting.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class QueryBuilderTest {
+
+    @Test
+    public void withPagingTest() {
+        QueryBuilder queryBuilder = new QueryBuilder();
+        PagingParams pagingParams = new PagingParams();
+        pagingParams.setCount(10);
+        pagingParams.setPage(1);
+        String query = "query";
+        queryBuilder = queryBuilder.append(query);
+        queryBuilder = queryBuilder.withPaging(pagingParams);
+        assertTrue(queryBuilder.getPagedQuery().equalsIgnoreCase("select /*+ PARALLEL */ * FROM (\n" + query + ") WHERE rn between :paging_start and :paging_end"));
+    }
+}
