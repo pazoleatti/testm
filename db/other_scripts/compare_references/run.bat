40ffdb50e0563a01e452dbaa54ceafc06275df13
ankalex <ankalex@mail.ru>
2018-07-20 08:16:33 +0300

    https://jira.aplana.com/browse/SBRFNDFL-5053 - Скрипт выверки значений справочников на соответствие эталонным значениям (описанным в постановке)

--- db/other_scripts/compare_references/run.bat
+++ db/other_scripts/compare_references/run.bat
@@ -4,7 +4,7 @@ SET AUTH=system/oracle@172.19.214.45:1521/orcl.aplana.local
 
 SET SCHEME=NDFL_UNSTABLE
 
-SET ORA_BIN=D:\app\client\RNurgaleev\product\12.1.0\client_1\BIN
+SET ORA_BIN=c:\app\client\akorobko\product\12.1.0\client_1\BIN
 SET LOG_DIR=_logs
 SET BAD_DIR=_bad
 SET nls_lang=AMERICAN_AMERICA.AL32UTF8
@@ -13,7 +13,7 @@ ECHO ## DB: %AUTH%
 
 MKDIR %LOG_DIR%
 
-DEL /s /q /f %LOG_DIR%\*.txt
+DEL /s /q /f %LOG_DIR%\*.csv
 
 ECHO ## compare_references
 "%ORA_BIN%\sqlplus" %AUTH% @"compare_references.sql" %LOG_DIR%/compare_references.csv %SCHEME%
