9ca5f150ac8ea23a6f623936c5af0e546a0776ac
ankalex <ankalex@mail.ru>
2018-07-25 16:40:20 +0300

    https://jira.aplana.com/browse/SBRFNDFL-5206 - Подготовить скрипт БД, который в список тербанков для каждого ФЛ из справочника физических лиц пропишет Центральный аппарат.

---
+++ db/other_scripts/add_central_apparatus_to_ref_book_person_tb/insert.sql
@@ -0,0 +1,12 @@
+set serveroutput on;
+
+begin
+	insert into ref_book_person_tb(id,record_id,version,status,person_id,tb_department_id)
+	select seq_ref_book_record.nextval as id,seq_ref_book_record_row_id.nextval as record_id,to_date('01.07.2018','dd.mm.yyyy') as version,0 as status,id as person_id,113 as tb_department_id
+	from ref_book_person rbp where not exists (select * from ref_book_person_tb rbpt where rbpt.person_id=rbp.id);
+
+	DBMS_OUTPUT.PUT_LINE('Inserted ' || SQL%ROWCOUNT || ' rows into REF_BOOK_PERSON_TB.');	
+end;
+/
+
+exit;
---
+++ db/other_scripts/add_central_apparatus_to_ref_book_person_tb/run.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+REM ����� ��������� ��� � ������ ��������� ����� � ����� ���� ������
+REM ������: user_name/password@host:port/service_name
+SET AUTH=ndfl_schema/schema_password@172.19.214.45:1521/orcl.aplana.local
+REM ����� ��������� ���� � ����� ORACLE_HOME\BIN
+SET ORA_BIN=c:\app\client\akorobko\product\12.1.0\client_1\BIN
+SET LOG_DIR=_logs
+REM SET BAD_DIR=_bad
+SET nls_lang=AMERICAN_AMERICA.AL32UTF8
+
+ECHO ## DB: %AUTH%
+
+SET MODE=1
+MKDIR %LOG_DIR%
+REM MKDIR %BAD_DIR%
+
+DEL /s /q /f %LOG_DIR%\*.txt
+REM DEL /s /q /f %BAD_DIR%\*.*
+
+ECHO ## insert
+"%ORA_BIN%\sqlplus" %AUTH% @"insert.sql" > "%LOG_DIR%/insert.txt"
+
+rem �������� ������ ���������� �������������������
+PAUSE
