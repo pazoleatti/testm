047621e1d79c08151c74b5105ce2ee0ebd82e4ec
ankalex <ankalex@mail.ru>
2018-07-26 12:28:05 +0300

    https://jira.aplana.com/browse/SBRFNDFL-5206 - Подготовить скрипт БД, который в список тербанков для каждого ФЛ из справочника физических лиц пропишет Центральный аппарат.

--- db/other_scripts/add_central_apparatus_to_ref_book_person_tb/insert.sql
+++ db/other_scripts/add_central_apparatus_to_ref_book_person_tb/insert.sql
@@ -1,8 +1,8 @@
 set serveroutput on;
 
 begin
-	insert into ref_book_person_tb(id,record_id,version,status,person_id,tb_department_id)
-	select seq_ref_book_record.nextval as id,seq_ref_book_record_row_id.nextval as record_id,to_date('01.07.2018','dd.mm.yyyy') as version,0 as status,id as person_id,113 as tb_department_id
+	insert into ref_book_person_tb(id,record_id,version,status,person_id,tb_department_id,import_date)
+	select seq_ref_book_record.nextval as id,seq_ref_book_record_row_id.nextval as record_id,to_date('01.01.2017','dd.mm.yyyy') as version,0 as status,id as person_id,113 as tb_department_id,to_timestamp('01.07.2018','dd.mm.yyyy') as import_date
 	from ref_book_person rbp where not exists (select * from ref_book_person_tb rbpt where rbpt.person_id=rbp.id);
 
 	DBMS_OUTPUT.PUT_LINE('Inserted ' || SQL%ROWCOUNT || ' rows into REF_BOOK_PERSON_TB.');	
