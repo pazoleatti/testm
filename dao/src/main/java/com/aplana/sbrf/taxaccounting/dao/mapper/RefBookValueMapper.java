06460ca1e1035351a724732afc7fbeeda6d213e3
Denis Novikov <Denis.Novikov@aplana.com>
2018-09-10 16:21:33 +0500

    https://jira.aplana.com/browse/SBRFNDFL-5501 Нумерацию в настройках подразделений вернул

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/mapper/RefBookValueMapper.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/mapper/RefBookValueMapper.java
@@ -33,8 +33,8 @@ public class RefBookValueMapper extends RefBookAbstractValueMapper {
     protected Map<String, RefBookValue> createResult(ResultSet rs) throws SQLException {
         Map<String, RefBookValue> result = new HashMap<String, RefBookValue>();
         result.put(RefBook.RECORD_ID_ALIAS, new RefBookValue(RefBookAttributeType.NUMBER, SqlUtils.getLong(rs, RefBook.RECORD_ID_ALIAS)));
-        if (SqlUtils.isExistColumn(rs, RefBook.RECORD_SORT_ALIAS)) {
-            result.put(RefBook.RECORD_SORT_ALIAS, new RefBookValue(RefBookAttributeType.NUMBER, SqlUtils.getLong(rs, RefBook.RECORD_SORT_ALIAS)));
+        if (SqlUtils.isExistColumn(rs, "ROW_ORD")) {// для настроек подразделений
+            result.put("ROW_ORD", new RefBookValue(RefBookAttributeType.NUMBER, SqlUtils.getLong(rs, "ROW_ORD")));
         }
         if (this.refBook.isVersioned() && this.refBook.getId() != RefBook.Id.DEPARTMENT.getId()) {
             result.put(RefBook.BUSINESS_ID_ALIAS, new RefBookValue(RefBookAttributeType.NUMBER, SqlUtils.getLong(rs, RefBook.BUSINESS_ID_ALIAS)));
