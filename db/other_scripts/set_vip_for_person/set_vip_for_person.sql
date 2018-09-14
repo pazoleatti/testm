27e57770990d191d19526e5eaf961859b66b778e
Andrey Mandzyak <amandzyak@it.ru>
2018-09-14 14:25:48 +0500

    SBRFNDFL-5571. Скрипт устанавливающий признак Важность физлицу по ИД ФЛ

--- db/other_scripts/set_vip_for_person/set_vip_for_person.sql
+++ db/other_scripts/set_vip_for_person/set_vip_for_person.sql
@@ -9,7 +9,6 @@ declare
 begin
   v_fio := 'Апланянц Аплан Апланардович';
   v_vip := 1;
-
   select count(id) into v_count_id from REF_BOOK_PERSON where upper(last_name || ' ' || first_name || ' ' || middle_name) = upper(v_fio);
   if v_count_id > 0 then
      DBMS_OUTPUT.PUT_LINE('Person FIO "' || v_fio || '" set VIP=' || v_vip || ' for:');
