0f0f9e5fa4143b1208b6dfdec33a7183a4547bc9
Andrey Mandzyak <amandzyak@it.ru>
2018-09-14 13:54:43 +0500

    SBRFNDFL-5571. Скрипт устанавливающий признак Важность физлицу по ИД ФЛ

--- db/other_scripts/set_vip_for_person/run.bat
+++ db/other_scripts/set_vip_for_person/run.bat
@@ -1,16 +1,13 @@
-﻿@ECHO OFF
+﻿mode con 1251
+@ECHO OFF
 REM строка подключения к бд
 SET AUTH=ndfl_schema/schema_password@host:port/service_name
 REM путь к папке BIN клиента oracle
 SET ORA_BIN=c:\app\client\user\product\12.2.0\client_1\BIN
 REM имя каталога для вывода результата выполнения
 SET LOG_DIR=_logs
-REM ИД ФЛ
-SET RECORD_ID=1234567
-REM признак Важность ФЛ
-SET VIP_FLAG=0
 
-SET nls_lang=AMERICAN_AMERICA.AL32UTF8
+SET nls_lang=AMERICAN_AMERICA.CL8MSWIN1251
 
 ECHO ## DB: %AUTH%
 
@@ -18,7 +15,7 @@ MKDIR %LOG_DIR%
 
 DEL /s /q /f %LOG_DIR%\*.log
 
-ECHO ## set VIP flag for person RECORD_ID
-"%ORA_BIN%\sqlplus" %AUTH% @"set_vip_for_person.sql" %LOG_DIR%/set_vip_for_person.log %RECORD_ID% %VIP_FLAG%
+ECHO ## set VIP flag for person 
+"%ORA_BIN%\sqlplus" %AUTH% @"set_vip_for_person.sql" %LOG_DIR%/set_vip_for_person.log
 
 PAUSE
