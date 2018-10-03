f3035c392ce286fe461f5778e4735c1aeb047d5e
Andrey Mandzyak <amandzyak@it.ru>
2018-10-03 13:24:26 +0500

    SBRFNDFL-5595. Патч БД 3.1.1

---
+++ database/database-3.1.1/01_update_ddl.sql
@@ -0,0 +1,7 @@
+set serveroutput on;
+
+--create views
+prompt create views
+@@01_01_create_views.sql;
+
+exit;
---
+++ database/database-3.1.1/05_gather_statistics.sql
@@ -0,0 +1,6 @@
+set serveroutput on;
+
+prompt gather schema statistics
+@@05_01_gather_schema_statistics.sql;
+
+exit;
