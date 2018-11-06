c6950ccb8218f4b1e66e1328e52a7df7f8d7ad9b
Denis Novikov <Denis.Novikov@aplana.com>
2018-11-06 12:15:16 +0500

    https://jira.aplana.com/browse/SBRFNDFL-5812 Справочник "Подразделения". Не отображаются параметры записей

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/result/RefBookDepartmentDTO.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/result/RefBookDepartmentDTO.java
@@ -1,9 +1,13 @@
 package com.aplana.sbrf.taxaccounting.model.result;
 
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAttributeType;
 import com.aplana.sbrf.taxaccounting.model.refbook.RefBookDepartment;
+import com.aplana.sbrf.taxaccounting.model.refbook.RefBookValue;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * DTO подразделения с дочерними элементами
@@ -28,5 +32,34 @@ public class RefBookDepartmentDTO {
             }
         }
         return children;
+    }
+
+    public Map<String, RefBookValue> getMapValues() {
+        Map<String, RefBookValue> record = new HashMap<>();
+        record.put("CODE", new RefBookValue(RefBookAttributeType.NUMBER, department.getCode()));
+        record.put("NAME", new RefBookValue(RefBookAttributeType.STRING, department.getName()));
+        record.put("SHORTNAME", new RefBookValue(RefBookAttributeType.STRING, department.getShortName()));
+        record.put("PARENT_ID", new RefBookValue(RefBookAttributeType.REFERENCE, getParentValue()));
+        record.put("TYPE", new RefBookValue(RefBookAttributeType.REFERENCE, getTypeValue()));
+        record.put("TB_INDEX", new RefBookValue(RefBookAttributeType.STRING, department.getTbIndex()));
+        record.put("SBRF_CODE", new RefBookValue(RefBookAttributeType.STRING, department.getSbrfCode()));
+        record.put("IS_ACTIVE", new RefBookValue(RefBookAttributeType.NUMBER, department.isActive() ? 1 : 0));
+        return record;
+    }
+
+    private Map<String, RefBookValue> getParentValue() {
+        Map<String, RefBookValue> parentRecord = new HashMap<>();
+        if (department.getParent() != null) {
+            parentRecord.put("NAME", new RefBookValue(RefBookAttributeType.STRING, department.getParent().getName()));
+        }
+        return parentRecord;
+    }
+
+    private Map<String, RefBookValue> getTypeValue() {
+        Map<String, RefBookValue> parentRecord = new HashMap<>();
+        if (department.getType() != null) {
+            parentRecord.put("NAME", new RefBookValue(RefBookAttributeType.STRING, department.getType().getLabel()));
+        }
+        return parentRecord;
     }
 }
