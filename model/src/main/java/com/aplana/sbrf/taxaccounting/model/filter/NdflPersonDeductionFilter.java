d7809a639a8d3ee9eb70da3e9c1ca16bf1c7b4d3
Denis Novikov <Denis.Novikov@aplana.com>
2018-11-07 20:30:51 +0500

    https://jira.aplana.com/browse/SBRFNDFL-5616 Некорректно работает фильтр по "АСНУ" в ПНФ и КНФ РНУ НДФЛ

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/filter/NdflPersonDeductionFilter.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/filter/NdflPersonDeductionFilter.java
@@ -1,6 +1,10 @@
 package com.aplana.sbrf.taxaccounting.model.filter;
 
 import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAsnu;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Date;
 import java.util.List;
@@ -8,7 +12,15 @@ import java.util.List;
 /**
  * Модель для параметров Фильтра вкладки "Сведения о вычетах" страницу РНУ НДФЛ
  */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class NdflPersonDeductionFilter {
+    /**
+     * Общий фильтр
+     */
+    private NdflFilter ndflFilter;
     /**
      * id операции
      */
@@ -76,144 +88,11 @@ public class NdflPersonDeductionFilter {
      */
     private String modifiedBy;
 
-    /**
-     * АСНУ
-     */
-    private List<RefBookAsnu> asnu;
-
-    public String getOperationId() {
-        return operationId;
-    }
-
-    public void setOperationId(String operationId) {
-        this.operationId = operationId;
-    }
-
-    public String getDeductionCode() {
-        return deductionCode;
-    }
-
-    public void setDeductionCode(String deductionCode) {
-        this.deductionCode = deductionCode;
-    }
-
-    public Date getDeductionDateFrom() {
-        return deductionDateFrom;
-    }
-
-    public void setDeductionDateFrom(Date deductionDateFrom) {
-        this.deductionDateFrom = deductionDateFrom;
-    }
-
-    public Date getDeductionDateTo() {
-        return deductionDateTo;
-    }
-
-    public void setDeductionDateTo(Date deductionDateTo) {
-        this.deductionDateTo = deductionDateTo;
-    }
-
-    public Date getPeriodPrevDateFrom() {
-        return periodPrevDateFrom;
-    }
-
-    public void setPeriodPrevDateFrom(Date periodPrevDateFrom) {
-        this.periodPrevDateFrom = periodPrevDateFrom;
-    }
-
-    public Date getPeriodPrevDateTo() {
-        return periodPrevDateTo;
-    }
-
-    public void setPeriodPrevDateTo(Date periodPrevDateTo) {
-        this.periodPrevDateTo = periodPrevDateTo;
-    }
-
-    public String getNotifType() {
-        return notifType;
-    }
-
-    public void setNotifType(String notifType) {
-        this.notifType = notifType;
-    }
-
-    public String getNotifNum() {
-        return notifNum;
-    }
-
-    public void setNotifNum(String notifNum) {
-        this.notifNum = notifNum;
-    }
-
-    public String getNotifSource() {
-        return notifSource;
-    }
-
-    public void setNotifSource(String notifSource) {
-        this.notifSource = notifSource;
-    }
-
-    public Date getNotifDateFrom() {
-        return notifDateFrom;
-    }
-
-    public void setNotifDateFrom(Date notifDateFrom) {
-        this.notifDateFrom = notifDateFrom;
-    }
-
-    public Date getNotifDateTo() {
-        return notifDateTo;
-    }
-
-    public void setNotifDateTo(Date notifDateTo) {
-        this.notifDateTo = notifDateTo;
-    }
-
-    public String getRowNum() {
-        return rowNum;
-    }
-
-    public void setRowNum(String rowNum) {
-        this.rowNum = rowNum;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Date getModifiedDateFrom() {
-        return modifiedDateFrom;
-    }
-
-    public void setModifiedDateFrom(Date modifiedDateFrom) {
-        this.modifiedDateFrom = modifiedDateFrom;
-    }
-
-    public Date getModifiedDateTo() {
-        return modifiedDateTo;
-    }
-
-    public void setModifiedDateTo(Date modifiedDateTo) {
-        this.modifiedDateTo = modifiedDateTo;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
+    public NdflPersonDeductionFilter(NdflFilter ndflFilter) {
+        this.ndflFilter = ndflFilter;
     }
 
     public List<RefBookAsnu> getAsnu() {
-        return asnu;
-    }
-
-    public void setAsnu(List<RefBookAsnu> asnu) {
-        this.asnu = asnu;
+        return ndflFilter.getIncome().getAsnu();
     }
 }
--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/filter/NdflPersonIncomeFilter.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/filter/NdflPersonIncomeFilter.java
@@ -2,6 +2,10 @@ package com.aplana.sbrf.taxaccounting.model.filter;
 
 import com.aplana.sbrf.taxaccounting.model.URM;
 import com.aplana.sbrf.taxaccounting.model.refbook.RefBookAsnu;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -10,6 +14,10 @@ import java.util.List;
 /**
  * Модель для параметров Фильтра вкладки "Сведения о доходах и НДФЛ" страницу РНУ НДФЛ
  */
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class NdflPersonIncomeFilter {
     /**
      * Общий фильтр
@@ -88,7 +96,7 @@ public class NdflPersonIncomeFilter {
     /**
      * Данные УРМ
      */
-    private List<URM> urmList = new ArrayList<>();;
+    private List<URM> urmList = new ArrayList<>();
 
     /**
      * Номер строки
@@ -110,209 +118,12 @@ public class NdflPersonIncomeFilter {
      * Обновил
      */
     private String modifiedBy;
-
     /**
      * АСНУ
      */
     private List<RefBookAsnu> asnu;
 
-    public NdflFilter getNdflFilter() {
-        return ndflFilter;
-    }
-
-    public void setNdflFilter(NdflFilter ndflFilter) {
+    public NdflPersonIncomeFilter(NdflFilter ndflFilter) {
         this.ndflFilter = ndflFilter;
-    }
-
-    public String getOperationId() {
-        return operationId;
-    }
-
-    public void setOperationId(String operationId) {
-        this.operationId = operationId;
-    }
-
-    public String getKpp() {
-        return kpp;
-    }
-
-    public void setKpp(String kpp) {
-        this.kpp = kpp;
-    }
-
-    public String getOktmo() {
-        return oktmo;
-    }
-
-    public void setOktmo(String oktmo) {
-        this.oktmo = oktmo;
-    }
-
-    public String getIncomeCode() {
-        return incomeCode;
-    }
-
-    public void setIncomeCode(String incomeCode) {
-        this.incomeCode = incomeCode;
-    }
-
-    public String getIncomeAttr() {
-        return incomeAttr;
-    }
-
-    public void setIncomeAttr(String incomeAttr) {
-        this.incomeAttr = incomeAttr;
-    }
-
-    public String getTaxRate() {
-        return taxRate;
-    }
-
-    public void setTaxRate(String taxRate) {
-        this.taxRate = taxRate;
-    }
-
-    public String getNumberPaymentOrder() {
-        return numberPaymentOrder;
-    }
-
-    public void setNumberPaymentOrder(String numberPaymentOrder) {
-        this.numberPaymentOrder = numberPaymentOrder;
-    }
-
-    public Date getTransferDateFrom() {
-        return transferDateFrom;
-    }
-
-    public void setTransferDateFrom(Date transferDateFrom) {
-        this.transferDateFrom = transferDateFrom;
-    }
-
-    public Date getTransferDateTo() {
-        return transferDateTo;
-    }
-
-    public void setTransferDateTo(Date transferDateTo) {
-        this.transferDateTo = transferDateTo;
-    }
-
-    public Date getCalculationDateFrom() {
-        return calculationDateFrom;
-    }
-
-    public void setCalculationDateFrom(Date calculationDateFrom) {
-        this.calculationDateFrom = calculationDateFrom;
-    }
-
-    public Date getCalculationDateTo() {
-        return calculationDateTo;
-    }
-
-    public void setCalculationDateTo(Date calculationDateTo) {
-        this.calculationDateTo = calculationDateTo;
-    }
-
-    public Date getPaymentDateFrom() {
-        return paymentDateFrom;
-    }
-
-    public void setPaymentDateFrom(Date paymentDateFrom) {
-        this.paymentDateFrom = paymentDateFrom;
-    }
-
-    public Date getPaymentDateTo() {
-        return paymentDateTo;
-    }
-
-    public void setPaymentDateTo(Date paymentDateTo) {
-        this.paymentDateTo = paymentDateTo;
-    }
-
-    public Date getAccruedDateFrom() {
-        return accruedDateFrom;
-    }
-
-    public void setAccruedDateFrom(Date accruedDateFrom) {
-        this.accruedDateFrom = accruedDateFrom;
-    }
-
-    public Date getAccruedDateTo() {
-        return accruedDateTo;
-    }
-
-    public void setAccruedDateTo(Date accruedDateTo) {
-        this.accruedDateTo = accruedDateTo;
-    }
-
-    public Date getPayoutDateFrom() {
-        return payoutDateFrom;
-    }
-
-    public void setPayoutDateFrom(Date payoutDateFrom) {
-        this.payoutDateFrom = payoutDateFrom;
-    }
-
-    public Date getPayoutDateTo() {
-        return payoutDateTo;
-    }
-
-    public void setPayoutDateTo(Date payoutDateTo) {
-        this.payoutDateTo = payoutDateTo;
-    }
-
-    public List<URM> getUrmList() {
-        return urmList;
-    }
-
-    public void setUrmList(List<URM> urmList) {
-        this.urmList = urmList;
-    }
-
-    public String getRowNum() {
-        return rowNum;
-    }
-
-    public void setRowNum(String rowNum) {
-        this.rowNum = rowNum;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Date getModifiedDateFrom() {
-        return modifiedDateFrom;
-    }
-
-    public void setModifiedDateFrom(Date modifiedDateFrom) {
-        this.modifiedDateFrom = modifiedDateFrom;
-    }
-
-    public Date getModifiedDateTo() {
-        return modifiedDateTo;
-    }
-
-    public void setModifiedDateTo(Date modifiedDateTo) {
-        this.modifiedDateTo = modifiedDateTo;
-    }
-
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
-
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
-
-    public List<RefBookAsnu> getAsnu() {
-        return asnu;
-    }
-
-    public void setAsnu(List<RefBookAsnu> asnu) {
-        this.asnu = asnu;
     }
 }
