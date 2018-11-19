79b48ec81c750f0b53f20d678b6ee5509b32330c
ytrofimov <Yuriy.Trofimov@aplana.com>
2018-11-19 15:07:40 +0300

    https://jira.aplana.com/browse/SBRFNDFL-6031 Dao Exception при сохранении пустого статуса налогоплательщика

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/person/RegistryPersonMapper.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/refbook/person/RegistryPersonMapper.java
@@ -1,5 +1,6 @@
 package com.aplana.sbrf.taxaccounting.dao.impl.refbook.person;
 
+import com.aplana.sbrf.taxaccounting.dao.impl.util.SqlUtils;
 import com.aplana.sbrf.taxaccounting.model.refbook.*;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -10,9 +11,9 @@ public class RegistryPersonMapper implements RowMapper<RegistryPerson> {
     @Override
     public RegistryPerson mapRow(ResultSet rs, int rowNum) throws SQLException {
         RegistryPerson result = new RegistryPerson();
-        result.setId(rs.getLong("id"));
-        result.setRecordId(rs.getLong("record_id"));
-        result.setOldId(rs.getLong("old_id"));
+        result.setId(SqlUtils.getLong(rs,"id"));
+        result.setRecordId(SqlUtils.getLong(rs,"record_id"));
+        result.setOldId(SqlUtils.getLong(rs,"old_id"));
         result.setVip(rs.getBoolean("vip"));
         result.setFirstName(rs.getString("first_name"));
         result.setLastName(rs.getString("last_name"));
@@ -26,7 +27,7 @@ public class RegistryPersonMapper implements RowMapper<RegistryPerson> {
         result.setSnils(rs.getString("snils"));
 
         RefBookDocType docType = new RefBookDocType();
-        docType.setId(rs.getLong("doc_type_id"));
+        docType.setId(SqlUtils.getLong(rs,"doc_type_id"));
         docType.setName(rs.getString("doc_name"));
         docType.setCode(rs.getString("doc_code"));
         docType.setPriority(rs.getInt("doc_type_priority"));
@@ -38,25 +39,25 @@ public class RegistryPersonMapper implements RowMapper<RegistryPerson> {
 
         IdDoc personDocument = new IdDoc();
         personDocument.setDocType(docType);
-        personDocument.setId(rs.getLong("d_id"));
+        personDocument.setId(SqlUtils.getLong(rs,"d_id"));
         personDocument.setDocumentNumber(rs.getString("doc_number"));
         personDocument.setPerson(docPersonStub);
 
-        if (personDocument.getId().equals(0L)) {
+        if (personDocument.getId() == null) {
             result.setReportDoc(null);
         } else {
             result.setReportDoc(personDocument);
         }
 
         RefBookCountry citizenship = new RefBookCountry();
-        citizenship.setId(rs.getLong("citizenship_country_id"));
+        citizenship.setId(SqlUtils.getLong(rs,"citizenship_country_id"));
         citizenship.setCode(rs.getString("citizenship_country_code"));
         citizenship.setName(rs.getString("citizenship_country_name"));
 
         result.setCitizenship(citizenship);
 
         RefBookAsnu refBookAsnu = new RefBookAsnu();
-        refBookAsnu.setId(rs.getLong("asnu_id"));
+        refBookAsnu.setId(SqlUtils.getLong(rs,"asnu_id"));
         refBookAsnu.setCode(rs.getString("asnu_code"));
         refBookAsnu.setName(rs.getString("asnu_name"));
         refBookAsnu.setType(rs.getString("asnu_type"));
@@ -65,7 +66,7 @@ public class RegistryPersonMapper implements RowMapper<RegistryPerson> {
         result.setSource(refBookAsnu);
 
         RefBookTaxpayerState taxpayerStatus = new RefBookTaxpayerState();
-        taxpayerStatus.setId(rs.getLong("state_id"));
+        taxpayerStatus.setId(SqlUtils.getLong(rs, "state_id"));
         taxpayerStatus.setCode(rs.getString("state_code"));
         taxpayerStatus.setName(rs.getString("state_name"));
 
@@ -73,7 +74,7 @@ public class RegistryPersonMapper implements RowMapper<RegistryPerson> {
 
         Address address = new Address();
         RefBookCountry country = new RefBookCountry();
-        country.setId(rs.getLong("address_country_id"));
+        country.setId(SqlUtils.getLong(rs,"address_country_id"));
         country.setCode(rs.getString("address_country_code"));
         country.setName(rs.getString("address_country_name"));
 
