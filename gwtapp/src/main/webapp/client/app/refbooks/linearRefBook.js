e9a228afc6e82a1a64e4cb575a3684a9ff266649
Denis Novikov <Denis.Novikov@aplana.com>
2018-12-12 17:39:19 +0500

    https://jira.aplana.com/browse/SBRFNDFL-5447 Реализовать для справочника "Коды видов дохода" корректное отображение информации о типе вычета

--- gwtapp/src/main/webapp/client/app/refbooks/linearRefBook.js
+++ gwtapp/src/main/webapp/client/app/refbooks/linearRefBook.js
@@ -110,33 +110,33 @@
                  */
                 function refBookValueFormatter(cellValue, options, row) {
                     var colModel = options.index ? options : options.colModel;
-                    var record = row[colModel.index];
+                    var refBookValue = row[colModel.index];
                     var value = "";
                     switch (colModel.type) {
                         case 'STRING':
                         case 'NUMBER':
-                            value = record && typeof record.value !== 'undefined' ? record.value : "";
+                            value = refBookValue && typeof refBookValue.value !== 'undefined' ? refBookValue.value : "";
                             break;
                         case 'DATE':
                             // Особенность для справочника "Коды, определяющие налоговый (отчётный) период" - год отображать не надо
                             var formatter = $scope.refBook.id === APP_CONSTANTS.REFBOOK.PERIOD_CODE ? $filter('dateWithoutYearFormatter') : $filter('dateFormatter');
-                            value = record && typeof record.value !== 'undefined' ? formatter(record.value) : "";
+                            value = refBookValue && typeof refBookValue.value !== 'undefined' ? formatter(refBookValue.value) : "";
                             break;
                         case 'REFERENCE':
-                            value = record && typeof record.referenceObject !== 'undefined' ? refCellFormatter(colModel, record) : "";
+                            value = refBookValue && typeof refBookValue.referenceObject !== 'undefined' ? refCellFormatter(colModel, refBookValue) : "";
                             break;
                     }
                     return value;
                 }
 
-                function refCellFormatter(colModel, record) {
+                function refCellFormatter(colModel, refBookValue) {
                     if (colModel.attribute.refBookId === APP_CONSTANTS.REFBOOK.DEDUCTION_MARK) {
                         return $filter("codeNameFormatter")({
-                            'code': record.referenceObject["CODE"].value,
-                            'name': record.referenceObject["NAME"].value
+                            'code': refBookValue.referenceObject["CODE"].value,
+                            'name': refBookValue.referenceObject["NAME"].value
                         });
                     } else {
-                        return record.referenceObject[colModel.attribute.refBookAttribute.alias].value;
+                        return refBookValue.referenceObject[colModel.attribute.refBookAttribute.alias].value;
                     }
                 }
 
