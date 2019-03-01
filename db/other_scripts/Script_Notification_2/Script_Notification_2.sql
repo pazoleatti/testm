a9592bffb4d385a7e68ccb53495466dec60b6942
Andrey Mandzyak <amandzyak@it.ru>
2019-03-01 13:39:18 +0500

    https://jira.aplana.com/browse/SBRFNDFL-6779 - Скрипт 2_выгрузки текстов ОПОВЕЩЕНИЙ о загрузках ТФ, проверках и идентификации в файлы на ПСИ стенде

--- db/other_scripts/Script_Notification_2/Script_Notification_2.sql
+++ db/other_scripts/Script_Notification_2/Script_Notification_2.sql
@@ -10,20 +10,10 @@ WHENEVER SQLERROR EXIT;
 variable v_user_id number;
 variable v_begin_date varchar2(10);
 variable v_end_date varchar2(10);
-variable v_file_name varchar2(100);
 
 exec :v_user_id := &1;
 exec :v_begin_date:='&2';
 exec :v_end_date:='&3';
-exec :v_file_name:='_logs\NOTIFICATIONS_20190301-1140.csv';
-
---spool '_logs\Оповещения_'||to_char(systimestamp,'yyyymmdd-hhmiss')||'.csv'
---spool '_logs\Оповещения_.csv'
---spool &:v_file_name
---spool '_logs\NOTIFICATIONS_20190301-1140.csv'
-
-
---spool '_logs\1.csv'
 
 spool &4
 
--- db/other_scripts/Script_Notification_2/run.bat
+++ db/other_scripts/Script_Notification_2/run.bat
@@ -1,4 +1,4 @@
-﻿@ECHO OFF
+@ECHO OFF
 REM ����� ��������� ��� � ������ ��������� ����� � ����� ���� ������
 REM USER test
 SET USER_ID=14450
@@ -11,9 +11,9 @@ SET BEGIN_DATE=01.02.2019
 REM End Date
 SET END_DATE=10.02.2019
 REM ������: user_name/password@host:port/service_name
-SET AUTH=NDFL_UNSTABLE/NDFL_UNSTABLE@172.19.214.45:1521/orcl.aplana.local
+SET AUTH=ndfl_schema/schema_password@host:port/service_name
 REM ����� ��������� ���� � ����� ORACLE_HOME\BIN
-SET ORA_BIN=C:\app\client\ghy\product\12.2.0\client_1\bin
+SET ORA_BIN=c:\app\client\user\product\12.2.0\client_1\BIN
 SET LOG_DIR=_logs
 SET FILE_NAME=NOTIFICATION_201903-1140.csv
 SET nls_lang=AMERICAN_AMERICA.AL32UTF8
