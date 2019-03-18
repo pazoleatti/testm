b4e1a37b52e1152b33069290f229242753859299
Andrey Mandzyak <amandzyak@it.ru>
2019-03-18 14:50:10 +0500

    https://jira.aplana.com/browse/SBRFNDFL-6954 - Реализовать скрипт добавления в БД новых ОКТМО

--- database/database-3.5/05_exec_scripts.sql
+++ database/database-3.5/05_exec_scripts.sql
@@ -1,6 +1,9 @@
 set serveroutput on;
-
+spool &1
 prompt update-ndfl_person_income
 @@scripts/update-ndfl_person_income.sql;
+spool &2
+prompt merge_ref_book_oktmo
+@@scripts/merge_ref_book_oktmo.sql;
 
 exit;
