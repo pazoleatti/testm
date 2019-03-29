abf4a74ad0dc3225ea6b50946811ab9e1605e9cc
Andrey Mandzyak <amandzyak@it.ru>
2019-03-29 12:28:22 +0500

    https://jira.aplana.com/browse/SBRFNDFL-7117 - Выявить новые элементы справочника ОКТМО путем сравнения со справочником УН и загрузить недостающие ОКТМО в ФП НДФЛ

--- db/other_scripts/compare_REF_BOOK_OKTMO/compare_REF_BOOK_OKTMO.sql
+++ db/other_scripts/compare_REF_BOOK_OKTMO/compare_REF_BOOK_OKTMO.sql
@@ -14,16 +14,18 @@ select 'Строки, которых нет в эталоне' from dual;
 select '"'||to_char(b.id)||'";"'||b.code||'";"'||b.name||'";"'||to_char(b.version,'dd.mm.yyyy')||'";"'||to_char(b.status)||'";"'||to_char(b.record_id)||'";"'||to_char(b.razd)||'"'
 from &3..REF_BOOK_OKTMO a
 full outer join &2..REF_BOOK_OKTMO b 
-on a.code=b.code and a.name=b.name and a.version=b.version and a.status=b.status
+on a.code=b.code 
+-- and a.name=b.name and a.version=b.version and a.status=b.status
 where a.id is null
 order by b.code, b.name;
 
 select 'Строки, которые есть в эталоне, но отсутствуют в схеме' from dual; 
 
-select '"'||to_char(a.id)||'";"'||a.code||'";"'||a.name||'";"'||to_char(a.version,'dd.mm.yyyy')||'";"'||to_char(a.status)||'";"'||to_char(a.record_id)||'"'
+select '"'||to_char(a.id)||'";"'||a.code||'";"'||a.name||'";"'||to_char(a.version,'dd.mm.yyyy')||'";"'||to_char(a.status)||'";"'||to_char(a.record_id)||'";"'||to_char(a.section)||'"'
 from &3..REF_BOOK_OKTMO a
 full outer join &2..REF_BOOK_OKTMO b 
-on a.code=b.code and a.name=b.name and a.version=b.version and a.status=b.status
+on a.code=b.code
+-- and a.name=b.name and a.version=b.version and a.status=b.status
 where b.id is null
 order by a.code, a.name;
 
