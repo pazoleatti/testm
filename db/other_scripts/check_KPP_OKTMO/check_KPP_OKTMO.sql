9ab282064070ac768393a9dfb672c5d07311fedb
Andrey Mandzyak <amandzyak@it.ru>
2019-04-03 09:17:45 +0500

    https://jira.aplana.com/browse/SBRFNDFL-7147 - Написать скрипт проверки наличия КПП-ОКТМО, указанных в ПНФ, в настройках подразделений

--- db/other_scripts/check_KPP_OKTMO/check_KPP_OKTMO.sql
+++ db/other_scripts/check_KPP_OKTMO/check_KPP_OKTMO.sql
@@ -21,33 +21,37 @@ select
 distinct 
 kpp, oktmo, declaration_data_id, dep_name, rep_name, exists_flag
 from (
-select dd.kpp, dd.oktmo, dd.id as declaration_data_id, drp.department_id, d.name as dep_name, rp.id, to_char(tp.year)||' '||rp.name as rep_name, det.detail_id, det.start_date, det.end_date
+select npi.kpp, npi.oktmo, dd.id as declaration_data_id, drp.department_id, d.name as dep_name, rp.id, to_char(tp.year)||' '||rp.name as rep_name, det.detail_id, det.start_date, det.end_date
 ,case when det.detail_id is null then 'отсутствует' else 'неактуальна' end exists_flag
 from 
-DECLARATION_DATA dd
-,DEPARTMENT_REPORT_PERIOD drp
+ndfl_person_income npi,
+ndfl_person np,
+declaration_data dd
+,department_report_period drp
 ,department d
-,REPORT_PERIOD rp
+,report_period rp
 ,tax_period tp
 ,(
 select rbnd.id as detail_id, rbnd.record_id, rbnd.kpp, rbo.code as oktmo, rbnd.version as start_date
 ,(select min(rbnd2.version) from ref_book_ndfl_detail rbnd2 where rbnd2.status <> -1 and rbnd2.record_id = rbnd.record_id and rbnd2.version>rbnd.version) as end_date
 from 
 ref_book_ndfl_detail rbnd
-,REF_BOOK_OKTMO rbo
+,ref_book_oktmo rbo
 where
 rbo.id=rbnd.oktmo
 and rbnd.status=0
 ) det
 where
-dd.kpp is not null and 
-dd.oktmo is not null
+npi.kpp  is not null and 
+npi.oktmo is not null
+and np.id = npi.ndfl_person_id
+and dd.id = np.declaration_data_id
 and drp.id = dd.department_report_period_id
 and d.id = drp.department_id
 and rp.id=drp.report_period_id
 and tp.id=rp.tax_period_id
-and det.kpp(+) = dd.kpp
-and det.oktmo(+) = dd.oktmo
+and det.kpp(+) = npi.kpp
+and det.oktmo(+) = npi.oktmo
 )
 where 
 (detail_id is null)
