46ac7a9f2576deb2fff9a19a6dfe43ffa4e3bee1
ytrofimov <Yuriy.Trofimov@aplana.com>
2019-04-18 14:26:25 +0300

    https://jira.aplana.com/browse/SBRFNDFL-7397 Триггер на отмену установки блокировки источников при консолидации

--- database/database-3.5-hotfix_1804_1/run.bat
+++ database/database-3.5-hotfix_1804_1/run.bat
@@ -5,17 +5,21 @@ SET AUTH=ndfl_schema/schema_password@host:port/service_name
 REM ����� ��������� ���� � ����� ORACLE_HOME\BIN
 SET ORA_BIN=c:\app\client\user\product\12.2.0\client_1\BIN
 SET LOG_DIR=_logs
+SET BAD_DIR=_bad
 SET nls_lang=AMERICAN_AMERICA.AL32UTF8
 
 ECHO ## DB: %AUTH%
 
 SET MODE=1
 MKDIR %LOG_DIR%
+MKDIR %BAD_DIR%
 
 DEL /s /q /f %LOG_DIR%\*.txt
+DEL /s /q /f %LOG_DIR%\*.csv
+DEL /s /q /f %BAD_DIR%\*.*
 
-ECHO ## create_trigger
-"%ORA_BIN%\sqlplus" %AUTH% @"create_trigger.sql" %LOG_DIR%/create_trigger.txt
+ECHO ## templates
+"%ORA_BIN%\sqlplus" %AUTH% @"02_templates.sql" %LOG_DIR%/02_templates.txt %ORA_BIN% %AUTH% %LOG_DIR% %BAD_DIR%
 
 rem �������� ������ ���������� �������������������
 PAUSE
