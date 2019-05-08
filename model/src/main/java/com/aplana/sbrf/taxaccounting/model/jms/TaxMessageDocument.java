fd91e2db416945204be843be66ab8f93522ada73
ytrofimov <Yuriy.Trofimov@aplana.com>
2019-05-08 15:16:08 +0300

    https://jira.aplana.com/browse/SBRFNDFL-7164 Реализовать отправку файлов отчетности сообщений в ЭДО

---
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/jms/TaxMessageDocument.java
@@ -0,0 +1,38 @@
+package com.aplana.sbrf.taxaccounting.model.jms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+/**
+ * Сообщение для отправки в ЭДО
+ */
+@Getter
+@Setter
+@XmlAccessorType(XmlAccessType.NONE)
+@XmlRootElement(name = "TaxMessageDocument")
+public class TaxMessageDocument extends BaseMessage {
+
+    @XmlElement(name = "UUID")
+    private String uuid;
+
+    @XmlElement(name = "datetime")
+    private Date dateTime;
+
+    @XmlElement(name = "source")
+    private Integer source;
+
+    @XmlElement(name = "destination")
+    private Integer destination;
+
+    @XmlElement(name = "login")
+    private String login;
+
+    @XmlElement(name = "filename")
+    private String filename;
+}
--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/refbook/RefBookFormType.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/refbook/RefBookFormType.java
@@ -1,29 +1,37 @@
 package com.aplana.sbrf.taxaccounting.model.refbook;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 /**
  * Виды налоговых форм
  *
  * @author dloshkarev
  */
+@Getter
+@Setter
+@NoArgsConstructor
 public class RefBookFormType extends RefBookSimple<Long> {
+
+    //РНУ_НДФЛ
+    public final static RefBookFormType NDFL_PRIMARY = new RefBookFormType(2);
+
+    //2-НДФЛ (1)
+    public final static RefBookFormType NDFL_2_1 = new RefBookFormType(3);
+
+    //2-НДФЛ (2)
+    public final static RefBookFormType NDFL_2_2 = new RefBookFormType(4);
+
+    //6-НДФЛ
+    public final static RefBookFormType NDFL_6 = new RefBookFormType(5);
+
+    public RefBookFormType(long id) {
+        super(id);
+    }
 
     private String name;
 
     private String code;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
 }
