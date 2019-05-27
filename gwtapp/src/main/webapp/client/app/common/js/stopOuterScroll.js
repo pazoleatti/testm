717b22f157a6c49dd045df985c0365b79d861fa7
Denis Novikov <Denis.Novikov@aplana.com>
2019-05-27 13:18:00 +0500

    https://jira.aplana.com/browse/SBRFNDFL-7670 Скроллы на страницах с таблицами

--- gwtapp/src/main/webapp/client/app/common/js/stopOuterScroll.js
+++ gwtapp/src/main/webapp/client/app/common/js/stopOuterScroll.js
@@ -5,12 +5,7 @@ var stopOuterScroll = function (element) {
     element[0].addEventListener('wheel', wheelHandler);
 
     function wheelHandler(event) {
-        var $this = $(this);
-        var scrollTop = this.scrollTop;
-        var scrollHeight = this.scrollHeight;
-        var height = this.clientHeight;
         var delta = event.deltaY;
-        var up = delta < 0;
 
         var prevent = function () {
             event.stopPropagation();
@@ -18,13 +13,12 @@ var stopOuterScroll = function (element) {
             event.cancelBubble = true;  // IE events
             event.returnValue = false;  // IE events
         };
-        if (!up && scrollHeight - height - scrollTop === 0) {
-            $this.scrollTop(scrollHeight);
-            return prevent();
-        } else if (up && scrollTop === 0) {
-            $this.scrollTop(0);
-            return prevent();
-        }
+
+        event.currentTarget.scrollTop += sign(delta) * event.currentTarget.offsetHeight * 0.15;
+        return prevent();
+    }
+    function sign(x) {
+        return ((x > 0) - (x < 0)) || +x;
     }
 };
 
