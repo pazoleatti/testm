757e41d95d273cd442e1645a36a090a23c1f7f2e
Svetlana Kononova <SKononova@aplana.com>
2019-07-16 11:11:01 +0300

    Актуализация патча 3.8

--- database/install.bat
+++ database/install.bat
@@ -24,7 +24,7 @@ cd PATCHES
 
 "%ORA_BIN%\sqlplus" %DBA_AUTH% @"database-3.7.1/patch_03_007_01.sql" %NDFL_USR% ../_log/patch_03_007_01.log %NSI_USR% %ORA_BIN% %DBA_AUTH% 
 
-"%ORA_BIN%\sqlplus" %DBA_AUTH% @"database-3.8/patch_03_008_00.sql" %NDFL_USR% ../_log/patch_03_008_00.log %NSI_USR% %ORA_BIN% %DBA_AUTH% 
+"%ORA_BIN%\sqlplus" %DBA_AUTH% @"database-3.8/patch_03_008_00.sql" %NDFL_USR% ../_log/patch_03_008_00.log %NSI_USR% %ORA_BIN% %DBA_AUTH% %TAXREC_USR%
 
 
 cd ..
