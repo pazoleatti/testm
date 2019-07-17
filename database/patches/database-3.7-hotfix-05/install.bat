8a8d37d1fe50f746f230138fd84f283b9ed65430
Svetlana Kononova <SKononova@aplana.com>
2019-07-17 12:06:37 +0300

    Шаблон для хотфикса 3.7-hotfix-05

---
+++ database/patches/database-3.7-hotfix-05/install.bat
@@ -0,0 +1,24 @@
+@echo off
+CALL settings.bat
+
+echo on
+
+IF EXIST _log\create_version_history.log DEL /s /q /f _log\create_version_history.log
+IF EXIST _log\patch_03_007_hotfix_05.log DEL /s /q /f _log\patch_03_007_hotfix_05.log
+
+
+IF NOT EXIST _log MKDIR _log
+IF NOT EXIST _bad MKDIR _bad
+DEL /s /q /f _log\*.*
+DEL /s /q /f _bad\*.*
+
+SET nls_lang=AMERICAN_AMERICA.AL32UTF8
+
+"%ORA_BIN%\sqlplus" %DBA_AUTH% @"tech/create_version_history.sql" %NDFL_USR% _log/create_version_history.log
+
+
+
+"%ORA_BIN%\sqlplus" %DBA_AUTH% @"patch_03_007_hotfix_05.sql" %NDFL_USR% _log/patch_03_007_hotfix_05.log %ORA_BIN% %DBA_AUTH% 
+
+"%ORA_BIN%\sqlplus" %DBA_AUTH% @"tech/result_log.sql" %NDFL_USR% _log/result_log.log
+pause
