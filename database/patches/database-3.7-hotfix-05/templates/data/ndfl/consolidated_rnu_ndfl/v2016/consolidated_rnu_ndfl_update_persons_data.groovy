acf0911e51a831c1e85f26822637708ae5cd00ac
ytrofimov <Yuriy.Trofimov@aplana.com>
2019-07-17 13:20:19 +0300

    https://jira.aplana.com/browse/SBRFNDFL-8093 Оптимизация операции обновления ФЛ в ПНФ и КНФ

--- database/patches/database-3.7-hotfix-05/templates/data/ndfl/consolidated_rnu_ndfl/v2016/consolidated_rnu_ndfl_update_persons_data.groovy
+++ database/patches/database-3.7-hotfix-05/templates/data/ndfl/consolidated_rnu_ndfl/v2016/consolidated_rnu_ndfl_update_persons_data.groovy
@@ -1,6 +1,8 @@
 package form_template.ndfl.consolidated_rnu_ndfl.v2016
 
 import com.aplana.sbrf.taxaccounting.AbstractScriptClass
+import com.aplana.sbrf.taxaccounting.dao.impl.NdflPersonDaoImpl
+import com.aplana.sbrf.taxaccounting.dao.impl.util.SqlUtils
 import com.aplana.sbrf.taxaccounting.model.*
 import com.aplana.sbrf.taxaccounting.model.log.LogEntry
 import com.aplana.sbrf.taxaccounting.model.log.LogLevel
@@ -18,6 +20,12 @@ import com.aplana.sbrf.taxaccounting.script.service.ReportPeriodService
 import com.aplana.sbrf.taxaccounting.script.service.util.ScriptUtils
 import groovy.transform.TypeChecked
 import groovy.transform.TypeCheckingMode
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
+
+import java.sql.ResultSet
+import java.sql.SQLException
 
 new UpdatePersonsData(this).run()
 
@@ -31,6 +39,7 @@ class UpdatePersonsData extends AbstractScriptClass {
     DepartmentService departmentService
     RefBookFactory refBookFactory
     ReportPeriodService reportPeriodService
+    NamedParameterJdbcTemplate namedParameterJdbcTemplate
 
     // Кэш провайдеров
     Map<Long, RefBookDataProvider> providerCache = [:]
@@ -58,6 +67,7 @@ class UpdatePersonsData extends AbstractScriptClass {
         if (scriptClass.getBinding().hasVariable("reportPeriodService")) {
             this.reportPeriodService = (ReportPeriodService) scriptClass.getProperty("reportPeriodService")
         }
+        this.namedParameterJdbcTemplate = (NamedParameterJdbcTemplate) scriptClass.getProperty("namedParameterJdbcTemplate")
     }
 
     @Override
@@ -87,7 +97,58 @@ class UpdatePersonsData extends AbstractScriptClass {
 
     void doUpdate() {
         List<NdflPerson> declarationDataPersonList = ndflPersonService.findNdflPersonWithOperations(declarationData.id)
-        List<NdflPerson> refBookPersonList = ndflPersonService.fetchRefBookPersonsAsNdflPerson(declarationData.id, new Date())
+        String sql = "SELECT rbp.id, rbp.record_id AS inp, rbp.last_name, rbp.first_name, rbp.middle_name, rbp.birth_date, rbc.code AS citizenship, rbp.inn, rbp.inn_foreign, rbts.code AS status, \n" +
+                "rbp.snils, rbdt.code AS id_doc_type, rbid.doc_number, rbp.region_code, rbp.postal_code, rbp.district, rbp.city, rbp.locality, rbp.street, rbp.house, rbp.build, rbp.appartment,\n" +
+                "rbp.address_foreign as address, rbc2.code AS country_code \n" +
+                "FROM ndfl_person np \n" +
+                "join ref_book_person rbp_d ON rbp_d.id = np.person_id \n" +
+                "join ref_book_person rbp on rbp.record_id=rbp_d.record_id and (rbp.start_date <= :date and (rbp.end_date >= :date or rbp.end_date is null)) AND rbp.record_id = rbp.old_id\n" +
+                "LEFT JOIN ref_book_country rbc ON rbp.citizenship = rbc.id AND rbc.status = 0 \n" +
+                "LEFT JOIN ref_book_country rbc2 ON rbp.country_id = rbc2.id AND rbc2.status = 0 \n" +
+                "LEFT JOIN ref_book_taxpayer_state rbts ON rbp.taxpayer_state = rbts.id AND rbts.status = 0 \n" +
+                "LEFT JOIN ref_book_id_tax_payer ritp ON ritp.person_id = rbp.id \n" +
+                "LEFT JOIN ref_book_id_doc rbid ON rbid.id = rbp.report_doc \n" +
+                "LEFT JOIN ref_book_doc_type rbdt ON rbid.doc_id = rbdt.id AND rbdt.status = 0 \n" +
+                "WHERE np.declaration_data_id = :dd"
+
+        MapSqlParameterSource params = new MapSqlParameterSource("date", new java.util.Date())
+        params.addValue("dd", declarationData.id)
+
+        List<NdflPerson> refBookPersonList = namedParameterJdbcTemplate.query(sql, params, new RowMapper<NdflPerson>() {
+            @Override
+            NdflPerson mapRow(ResultSet rs, int rowNum) throws SQLException {
+                NdflPerson person = new NdflPerson();
+
+                person.setPersonId(SqlUtils.getLong(rs, "id"));
+                person.setRecordId(SqlUtils.getLong(rs, "inp"));
+                ;
+                person.setInp(String.valueOf(SqlUtils.getLong(rs, "inp")));
+                person.setLastName(rs.getString("last_name"));
+                person.setFirstName(rs.getString("first_name"));
+                person.setMiddleName(rs.getString("middle_name"));
+                person.setBirthDay(rs.getDate("birth_date"));
+                person.setCitizenship(rs.getString("citizenship"));
+                person.setInnNp(rs.getString("inn"));
+                person.setInnForeign(rs.getString("inn_foreign"));
+                person.setStatus(rs.getString("status"));
+                person.setSnils(rs.getString("snils"));
+                person.setIdDocType(rs.getString("id_doc_type"));
+                person.setIdDocNumber(rs.getString("doc_number"));
+                person.setRegionCode(rs.getString("region_code"));
+                person.setPostIndex(rs.getString("postal_code"));
+                person.setArea(rs.getString("district"));
+                person.setCity(rs.getString("city"));
+                person.setLocality(rs.getString("locality"));
+                person.setStreet(rs.getString("street"));
+                person.setHouse(rs.getString("house"));
+                person.setBuilding(rs.getString("build"));
+                person.setFlat(rs.getString("appartment"));
+                person.setCountryCode(rs.getString("country_code"));
+                person.setAddress(rs.getString("address"));
+
+                return person;
+            }
+        })
         List<NdflPerson> toUpdatePersons = []
         List<LogEntry> logs = []
         Map<Long, NdflPerson> personIdAssociatedRefBookPersons = [:]
