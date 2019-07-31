641ed7eec4c2001967cba99efe183218704a454e
ADudenko <ADudenko@aplana.com>
2019-07-31 19:12:42 +0400

    https://jira.aplana.com/browse/SBRFNDFL-7097 рефакторинг модельных классов JMS сообщений

--- model/src/main/java/com/aplana/sbrf/taxaccounting/model/jms/BaseMessage.java
+++ model/src/main/java/com/aplana/sbrf/taxaccounting/model/jms/BaseMessage.java
@@ -6,6 +6,8 @@ import lombok.Setter;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import java.util.Date;
 
 /**
  * Базовый класс для сообщений JMS
@@ -18,4 +20,15 @@ public abstract class BaseMessage {
     @XmlAttribute
     protected String formatVersion;
 
+    @XmlElement(name = "UUID")
+    private String uuid;
+
+    @XmlElement(name = "datetime")
+    private Date dateTime = new Date();
+
+    @XmlElement(name = "source")
+    private Integer source;
+
+    @XmlElement(name = "destination")
+    private Integer destination;
 }
