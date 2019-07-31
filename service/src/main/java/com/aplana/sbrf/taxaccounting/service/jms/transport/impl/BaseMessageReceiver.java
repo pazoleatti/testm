5812edbbf940583e7152d5624e9afe612c105f90
ADudenko <ADudenko@aplana.com>
2019-07-31 19:07:40 +0400

    удалил контроллер, который использовался для тестирования MQ + временные изменения в слушателе входящей очереди MQ

--- service/src/main/java/com/aplana/sbrf/taxaccounting/service/jms/transport/impl/BaseMessageReceiver.java
+++ service/src/main/java/com/aplana/sbrf/taxaccounting/service/jms/transport/impl/BaseMessageReceiver.java
@@ -1,12 +1,22 @@
 package com.aplana.sbrf.taxaccounting.service.jms.transport.impl;
 
+import com.aplana.sbrf.taxaccounting.model.jms.TaxMessageReceipt;
+import com.aplana.sbrf.taxaccounting.service.TransactionHelper;
+import com.aplana.sbrf.taxaccounting.service.TransactionLogic;
 import com.aplana.sbrf.taxaccounting.service.jms.JmsBaseConfig;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.config.JmsListenerEndpointRegistry;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.StringReader;
 
 /**
  * Получатель сообщений.
@@ -15,12 +25,34 @@ import org.springframework.stereotype.Component;
 @Component
 public class BaseMessageReceiver {
 
+    private static final Log LOG = LogFactory.getLog(BaseMessageReceiver.class);
+
     @Autowired
     private JmsListenerEndpointRegistry jmsListenerEndpointRegistry;
 
+    @Autowired
+    private TransactionHelper transactionHelper;
+
     @JmsListener(destination = JmsBaseConfig.TO_NDFL_QUEUE, id="messageReceiver", containerFactory = "jmsListenerContainerFactory")
     public void handleMessage(Message<String> message) {
-        System.out.println("Raw message: " + message.getPayload());
+        final String messageContent = message.getPayload();
+        transactionHelper.executeInNewTransaction(new TransactionLogic<Object>() {
+            @Override
+            public Object execute() {
+                try {
+                    JAXBContext jaxbContext = JAXBContext.newInstance(TaxMessageReceipt.class);
+                    Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+                    StringReader messageReader = new StringReader(messageContent);
+                    TaxMessageReceipt taxMessageReceipt = (TaxMessageReceipt) jaxbUnmarshaller.unmarshal(messageReader);
+                } catch (JAXBException e) {
+                    LOG.warn("Ошибка парсинга XML сообщения: " + messageContent, e);
+                }
+                return null;
+            }
+        });
+
+        System.out.println("Raw message: " + messageContent);
         System.out.println("Headers: " + message.getHeaders());
     }
 
