c873667d4b694037bb601980dc49dcd9ed3870a1
Svetlana Kononova <SKononova@aplana.com>
2019-08-01 12:00:08 +0300

    Изменение патча 3.9

--- database/install.bat
+++ database/install.bat
@@ -3,17 +3,10 @@ CALL settings.bat
 
 echo on
 
-IF EXIST _log\create_version_history.log DEL /s /q /f _log\create_version_history.log
-IF EXIST _log\patch_03_007_00.log DEL /s /q /f _log\patch_03_007_00.log
-IF EXIST _log\patch_03_007_01.log DEL /s /q /f _log\patch_03_007_01.log
-IF EXIST _log\patch_03_008_00.log DEL /s /q /f _log\patch_03_008_00.log
-IF EXIST _log\patch_03_009_00.log DEL /s /q /f _log\patch_03_009_00.log
-
-
 IF NOT EXIST _log MKDIR _log
 IF NOT EXIST _bad MKDIR _bad
-DEL /s /q /f _log\*.*
-DEL /s /q /f _bad\*.*
+IF EXIST _log\*.* DEL /s /q /f _log\*.*
+IF EXIST _bad\*.* DEL /s /q /f _bad\*.*
 
 SET nls_lang=AMERICAN_AMERICA.AL32UTF8
 
@@ -25,7 +18,7 @@ cd PATCHES
 
 "%ORA_BIN%\sqlplus" %DBA_AUTH% @"database-3.7.1/patch_03_007_01.sql" %NDFL_USR% ../_log/patch_03_007_01.log %NSI_USR% %ORA_BIN% %DBA_AUTH% 
 
-"%ORA_BIN%\sqlplus" %DBA_AUTH% @"database-3.8/patch_03_008_00.sql" %NDFL_USR% ../_log/patch_03_008_00.log %NSI_USR% %ORA_BIN% %DBA_AUTH% %TAXREC_USR%
+"%ORA_BIN%\sqlplus" %DBA_AUTH% @"database-3.8/patch_03_008_01.sql" %NDFL_USR% ../_log/patch_03_008_01.log %NSI_USR% %ORA_BIN% %DBA_AUTH% %TAXREC_USR%
 
 "%ORA_BIN%\sqlplus" %DBA_AUTH% @"database-3.9/patch_03_009_00.sql" %NDFL_USR% ../_log/patch_03_009_00.log %NSI_USR% %ORA_BIN% %DBA_AUTH% %TAXREC_USR%
 
