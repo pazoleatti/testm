34d23a9124ef1749d17104510d309a6dd5a01536
Andrey Voynov <Andrey.Voynov@aplana.com>
2019-09-30 15:48:45 +0300

    https://jira.aplana.com/browse/SBRFNDFL-8479 Реализовать определение номера справки и номера корректировки для справок 2-НДФЛ сформированных в корректировочном периоде

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/NdflReferenceDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/NdflReferenceDaoImpl.java
@@ -1,11 +1,14 @@
 package com.aplana.sbrf.taxaccounting.dao.impl;
 
 import com.aplana.sbrf.taxaccounting.dao.refbook.NdflReferenceDao;
+import com.aplana.sbrf.taxaccounting.model.refbook.NumFor2Ndfl;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -29,5 +32,55 @@ public class NdflReferenceDaoImpl extends AbstractDao implements NdflReferenceDa
             }
         });
         return uniqueRecordIds.size();
+    }
+
+    public Integer getNextSprNum(Integer year) {
+        Integer sequenceCount = getJdbcTemplate().queryForObject("select count(*) from user_sequences where sequence_name='SEQ_NDFL_REFERENCES_" + year + "'", Integer.class);
+        if (sequenceCount == 0) {
+            createSequence(year);
+        }
+        return getJdbcTemplate().queryForObject("select SEQ_NDFL_REFERENCES_" + year + ".nextval from dual", Integer.class);
+
+    }
+
+    private void createSequence(Integer year) {
+        getJdbcTemplate().execute("CREATE SEQUENCE SEQ_NDFL_REFERENCES_" + year
+                + " MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER NOCYCLE");
+    }
+
+    @Override
+    public List<NumFor2Ndfl> getCorrSprNum(Long personId, int year, String kpp, String oktmo, int declarationTypeId) {
+        return getJdbcTemplate().query("select nr.num, nr.correction_num " +
+                        "from ndfl_references nr " +
+                        "left join declaration_data dd on nr.declaration_data_id = dd.id " +
+                        //Год отчетного периода
+                        "left join department_report_period drp on dd.department_report_period_id = drp.id " +
+                        "left join report_period rp on drp.report_period_id = rp.id " +
+                        "left join tax_period tp on rp.tax_period_id = tp.id " +
+                        //Физлицо
+                        "left join ref_book_person p on nr.person_id = p.id " +
+                        //Вид формы
+                        "left join declaration_template dtemp on dd.declaration_template_id = dtemp.id " +
+                        "left join declaration_type dtype on dtemp.declaration_type_id = dtype.id " +
+                        "where p.record_id = ? " + // Физическое лицо
+                        "and tp.year = ? " + // Форма.Период.Год
+                        "and dd.kpp = ? " + // Форма.КПП
+                        "and dd.oktmo = ? " + // Форма."ОКТМО"
+                        "and dtype.id = ? ", // Форма."Макет формы"."Вид формы"
+                new Object[]{personId, year, kpp, oktmo, declarationTypeId},
+                new _2NdflNumRowMapper());
+    }
+
+    private static final class _2NdflNumRowMapper implements RowMapper<NumFor2Ndfl> {
+        @Override
+        public NumFor2Ndfl mapRow(ResultSet rs, int index) throws SQLException {
+
+            NumFor2Ndfl numFor2Ndfl = new NumFor2Ndfl();
+
+            numFor2Ndfl.setSprNum(rs.getInt("num"));
+            numFor2Ndfl.setCorrNum(rs.getInt("correction_num"));
+
+            return numFor2Ndfl;
+        }
     }
 }
--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/refbook/NdflReferenceDao.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/refbook/NdflReferenceDao.java
@@ -1,5 +1,7 @@
 package com.aplana.sbrf.taxaccounting.dao.refbook;
 
+import com.aplana.sbrf.taxaccounting.model.refbook.NumFor2Ndfl;
+
 import java.util.List;
 
 public interface NdflReferenceDao {
@@ -11,4 +13,23 @@ public interface NdflReferenceDao {
      * @param value
      */
     int updateField(List<Long> uniqueRecordIds, String alias, String value);
+
+    /**
+     * Получение номера справки из сиквенса по отчетному году. В случае отсутствия сиквенса, его создание.
+     * @param year год отчетного периода, по которому будет выбран/создан сиквенс
+     * @return уникальный номер справки
+     */
+    Integer getNextSprNum(Integer year);
+
+    /**
+     * Получение номера справки для формирования 2-НДФЛ
+     * @param personId физлицо
+     * @param year ОНФ."Период"."Год"
+     * @param kpp ОНФ.КПП
+     * @param oktmo ОНФ.ОКТМО
+     * @param declarationTypeId Форма."Макет формы"."Вид формы"
+     * @return список номеров справок и номеров корректировок, подходящих под параметры
+     *
+     */
+    List<NumFor2Ndfl> getCorrSprNum(Long personId, int year, String kpp, String oktmo, int declarationTypeId);
 }
