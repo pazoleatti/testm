f0803eb8b377fa71d413750355610ee87f44cddf
ytrofimov <Yuriy.Trofimov@aplana.com>
2019-10-01 09:30:15 +0300

    Revert: https://jira.aplana.com/browse/SBRFNDFL-8700 Выпилить из 3.10 функционал по периодам для видов форм (Временно отключил некоторые тесты)

--- dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/ReportPeriodDaoTest.java
+++ dao/src/test/java/com/aplana/sbrf/taxaccounting/dao/impl/ReportPeriodDaoTest.java
@@ -8,7 +8,6 @@ import com.aplana.sbrf.taxaccounting.model.TaxType;
 import com.aplana.sbrf.taxaccounting.model.exception.DaoException;
 import com.aplana.sbrf.taxaccounting.model.result.ReportPeriodResult;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,8 +31,6 @@ import static org.junit.Assert.assertTrue;
 @ContextConfiguration({"ReportPeriodDaoTest.xml"})
 @Transactional
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-//TODO включить в версии 3.10.1
-@Ignore
 public class ReportPeriodDaoTest {
 
     @Autowired
