89c6db3b449820ca0d4c10e6717bbfb31862f499
Andrey Voynov <Andrey.Voynov@aplana.com>
2019-10-09 12:08:05 +0300

    https://jira.aplana.com/browse/SBRFNDFL-8762 В gui 2-НДФЛ не увеличивается номер корректировки для форм в корректировочном периоде

--- src/main/resources/form_template/ndfl/report_2ndfl_1/v2016/report_2ndfl.groovy
+++ src/main/resources/form_template/ndfl/report_2ndfl_1/v2016/report_2ndfl.groovy
@@ -1134,6 +1134,8 @@ class Report2Ndfl extends AbstractScriptClass {
             ScriptUtils.checkInterrupted()
             List<DeclarationData> existingDeclarations = declarationService.findAllByTypeIdAndReportPeriodIdAndKppAndOktmo(
                     declarationTemplate.type.id, departmentReportPeriod.reportPeriod.id, departmentConfig.kpp, departmentConfig.oktmo.code)
+            def lastSentForms = existingDeclarations.findAll() { it.docStateId != RefBookDocState.NOT_SENT.id }
+            def lastSentForm = lastSentForms.max({ it.correctionNum })
             declarationData = new DeclarationData()
             declarationData.declarationTemplateId = declarationTemplate.id
             declarationData.kpp = departmentConfig.kpp
@@ -1142,6 +1144,11 @@ class Report2Ndfl extends AbstractScriptClass {
             declarationData.adjustNegativeValues = reportFormsCreationParams.adjustNegativeValues
             declarationData.taxRefundReflectionMode = reportFormsCreationParams.taxRefundReflectionMode
             declarationData.docStateId = RefBookDocState.NOT_SENT.id
+            declarationData.correctionNum = lastSentForm
+                    ? (lastSentForm.docStateId in [RefBookDocState.REJECTED.id, RefBookDocState.ERROR.id]
+                            ? lastSentForm.correctionNum
+                            : lastSentForm.correctionNum + 1)
+                    : 0
             declarationData.departmentReportPeriodId = departmentReportPeriod.id
             declarationData.reportPeriodId = departmentReportPeriod.reportPeriod.id
             declarationData.departmentId = departmentReportPeriod.departmentId
