586220acaf9c6ed66c98e923d436dd599b366626
Svetlana Kononova <SKononova@aplana.com>
2019-10-20 16:11:59 +0300

    Хотфикс03

--- database/patches/database-3.9.2.3-hotfix-03/02_dml_update.sql
+++ database/patches/database-3.9.2.3-hotfix-03/02_dml_update.sql
@@ -1,3 +1,7 @@
+alter trigger REF_BOOK_ID_DOC_BEFORE_INS_UPD disable;
+/
+alter trigger REF_BOOK_PERSON_BEFORE_INS_UPD disable;
+/
 
 declare 
   v_task_name varchar2(128):='insert_update_delete block #1 - update NDFL_PERSON';  
@@ -51,6 +55,11 @@ EXCEPTION
 end;
 /
 COMMIT;
+
+alter trigger REF_BOOK_ID_DOC_BEFORE_INS_UPD enable;
+/
+alter trigger REF_BOOK_PERSON_BEFORE_INS_UPD enable;
+/
 
 
 
--- database/patches/database-3.9.2.3-hotfix-03/03_triggers_indexes.sql
+++ database/patches/database-3.9.2.3-hotfix-03/03_triggers_indexes.sql
@@ -22,11 +22,13 @@ commit;
 
 DECLARE
 	v_run_condition number(1);
-	v_task_name varchar2(128):='indexes block #1 - idx_ref_book_id_doc_srch_doc';  
+	v_task_name varchar2(128):='indexes block #1 - IDX_NDFL_PERSON_SEARCH_DOC';  
 BEGIN
-	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOK_ID_DOC_SRCH_DOC';
+	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_NDFL_PERSON_SEARCH_DOC';
 	IF v_run_condition=1 THEN
-	        execute immediate 'create index idx_ref_book_id_doc_srch_doc on ref_book_id_doc (search_doc_number asc)';
+	        execute immediate '		create index idx_ndfl_person_search_doc on ndfl_person 
+			(declaration_data_id asc, person_id asc, id, search_doc_number asc) 
+		PCTFREE 50 INITRANS 50 COMPUTE STATISTICS';
 		dbms_output.put_line(v_task_name||'[INFO ]:'||' Success');
 	ELSE
 		dbms_output.put_line(v_task_name||'[WARNING]:'||' changes had already been implemented');
@@ -41,11 +43,14 @@ commit;
 
 DECLARE
 	v_run_condition number(1);
-	v_task_name varchar2(128):='indexes block #2 - idx_ref_book_person_srch_fio';  
+	v_task_name varchar2(128):='indexes block #2 - IDX_NDFL_PERSON_SEARCH_FIO';  
 BEGIN
-	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOK_PERSON_SRCH_FIO';
+	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_NDFL_PERSON_SEARCH_FIO';
 	IF v_run_condition=1 THEN
-	        execute immediate 'create index idx_ref_book_person_srch_fio  on ref_book_person (search_last_name asc, search_first_name asc, search_middle_name asc, birth_date asc)';
+	        execute immediate '		create index idx_ndfl_person_search_fio on ndfl_person 
+			(declaration_data_id asc, person_id asc, id, search_last_name asc, 
+			search_first_name asc, search_middle_name asc, birth_day asc) 
+		PCTFREE 50 INITRANS 50 COMPUTE STATISTICS';
 		dbms_output.put_line(v_task_name||'[INFO ]:'||' Success');
 	ELSE
 		dbms_output.put_line(v_task_name||'[WARNING]:'||' changes had already been implemented');
@@ -60,11 +65,13 @@ commit;
 
 DECLARE
 	v_run_condition number(1);
-	v_task_name varchar2(128):='indexes block #3 - idx_ref_book_person_srch_inn';  
+	v_task_name varchar2(128):='indexes block #3 - IDX_NDFL_PERSON_SEARCH_INN';  
 BEGIN
-	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOK_PERSON_SRCH_INN';
+	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_NDFL_PERSON_SEARCH_INN';
 	IF v_run_condition=1 THEN
-	        execute immediate 'create index idx_ref_book_person_srch_inn on ref_book_person (search_inn asc)';
+	        execute immediate '		create index idx_ndfl_person_search_inn on ndfl_person 
+			(declaration_data_id asc, person_id asc, id, search_inn asc) 
+		PCTFREE 50 INITRANS 50 COMPUTE STATISTICS';
 		dbms_output.put_line(v_task_name||'[INFO ]:'||' Success');
 	ELSE
 		dbms_output.put_line(v_task_name||'[WARNING]:'||' changes had already been implemented');
@@ -80,11 +87,13 @@ commit;
 
 DECLARE
 	v_run_condition number(1);
-	v_task_name varchar2(128):='indexes block #4 - idx_ref_book_person_srch_innf';  
+	v_task_name varchar2(128):='indexes block #4 - IDX_NDFL_PERSON_SEARCH_INNF';  
 BEGIN
-	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOK_PERSON_SRCH_INNF';
+	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_NDFL_PERSON_SEARCH_INNF';
 	IF v_run_condition=1 THEN
-	        execute immediate 'create index idx_ref_book_person_srch_innf on ref_book_person (search_inn_foreign asc)';
+	        execute immediate '		create index idx_ndfl_person_search_innf on ndfl_person 
+			(declaration_data_id asc, person_id asc, id, search_inn_foreign asc) 
+		PCTFREE 50 INITRANS 50 COMPUTE STATISTICS';
 		dbms_output.put_line(v_task_name||'[INFO ]:'||' Success');
 	ELSE
 		dbms_output.put_line(v_task_name||'[WARNING]:'||' changes had already been implemented');
@@ -99,11 +108,13 @@ commit;
 
 DECLARE
 	v_run_condition number(1);
-	v_task_name varchar2(128):='indexes block #5 - idx_ref_book_person_srch_snils';  
+	v_task_name varchar2(128):='indexes block #5 - IDX_NDFL_PERSON_SEARCH_SNILS';  
 BEGIN
-	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOK_PERSON_SRCH_SNILS';
+	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_NDFL_PERSON_SEARCH_SNILS';
 	IF v_run_condition=1 THEN
-	        execute immediate 'create index idx_ref_book_person_srch_snils on ref_book_person (search_snils asc)';
+	        execute immediate '		create index idx_ndfl_person_search_snils on ndfl_person 
+			(declaration_data_id, person_id, id, search_snils) 
+		PCTFREE 50 INITRANS 50 COMPUTE STATISTICS';
 		dbms_output.put_line(v_task_name||'[INFO ]:'||' Success');
 	ELSE
 		dbms_output.put_line(v_task_name||'[WARNING]:'||' changes had already been implemented');
@@ -199,7 +210,7 @@ DECLARE
 BEGIN
 	select decode(count(*),0,1,0) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOK_PERSON_SRCH_SNILS';
 	IF v_run_condition=1 THEN
-	        execute immediate 'create index idx_ref_book_person_srch_snils on ref_book_person (search_snils asc)';
+	        execute immediate 'create index IDX_REF_BOOK_PERSON_SRCH_SNILS on ref_book_person (search_snils asc, start_date asc, end_date asc, id asc, record_id asc)';
 		dbms_output.put_line(v_task_name||'[INFO ]:'||' Success');
 	ELSE
 		dbms_output.put_line(v_task_name||'[WARNING]:'||' changes had already been implemented');
