9d9dd94d52e9f4734d12e249f813ebe0bf364ffe
Svetlana Kononova <SKononova@aplana.com>
2019-10-20 19:50:15 +0300

    В хотфиксе 3 отключаются все триггеры на обновляемые таблицы (на ndfl_person - при наличии)

--- database/patches/database-3.9.2.3-hotfix-03/02_dml_update.sql
+++ database/patches/database-3.9.2.3-hotfix-03/02_dml_update.sql
@@ -1,10 +1,25 @@
+
 alter trigger REF_BOOK_ID_DOC_BEFORE_INS_UPD disable;
 /
 alter trigger REF_BOOK_PERSON_BEFORE_INS_UPD disable;
 /
 
 declare 
-  v_task_name varchar2(128):='insert_update_delete block #1 - update NDFL_PERSON';  
+	v_cnt number(10);
+begin
+	select count(*)  into v_cnt  from user_triggers where trigger_name='NDFL_PERSON_BEFORE_INS_UPD' and status='ENABLED';
+	if (v_cnt)>0 then
+	   execute immediate 'alter trigger NDFL_PERSON_BEFORE_INS_UPD disable';
+	   dbms_output.put_line('Triggers disabled');
+	end if;	
+EXCEPTION
+  when OTHERS then 
+    dbms_output.put_line('Triggers error[FATAL]:'||sqlerrm);		
+end;
+/
+
+declare 
+	  v_task_name varchar2(128):='insert_update_delete block #1 - update NDFL_PERSON';  
 begin
 	update ndfl_person set search_doc_number=regexp_replace(id_doc_number,'[^0-9A-Za-zА-Яа-я]',''),
 		search_LAST_NAME = replace(nvl(last_name,'empty'),' ',''),
