c99f4fb4d44490d536b485dd145000fd3e3daff5
Svetlana Kononova <SKononova@aplana.com>
2019-10-21 14:24:06 +0300

    В хотфиксе 7 остается только пересборка индексов

--- database/patches/database-3.9.2.3-hotfix-08/patch_03_009_02_03_hotfix_08.sql
+++ database/patches/database-3.9.2.3-hotfix-08/patch_03_009_02_03_hotfix_08.sql
@@ -18,7 +18,7 @@ WHENEVER SQLERROR EXIT SQL.SQLCODE
 begin  
    :USER_NDFL := upper('&1');
    select to_char(systimestamp,'yyyy.mm.dd hh24:mi:ss:FF3') into :start_time from dual;	
-   :version := '03.009.02.03.fx7';
+   :version := '03.009.02.03.fx8';
    :STATUS := 'OK';   
 end;
 /
@@ -36,11 +36,10 @@ end;
 
 PROMPT ## Beginning Installing Hotfix
 
-PROMPT ##indexes
-@03_indexes.sql 
+PROMPT ##templates
+@02_templates.sql "_log/3.9.2.3_hotfix_08_templates.txt" "&3" "&4" "_log" "_bad"
 
-
-insert into version_history (version, scrname, status, start_time, end_time) values (:version, 'patch_03_009_02_hotfix_07.sql', :status, to_timestamp(:start_time,'yyyy.mm.dd hh24:mi:ss:FF3'), systimestamp);
+insert into version_history (version, scrname, status, start_time, end_time) values (:version, 'patch_03_009_02_hotfix_08.sql', :status, to_timestamp(:start_time,'yyyy.mm.dd hh24:mi:ss:FF3'), systimestamp);
 commit;
 
 PROMPT Installation complete
