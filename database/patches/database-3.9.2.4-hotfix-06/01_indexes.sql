0bd4e8d504d9938e2135fa50334d732b86088691
Svetlana Kononova <SKononova@aplana.com>
2019-10-25 10:53:04 +0300

    Хотфикс 6: добавлен скрипт проверки 6-ндфл

--- database/patches/database-3.9.2.4-hotfix-06/01_indexes.sql
+++ database/patches/database-3.9.2.4-hotfix-06/01_indexes.sql
@@ -3,9 +3,11 @@ DECLARE
 	v_run_condition number(1);
 	v_task_name varchar2(128):='indexes block #1  - idx_ref_book_cal_datetype';  
 BEGIN
-	select decode(count(*),1,0,1) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOKCAL_DATETYPE';
+	select count(*) into v_run_condition from user_indexes where INDEX_NAME='IDX_REF_BOOK_CAL_DATETYPE';
+	if (v_run_condition =0) then
         execute immediate 'create unique index idx_ref_book_cal_datetype on ref_book_calendar (cdate asc, ctype asc)';
 	dbms_output.put_line(v_task_name||'[INFO ]:'||' Success');
+	end if;
 
 EXCEPTION
 	when OTHERS then
