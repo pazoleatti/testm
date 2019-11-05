492605e9e371663332a6a85cf232a6a1d145463f
Andrey Voynov <Andrey.Voynov@aplana.com>
2019-11-05 18:00:46 +0300

    https://jira.aplana.com/browse/SBRFNDFL-9075 Ошибка при создание ОНФ в автоматическом режиме

--- dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/NdflReferenceDaoImpl.java
+++ dao/src/main/java/com/aplana/sbrf/taxaccounting/dao/impl/NdflReferenceDaoImpl.java
@@ -46,7 +46,7 @@ public class NdflReferenceDaoImpl extends AbstractDao implements NdflReferenceDa
 
     @Override
     public void createSequence(Integer year) {
-        getJdbcTemplateSecondary().execute("CREATE SEQUENCE SEQ_NDFL_REFERENCES_" + year
+        getJdbcTemplate().execute("CREATE SEQUENCE SEQ_NDFL_REFERENCES_" + year
                 + " MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER NOCYCLE");
     }
 
